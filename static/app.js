8da50c2e8adb3016b972e82fb48dd019251aadbc
Paul Biggar <paul.biggar@gmail.com>
2016-12-07 19:40:27 -0500

    Strip out unused stuff

--- static/app.js
+++ static/app.js
@@ -1,86 +1,36 @@
-/*
- * How is the next application state calculated,
- * given the current state and the action?
- */
 const counter = (state = 0, action) => {
   switch (action.type) {
   case 'INCREMENT':
     return state + 1;
-  case 'DECREMENT':
-    return state - 1;
   default:
     return state;
   }
 }
 
-/*
- * What does UI look like, assuming it doesn't know
- * about the state or actions, and is a function
- * of the props?
- */
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
+const Counter = ({value, onIncrement}) => (
     <div>
     <h1>{value}</h1>
     <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
     </div>
 );
 
-/*
- * Which injected props should be calculated
- * from the application state and how?
- */
 const mapStateToProps = (state) => {
-  return {
-    value: state
-  };
+  return {value: state};
 }
 
-/*
- * Which injected props should be callbacks
- * that dispatch actions, and which actions?
- */
 const mapDispatchToProps = (dispatch) => {
   return {
     onIncrement: () => {
       dispatch({
         type: 'INCREMENT'
       })
-    },
-    onDecrement: () => {
-      dispatch({
-        type: 'DECREMENT'
-      })
     }
   };
 };
 
-/*
- * Let's create a container component
- * that injects props into the pure UI component
- * according to the instructions above, but
- * instead of all those props, accepts a store.
- */
-const { connect } = ReactRedux;
-const CounterContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Counter);
+const CounterContainer = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(Counter);
+const store = Redux.createStore(counter);
 
-/*
- * Let's create a store.
- */
-const { createStore } = Redux;
-const store = createStore(counter);
-
-/*
- * Finally, render the container,
- * passing the store to it.
- */
 ReactDOM.render(
     <CounterContainer store={store} />,
   document.getElementById('root')
