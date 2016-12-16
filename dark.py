da42b5a10c594d2778eb85a2276091e9f5112a91
Paul Biggar <paul.biggar@gmail.com>
2016-12-16 17:57:44 -0500

    Refactor, better errors

--- dark.py
+++ dark.py
@@ -5,6 +5,7 @@ from werkzeug.utils import redirect
 import json
 import pickle
 import os.path
+import traceback
 
 import fields
 import graph
@@ -21,91 +22,99 @@ class Dark(server.Server):
     if os.path.isfile("dark.graph"):
       self.graph = pickle.load(open( "dark.graph", "rb"))
 
-    self.add_graph_routes()
-    self.add_api_routes()
+    self.add_ui_route()
+    self.add_api_route()
 
-  def add_api_routes(self):
-    def fn(request):
-      str_response = request.data.decode('utf-8')
-      params = json.loads(str_response)
-      print("Requesting: " + str(params))
+  def handler(self, request):
+    str_response = request.data.decode('utf-8')
+    params = json.loads(str_response)
+    print("Requesting: " + str(params))
 
-      command = params["command"]
-      args = params["args"]
+    command = params["command"]
+    args = params["args"]
 
-      G = self.graph
-      cursor = None
+    G = self.graph
+    cursor = None
 
-      if command == "add_datastore":
-        name = args["name"]
-        node = datastore.Datastore(name)
-        node.x = args["x"]
-        node.y = args["y"]
-        G.add_datastore(node)
-        cursor = node
+    if command == "add_datastore":
+      name = args["name"]
+      node = datastore.Datastore(name)
+      node.x = args["x"]
+      node.y = args["y"]
+      G.add_datastore(node)
+      cursor = node
 
-      elif command == "add_datastore_field":
-        ds = G.datastores[args["id"]]
-        fieldname = args["name"]
-        typename = args["type"]
-        fieldFn = getattr(fields, typename)
-        ds.add_field(fieldFn(fieldname))
+    elif command == "add_datastore_field":
+      ds = G.datastores[args["id"]]
+      fieldname = args["name"]
+      typename = args["type"]
+      fieldFn = getattr(fields, typename)
+      ds.add_field(fieldFn(fieldname))
 
-      elif command == "add_function_call":
-        nodename = args["name"]
-        node = graph.Node(nodename)
-        node.x = args["x"]
-        node.y = args["y"]
-        G._add(node)
-        cursor = node
+    elif command == "add_function_call":
+      nodename = args["name"]
+      node = graph.Node(nodename)
+      node.x = args["x"]
+      node.y = args["y"]
+      G._add(node)
+      cursor = node
 
-      elif command == "update_node_position":
-        nodeid = args["id"]
-        node = G.get_node(nodeid)
-        node.x = args["x"]
-        node.y = args["y"]
+    elif command == "update_node_position":
+      nodeid = args["id"]
+      node = G.get_node(nodeid)
+      node.x = args["x"]
+      node.y = args["y"]
 
-      elif command == "add_edge":
-        src = G.get_node(args["src"])
-        target = G.get_node(args["target"])
-        paramname = args["param"]
-        G.add_edge(src, target, paramname)
+    elif command == "add_edge":
+      src = G.get_node(args["src"])
+      target = G.get_node(args["target"])
+      paramname = args["param"]
+      G.add_edge(src, target, paramname)
 
-      elif command == "delete_node":
-        node = G.get_node(args["id"])
-        G.delete_node(node)
+    elif command == "delete_node":
+      node = G.get_node(args["id"])
+      G.delete_node(node)
 
-      elif command == "clear_edges":
-        node = G.get_node(args["id"])
-        G.clear_edges(node)
+    elif command == "clear_edges":
+      node = G.get_node(args["id"])
+      G.clear_edges(node)
 
-      elif command == "remove_last_field":
-        node = G.get_node(args["id"])
-        if node.is_datastore():
-          node.remove_last_field()
-        else:
-          node.remove_last_edge()
-
-      elif command == "load_initial_graph":
-        pass
-
+    elif command == "remove_last_field":
+      node = G.get_node(args["id"])
+      if node.is_datastore():
+        node.remove_last_field()
       else:
-        raise Exception("Invalid command: " + str(request.data))
+        node.remove_last_edge()
+
+    elif command == "load_initial_graph":
+      pass
+
+    else:
+      raise Exception("Invalid command: " + str(request.data))
+
+    return cursor
 
 
-      response = G.to_frontend(cursor)
-      print("Responding: " + str(response))
+  def add_api_route(self):
+    def endpoint(request):
+      try:
+        cursor = self.handler(request)
+        response = self.graph.to_frontend(cursor)
+        print("Responding: " + str(response))
 
-      # Roundtrip so we find bugs early
-      pickle.dump(G, open( "dark.graph", "wb" ))
-      self.graph = pickle.load(open( "dark.graph", "rb"))
+        # Roundtrip so we find bugs early
+        pickle.dump(self.graph, open( "dark.graph", "wb" ))
+        self.graph = pickle.load(open( "dark.graph", "rb"))
 
-      return Response(response=response)
+        return Response(response=response)
 
+      except BaseException as e:
+        traceback.print_exc()
+        return Response(status=500, response=str(e))
 
-    self.url_map.add(Rule('/admin/api/rpc', endpoint=fn))
+    self.url_map.add(Rule('/admin/api/rpc', endpoint=endpoint))
 
-  def add_graph_routes(self):
+  def add_ui_route(self):
     def fn(request):
       return self.render_template('graphelm.html')
     self.url_map.add(Rule('/admin/ui', endpoint=fn))
