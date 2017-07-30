3640a5deb85ee321c3d14584e51e261dafa743d6
Paul Biggar <paul.biggar@gmail.com>
2017-07-30 19:40:37 -0400

    Handle bools, lists, and objects

--- server/lib/runtime.ml
+++ server/lib/runtime.ml
@@ -4,13 +4,17 @@ open Types
 (* ------------------------- *)
 (* Values *)
 (* ------------------------- *)
-type dval = DInt of int
-          | DStr of string
-          | DChar of char
-          | DFloat of float
-          | DAnon of id * (dval -> dval)
-          | DObj
-          | DIncomplete [@@deriving show]
+module ObjMap = String.Map
+type objmap = dval ObjMap.t [@opaque]
+and dval = DInt of int
+         | DStr of string
+         | DChar of char
+         | DFloat of float
+         | DBool of bool
+         | DAnon of id * (dval -> dval)
+         | DList of dval list
+         | DObj of objmap
+         | DIncomplete [@@deriving show]
 
 module ParamMap = String.Map
 type param_map = dval ParamMap.t
@@ -35,14 +39,25 @@ let parse (str : string) : dval =
       | Failure _ ->
         Exception.raise ("Cannot parse value: " ^ str)
 
-let to_repr (dv : dval) : string =
+let rec to_repr (dv : dval) : string =
   match dv with
   | DInt i -> string_of_int i
+  | DBool true -> "true"
+  | DBool false -> "false"
   | DStr s -> "\"" ^ s ^ "\""
   | DFloat f -> string_of_float f
   | DChar c -> "'" ^ (Char.to_string c) ^ "'"
   | DAnon _ -> "<anon>"
   | DIncomplete -> "<incomplete>"
+  | DList l ->
+    "[ " ^ ( String.concat ~sep:", " (List.map ~f:to_repr l)) ^ " ]"
+  | DObj o ->
+    let strs = ObjMap.fold o
+        ~init:[]
+        ~f:(fun ~key ~data l -> (key ^ ": " ^ to_repr data) :: l) in
+
+    "{ " ^ (String.concat ~sep:", " strs) ^ " }"
+  | _ -> failwith "to_repr not implemented yet"
 
 
 let equal_dval (a: dval) (b: dval) = (to_repr a) = (to_repr b)
@@ -51,10 +66,14 @@ let get_type (dv : dval) : string =
   match dv with
   | DInt _ -> "Integer"
   | DStr _ -> "String"
+  | DBool _ -> "Bool"
   | DFloat _ -> "Float"
   | DChar _ -> "Char"
   | DAnon _ -> "Anonymous function"
+  | DList _ -> "List"
+  | DObj _ -> "Object"
   | DIncomplete -> "n/a"
+  (* | _ -> failwith "get_type not implemented yet" *)
 
 let to_error_repr (dv : dval) : string =
   (to_repr dv) ^ " (" ^ (get_type dv) ^ ")"
@@ -64,6 +83,28 @@ let to_char (dv : dval) : char =
   | DChar c -> c
   | _ -> Exception.raise "Not a char"
 
+(* ------------------------- *)
+(* JSON *)
+(* ------------------------- *)
+
+let rec json2dval_ (json : Yojson.Safe.json) : dval =
+  match json with
+  | `Int i -> DInt i
+  | `String s -> DStr s
+  | `Bool b -> DBool b
+  | `Assoc alist -> DObj (List.fold_left
+                        alist
+                        ~f:(fun m (k,v) -> ObjMap.add m k (json2dval_ v))
+                        ~init:ObjMap.empty)
+  | `List l -> DList (List.map ~f:json2dval_ l)
+  | j -> DStr ( "<todo, incomplete conversion: "
+                ^ (Yojson.Safe.to_string j)
+                ^ ">")
+
+let json2dval (json : string) : dval =
+  json |> Yojson.Safe.from_string |> json2dval_
+
+let dval2json (v: dval) : string = "{}"
 
 
 (* ------------------------- *)
--- server/lib/twitter.ml
+++ server/lib/twitter.ml
@@ -9,9 +9,8 @@ let bearer =
   ^ "yK2a8ZsTGVX32KdXY%3DzKh8JxqSB8tkLKzVgEY3"
   ^ "Pagi8le92ZQE5PXTqimhtVRqyjeWRz"
 
-let json2dval (json : string) = Runtime.DStr json
+
 let dval2query (v: dval) : string = "screen_name=ellenchisa"
-let dval2json (v: dval) : string = "{}"
 
 let call (endpoint: string) (verb: Http.verb) (argument: dval) : dval =
   let prefix = "https://api.twitter.com/1.1/" in
@@ -23,11 +22,11 @@ let call (endpoint: string) (verb: Http.verb) (argument: dval) : dval =
       let url = prefix ^ endpoint ^ "?" ^ query_string in
       Http.call url verb headers ""
     | POST ->
-      let body = dval2json argument in
+      let body = Runtime.dval2json argument in
       let url = prefix ^ endpoint in
       Http.call url verb headers body
   in
-  json2dval result
+  Runtime.json2dval result
 
 let get (url: string) (arg: dval) : dval =
   call url GET arg
