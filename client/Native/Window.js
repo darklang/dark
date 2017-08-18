7195e4c75c09ad0c9b482eef03edaaf6afcd98c8
Paul Biggar <paul.biggar@gmail.com>
2017-08-18 16:49:32 -0400

    Remove dead code

--- client/Native/Window.js
+++ client/Native/Window.js
@@ -5,15 +5,4 @@ var _user$project$Native_Window = {
       height: window.innerHeight
     }
   }
-  // var size = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)	{
-  //   callback(_elm_lang$core$Native_Scheduler.succeed({
-  //     width: window.innerWidth,
-  //     height: window.innerHeight
-  //   }));
-  // });
-
-  // return {
-  //   size: size
-  // };
-
 };
