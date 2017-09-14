4b47244741717ebe4aaba922343dd59712c870b6
Paul Biggar <paul.biggar@gmail.com>
2017-09-14 14:06:25 -0700

    Fix string slicing bug

--- server/lib/util.ml
+++ server/lib/util.ml
@@ -37,17 +37,19 @@ let writefile f str : unit =
 let create_id (_ : unit) : int =
   Random.int (Int.pow 2 29)
 
-let inspecT ?(formatter=Batteries.dump) ?(start=0) ?(stop=(-1)) (msg : string) (x : 'a) : unit =
+let inspecT ?(formatter=Batteries.dump) ?(start=0) ?(stop=0) (msg : string) (x : 'a) : unit =
   let red = "\x1b[6;31m" in
   let reset = "\x1b[0m" in
   let prefix = red ^ "inspect: " ^ msg ^ ": " ^ reset in
   x
   |> formatter
-  |> (fun s -> String.slice s start stop)
+  |> (fun s ->
+  let last = String.length s - 1 in
+    String.slice s (min start last) (min stop last))
   |> (^) prefix
   |> print_endline
 
-let inspect ?(formatter=Batteries.dump) ?(start=0) ?(stop=(-1)) (msg : string) (x : 'a) : 'a =
+let inspect ?(formatter=Batteries.dump) ?(start=0) ?(stop=0) (msg : string) (x : 'a) : 'a =
   inspecT msg ~formatter ~start ~stop x;
   x
 
