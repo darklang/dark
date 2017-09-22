90ceb1bd047cbf339c28ace756733641fc66be69
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2017-09-22 12:20:35 -0400

    Workaround mirage/ocaml-cohttp#511

--- server/bin/dark.ml
+++ server/bin/dark.ml
@@ -5,4 +5,6 @@ let () =
   let () = Printexc.record_backtrace true in
   let () = Exn.initialize_module () in
   print_endline "Starting server";
+  let () = Lwt.async_exception_hook := ignore in
+  (* see https://github.com/mirage/ocaml-cohttp/issues/511 *)
   Server.run ()
