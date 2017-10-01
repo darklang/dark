7d87631514d1c5e80ab4b4e48ab02c1678e48480
Paul Biggar <paul.biggar@gmail.com>
2017-09-30 21:03:59 -0700

    Use the new errors for String::foreach

--- server/lib/stdlib.ml
+++ server/lib/stdlib.ml
@@ -329,6 +329,7 @@ let fns : Lib.shortfn list = [
             else
               error
                 ~actual:(DList result)
+                ~result:(DList result)
                 ~long:("String::foreach needs to get chars back in order to reassemble them into a string. The values returned by your code are not chars, for example " ^ (to_repr !example_value) ^ " is a " ^ (get_type !example_value))
                 ~expected:"every value to be a char"
                 "Foreach expects you to return chars"
