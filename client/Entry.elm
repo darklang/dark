537464a03eab392dcda758c2be2debff170db96c
Paul Biggar <paul.biggar@gmail.com>
2017-10-11 01:45:32 -0700

    First part of the new expression parser

--- client/Entry.elm
+++ client/Entry.elm
@@ -2,6 +2,8 @@ module Entry exposing (..)
 
 -- builtins
 import Task
+import Char
+import Result
 
 -- lib
 import Dom
@@ -162,8 +164,153 @@ refocus re default =
     FocusNext id -> if re then FocusExact id else FocusNext id
     f -> f
 
+type alias EAST = EExpr
+type EExpr = EExpr String (List EArg)
+type EArg = EValue String
+          | ENode String
+          | EKnownTipe Tipe
+          | EAnother EExpr
+
+
+type alias SyntaxTree = STExpr
+
+type STValue = STValue String
+
+type STExpr = STEFnCall STFnCall
+            | STEValue STValue
+            | STEVar STVar
+
+type STVar = STVar String
+
+--type FnCall = FnCall (STExpr) String (List STExpr)
+type STFnCall = STFnCall String (List STExpr)
+
+--------------------------------
+-- parsing framework
+--------------------------------
+
+-- Error is a string. 2nd string is the remaining string after the successful application of the rule.
+type alias EResult a = Result String (String, a)
+
+-- if the match is successful, return it. If not, ignore it and get the next one. (TODO: build up error string)
+type alias MatchRule a b = { start: Char -> Bool -- if true start matching
+                           , body: Char -> Bool -- while true keep consuming
+                           , end: Maybe Char -> Bool -- must also be true to match (can be discarded in postProcess)
+                           , postProcess: Char -> String -> Maybe Char -> b
+                           , constructor: (b -> a)
+                           }
+
+(|.|) : (String -> EResult a) -> (String -> EResult a) -> String -> EResult a
+(|.|) a b str =
+  let result = a str in
+  if Util.resultIsOk result
+  then result
+  else b str
+
+-- Consumes `str` so long as the conditions are correct. If the conditions are not matched, return a failure.
+-- consume the first char, check it, put it in the consumed pile
+-- consume chars so long as the body is true
+-- when the body is no longer true, check the _next_ char.
+-- call postProcess with the first, last and middle, turn into a string
+-- pass the consumed stirng into the constructor
+-- pass the rest of the string into the first value in the pair
+-- if there's a failure, return a failing result.
+match : MatchRule a b -> String -> EResult a
+match rule str =
+  let consume : (Char -> Bool) -> String -> (String, String)
+      consume fn str = -- (remaining, consumed)
+        case String.uncons str of
+          Nothing -> (str, "")
+          Just (char, rest) ->
+            if fn char
+            then let (remaining, consumed) = consume fn rest in (remaining, String.cons char consumed)
+            else (String.cons char rest, "")
+  in
+    case String.uncons str of
+      Nothing -> Result.Err "First char doesn't match"
+      Just (first, rest) ->
+        let (unconsumed, consumed) = consume rule.body rest
+            (last, remaining) = case String.uncons unconsumed of
+              Nothing -> (Nothing, unconsumed)
+              Just (last, remaining)  -> (Just last, remaining)
+        in
+          if rule.start first && rule.end last
+          then Result.Ok (remaining, rule.postProcess first consumed last |> rule.constructor)
+          else Result.Err "Last char doesn't match"
+
+
+parse : String -> Result String SyntaxTree
+parse str =
+  case matchExpr str of
+    Result.Ok ("", a) -> Result.Ok a
+    Result.Ok (unconsumed, consumed) -> Result.Err <| "Got a " ++ toString consumed ++ " but didn't fully parse: " ++ unconsumed
+    Result.Err err -> Result.Err err
+
+matchValue : String -> EResult STExpr
+matchValue = matchString |.| matchNumber |.| matchChar
+
+matchExpr : String -> EResult STExpr
+matchExpr = matchValue |.| matchVar --|.| matchFnCall
+
+matchString : String -> EResult STExpr
+matchString =
+  match { start = (==) '"'
+        , body = (/=) '"'
+        , end = (==) <| Just '"'
+        , constructor = STValue >> STEValue
+        , postProcess = \start body end -> String.cons start body ++ String.fromChar (end |> Util.deMaybe)
+        }
+
+matchChar : String -> EResult STExpr
+matchChar =
+  match { start = (==) '\''
+        , body = (/=) '\''
+        , end = (==) <| Just '\''
+        , constructor = STValue >> STEValue
+        , postProcess = \start body end -> String.cons start body ++ String.fromChar (end |> Util.deMaybe)
+        }
+
+
+matchNumber : String -> EResult STExpr
+matchNumber =
+  match { start = Char.isDigit
+        , body = \c -> Char.isDigit c || c == '.'
+        , end = always True
+        , constructor = STValue >> STEValue
+        , postProcess = \start body end -> String.cons start body
+        }
+
+matchVar : String -> EResult STExpr
+matchVar =
+  match { start = (==) '$'
+        , body = always False
+        , end = \c -> c |> Maybe.withDefault 'A' |> Char.isLower
+        , constructor = STVar >> STEVar
+        , postProcess = \start body end -> end |> Util.deMaybe |> String.fromChar
+        }
+
+-- matchFnCall str =
+--   let list = String.toList str
+--   String.fol
+  
+--   if 
+
+
+  
+ 
+submit2 : Model -> Bool -> EntryCursor -> String -> Modification
+submit2 m re cursor value =
+  let ast = parse value
+  in case ast of
+    Ok east -> Error <| toString east
+    Err error -> Error <| toString error 
+
+
 submit : Model -> Bool -> EntryCursor -> String -> Modification
-submit m re cursor value =
+submit = submit1
+
+submit1 : Model -> Bool -> EntryCursor -> String -> Modification
+submit1 m re cursor value =
   let id = G.gen_id () in
   case cursor of
     Creating pos ->
