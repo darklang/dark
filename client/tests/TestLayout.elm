0c0cb9d4cbf686aadb42c037e5a6c15397851196
Paul Biggar <paul.biggar@gmail.com>
2017-10-23 16:39:17 -0700

    Add a (failing) layout test

--- client/tests/TestLayout.elm
+++ client/tests/TestLayout.elm
@@ -9,6 +9,7 @@ import Json.Decode as JSD
 import Dict
 
 -- libs
+import List.Extra as LE
 
 -- dark
 import Autocomplete
@@ -20,8 +21,48 @@ import Util exposing (deMaybe)
 import Entry
 import Types exposing (..)
 
-all : Test
-all =
+center : Pos
+center = {x=475, y=325}
+
+opNamesAndPoses : List RPC -> List (String, MPos)
+opNamesAndPoses rpcs =
+  List.map
+    (\rpc ->
+      case rpc of
+        AddFunctionCall id name pos -> (name, pos)
+        AddBlock _ pos _ args -> ("Block: " ++ String.join ", " args, pos)
+        AddValue _ val pos -> (val, pos)
+    -- | SetConstant Name (ID, ParamName)
+    -- | SetEdge ID (ID, ParamName)
+    -- | DeleteNode ID
+    -- | UpdateNodeCursor ID Cursor
+    -- | UpdateNodePosition ID MPos
+        x -> ("other: " ++ toString x, NoPos Nothing)
+    ) rpcs
+
+testIfAlone : Test
+testIfAlone =
+  test "layout_ifAlone"
+  (\_ ->
+    let m = Defaults.defaultModel Defaults.defaultEditor
+        m2 = { m | complete = Autocomplete.init DarkTestCode.functions}
+        cursor = Creating center
+        mod = Entry.submit m2 False cursor "if" in
+    case mod of
+      RPC (rpcs, _) ->
+        let ops = opNamesAndPoses rpcs
+            expected = [ ("if", Root center)
+                       , ("Block: then", Dependent Nothing)
+                       , ("Block: else", Dependent Nothing)]
+        in if (Util.containsOrdered expected ops)
+           then Expect.pass
+           else Expect.equal ops expected
+      _ -> Expect.fail "wrong type of mod"
+   )
+
+
+testAddBlockAsNewRoot : Test
+testAddBlockAsNewRoot =
   test "layout_equals_ifarg"
   (\_ ->
     let json = DarkTestData.simple_equals
