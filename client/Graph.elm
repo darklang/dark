dcec35ccc8662f1bdb44cfefdfec024da0e4d34b
Paul Biggar <paul.biggar@gmail.com>
2017-10-24 20:06:11 -0700

    Be more clear about who positions nodes

--- client/Graph.elm
+++ client/Graph.elm
@@ -559,66 +559,73 @@ max4 w x y z = max x y |> max z |> max w
 
 posRoot : Model -> DepType -> LRoot -> Int -> Int -> Model
 posRoot m depType (LRoot n args children) x y =
+  let _ = Debug.log ("posRoot: " ++ n.name) (x,y) in
   let m2 = position m depType n x y
-      (_, nextY, _, _, m3) = posArgs m2 depType args x y
-      (_, _, _, _, m4) = posChildren m3 depType children x nextY
+      (_, maxY, m3) = posArgs m2 depType args (x+blockIndent) (y+ySpacing)
+      (_, _, m4) = posChildren m3 depType children x maxY
   in m4
 
 type alias TraversalInfo = (NextX, NextY, MaxX, MaxY, Model)
+type alias SpaceInfo = (MaxX, MaxY, Model)
+
 posArg : DepType -> LArg -> TraversalInfo -> TraversalInfo
-posArg depType (LArg n children) (x, y, maxX, maxY, m) =
+posArg depType (LArg n children) ((x, y, maxX, maxY, m) as ti) =
+  let _ = debug "posArg" n ti in
   if seen m n then (x,y,x,y,m)
   else
     let m2 = position m depType n x y in
-    let (_, _, maxXcs, maxYcs, m3) = posChildren m2 depType children x y
+    let (maxXcs, maxYcs, m3) = posChildren m2 depType children x (y+ySpacing)
         newX = (max3 (x + nodeWidth n) maxXcs maxX) + paramSpacing
     in (newX, y, newX, max maxYcs maxY, m3)
 
-posArgs : Model -> DepType -> List LArg -> NextX -> NextY -> TraversalInfo
+posArgs : Model -> DepType -> List LArg -> NextX -> NextY -> SpaceInfo
 posArgs m depType args x y =
-  if List.length args == 0
-  then (x, y, x, y, m)
-  else List.foldl (posArg depType) (x+blockIndent, y+ySpacing, x+blockIndent, y+ySpacing, m) args
+  let (_, _, maxX, maxY, m2) = List.foldl (posArg depType) (x, y, x, y, m) args
+  in (maxX, maxY, m2)
+
+debug : String -> Node -> TraversalInfo -> ()
+debug fn n (x, y, maxX, maxY, _) =
+  let _ = Debug.log (fn ++ ": " ++ n.name) (x, y, maxX, maxY) in ()
+
+
 
 posChild : DepType -> LChild -> TraversalInfo -> TraversalInfo
-posChild depType (LChild n args children parents) (x, y, maxX, maxY, m) =
+posChild depType ((LChild n args children parents) as lc) ((x, y, maxX, maxY, m) as ti) =
+  let _ = debug "posChild" n ti in
   if seen m n then (x,y,maxX,maxY,m)
   else
-    let (_, nextY, maxXps, maxYps, m2) = posParents m depType parents (x+paramSpacing) y
-        m3 = position m2 depType n x nextY
+    let (maxXps, maxYps, m2) = posParents m depType parents (x+paramSpacing) y
+        m3 = position m2 depType n x maxYps
 
         -- blocks should be calculated before children, as we need to
         -- know how deep they are before we can start to position other
         -- outgoing nodes below them.
-        (_, _, maxXas, maxYas, m4) = posArgs m3 depType args x maxYps
-        (_, _, maxXcs, maxYcs, m5) = posChildren m4 depType children x maxYas
+        (maxXas, maxYas, m4) = posArgs m3 depType args (x+blockIndent) (maxYps+ySpacing)
+        (maxXcs, maxYcs, m5) = posChildren m4 depType children x maxYas
         newX = max4 maxXps maxXas maxXcs (x + nodeWidth n)
     in (newX+paramSpacing, y, newX, max maxYcs y, m5)
 
-posChildren : Model -> DepType -> List LChild -> NextX -> NextY -> TraversalInfo
+posChildren : Model -> DepType -> List LChild -> NextX -> NextY -> SpaceInfo
 posChildren m depType children x y =
-  if List.length children == 0
-  then (x,y,x,y,m)
-  else List.foldl (posChild depType) (x, y, x, y+ySpacing, m) children
+  let (_, _, maxX, maxY, m2) = List.foldl (posChild depType) (x, y, x, y, m) children
+  in (maxX, maxY, m2)
 
 posParent : DepType -> LParent -> TraversalInfo -> TraversalInfo
-posParent depType (LParent n parents) (x, y, maxX, maxY, m) =
+posParent depType (LParent n parents) ((x, y, maxX, maxY, m) as ti) =
+  let _ = debug "posParent" n ti in
   if seen m n then (x,y,x,y,m)
   else
     -- TODO see if we can take the first position away
     let m2 = position m depType n x y in -- don't have position yet, but dont want to visit twice
-    let (_, nextY, maxXps, maxYps, m3) = posParents m2 depType parents x y
-        m4 = position m3 depType n x nextY
+    let (maxXps, maxYps, m3) = posParents m2 depType parents x y
+        m4 = position m3 depType n x maxYps
         newX = (max3 (x + nodeWidth n) maxXps maxX) + paramSpacing
-    in (newX, y, newX, max maxYps maxY, m4)
+    in (newX, y, newX, ySpacing+(max maxYps maxY), m4)
 
-posParents : Model -> DepType -> List LParent -> NextX -> NextY -> TraversalInfo
+posParents : Model -> DepType -> List LParent -> NextX -> NextY -> SpaceInfo
 posParents m depType parents x y =
-  if List.length parents == 0
-  then (x,y,x,y,m)
-  else
-    let (nextX, nextY, maxX, maxY, m2) = List.foldl (posParent depType) (x, y, x, y, m) parents
-    in (nextX, maxY+ySpacing, maxX, maxY+ySpacing, m2)
+  let (_, _, maxX, maxY, m2) = List.foldl (posParent depType) (x, y, x, y, m) parents
+  in (maxX, maxY, m2)
 
 
 root2layout : Model -> Node -> LRoot
@@ -649,11 +656,11 @@ arg2layout m n =
 
 parent2layout : Model -> Node -> LParent
 parent2layout m n =
-  LParent n (List.map (parent2layout m) (incomingNodes m n))
+  LParent n (List.map (parent2layout m) (incomingNodes m n |> List.filter isNotBlock ))
 
 child2layout : Model -> Node -> LChild
 child2layout m n =
-  let parents = incomingNodes m n -- TODO: filter by parents already posed
+  let parents = incomingNodes m n  |> List.filter isNotBlock -- TODO: filter by parents already posed
       outgoing = outgoingNodes m n
       args = List.filter isArg outgoing
       children = List.filter isNotArg outgoing
