747b649871b6abdf0980bcca3eda72500ef3df0f
Paul Biggar <paul.biggar@gmail.com>
2017-10-29 15:42:53 -0700

    Add DB::delete and DB::keys

--- server/lib/libdb.ml
+++ server/lib/libdb.ml
@@ -1,7 +1,7 @@
 open Core
 open Lib
+open Runtime
 
-module RT = Runtime
 module PG = Postgresql
 
 let conn =
@@ -18,32 +18,51 @@ let create_table (name:string) (tipes : (string*string) list) : unit =
 let kv_create name = create_table name [ "key", "VARCHAR PRIMARY KEY"
                                        ; "val", "VARCHAR"]
 
-let kv_upsert (table: string) (key: string) (value: RT.dval) : unit =
-  let valstr = RT.dval_to_json_string value in
+let kv_upsert (table: string) (key: string) (value: dval) : unit =
+  let valstr = dval_to_json_string value in
   let cmd = "INSERT INTO \"" ^ table ^ "\" VALUES ('" ^ key ^ "', '" ^ valstr
             ^ "') ON CONFLICT (key) DO UPDATE SET val = '" ^ valstr ^ "'" in
   ignore (conn#exec ~expect:[PG.Command_ok] cmd)
 
-let kv_fetch (table: string) (key: string) : RT.dval =
+let kv_fetch (table: string) (key: string) : dval =
   let cmd = "SELECT (val) FROM \"" ^ table ^ "\" WHERE key = '" ^ key ^ "' LIMIT 1" in
   let res = conn#exec cmd in
   if res#ntuples = 0 then DNull
-  else if res#ntuples = 1 then (res#get_tuple 0).(0) |> RT.parse
+  else if res#ntuples = 1 then (res#get_tuple 0).(0) |> parse
   else Exception.internal "more tuples than expected"
 
-let kv_fetch_all (table: string) : RT.dval =
+let kv_fetch_all (table: string) : dval =
   "SELECT * FROM \"" ^ table ^ "\""
   |> conn#exec
   |> (fun res -> res#get_all_lst)
   |> List.map ~f:(fun row ->
       match row with
-      | [key; value] -> (key, value |> RT.parse)
+      | [key; value] -> (key, value |> parse)
       | l -> Exception.internal ("Expected key,value list, got: " ^
                                  (String.concat ~sep:", " l)))
-  |> RT.to_dobj
+  |> to_dobj
+
+let kv_keys (table: string) : dval =
+  "SELECT (key) FROM \"" ^ table ^ "\""
+  |> conn#exec
+  |> (fun res -> res#get_all_lst)
+  |> List.map ~f:(fun row ->
+      match row with
+      | [key] -> DStr key
+      | l -> Exception.internal ("Expected key list, got: " ^
+                                 (String.concat ~sep:", " l)))
+  |> (fun l -> DList l)
+
+let kv_delete (table: string) (key: string) : dval =
+  "DELETE FROM \"" ^ table ^ "\"" ^ " WHERE key = '" ^ key ^ "'"
+  |> conn#exec
+  (* this returns an empty list, though postgres docs say should return the deletion count... *)
+  |> fun x -> DNull
 
 
-let with_postgres (table: RT.opaque) fn =
+
+
+let with_postgres (table: opaque) fn =
   try
      let t = table#get in
      let _ = kv_create t in
@@ -71,6 +90,22 @@ let fns : Lib.shortfn list = [
   ;
 
 
+  { n = "DB::delete"
+  ; o = []
+  ; p = [par "table" TOpaque; par "key" TStr]
+  ; r = TAny
+  ; d = "Delete the value in `table` keyed by `key`"
+  ; f = InProcess
+        (function
+          | [DOpaque o; DStr key] ->
+            with_postgres o (fun table -> kv_delete table key)
+          | args -> fail args)
+  ; pr = None
+  ; pu = true
+  }
+  ;
+
+
   { n = "DB::fetch"
   ; o = []
   ; p = [par "table" TOpaque; par "key" TStr]
@@ -80,6 +115,22 @@ let fns : Lib.shortfn list = [
         (function
           | [DOpaque o; DStr key] ->
             with_postgres o (fun table -> kv_fetch table key)
+          | args -> fail args)
+  ; pr = None
+  ; pu = true
+  }
+  ;
+
+
+  { n = "DB::keys"
+  ; o = []
+  ; p = [par "table" TOpaque]
+  ; r = TAny
+  ; d = "Fetch all the keys in `table`"
+  ; f = InProcess
+        (function
+          | [DOpaque o] ->
+            with_postgres o (fun table -> kv_keys table)
           | args -> fail args)
   ; pr = None
   ; pu = true
