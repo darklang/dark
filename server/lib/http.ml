7ae32c957fe01f558103fc6a95308045d832825f
Ian Connolly <ian@connolly.io>
2017-12-01 14:45:20 -0800

    Remove old concept of 'controller'

--- server/lib/http.ml
+++ server/lib/http.ml
@@ -21,14 +21,8 @@ let split_uri_path (path: string) : string list =
   let subs = String.split ~on:'/' path in
   List.filter ~f:(fun x -> String.length x > 0) subs
 
-let controller (path_or_route : string) : string option =
-  match (split_uri_path path_or_route) with
-  | [] -> None
-  | [a] -> Some a
-  | a :: _ -> Some a
-
 let path_matches_route ~(path: string) (route: string) : bool =
-  (path = route) || ((controller path) = (controller route))
+  path = route
 
 let matching_routes ~(uri: Uri.t) (c: C.canvas) : Handler.handler list =
   let path = Uri.path uri in
@@ -55,17 +49,16 @@ let unbound_path_variables (path: string) : string list =
   List.drop (split_uri_path path) 1
 
 (* assumes route and path match *)
-let bind_route_params_exn ~(uri: Uri.t) ~(route: string) : (string * route_param_map) =
+let bind_route_params_exn ~(uri: Uri.t) ~(route: string) : route_param_map =
   let path = Uri.path uri in
   if path_matches_route ~path:path route
   then
     let rpm = RouteParamMap.empty in
     let rvars = route_variables route in
     let pvars = unbound_path_variables path in
-    let controller = controller path in
-    let rpm' = List.fold_left ~init:rpm ~f:(fun rpm1 (r,p) -> RouteParamMap.add rpm1 ~key:r ~data:p) (List.zip_exn rvars pvars) in
-    match controller with
-    | None -> Exception.internal "Unable to parse controller from path"
-    | Some c -> (c, rpm')
+    List.fold_left
+      ~init:rpm
+      ~f:(fun rpm1 (r,p) -> RouteParamMap.add rpm1 ~key:r ~data:p)
+      (List.zip_exn rvars pvars)
   else Exception.internal "Attempted to parse path into route that does not match"
 
