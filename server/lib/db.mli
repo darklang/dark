4961d27f282a23c25eb3fce7a7435c7c35208e59
Ian Connolly <ian@connolly.io>
2017-12-11 09:03:45 -0800

    Close Types.RuntimeT

--- server/lib/db.mli
+++ server/lib/db.mli
@@ -1,5 +1,4 @@
 open Types
-open Types.RuntimeT
 
 val with_postgres : (unit -> 'b) -> 'b
 
@@ -8,14 +7,14 @@ val insert : DbT.db -> RuntimeT.dval_map -> unit
 val fetch_all : DbT.db -> RuntimeT.dval
 val fetch_by : DbT.db -> string -> RuntimeT.dval -> RuntimeT.dval
 val delete : DbT.db -> RuntimeT.dval -> unit
-val keys : DbT.db -> dval
+val keys : DbT.db -> RuntimeT.dval
 val update : DbT.db -> RuntimeT.dval -> unit
 
 (* DB schema modifications *)
 val create_new_db : tlid -> string -> unit
 val add_db_col : id -> id -> DbT.db -> DbT.db
 val set_col_name : id -> string -> DbT.db -> DbT.db
-val set_db_col_type : id -> tipe -> DbT.db -> DbT.db
+val set_db_col_type : id -> RuntimeT.tipe -> DbT.db -> DbT.db
 
 (* DB schema modifications *)
 val dbs_as_env : DbT.db list -> RuntimeT.dval_map
