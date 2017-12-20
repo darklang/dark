306c6056f2ecf839108bf34b5580cba10f05e053
Paul Biggar <paul.biggar@gmail.com>
2017-12-20 13:11:41 -0800

    Use testcafe

--- integration-tests/enter_changes_state.js
+++ integration-tests/enter_changes_state.js
@@ -1,4 +1,29 @@
-function testFn(p) {
-  p.sendEvent('keypress', p.event.key.Enter);
-  waitFor(p, "#entryBox", finishTest)
-}
+// function testFn(p) {
+//   p.sendEvent('keypress', p.event.key.Enter);
+//   waitFor(p, "#entryBox", finishTest)
+// }
+//
+
+import { Selector } from 'testcafe';
+
+fixture `Getting started`
+  .page `http://test_enter_changes_state.localhost:8000/admin/integration_test`;
+
+
+test('enter_changes_state', async t => {
+  const pageLoaded = Selector('#darkErrors').exists;
+  const entryBoxAvailable = Selector('#entryBox').exists;
+  const signal = Selector('#integrationTestSignal');
+
+  await t
+    .setTestSpeed(0.5)
+    .setPageLoadTimeout(0)
+    .expect(pageLoaded).ok()
+    .pressKey("enter")
+    .expect(entryBoxAvailable).ok()
+    .click("#finishIntegrationTest")
+    .expect(signal.exists).ok()
+    .takeScreenshot("a.png")
+    .expect(signal.hasClass("success")).eql(true)
+    ;
+});
--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -7,5 +7,4 @@ set -x
 git checkout -- server/appdata/test_enter_changes_state.dark
 git checkout -- server/appdata/test_field_access.dark
 
-phantomjs integration-tests/driveby.js "enter_changes_state"
-phantomjs integration-tests/driveby.js "field_access"
+testcafe --screenshots integration-tests/screenshots/ "chrome:headless" integration-tests/enter_changes_state.js
