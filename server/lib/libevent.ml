7e7e26bc082c2c1cdd6b3fcfd14533a369614538
Ian Connolly <ian@connolly.io>
2018-02-28 16:40:28 -0800

    No reason not to return the enqueued value

--- server/lib/libevent.ml
+++ server/lib/libevent.ml
@@ -9,13 +9,13 @@ let fns : Lib.shortfn list = [
   { pns = ["emit"]
   ; ins = []
   ; p = [par "Space" TStr; par "Name" TStr; par "Data" TAny]
-  ; r = TNull
+  ; r = TAny
   ; d = "Emit event `name` in `space`, passing along `data` as a parameter"
   ; f = InProcess
         (function
           | [DStr space; DStr name; data] ->
             Event_queue.enqueue space name data;
-            DNull
+            data
           | args -> fail args)
   ; pr = None
   ; ps = false
