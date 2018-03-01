b2cbd0c2590e392140b5bb2fc283e00eea00135c
Paul Biggar <paul.biggar@gmail.com>
2018-02-28 21:36:30 -0800

    Solve integration test slowdown

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -9,16 +9,21 @@ rm -f $(git ls-files server/appdata/test_*.dark -o)
 PATTERN=${@}
 PATTERN=${PATTERN:-".*"} # default to .*
 
+# Slowing this down massively slows down the test suite. If needed, we
+# can change this on an individual action:
+# https://devexpress.github.io/testcafe/documentation/test-api/actions/action-options.html#basic-action-options
+SPEED=1
+
 testcafe \
   --selector-timeout 50 \
   --assertion-timeout 50 \
   --app-init-delay 0 \
   --pageload-timeout 0 \
-  --speed 0.4 \
+  --speed $SPEED \
   --screenshots-on-fails \
   --screenshots integration-tests/screenshots/ \
   --concurrency 4 \
-  --report spec,json:integration-tests/report.json \
+  --reporter spec,json:integration-tests/report.json \
   --test-grep "$PATTERN" \
   "chrome:headless" \
   integration-tests/tests.js
