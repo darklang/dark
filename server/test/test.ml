82a70a52d28d3d43337871551dedfd750a00a4e9
Paul Biggar <paul.biggar@gmail.com>
2018-02-28 17:37:28 -0800

    Re-enable one server test

--- server/test/test.ml
+++ server/test/test.ml
@@ -10,6 +10,7 @@ module TL = Toplevel
 module Map = Map.Poly
 module AT = Alcotest
 
+let fid = Util.create_id
 
 let handle_exception e =
   (* Builtin testing doesnt seem to print exceptions *)
@@ -19,41 +20,65 @@ let handle_exception e =
   print_endline (Backtrace.to_string bt);
   raise e (* still need to raise so the test doesn't pass *)
 
+
+let ops2c (name: string) (ops: Op.op list) : C.canvas ref =
+  let c = C.create name in
+  C.add_ops c [] ops;
+  c
+
+let check_canvas = AT.check (AT.testable C.pp_canvas C.equal_canvas)
+
+let handler ast =
+  let id = fid () in
+  let b () = Types.Blank (fid ()) in
+  Op.SetHandler ( id
+                , {x=5;y=6}
+                , { tlid = id
+                  ; ast = ast
+                  ; spec = { module_ = b ()
+                           ; name = b ()
+                           ; modifier = b ()
+                           ; types = { input = b ()
+                                     ; output = b () }}})
+
+
 (* ----------------------- *)
 (* The tests *)
 (* ----------------------- *)
 
-(* let t_undo_fns () = *)
-(*   let n1 = Op.Savepoint in *)
-(*   let n2 = Op.Add_fn_call (fid (), Free, "-") in *)
-(*   let n3 = Op.Add_value (fid (), Free, "-86") in *)
-(*   let n4 = Op.Set_edge (Op.id_of n3, Op.id_of n2, "b") in *)
-(*   let u = Op.Undo in *)
-(*   let r = Op.Redo in *)
-(*  *)
-(*   AT.check AT.int "undocount" *)
-(*   3 (G.undo_count !(ops2g "g" [n1; n1; n1; n1; n2; n3; n4; u; u; u])); *)
-(*  *)
-(*   AT.check AT.bool "redoable" true *)
-(*     (G.is_redoable !(ops2g "g" [n1; n2; n3; n4; u])); *)
-(*   AT.check AT.bool "undoable" true *)
-(*     (G.is_undoable !(ops2g "g" [n1; n2; n3; n4])); *)
-(*  *)
-(*  *)
-(*   AT.check AT.bool "not redoable" false *)
-(*     (G.is_redoable !(ops2g "g" [n1; n2; n3; n4; u; r])); *)
-(*   AT.check AT.bool "not undoable" false *)
-(*     (G.is_undoable !(ops2g "g" [n1; n2; n3; n4; u])); *)
-(*  *)
-(*  *)
-(*   let both = !(ops2g "g" [n1; n1; n2; n3; n4; u; r; u]) in *)
-(*   AT.check AT.bool "both_undo" true (G.is_undoable both); *)
-(*   AT.check AT.bool "both_redo" true (G.is_redoable both); *)
-(*  *)
-(*   let neither = !(ops2g "g" [n2; n3; n4]) in *)
-(*   AT.check AT.bool "neither_undo" false (G.is_undoable neither); *)
-(*   AT.check AT.bool "neither_redo" false (G.is_redoable neither) *)
-(*  *)
+let t_undo_fns () =
+  let h () = Ast.Hole (fid ()) in
+  let v str = Ast.Value (fid (), str) in
+  let n1 = Op.Savepoint in
+  let n2 = handler (FnCall (fid (), "-", [h (); h ()])) in
+  let n3 = handler (FnCall (fid (), "-", [v "3"; h ()])) in
+  let n4 = handler (FnCall (fid (), "-", [v "3"; v "4"])) in
+  let u = Op.Undo in
+  let r = Op.Redo in
+
+  AT.check AT.int "undocount"
+  3 (C.undo_count !(ops2c "g" [n1; n1; n1; n1; n2; n3; n4; u; u; u]));
+
+  AT.check AT.bool "redoable" true
+    (C.is_redoable !(ops2c "g" [n1; n2; n3; n4; u]));
+  AT.check AT.bool "undoable" true
+    (C.is_undoable !(ops2c "g" [n1; n2; n3; n4]));
+
+
+  AT.check AT.bool "not redoable" false
+    (C.is_redoable !(ops2c "g" [n1; n2; n3; n4; u; r]));
+  AT.check AT.bool "not undoable" false
+    (C.is_undoable !(ops2c "g" [n1; n2; n3; n4; u]));
+
+
+  let both = !(ops2c "g" [n1; n1; n2; n3; n4; u; r; u]) in
+  AT.check AT.bool "both_undo" true (C.is_undoable both);
+  AT.check AT.bool "both_redo" true (C.is_redoable both);
+
+  let neither = !(ops2c "g" [n2; n3; n4]) in
+  AT.check AT.bool "neither_undo" false (C.is_undoable neither);
+  AT.check AT.bool "neither_redo" false (C.is_redoable neither)
+
 (* let t_undo () = *)
 (*   let n1 = Op.Add_fn_call (fid (), Free, "-") in *)
 (*   let u1 = Op.Savepoint in *)
@@ -63,7 +88,7 @@ let handle_exception e =
 (*   let n3 = Op.Add_value (fid (), Free, "3") in *)
 (*   let e2 = Op.Set_edge (Op.id_of n3, Op.id_of n1, "b") in *)
 (*   let ops1 = [n1; u1; n2; e1; u2; n3; e2] in *)
-(*  *)
+
 (*   let u3 = Op.Savepoint in *)
 (*   let n4 = Op.Add_value (fid (), Free, "6") in *)
 (*   let e3 = Op.Set_edge (Op.id_of n4, Op.id_of n1, "b") in *)
@@ -71,46 +96,46 @@ let handle_exception e =
 (*   let n5 = Op.Add_value (fid (), Free, "-86") in *)
 (*   let e4 = Op.Set_edge (Op.id_of n5, Op.id_of n1, "b") in *)
 (*   let ops2 = [u3; n4; e3; u4; n5; e4] in *)
-(*  *)
+
 (*   let u5 = Op.Undo in *)
 (*   let u6 = Op.Undo in *)
 (*   let u7 = Op.Redo in *)
 (*   let u8 = Op.Redo in *)
 (*   let u9 = Op.Undo in *)
 (*   let u10 = Op.Redo in *)
-(*  *)
+
 (*   (* Check assumptions *) *)
 (*   let r = execute_ops ops1 n1 in *)
 (*   check_dval "t_undo_1" (DInt 2) r; *)
 (*   let r2 = execute_ops (List.append ops1 ops2) n1 in *)
 (*   check_dval "t_undo_2" (DInt 91) r2; *)
-(*  *)
+
 (*   (* First undo *) *)
 (*   let r3 = execute_ops (List.concat [ops1; ops2; [u5]]) n1 in *)
 (*   check_dval "t_undo_3" (DInt (-1)) r3; *)
-(*  *)
+
 (*   (* Second undo *) *)
 (*   let r4 = execute_ops (List.concat [ops1; ops2; [u5;u6]]) n1 in *)
 (*   check_dval "t_undo_4" (DInt 2) r4; *)
-(*  *)
+
 (*   (* First redo *) *)
 (*   let r5 = execute_ops (List.concat [ops1; ops2; [u5;u6;u7]]) n1 in *)
 (*   check_dval "t_undo_5" (DInt (-1)) r5; *)
-(*  *)
+
 (*   (* Second redo *) *)
 (*   let r6 = execute_ops (List.concat [ops1; ops2; [u5;u6;u7;u8]]) n1 in *)
 (*   check_dval "t_undo_6" (DInt 91) r6; *)
-(*  *)
+
 (*   (* Another undo *) *)
 (*   let r7 = execute_ops (List.concat [ops1; ops2; [u5;u6;u7;u8;u9]]) n1 in *)
 (*   check_dval "t_undo_7" (DInt (-1)) r7; *)
-(*  *)
+
 (*   (* Another redo *) *)
 (*   let r8 = execute_ops (List.concat [ops1; ops2; [u5;u6;u7;u8;u9;u10]]) n1 in *)
 (*   check_dval "t_undo_8" (DInt 91) r8 *)
-(*  *)
 
-(*  *)
+
+
 (* let t_int_add_works () = *)
 (*   (* Couldn't call Int::add *) *)
 (*   let add = Op.Add_fn_call (fid (), Free, "Int::add") in *)
@@ -120,28 +145,9 @@ let handle_exception e =
 (*   let e2 = Op.Set_edge (Op.id_of v1, Op.id_of add, "a") in *)
 (*   let r = execute_ops [add; v1; v2; e2; e1] add in *)
 (*   check_dval "int_add" (DInt 8) r *)
-(*  *)
-
-let ops2c (name: string) (ops: Op.op list) : C.canvas ref =
-  let c = C.create name in
-  C.add_ops c [] ops;
-  c
-
-let check_canvas = AT.check (AT.testable C.pp_canvas C.equal_canvas)
-
-let empty_handler_spec : Handler.spec
-  = { name = Blank 9356
-    ; module_ = Blank 465
-    ; modifier = Blank 576
-    ; types = { input = Blank 98
-              ; output = Blank 67}}
 
 let t_load_save _ =
-  let n1 = Op.SetHandler ( 1
-                         , { x = 0; y = 0 }
-                         , { tlid = 1
-                           ; ast = Ast.Value (123, "5")
-                           ; spec = empty_handler_spec}) in
+  let n1 = handler (Ast.Value (123, "5")) in
   let name = "test_load_save" in
   let c = ops2c name [n1] in
   let _ = C.save !c in
@@ -205,13 +211,11 @@ let t_hmac_signing _ =
 let suite =
   Exn.initialize_module ();
   Printexc.record_backtrace true;
-  [
-    "roundtrip through saving and loading", `Slow, t_load_save
+  [ "roundtrip through saving and loading", `Slow, t_load_save
   ; "hmac signing works", `Slow, t_hmac_signing
     (* This test is broken, see comment in Api.json2op *)
-  (* ; "functions with edges work too" >:: t_fns_with_edges *)
   (* ; "undos", `Slow, t_undo *)
-  (* ; "undo_fns", `Slow, t_undo_fns *)
+  ; "undo_fns", `Slow, t_undo_fns
   ]
 
 let () =
