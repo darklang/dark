d9fffcd12aee13d481a429334e405ebd647f1fac
Paul Biggar <paul.biggar@gmail.com>
2018-03-03 19:33:17 -0800

    Make sure rundir is an absolute path

--- server/lib/config.ml
+++ server/lib/config.ml
@@ -1,7 +1,10 @@
 open Core
 
 let run_dir : string =
-  (Sys.getenv_exn "DARK_CONFIG_VAR_RUN_DIR") ^ "/"
+  let dir = Sys.getenv_exn "DARK_CONFIG_VAR_RUN_DIR" in
+  if not (Filename.is_absolute dir)
+  then Exception.internal "FAIL: DARK_CONFIG_VAR_RUN_DIR is not absolute"
+  else dir ^ "/"
 
 let appdata_dir : string =
   run_dir ^ "appdata/"
