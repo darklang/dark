0643bc7c9fa1c14e2708aaa1992ac18bcd2a8e48
Paul Biggar <paul.biggar@gmail.com>
2018-03-05 17:22:32 -0800

    Fix cron locally

--- server/bin/trigger_queue_workers.ml
+++ server/bin/trigger_queue_workers.ml
@@ -1,16 +1,6 @@
 open Core
 
 let _ =
-  let _ =
-    if String.is_substring ~substring:"server/" (Sys.getcwd ())
-    then ()
-    else
-      try
-        Sys.chdir "server"
-      with
-      | e ->
-        raise e
-  in
   Dark.Init.init ();
   Dark.Queue_worker.dequeue_and_evaluate_all ()
   |> Out_channel.print_endline
