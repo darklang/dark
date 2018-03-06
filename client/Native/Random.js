6f6b5af2187d1e15bf30bbd6ac32241a6eadef48
Paul Biggar <paul.biggar@gmail.com>
2018-03-06 10:40:40 -0800

    Use genuine randomness to avoid collisions better

--- client/Native/Random.js
+++ client/Native/Random.js
@@ -1,1 +1,7 @@
-var _user$project$Native_Random = { random: function(a) { return Math.round(1024*1024*1024 * Math.random()) }}
+var _user$project$Native_Random = {
+  random: function(a) {
+    var array = new Uint32Array(1);
+    window.crypto.getRandomValues(array);
+    return array[0];
+  }
+}
