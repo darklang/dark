780aadf19c2f97f8038f97da6819d526da3d33e6
Paul Biggar <paul.biggar@gmail.com>
2018-03-08 15:45:02 -0800

    Properly clean up old testfiles

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -41,7 +41,8 @@ sudo service dnsmasq --full-restart
 # https://devexpress.github.io/testcafe/documentation/test-api/actions/action-options.html#basic-action-options
 SPEED=1
 
-rm -Rf "${DARK_CONFIG_PERSIST_DIR}/completed_tests"
+echo "Clearing old test files"
+rm -Rf "${DARK_CONFIG_RUN_DIR}/completed_tests/*"
 
 TEST_HOST="integration-tests:$PORT" \
   testcafe \
