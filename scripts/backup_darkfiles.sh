674a95f9133d8b761359a8a90af61e90334a803d
Paul Biggar <paul.biggar@gmail.com>
2018-03-08 15:28:47 -0800

    Make backup run in container

--- scripts/backup_darkfiles.sh
+++ scripts/backup_darkfiles.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
+./scripts/support/assert-in-container $0 $@
 
 DATE=`date '+%Y_%m_%d-%H_%M_%S'`
-DEST="persistdir/backup_appdata/${DATE}"
+DEST="${DARK_CONFIG_PERSIST_DIR}/backup_appdata/${DATE}"
 
 mkdir -p $DEST
 
-cp -R runtime/appdata "${DEST}"
+cp -R "${DARK_CONFIG_PERSIST_DIR}/appdata" "${DEST}"
 
 echo "Copied into $DEST"
