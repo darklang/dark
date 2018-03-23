b8f34b6ee64563948c69e128c861c2aee8246f97
Paul Biggar <paul.biggar@gmail.com>
2018-03-22 20:08:51 -0700

    Split View into 7 files
    I didnt use a View directory because it broke the elm-make-vim scripts

---
+++ client/ViewEntry.elm
@@ -0,0 +1,201 @@
+-- The Entry is the form that we use to enter text.
+module ViewEntry exposing (viewEntry, entryHtml)
+
+
+-- builtin
+
+-- lib
+import Html
+import Html.Attributes as Attrs
+import Html.Events as Events
+import String.Extra as SE
+
+-- dark
+import Types exposing (..)
+import Util exposing (deMaybe)
+import Defaults
+import Autocomplete
+import ViewUtils exposing (..)
+
+
+
+viewEntry : Model -> List (Html.Html Msg)
+viewEntry m =
+  case unwrapState m.state of
+    Entering (Creating pos) ->
+      let html =
+            Html.div
+              [Attrs.class "omnibox"]
+              [entryHtml True "" m.complete]
+      in [placeHtml m pos html]
+    _ ->
+      []
+
+-- The view we see is different from the value representation in a few
+-- ways:
+-- - the start and end quotes are skipped
+-- - all other quotes are escaped
+transformToStringEntry : String -> String
+transformToStringEntry s_ =
+  -- the first time we won't have a closing quote so add it
+  let s = if String.endsWith "\"" s_ then s_ else s_ ++ "\"" in
+  s
+  |> String.dropLeft 1
+  |> String.dropRight 1
+  |> Util.replace "\\\\\"" "\""
+
+transformFromStringEntry : String -> String
+transformFromStringEntry s =
+  let s2 = s
+           |> Util.replace "\"" "\\\""
+  in
+  "\"" ++ s2 ++ "\""
+
+stringEntryHtml : Autocomplete -> Html.Html Msg
+stringEntryHtml ac =
+  let
+      -- stick with the overlapping things for now, just ignore the back
+      -- one
+      value = transformToStringEntry ac.value
+      length = value
+               |> String.length
+               |> max 3
+               |> (+) 1
+
+
+      smallInput =
+        Html.input [ Attrs.id Defaults.entryID
+                   , Events.onInput (EntryInputMsg << transformFromStringEntry)
+                   , nothingMouseEvent "mouseup"
+                   , nothingMouseEvent "mouseclick"
+                   , nothingMouseEvent "mousedown"
+                   , Attrs.value value
+                   , widthInCh length
+                   , Attrs.spellcheck False
+                   , Attrs.autocomplete False
+                   ] []
+
+
+      largeInput =
+        Html.textarea [ Attrs.id Defaults.entryID
+                      , Events.onInput (EntryInputMsg << transformFromStringEntry)
+                      , Attrs.value value
+                      , Attrs.spellcheck False
+                      -- Stop other events firing
+                      , nothingMouseEvent "mouseup"
+                      , nothingMouseEvent "mouseclick"
+                      , nothingMouseEvent "mousedown"
+                      , Attrs.cols 50
+                      , Attrs.rows (5 + SE.countOccurrences "\n" value)
+                      , Attrs.autocomplete False
+                      ] []
+    in
+    if Autocomplete.isSmallStringEntry ac
+    then
+      Html.div
+      [ Attrs.class "string-entry small-string-entry"
+      , widthInCh (length + 3)
+      ]
+      [
+        Html.form
+        [ Events.onSubmit (EntrySubmitMsg)
+        , Attrs.class "string-container"
+        ]
+        [ smallInput ]
+      ]
+    else
+       Html.div
+      [ Attrs.class "string-entry big-string-entry" ]
+      [
+        Html.form
+        [ Events.onSubmit (EntrySubmitMsg)
+        , Attrs.class "string-container"
+        ]
+        [ largeInput ]
+      ]
+
+
+normalEntryHtml : String -> Autocomplete -> Html.Html Msg
+normalEntryHtml placeholder ac =
+  let autocompleteList =
+        (List.indexedMap
+           (\i item ->
+              let highlighted = ac.index == i
+                  hlClass = if highlighted then " highlighted" else ""
+                  name = Autocomplete.asName item
+              in Html.li
+                [ Attrs.class <| "autocomplete-item" ++ hlClass
+                , eventNoPropagation "mouseup"
+                    (\_ -> AutocompleteClick name)
+                ]
+                [ Html.text name
+                , Html.span
+                    [Attrs.class "types"]
+                    [Html.text <| Autocomplete.asTypeString item ]
+                ])
+           (List.concat ac.completions))
+
+      autocomplete = Html.ul
+                     [ Attrs.id "autocomplete-holder" ]
+                     autocompleteList
+
+
+      -- two overlapping input boxes, one to provide suggestions, one
+      -- to provide the search
+      (indent, suggestion, search) =
+        Autocomplete.compareSuggestionWithActual ac ac.value
+
+      indentWidth = String.length indent
+      searchWidth = search ++ indent
+                    |> String.length
+                    |> (\l -> if l == 0
+                              then max (String.length placeholder) 6
+                              else l)
+                    |> (+) 1
+      searchInput = Html.input [ Attrs.id Defaults.entryID
+                               , Events.onInput EntryInputMsg
+                               , Attrs.style [("text-indent", inCh indentWidth)]
+                               , Attrs.value search
+                               , Attrs.placeholder placeholder
+                               , Attrs.spellcheck False
+                               , Attrs.autocomplete False
+                               , widthInCh searchWidth
+                               ] []
+      suggestionInput = Html.input [ Attrs.id "suggestionBox"
+                                   , Attrs.disabled True
+                                   , Attrs.value suggestion
+                                   , widthInCh searchWidth
+                                   ] []
+
+      input = Html.div
+              [ Attrs.id "search-container"
+              , widthInCh searchWidth
+              ]
+              [searchInput, suggestionInput]
+
+      viewForm = Html.form
+                 [ Events.onSubmit (EntrySubmitMsg) ]
+                 [ input, autocomplete ]
+
+      wrapper = Html.div
+                [ Attrs.class "entry"
+                , widthInCh searchWidth
+                ]
+                [ viewForm ]
+  in wrapper
+
+entryHtml : Bool -> String -> Autocomplete -> Html.Html Msg
+entryHtml allowStringEntry placeholder ac =
+  if allowStringEntry && Autocomplete.isStringEntry ac
+  then stringEntryHtml ac
+  else normalEntryHtml placeholder ac
+
+
+
+
+
+
+
+
+
+
---
+++ client/ViewRoutingTable.elm
@@ -0,0 +1,154 @@
+module ViewRoutingTable exposing (viewRoutingTable)
+
+-- builtin
+
+-- lib
+import Html
+import Html.Attributes as Attrs
+import List.Extra as LE
+
+-- dark
+import Types exposing (..)
+import Viewport
+import Toplevel as TL
+import Blank as B
+import Toplevel
+import ViewUtils exposing (..)
+
+
+
+type alias Collapsed = { name: Maybe String
+                       , prefix: List String
+                       , verbs: List (String, Pos)}
+
+collapseHandlers : List Toplevel -> List Collapsed
+collapseHandlers tls =
+  let asCollapsed =
+        tls
+        |> List.filterMap
+            (\tl ->
+              case TL.asHandler tl of
+                Just h -> Just (tl.pos, h)
+                Nothing -> Nothing)
+        |> List.map
+             (\(pos, h) ->
+               { name =
+                   case B.flattenFF h.spec.name of
+                     F _ s -> Just s
+                     Blank _ -> Nothing
+                     Flagged _ _ _ _ -> Debug.crash "ch - tls"
+               , prefix = []
+               , verbs =
+                   case B.flattenFF h.spec.modifier of
+                     F _ s -> [(s, pos)]
+                     Blank _ -> []
+                     Flagged _ _ _ _ -> Debug.crash "ch - verbs"
+               })
+        |> List.sortBy (\c -> Maybe.withDefault "ZZZZZZ" c.name)
+  in
+    prefixify <|
+    List.foldr (\curr list ->
+      case list of
+        [] -> [curr]
+        prev :: rest ->
+          if prev.name == curr.name
+          then
+            let new = { prev | verbs = prev.verbs ++ curr.verbs }
+            in new :: rest
+          else
+            curr :: prev :: rest
+    ) [] asCollapsed
+
+
+prefixify : List Collapsed -> List Collapsed
+prefixify hs =
+  case hs of
+    [] -> hs
+    [_] -> hs
+    h :: rest ->
+      case h.name of
+        Nothing -> h :: prefixify rest
+        Just name ->
+          let len = String.length name
+              makePrefix : Collapsed -> Collapsed
+              makePrefix h2 =
+                case h2.name of
+                  Just name2 ->
+                    let newName = String.dropLeft len name2 in
+                    { h2 | name = Just newName
+                         , prefix = h.prefix ++ [name]
+                    }
+                  _ -> h2
+              isPrefixOf h2 =
+                case h2.name of
+                  Nothing -> False
+                  Just n2 -> String.startsWith name n2
+          in
+          -- this should short circuit immediately when not matching, as
+          -- first handler will make the fn succeed
+          case LE.splitWhen (\h2 -> not (isPrefixOf h2)) rest of
+            Nothing ->
+              -- never hits, so everything is prefixed
+              h :: (rest |> List.map makePrefix |> prefixify)
+            Just (matched, unmatched) ->
+              h :: (prefixify <| (List.map makePrefix matched) ++ unmatched)
+
+
+
+viewRoutingTable : Model -> Html.Html Msg
+viewRoutingTable m =
+  let span class subs = Html.span [Attrs.class class] subs
+      text class msg = span class [Html.text msg]
+      div class subs = Html.div [Attrs.class class] subs
+
+      handlers = m.toplevels |> collapseHandlers
+      handlerCount = List.length handlers
+      def s = Maybe.withDefault "<not entered>" s
+      link h =
+        if List.member "GET" (List.map Tuple.first h.verbs)
+        then
+          case h.name of
+            Just n ->
+              let source = String.join "" (h.prefix ++ [n]) in
+              Html.a [ Attrs.class "external"
+                     , Attrs.href source
+                     , Attrs.target "_blank"
+                     ]
+                     [fontAwesome "external-link"]
+            Nothing ->
+              Html.div [] []
+        else
+          Html.div [] []
+
+      internalLinks h =
+        h.verbs
+        |> List.map
+          (\(verb, pos) ->
+            Html.a
+            [ eventNoPropagation "mouseup"
+                (\_ -> (NavigateTo (Viewport.urlForPos pos)))
+            , Attrs.src ""
+            , Attrs.class "verb-link as-pointer"
+            ]
+            [ Html.text verb ])
+        |> List.intersperse (Html.text ",")
+      handlerHtml h =
+        div "handler" [ div "url"
+                          (  List.map (text "prefix") h.prefix
+                          ++ [text "name" (def h.name)])
+                      , link h
+                      , span "verbs" (internalLinks h)
+                      ]
+      header = div "header"
+                 [ text "http" "HTTP"
+                 , text "parens" "("
+                 , text "count" (toString handlerCount)
+                 , text "parens" ")"
+                 ]
+      routes = div "routes" (List.map handlerHtml handlers)
+      html = div "routing-table" [header, routes]
+
+  in placeHtml m (Viewport.toAbsolute m {vx=0, vy=0}) html
+
+
+
