ad1162895259ba5851c0c9a96dd51497222e00c2
Paul Biggar <paul.biggar@gmail.com>
2018-04-02 14:33:31 -0700

    Save cursorState

--- client/Editor.elm
+++ client/Editor.elm
@@ -2,9 +2,9 @@ module Editor exposing (..)
 
 -- builtin
 import Json.Decode as JSD
-import Json.Encode as JSE
 
 -- lib
+import Json.Encode as JSE
 
 -- dark
 
@@ -12,25 +12,28 @@ import Defaults
 import RPC
 import Types exposing (..)
 
+fromString : String -> SerializableEditor
+fromString json =
+  JSD.decodeString RPC.decodeSerializableEditor json
+  |> Result.withDefault Defaults.defaultEditor
+
+toString : SerializableEditor -> String
+toString se =
+  RPC.encodeSerializableEditor se
+  |> JSE.encode 0
+
 
 editor2model : SerializableEditor -> Model
 editor2model e =
   let m = Defaults.defaultModel in
   { m | syncEnabled = e.syncEnabled
       , clipboard = e.clipboard
-                    |> JSD.decodeValue
-                         (JSD.nullable RPC.decodePointerData)
-                    |> Result.withDefault Nothing
       , cursorState = e.cursorState
-                    |> JSD.decodeValue RPC.decodeCursorState
-                    |> Result.withDefault Deselected
   }
 
 model2editor : Model -> SerializableEditor
 model2editor m =
-  { clipboard = case m.clipboard of
-                  Nothing -> JSE.null
-                  Just pd -> RPC.encodePointerData pd
+  { clipboard = m.clipboard
   , syncEnabled = m.syncEnabled
-  , cursorState = RPC.encodeCursorState m.cursorState
+  , cursorState = m.cursorState
   }
