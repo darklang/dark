dd6a2622c18633c77087b7e66556906579917373
Zane Shannon <z@zcs.me>
2018-04-03 13:32:37 -0700

    plumbed viewing data connection in view.

---
+++ client/ViewData.elm
@@ -0,0 +1,12 @@
+module ViewData exposing (..)
+
+-- lib
+import Html
+
+-- dark
+import Types exposing (..)
+import ViewUtils exposing (..)
+
+viewHandler : ViewState -> Handler -> List (Html.Html Msg)
+viewHandler vs h =
+  []
