69f9db2c29ce9a8df4381f1257c13ca7285ecc47
Paul Biggar <paul.biggar@gmail.com>
2018-04-04 14:36:55 -0700

    Clear databases created by integration tests

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -53,6 +53,11 @@ rm -f ${DARK_CONFIG_RUN_DIR}/completed_tests/*
 rm -Rf ${DARK_CONFIG_RUN_DIR}/screenshots/*
 rm -f ${DARK_CONFIG_RUN_DIR}/integration.json
 
+echo "Clearing test DBs"
+TESTDBS=$(psql -d proddb -q --command 'SELECT table_name FROM information_schema.tables' | grep test_)
+psql -d proddb -c "DROP TABLE $TESTDBS;"
+
+
 TEST_HOST="integration-tests:$PORT" \
   testcafe \
     --selector-timeout 50 \
