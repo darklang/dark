11dbd025313a0e6476a37b46bb9a894ee6e7e8bc
Paul Biggar <paul.biggar@gmail.com>
2018-04-05 10:23:15 -0700

    Refactor the server a little bit

--- server/lib/server.ml
+++ server/lib/server.ml
@@ -13,6 +13,9 @@ module PReq = Parsed_request
 module FF = FeatureFlag
 
 
+(* ------------------------------- *)
+(* utils *)
+(* ------------------------------- *)
 type timing_header = string * float * string
 
 let server_timing (times: timing_header list) =
@@ -44,168 +47,175 @@ let get_ip_address ch : string =
   | `TCP (ip, port) -> Ipaddr.to_string ip
   | _ -> assert false
 
+(* ------------------------------- *)
+(* handlers for dark developers *)
+(* ------------------------------- *)
+let admin_rpc_handler body (host: string) : (Cohttp.Header.t * string) =
+  try
+    let (t1, params) = time "1-read-api-ops"
+      (fun _ -> Api.to_rpc_params body) in
+
+    let (t2, c) = time "2-load-saved-ops"
+      (fun _ -> C.load host params.ops) in
+
+    let (t3, envs) = time "3-create-envs"
+      (fun _ ->
+        Event_queue.set_scope !c.name;
+        C.create_environments !c host) in
+
+    let (t4, result) = time "4-to-frontend"
+      (fun _ -> C.to_frontend_string envs params.executable_fns !c) in
+
+    let (t5, _) = time "5-save-to-disk"
+      (fun _ ->
+        (* work out the result before we save it, incase it has a
+         stackoverflow or other crashing bug *)
+        if Api.causes_any_changes params
+        then C.save !c
+        else ()
+      ) in
+
+  Event_queue.unset_scope ~status:`OK;
+  (server_timing [t1; t2; t3; t4; t5], result)
+  with
+  | e ->
+    Event_queue.unset_scope ~status:`Err;
+    let bt = Backtrace.Exn.most_recent () in
+    let msg = Exn.to_string e in
+    print_endline ("Exception: " ^ msg);
+    print_endline (Backtrace.to_string bt);
+    raise e
+
+let admin_ui_handler () =
+  let template = Util.readfile_lwt (Config.templates_dir ^ "ui.html") in
+  template >|= Util.string_replace "ALLFUNCTIONS" (Api.functions)
+
+let static_handler uri =
+  let fname = S.resolve_file ~docroot:(Config.webroot_dir) ~uri in
+  S.respond_file ~fname ()
+
+let save_test_handler host =
+  let g = C.load host [] in
+  let filename = C.save_test !g in
+  S.respond_string ~status:`OK ~body:("Saved as: " ^ filename) ()
+
+
+(* -------------------------------------------- *)
+(* handlers for end users *)
+(* -------------------------------------------- *)
+let cors = ("Access-Control-Allow-Origin", "*")
+
+let options_handler (c: C.canvas) (req: CRequest.t) =
+  (*       allow (from the route matching) *)
+  (*       Access-Control-Request-Method: POST  *)
+  (* Access-Control-Request-Headers: X-PINGOTHER, Content-Type *)
+  (* This is just enough to fix conduit. Here's what we should do:
+   * https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS *)
+  let req_headers = Cohttp.Header.get (CRequest.headers req) "access-control-request-headers" in
+  let allow_headers =
+    match req_headers with
+    | Some h -> h
+    | None -> "*"
+  in
+  let headers = [( "Access-Control-Allow-Methods"
+                 , "GET,PUT,POST,DELETE,PATCH,HEAD,OPTIONS")
+                ; ("Access-Control-Allow-Origin", "*")
+                ; ("Access-Control-Allow-Headers", allow_headers)] in
+  S.respond_string ~status:`OK
+                   ~body:""
+                   ~headers:(Cohttp.Header.of_list headers)
+                   ()
+
+
+let user_page_handler (host: string) (ip: string) (uri: Uri.t) (req: CRequest.t) (body: string) =
+  let c = C.load host [] in
+  Event_queue.set_scope !c.name;
+  let verb = req |> CRequest.meth |> Cohttp.Code.string_of_method in
+  let pages = C.pages_matching_route ~uri ~verb !c in
+  let pages =
+    if List.length pages > 1
+    then List.filter ~f:(fun (has_vars, _) -> not has_vars) pages
+    else pages in
+  let pages = List.map ~f:Tuple.T2.get2 pages in
+
+  match pages with
+  | [] when String.Caseless.equal verb "OPTIONS" ->
+    options_handler !c req
+  | [] ->
+    S.respond_string ~status:`Not_found ~headers:(Cohttp.Header.of_list [cors]) ~body:"404: No page matches" ()
+  | [page] ->
+    let route = Handler.event_name_for_exn page in
+    let input = PReq.from_request req body in
+    Stored_event.store host page.tlid (PReq.to_dval input);
+    let bound = Http.bind_route_params_exn ~uri ~route in
+    let dbs = TL.dbs !c.toplevels in
+    let dbs_env = Db.dbs_as_exe_env (dbs) in
+    Db.cur_dbs := dbs;
+    let env = Util.merge_left bound dbs_env in
+    let env = Map.set ~key:"request" ~data:(PReq.to_dval input) env in
+
+    let headers = CRequest.headers req in
+    let ff = FF.fingerprint_user ip headers in
+    let session_headers = FF.session_headers headers ff in
+    let state : Ast.exec_state =
+      { ff = ff
+      ; tlid = page.tlid
+      ; hostname = !c.name
+      ; user_fns = !c.user_functions
+      ; exe_fn_ids = []
+      ; env = env} in
+    let result = Handler.execute state page in
+    (match result with
+    | DResp (http, value) ->
+      (match http with
+       | Redirect url ->
+         Event_queue.unset_scope ~status:`OK;
+         S.respond_redirect (Uri.of_string url) ()
+       | Response (code, resp_headers) ->
+         let body =
+           if List.exists resp_headers ~f:(fun (name, value) ->
+              String.lowercase name = "content-type"
+              && String.lowercase value = "text/html")
+           then Dval.to_simple_repr "<" ">" value
+           (* TODO: only pretty print for a webbrowser *)
+           else Dval.dval_to_pretty_json_string value
+         in
+         Event_queue.unset_scope ~status:`OK;
+         S.respond_string
+           ~status:(Cohttp.Code.status_of_code code)
+           ~headers:(Cohttp.Header.of_list
+                       ([cors] @ resp_headers @ session_headers))
+           ~body:body
+           ())
+    | _ ->
+      Event_queue.unset_scope ~status:`OK;
+      let body = Dval.dval_to_pretty_json_string result in
+      (* for demonstrations sake, let's return 200 Okay when
+       * no HTTP response object is returned *)
+      let code = 200 in
+      S.respond_string
+        ~status:(Cohttp.Code.status_of_code code)
+        ~headers:(Cohttp.Header.of_list ([cors] @ session_headers))
+        ~body:body
+        ())
+  | _ ->
+    Event_queue.unset_scope ~status:`Err;
+    S.respond_string ~status:`Internal_server_error ~headers:(Cohttp.Header.of_list [cors]) ~body:"500: More than one page matches" ()
+
+(* -------------------------------------------- *)
+(* The server, with auth and routing *)
+(* -------------------------------------------- *)
 
 let server () =
   let stop,stopper = Lwt.wait () in
 
   let callback (ch, conn) req req_body =
-    let admin_rpc_handler body (host: string) : (Cohttp.Header.t * string) =
-      try
-        let (t1, params) = time "1-read-api-ops"
-          (fun _ -> Api.to_rpc_params body) in
-
-        let (t2, c) = time "2-load-saved-ops"
-          (fun _ -> C.load host params.ops) in
-
-        let (t3, envs) = time "3-create-envs"
-          (fun _ ->
-            Event_queue.set_scope !c.name;
-            C.create_environments !c host) in
-
-        let (t4, result) = time "4-to-frontend"
-          (fun _ -> C.to_frontend_string envs params.executable_fns !c) in
-
-        let (t5, _) = time "5-save-to-disk"
-          (fun _ ->
-            (* work out the result before we save it, incase it has a
-             stackoverflow or other crashing bug *)
-            if Api.causes_any_changes params
-            then C.save !c
-            else ()
-          ) in
-
-      Event_queue.unset_scope ~status:`OK;
-      (server_timing [t1; t2; t3; t4; t5], result)
-      with
-      | e ->
-        Event_queue.unset_scope ~status:`Err;
-        let bt = Backtrace.Exn.most_recent () in
-        let msg = Exn.to_string e in
-        print_endline ("Exception: " ^ msg);
-        print_endline (Backtrace.to_string bt);
-        raise e
-    in
-
-    let admin_ui_handler () =
-      let template = Util.readfile_lwt (Config.templates_dir ^ "ui.html") in
-      template >|= Util.string_replace "ALLFUNCTIONS" (Api.functions)
-    in
-
-    let static_handler uri =
-      let fname = S.resolve_file ~docroot:(Config.webroot_dir) ~uri in
-      S.respond_file ~fname ()
-    in
-
-    let save_test_handler host =
-      let g = C.load host [] in
-      let filename = C.save_test !g in
-      S.respond_string ~status:`OK ~body:("Saved as: " ^ filename) ()
-    in
-
-    let options_handler (c: C.canvas) (req: CRequest.t) =
-      (*       allow (from the route matching) *)
-      (*       Access-Control-Request-Method: POST  *)
-      (* Access-Control-Request-Headers: X-PINGOTHER, Content-Type *)
-      (* This is just enough to fix conduit. Here's what we should do: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS *)
-      let req_headers = Cohttp.Header.get (CRequest.headers req) "access-control-request-headers" in
-      let allow_headers =
-        match req_headers with
-        | Some h -> h
-        | None -> "*"
-      in
-      let headers = [("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,HEAD,OPTIONS"); ("Access-Control-Allow-Origin", "*"); ("Access-Control-Allow-Headers", allow_headers)] in
-      S.respond_string ~status:`OK
-                       ~body:""
-                       ~headers:(Cohttp.Header.of_list headers)
-                       ()
-    in
-
-    let cors = ("Access-Control-Allow-Origin", "*") in
-
-    let user_page_handler (host: string) (uri: Uri.t) (req: CRequest.t) (body: string) =
-      let c = C.load host [] in
-      Event_queue.set_scope !c.name;
-      let verb = req |> CRequest.meth |> Cohttp.Code.string_of_method in
-      let pages = C.pages_matching_route ~uri ~verb !c in
-      let pages =
-        if List.length pages > 1
-        then List.filter ~f:(fun (has_vars, _) -> not has_vars) pages
-        else pages in
-      let pages = List.map ~f:Tuple.T2.get2 pages in
-
-
-      match pages with
-      | [] when String.Caseless.equal verb "OPTIONS" ->
-        options_handler !c req
-      | [] ->
-        S.respond_string ~status:`Not_found ~headers:(Cohttp.Header.of_list [cors]) ~body:"404: No page matches" ()
-      | [page] ->
-        let route = Handler.event_name_for_exn page in
-        let input = PReq.from_request req body in
-        Stored_event.store host page.tlid (PReq.to_dval input);
-        let bound = Http.bind_route_params_exn ~uri ~route in
-        let dbs = TL.dbs !c.toplevels in
-        let dbs_env = Db.dbs_as_exe_env (dbs) in
-        Db.cur_dbs := dbs;
-        let env = Util.merge_left bound dbs_env in
-        let env = Map.set ~key:"request" ~data:(PReq.to_dval input) env in
-
-        let headers = CRequest.headers req in
-        let ip = get_ip_address ch in
-        let ff = FF.fingerprint_user ip headers in
-        let session_headers = FF.session_headers headers ff in
-        let state : Ast.exec_state =
-          { ff = ff
-          ; tlid = page.tlid
-          ; hostname = !c.name
-          ; user_fns = !c.user_functions
-          ; exe_fn_ids = []
-          ; env = env} in
-        let result = Handler.execute state page in
-        (match result with
-        | DResp (http, value) ->
-          (match http with
-           | Redirect url ->
-             Event_queue.unset_scope ~status:`OK;
-             S.respond_redirect (Uri.of_string url) ()
-           | Response (code, resp_headers) ->
-             let body =
-               if List.exists resp_headers ~f:(fun (name, value) ->
-                  String.lowercase name = "content-type"
-                  && String.lowercase value = "text/html")
-               then Dval.to_simple_repr "<" ">" value
-               (* TODO: only pretty print for a webbrowser *)
-               else Dval.dval_to_pretty_json_string value
-             in
-             Event_queue.unset_scope ~status:`OK;
-             S.respond_string
-               ~status:(Cohttp.Code.status_of_code code)
-               ~headers:(Cohttp.Header.of_list
-                           ([cors] @ resp_headers @ session_headers))
-               ~body:body
-               ())
-        | _ ->
-          Event_queue.unset_scope ~status:`OK;
-          let body = Dval.dval_to_pretty_json_string result in
-          (* for demonstrations sake, let's return 200 Okay when
-           * no HTTP response object is returned *)
-          let code = 200 in
-          S.respond_string
-            ~status:(Cohttp.Code.status_of_code code)
-            ~headers:(Cohttp.Header.of_list ([cors] @ session_headers))
-            ~body:body
-            ())
-      | _ ->
-        Event_queue.unset_scope ~status:`Err;
-        S.respond_string ~status:`Internal_server_error ~headers:(Cohttp.Header.of_list [cors]) ~body:"500: More than one page matches" ()
-    in
-
     let subdomain =
       let domain = Uri.host (req |> CRequest.uri) |> Option.value ~default:"" in
       match String.split domain '.' with
-        | ["localhost"] -> "localhost"
-        | a :: rest -> a
-        | _ -> ""
+      | ["localhost"] -> "localhost"
+      | a :: rest -> a
+      | _ -> ""
     in
 
     let auth_then_handle handler =
@@ -265,31 +275,27 @@ let server () =
          try
            let uri = req |> CRequest.uri in
            let verb = req |> CRequest.meth in
-           (* let auth = req |> Request.headers |> Header.get_authorization in *)
 
            let domain = match subdomain with
              | "" ->  failwith @@ "Unsupported domain: " ^ subdomain
              | _ -> subdomain
            in
 
-           Log.infO "request" (domain, Cohttp.Code.string_of_method verb, ("http:" ^Uri.to_string uri));
+           let rpc body =
+             let (headers, response_body) = admin_rpc_handler body domain in
+             S.respond_string ~status:`OK ~body:response_body ~headers ()
+           in
 
+           Log.infO "request" (domain, Cohttp.Code.string_of_method verb, ("http:" ^Uri.to_string uri));
            match (Uri.path uri) with
+           | "/admin/api/rpc" -> rpc req_body
            | "/admin/api/get_analysis" ->
              (* Reuse the RPC handler because it basically does the same
               * thing. It shouldn't save because there are no ops sent.
               * It also sends too much data back, but we just ignore it
               * in the client. *)
              let empty_body = "{ ops: [], executable_fns: []}" in
-             let (headers, body) = admin_rpc_handler empty_body domain in
-             S.respond_string ~status:`OK ~body ~headers ()
-           | "/admin/api/rpc" ->
-             let (headers, body) = admin_rpc_handler req_body domain in
-             S.respond_string ~status:`OK ~body ~headers ()
-           | "/sitemap.xml" ->
-             S.respond_string ~status:`OK ~body:"" ()
-           | "/favicon.ico" ->
-             S.respond_string ~status:`OK ~body:"" ()
+             rpc empty_body
            | "/admin/api/shutdown" ->
              Lwt.wakeup stopper ();
              S.respond_string ~status:`OK ~body:"Disembowelment" ()
@@ -299,12 +305,13 @@ let server () =
              admin_ui_handler () >>= fun body -> S.respond_string ~status:`OK ~body ()
            | "/admin/api/save_test" ->
              save_test_handler domain
-           | p when (String.length p) < 8 ->
-             user_page_handler domain uri req req_body
-           | p when (String.equal (String.sub p ~pos:0 ~len:8) "/static/") ->
+           | "/sitemap.xml"
+           | "/favicon.ico" ->
+             S.respond_string ~status:`OK ~body:"" ()
+           | p when (String.is_prefix ~prefix:"/static/" p) ->
              static_handler uri
            | _ ->
-             user_page_handler domain uri req req_body
+             user_page_handler domain (get_ip_address ch) uri req req_body
          with
          | e ->
            let bt = Backtrace.Exn.most_recent () in
