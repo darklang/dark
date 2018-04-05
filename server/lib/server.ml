7700b426a88290b18eb181cac07596e1bb49c78a
Paul Biggar <paul.biggar@gmail.com>
2018-04-05 11:09:31 -0700

    Refactor out the admin part

--- server/lib/server.ml
+++ server/lib/server.ml
@@ -50,55 +50,6 @@ let get_ip_address ch : string =
 (* ------------------------------- *)
 (* handlers for dark developers *)
 (* ------------------------------- *)
-let admin_rpc_handler body (host: string) : (Cohttp.Header.t * string) =
-  try
-    let (t1, params) = time "1-read-api-ops"
-      (fun _ -> Api.to_rpc_params body) in
-
-    let (t2, c) = time "2-load-saved-ops"
-      (fun _ -> C.load host params.ops) in
-
-    let (t3, envs) = time "3-create-envs"
-      (fun _ ->
-        Event_queue.set_scope !c.name;
-        C.create_environments !c host) in
-
-    let (t4, result) = time "4-to-frontend"
-      (fun _ -> C.to_frontend_string envs params.executable_fns !c) in
-
-    let (t5, _) = time "5-save-to-disk"
-      (fun _ ->
-        (* work out the result before we save it, incase it has a
-         stackoverflow or other crashing bug *)
-        if Api.causes_any_changes params
-        then C.save !c
-        else ()
-      ) in
-
-  Event_queue.unset_scope ~status:`OK;
-  (server_timing [t1; t2; t3; t4; t5], result)
-  with
-  | e ->
-    Event_queue.unset_scope ~status:`Err;
-    let bt = Backtrace.Exn.most_recent () in
-    let msg = Exn.to_string e in
-    print_endline ("Exception: " ^ msg);
-    print_endline (Backtrace.to_string bt);
-    raise e
-
-let admin_ui_handler () =
-  let template = Util.readfile_lwt (Config.templates_dir ^ "ui.html") in
-  template >|= Util.string_replace "ALLFUNCTIONS" (Api.functions)
-
-let static_handler uri =
-  let fname = S.resolve_file ~docroot:(Config.webroot_dir) ~uri in
-  S.respond_file ~fname ()
-
-let save_test_handler host =
-  let g = C.load host [] in
-  let filename = C.save_test !g in
-  S.respond_string ~status:`OK ~body:("Saved as: " ^ filename) ()
-
 
 (* -------------------------------------------- *)
 (* handlers for end users *)
@@ -127,8 +78,8 @@ let options_handler (c: C.canvas) (req: CRequest.t) =
                    ()
 
 
-let user_page_handler (host: string) (ip: string) (uri: Uri.t) (req: CRequest.t) (body: string) =
-  let c = C.load host [] in
+let user_page_handler ~(domain: string) ~(ip: string) ~(uri: Uri.t) ~(body: string) (req: CRequest.t) =
+  let c = C.load domain [] in
   Event_queue.set_scope !c.name;
   let verb = req |> CRequest.meth |> Cohttp.Code.string_of_method in
   let pages = C.pages_matching_route ~uri ~verb !c in
@@ -146,7 +97,7 @@ let user_page_handler (host: string) (ip: string) (uri: Uri.t) (req: CRequest.t)
   | [page] ->
     let route = Handler.event_name_for_exn page in
     let input = PReq.from_request req body in
-    Stored_event.store host page.tlid (PReq.to_dval input);
+    Stored_event.store domain page.tlid (PReq.to_dval input);
     let bound = Http.bind_route_params_exn ~uri ~route in
     let dbs = TL.dbs !c.toplevels in
     let dbs_env = Db.dbs_as_exe_env (dbs) in
@@ -203,13 +154,149 @@ let user_page_handler (host: string) (ip: string) (uri: Uri.t) (req: CRequest.t)
     S.respond_string ~status:`Internal_server_error ~headers:(Cohttp.Header.of_list [cors]) ~body:"500: More than one page matches" ()
 
 (* -------------------------------------------- *)
-(* The server, with auth and routing *)
+(* Admin server *)
 (* -------------------------------------------- *)
+let admin_rpc_handler body (domain: string) : (Cohttp.Header.t * string) =
+  try
+    let (t1, params) = time "1-read-api-ops"
+      (fun _ -> Api.to_rpc_params body) in
+
+    let (t2, c) = time "2-load-saved-ops"
+      (fun _ -> C.load domain params.ops) in
+
+    let (t3, envs) = time "3-create-envs"
+      (fun _ ->
+        Event_queue.set_scope !c.name;
+        C.create_environments !c domain) in
+
+    let (t4, result) = time "4-to-frontend"
+      (fun _ -> C.to_frontend_string envs params.executable_fns !c) in
+
+    let (t5, _) = time "5-save-to-disk"
+      (fun _ ->
+        (* work out the result before we save it, incase it has a
+         stackoverflow or other crashing bug *)
+        if Api.causes_any_changes params
+        then C.save !c
+        else ()
+      ) in
+
+  Event_queue.unset_scope ~status:`OK;
+  (server_timing [t1; t2; t3; t4; t5], result)
+  with
+  | e ->
+    Event_queue.unset_scope ~status:`Err;
+    let bt = Backtrace.Exn.most_recent () in
+    let msg = Exn.to_string e in
+    print_endline ("Exception: " ^ msg);
+    print_endline (Backtrace.to_string bt);
+    raise e
+
+let admin_ui_handler () =
+  let template = Util.readfile_lwt (Config.templates_dir ^ "ui.html") in
+  template >|= Util.string_replace "ALLFUNCTIONS" (Api.functions)
+
+let save_test_handler domain =
+  let g = C.load domain [] in
+  let filename = C.save_test !g in
+  S.respond_string ~status:`OK ~body:("Saved as: " ^ filename) ()
+
+
+let auth_then_handle req subdomain handler =
+  let path = req |> CRequest.uri |> Uri.path in
+  (* only handle auth for admin routes *)
+  if String.is_prefix ~prefix:"/admin" path
+  then
+    (* let users use their domain as a prefix for scratch work *)
+    let auth_domain =
+      match String.split subdomain '-' with
+      | d :: scratch -> d
+      | _ -> subdomain
+    in
+    Auth.Session.of_request req
+    >>= function
+    | Ok (Some session) ->
+      if path = "/admin/logout"
+      then
+        Auth.Session.clear Auth.Session.backend session
+        >>= fun _ ->
+        let headers =
+          (Header.of_list 
+             (Auth.Session.clear_hdrs Auth.Session.cookie_key)) in
+        S.respond_redirect ~headers ~uri:(Uri.of_string "/admin/ui") ()
+      else
+        (match Auth.Session.user_for session with
+         | Some user ->
+           if Auth.has_access ~domain:auth_domain ~user
+           then handler (Header.init ())
+           else S.respond_string ~status:`Unauthorized ~body:"Unauthorized" ()
+         | None ->
+           S.respond_string ~status:`Unauthorized ~body:"Invalid Session" ())
+    | _ ->
+      let auth =
+        req
+        |> CRequest.headers
+        |> Header.get_authorization
+      in
+      match auth with
+      | (Some (`Basic (user, pass))) ->
+        (match Auth.authenticate ~domain:auth_domain ~username:user ~password:pass with
+         | Some user ->
+           Auth.Session.new_for_user user >>=
+           (fun session ->
+              let headers = Header.of_list (Auth.Session.to_cookie_hdrs Auth.Session.cookie_key session) in
+              handler headers)
+         | None ->
+          S.respond_string ~status:`Unauthorized ~body:"Bad credentials" ())
+      | None ->
+        S.respond_need_auth ~auth:(`Basic "dark") ()
+      | _ ->
+        S.respond_string ~status:`Unauthorized ~body:"Invalid session" ()
+  else
+    handler (Header.init ())
+
+let admin_handler ~(domain: string) ~(uri: Uri.t) ~stopper ~(body: string) (req: CRequest.t) headers =
+  let rpc ?(body: string option) () =
+    let empty_body = "{ ops: [], executable_fns: []}" in
+    let req_body = Option.value ~default:empty_body body in
+
+    let (headers, response_body) = admin_rpc_handler req_body domain in
+    S.respond_string ~status:`OK ~body:response_body ~headers ()
+  in
+
+  match Uri.path uri with
+  | "/admin/api/rpc" -> rpc ~body ()
+  | "/admin/api/get_analysis" ->
+    (* Reuse the RPC handler because it basically does the same
+     * thing. It shouldn't save because there are no ops sent.
+     * It also sends too much data back, but we just ignore it
+     * in the client. *)
+    rpc ()
+  | "/admin/api/shutdown" ->
+    Lwt.wakeup stopper ();
+    S.respond_string ~status:`OK ~body:"Disembowelment" ()
+  | "/admin/ui" ->
+    admin_ui_handler () >>= fun body -> S.respond_string ~status:`OK ~headers ~body ()
+  | "/admin/integration_test" ->
+    admin_ui_handler () >>= fun body -> S.respond_string ~status:`OK ~body ()
+  | "/admin/api/save_test" ->
+    save_test_handler domain
+  | _ -> failwith "Not an admin route"
+
+(* -------------------------------------------- *)
+(* The server *)
+(* -------------------------------------------- *)
+
+let static_handler uri =
+  let fname = S.resolve_file ~docroot:(Config.webroot_dir) ~uri in
+  S.respond_file ~fname ()
+
 
 let server () =
   let stop,stopper = Lwt.wait () in
 
   let callback (ch, conn) req req_body =
+    let ip = (get_ip_address ch) in
     let subdomain =
       let domain = Uri.host (req |> CRequest.uri) |> Option.value ~default:"" in
       match String.split domain '.' with
@@ -218,60 +305,9 @@ let server () =
       | _ -> ""
     in
 
-    let auth_then_handle handler =
-      let path = req |> CRequest.uri |> Uri.path in
-      (* only handle auth for admin routes *)
-      if String.is_prefix ~prefix:"/admin" path
-      then
-        (* let users use their domain as a prefix for scratch work *)
-        let auth_domain =
-          match String.split subdomain '-' with
-          | d :: scratch -> d
-          | _ -> subdomain
-        in
-        Auth.Session.of_request req
-        >>= function
-        | Ok (Some session) ->
-          if path = "/admin/logout"
-          then
-            Auth.Session.clear Auth.Session.backend session
-            >>= fun _ ->
-            S.respond_redirect ~headers:(Header.of_list (Auth.Session.clear_hdrs Auth.Session.cookie_key)) ~uri:(Uri.of_string "/admin/ui") ()
-          else
-            (match Auth.Session.user_for session with
-             | Some user ->
-               if Auth.has_access ~domain:auth_domain ~user
-               then handler (Header.init ())
-               else S.respond_string ~status:`Unauthorized ~body:"Unauthorized" ()
-             | None ->
-               S.respond_string ~status:`Unauthorized ~body:"Invalid Session" ())
-        | _ ->
-          let auth =
-            req
-            |> CRequest.headers
-            |> Header.get_authorization
-          in
-          match auth with
-          | (Some (`Basic (user, pass))) ->
-            (match Auth.authenticate ~domain:auth_domain ~username:user ~password:pass with
-             | Some user ->
-               Auth.Session.new_for_user user >>=
-               (fun session ->
-                  let headers = Header.of_list (Auth.Session.to_cookie_hdrs Auth.Session.cookie_key session) in
-                  handler headers)
-             | None ->
-              S.respond_string ~status:`Unauthorized ~body:"Bad credentials" ())
-          | None ->
-            S.respond_need_auth ~auth:(`Basic "dark") ()
-          | _ ->
-            S.respond_string ~status:`Unauthorized ~body:"Invalid session" ()
-      else
-        handler (Header.init ())
-    in
-
-    let route_handler in_headers  =
+    let route_handler =
       req_body |> Cohttp_lwt__Body.to_string >>=
-      (fun req_body ->
+      (fun body ->
          try
            let uri = req |> CRequest.uri in
            let verb = req |> CRequest.meth in
@@ -281,37 +317,18 @@ let server () =
              | _ -> subdomain
            in
 
-           let rpc body =
-             let (headers, response_body) = admin_rpc_handler body domain in
-             S.respond_string ~status:`OK ~body:response_body ~headers ()
-           in
-
            Log.infO "request" (domain, Cohttp.Code.string_of_method verb, ("http:" ^Uri.to_string uri));
            match (Uri.path uri) with
-           | "/admin/api/rpc" -> rpc req_body
-           | "/admin/api/get_analysis" ->
-             (* Reuse the RPC handler because it basically does the same
-              * thing. It shouldn't save because there are no ops sent.
-              * It also sends too much data back, but we just ignore it
-              * in the client. *)
-             let empty_body = "{ ops: [], executable_fns: []}" in
-             rpc empty_body
-           | "/admin/api/shutdown" ->
-             Lwt.wakeup stopper ();
-             S.respond_string ~status:`OK ~body:"Disembowelment" ()
-           | "/admin/ui" ->
-             admin_ui_handler () >>= fun body -> S.respond_string ~status:`OK ~headers:in_headers ~body ()
-           | "/admin/integration_test" ->
-             admin_ui_handler () >>= fun body -> S.respond_string ~status:`OK ~body ()
-           | "/admin/api/save_test" ->
-             save_test_handler domain
            | "/sitemap.xml"
            | "/favicon.ico" ->
              S.respond_string ~status:`OK ~body:"" ()
            | p when (String.is_prefix ~prefix:"/static/" p) ->
              static_handler uri
+           | p when  (String.is_prefix ~prefix:"/admin/" p) ->
+             auth_then_handle req subdomain
+               (admin_handler ~domain ~uri ~body ~stopper req)
            | _ ->
-             user_page_handler domain (get_ip_address ch) uri req req_body
+             user_page_handler ~domain  ~ip ~uri ~body req
          with
          | e ->
            let bt = Backtrace.Exn.most_recent () in
@@ -326,7 +343,7 @@ let server () =
            Lwt_io.printl (Backtrace.to_string bt) >>= fun () ->
            S.respond_string ~status:`Internal_server_error ~headers:(Cohttp.Header.of_list [cors]) ~body ())
     in
-    auth_then_handle route_handler
+    route_handler
   in
   S.create ~stop ~mode:(`TCP (`Port Config.port)) (S.make ~callback ())
 
