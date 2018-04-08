7dc9b469915331a81e41950cef3e7318d30314bf
Paul Biggar <paul.biggar@gmail.com>
2018-04-08 15:11:11 -0700

    Add tests for stored_events

--- server/lib/stored_event.mli
+++ server/lib/stored_event.mli
@@ -1,8 +1,8 @@
 open Core
 
 (* space, path, modifier *)
-type event_desc = string * string * string
-type four_oh_four = (event_desc * Types.RuntimeT.dval list)
+type event_desc = string * string * string [@@deriving show]
+type four_oh_four = event_desc * Types.RuntimeT.dval list [@@deriving show]
 
 val store_event : string -> event_desc -> Types.RuntimeT.dval -> unit
 val load_events : string -> event_desc -> Types.RuntimeT.dval list
