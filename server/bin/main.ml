5cc9081d8f79e4ea65013319c0c9594aa6e80084
Paul Biggar <paul.biggar@gmail.com>
2018-04-10 18:33:13 -0700

    Move shape data into init

--- server/bin/main.ml
+++ server/bin/main.ml
@@ -5,5 +5,4 @@ let () =
   (* see https://github.com/mirage/ocaml-cohttp/issues/511 *)
   let () = Lwt.async_exception_hook := ignore in
   Dark.Init.init ();
-  Dark.Serialize.write_shape_data ();
   Dark.Server.run ()
