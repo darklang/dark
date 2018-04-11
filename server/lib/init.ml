6fd24b11052b6e4a2e501b29d180563782d33f61
Paul Biggar <paul.biggar@gmail.com>
2018-04-10 18:31:17 -0700

    Move an env var into config

--- server/lib/init.ml
+++ server/lib/init.ml
@@ -7,25 +7,7 @@ let init () =
   if !has_inited
   then ()
   else
-    let _ =
-      match Sys.getenv "DARK_CONFIG_LOGLEVEL" with
-      | None -> ()
-      | Some s when String.Caseless.equal s "OFF" ->
-        Log.level := `Off
-      | Some s when String.Caseless.equal s "FATAL" ->
-        Log.level := `Fatal
-      | Some s when String.Caseless.equal s "ERROR" ->
-        Log.level := `Error
-      | Some s when String.Caseless.equal s "WARN" ->
-        Log.level := `Warn
-      | Some s when String.Caseless.equal s "INFO" ->
-        Log.level := `Info
-      | Some s when String.Caseless.equal s "DEBUG" ->
-        Log.level := `Debug
-      | Some s when String.Caseless.equal s "ALL" ->
-        Log.level := `All
-      | Some _ -> ()
-    in
+    Log.level := Config.log_level;
     Printexc.record_backtrace true;
     Exn.initialize_module ();
     (* init the Random module, will be seeded from /dev/urandom on Linux *)
