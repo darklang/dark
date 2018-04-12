93aab5f51ee6224028b61ad0f412b3b4d651fcbb
Ian Connolly <ian@connolly.io>
2018-04-11 18:57:13 -0700

    Implement copy + paste for function editing

--- client/Clipboard.elm
+++ client/Clipboard.elm
@@ -12,11 +12,16 @@ import Util exposing (deMaybe)
 
 copy : Model -> Toplevel -> (Maybe PointerData) -> Modification
 copy m tl mp =
-  case TL.asHandler tl of
-    Nothing -> NoChange
-    Just h ->
+  case tl.data of
+    TLDB _ ->
+      NoChange
+    TLHandler h ->
       case mp of
         Nothing -> CopyToClipboard (Just <| PExpr h.ast)
+        Just p -> CopyToClipboard (TL.find tl (P.toID p))
+    TLFunc f ->
+      case mp of
+        Nothing -> CopyToClipboard (Just <| PExpr f.ast)
         Just p -> CopyToClipboard (TL.find tl (P.toID p))
 
 cut : Model -> Toplevel -> PointerData -> Modification
@@ -25,18 +30,29 @@ cut m tl p =
       pred = TL.getPrevBlank tl (Just p)
              |> Maybe.map P.toID
   in
-    case TL.asHandler tl of
-      Nothing -> NoChange
-      Just h ->
-        let newClipboard = TL.find tl pid
-            newH = TL.delete tl p (gid ())
-                   |> TL.asHandler
-                   |> deMaybe "cut"
+      case tl.data of
+        TLDB _ ->
+          NoChange
+        TLHandler h ->
+          let newClipboard = TL.find tl pid
+              newH = TL.delete tl p (gid ())
+                    |> TL.asHandler
+                    |> deMaybe "cut"
 
-        in Many [ CopyToClipboard newClipboard
-                , RPC ( [ SetHandler tl.id tl.pos newH ]
-                        , FocusNext tl.id pred)
-                ]
+          in Many [ CopyToClipboard newClipboard
+                  , RPC ( [ SetHandler tl.id tl.pos newH ]
+                          , FocusNext tl.id pred)
+                  ]
+        TLFunc f ->
+          let newClipboard = TL.find tl pid
+              newF = TL.delete tl p (gid ())
+                    |> TL.asUserFunction
+                    |> deMaybe "cut"
+
+          in Many [ CopyToClipboard newClipboard
+                  , RPC ( [ SetFunction newF ]
+                          , FocusNext tl.id pred)
+                  ]
 
 paste : Model -> Toplevel -> ID -> Modification
 paste m tl id =
@@ -44,12 +60,20 @@ paste m tl id =
     Nothing -> NoChange
     Just pd ->
       let cloned = TL.clonePointerData pd
-      in case TL.asHandler tl of
-           Nothing -> NoChange
-           Just h ->
-             let newAst = AST.replace (TL.findExn tl id) cloned h.ast
-             in RPC ( [ SetHandler tl.id tl.pos { h | ast = newAst } ]
-                    , FocusNext tl.id (Just (P.toID cloned)))
+      in
+          case tl.data of
+            TLDB _ ->
+              NoChange
+            TLHandler h ->
+              let newAst = AST.replace (TL.findExn tl id) cloned h.ast
+              in
+                  RPC ( [ SetHandler tl.id tl.pos { h | ast = newAst } ]
+                  , FocusNext tl.id (Just (P.toID cloned)))
+            TLFunc f ->
+              let newAst = AST.replace (TL.findExn tl id) cloned f.ast
+              in
+                  RPC ( [ SetFunction { f | ast = newAst } ]
+                  , FocusNext tl.id (Just (P.toID cloned)))
 
 peek : Model -> Clipboard
 peek m =
@@ -69,5 +93,4 @@ newFromClipboard m pos =
       handler = { ast = ast, spec = spec }
   in
       RPC ([SetHandler nid pos handler], FocusNext nid Nothing)
-
 
