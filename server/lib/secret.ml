12570f338e5277ed7a0b8e384d6245df2ed40576
Paul Biggar <paul.biggar@gmail.com>
2018-04-12 10:51:27 -0700

    Remove secrets (they've been revoked already)

--- server/lib/secret.ml
+++ server/lib/secret.ml
@@ -1,21 +1,21 @@
 (* Keys for: https://apps.twitter.com/app/14082455 *)
 let twitter_bearer_token =
-  "AAAAAAAAAAAAAAAAAAAAAJfh1gAAAAAAazXXwsaMuN"
-  ^ "yK2a8ZsTGVX32KdXY%3DzKh8JxqSB8tkLKzVgEY3"
-  ^ "Pagi8le92ZQE5PXTqimhtVRqyjeWRz"
+  ""
+  ^ ""
+  ^ ""
 
 let twitter_CONSUMER_KEY =
-  "FOohmxHC3ExCFvLVbl8UySqFu"
+  ""
 
 let twitter_CONSUMER_SECRET =
-  "J5GgPfzqQvxSyjQFqP8MDE4diuZS87KtUDdhb1vBFA5BX0o7rP"
+  ""
 
 (* Secrets for @paulbiggar and this app *)
 let paulbiggar_ACCESS_TOKEN =
-  "86938585-uDxKWfIywAVnV7bVOnL171lFgQ45OIbswteqmMMSx"
+  ""
 
 let paulbiggar_ACCESS_TOKEN_SECRET =
-  "QzmAJlKk5HwYYHNrNLTr9V8NM9hzFyudVoHShOki7HKdz"
+  ""
 
 type twitter_secret = { consumer_key: string
                       ; consumer_secret: string
--- server/lib/twitter-token.sh
+++ server/lib/twitter-token.sh
@@ -2,16 +2,18 @@
 
 set -xeuo pipefail
 
+# Note: we've deleted these keys.
+
 # -------------
 # Get the bearer token
 # -------------
-# CONSUMER_KEY="FOohmxHC3ExCFvLVbl8UySqFu"
-# CONSUMER_SECRET="J5GgPfzqQvxSyjQFqP8MDE4diuZS87KtUDdhb1vBFA5BX0o7rP"
+# CONSUMER_KEY=""
+# CONSUMER_SECRET=""
 # concat="${CONSUMER_KEY}:${CONSUMER_SECRET}"
 
 # curl 'https://api.twitter.com/oauth2/token' --data 'grant_type=client_credentials' -H 'application/x-www-form-urlencoded;charset=UTF-8' --user "$concat"
 
-BEARER="AAAAAAAAAAAAAAAAAAAAAJfh1gAAAAAAazXXwsaMuNyK2a8ZsTGVX32KdXY%3DzKh8JxqSB8tkLKzVgEY3Pagi8le92ZQE5PXTqimhtVRqyjeWRz"
+BEARER=""
 
 # -------------
 # Make API call
