bf2bd17a8a58447d37d2e65f92c6a2b9b1eeebab
Paul Biggar <paul.biggar@gmail.com>
2018-04-14 16:45:01 -0700

    If you're on a nested, stay on a nested, and vice versa

--- client/Native/Size.js
+++ client/Native/Size.js
@@ -14,38 +14,51 @@ var _user$project$Native_Size = {
   },
 
   positions: function(tlid) {
+    let find = function (tl, nested) {
+      let results = [];
+      var selector;
+      if (nested) {
+        selector = ".blankOr.nested";
+      } else {
+        selector = ".blankOr:not(.nested)";
+      }
+      for (var bor of tl.querySelectorAll(selector)) {
+        let bb = bor.getBoundingClientRect();
+        var id = -1;
+        for (var c of bor.classList) {
+          let match = c.match(/id-(\d+)/);
+          if (match) {
+            id = parseInt(match[1]);
+          }
+        }
+        if (id == -1) {
+          console.log(bor.classList);
+          throw ("Couldn't find id property");
+        }
+        var v = { x: bb.x
+                , y: bb.y
+                , width : bb.width
+                , height: bb.height
+                , top: bb.top
+                , bottom: bb.bottom
+                , left: bb.left
+                , right: bb.right
+                , id: id
+                };
+        results.push(v);
+      }
+      return _elm_lang$core$Native_List.fromArray(results);
+    }
+
     let tls = document.getElementsByClassName("toplevel " + tlid);
     if (tls.length != 1) {
       console.log(tls);
       throw ("Couldn't find toplevel: " + tlid);
     }
+
     let tl = tls[0];
-    let results = [];
-    for (var bor of tl.getElementsByClassName("blankOr")) {
-      let bb = bor.getBoundingClientRect();
-      var id = -1;
-      for (var c of bor.classList) {
-        let match = c.match(/id-(\d+)/);
-        if (match) {
-          id = parseInt(match[1]);
-        }
-      }
-      if (id == -1) {
-        console.log(bor.classList);
-        throw ("Couldn't find id property");
-      }
-      var v = { x: bb.x
-              , y: bb.y
-              , width : bb.width
-              , height: bb.height
-              , top: bb.top
-              , bottom: bb.bottom
-              , left: bb.left
-              , right: bb.right
-              , id: id
-              };
-      results.push(v);
-    }
-    return _elm_lang$core$Native_List.fromArray(results);
+    return { nested: find(tl, true)
+           , atoms: find(tl, false)
+           }
   }
 };
