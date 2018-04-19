3de9534bc9a9a2e1ff4459b9ee16879be1a3688d
Ian Connolly <ian@connolly.io>
2018-04-19 16:08:52 -0700

    Revert "Add Rollbar to AST tracing ctx"
    This reverts commit 6fa54baf61f78ee02b3a80b0ca147eec6ba1fdb2.

--- server/lib/rollbar.mli
+++ server/lib/rollbar.mli
@@ -5,7 +5,7 @@ module CRequest = Cohttp_lwt_unix.Request
 type result = [`Success | `Failure | `Disabled]
 type err_ctx = Remote of CRequest.t
              | EventQueue
-             | Tracing
+
 
 (* Reports an exn with a backtrace to Rollbar asynchronously *)
 val report_lwt : exn -> Backtrace.t -> err_ctx -> result Lwt.t
