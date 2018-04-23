919b5cf845d7725271c7d74f25c957d851912424
Paul Biggar <paul.biggar@gmail.com>
2018-04-22 17:20:45 -0700

    Make Util.random work in the test suite

--- client/Native/Random.js
+++ client/Native/Random.js
@@ -1,7 +1,13 @@
 var _user$project$Native_Random = {
   random: function(a) {
-    var array = new Uint32Array(1);
-    window.crypto.getRandomValues(array);
-    return array[0];
+    if (window && window.crypto) {
+      var array = new Uint32Array(1);
+      window.crypto.getRandomValues(array);
+      return array[0];
+    }
+    else {
+      // testing
+      return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+    }
   }
 }
