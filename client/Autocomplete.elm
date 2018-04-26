ba6f1a30a82f41d64045ad2fffcb8d909fce5a4e
Paul Biggar <paul.biggar@gmail.com>
2018-04-26 12:20:43 -0700

    Allow DB names with numbers and underscores

--- client/Autocomplete.elm
+++ client/Autocomplete.elm
@@ -223,7 +223,7 @@ parseLiteral s =
 parseDBName : String -> Maybe DBName
 parseDBName s =
   if String.length s >= 4
-      && Util.reExactly "[A-Za-z]*" s
+      && Util.reExactly "[A-Za-z][a-zA-Z0-9_-]*" s
   then
     Just s
   else
