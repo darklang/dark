bcb0083202221a5510a0102b67b6aeca3e5938e2
Paul Biggar <paul.biggar@gmail.com>
2018-04-27 09:35:25 -0700

    Separate spaces in the routing table

--- client/ViewRoutingTable.elm
+++ client/ViewRoutingTable.elm
@@ -6,6 +6,7 @@ module ViewRoutingTable exposing (viewRoutingTable)
 import Html
 import Html.Attributes as Attrs
 import List.Extra as LE
+import Tuple2 as T2
 
 -- dark
 import Types exposing (..)
@@ -15,55 +16,84 @@ import Toplevel as TL
 import Blank as B
 import Toplevel
 import ViewUtils exposing (..)
+import SpecHeaders
 
 
 
-type alias Collapsed = { name: Maybe String
-                       , prefix: List String
-                       , verbs: List (String, Pos)}
+type alias Entry = { name: Maybe String
+                   , prefix: List String
+                   , verbs: List (String, Pos)
+                   , isHttp: Bool }
 
-collapseHandlers : List Toplevel -> List Collapsed
-collapseHandlers tls =
-  let asCollapsed =
-        tls
-        |> List.filterMap
-            (\tl ->
-              case TL.asHandler tl of
-                Just h -> Just (tl.pos, h)
-                Nothing -> Nothing)
-        |> List.map
-             (\(pos, h) ->
-               { name =
-                   case B.flattenFF h.spec.name of
-                     F _ s -> Just s
-                     Blank _ -> Nothing
-                     Flagged _ _ _ _ _ ->
-                       impossible ("FF in spec name", h.spec.name)
-               , prefix = []
-               , verbs =
-                   case B.flattenFF h.spec.modifier of
-                     F _ s -> [(s, pos)]
-                     Blank _ -> []
-                     Flagged _ _ _ _ _ ->
-                       impossible ("FF in spec modifier", h.spec.modifier)
-               })
-        |> List.sortBy (\c -> Maybe.withDefault "ZZZZZZ" c.name)
+spaceName : Toplevel -> Maybe String
+spaceName tl =
+  case tl.data of
+    TLHandler h -> h.spec.module_ |> B.toMaybe
+    _ -> Nothing
+
+splitBySpace : List Toplevel -> List (String, List Toplevel)
+splitBySpace tls =
+  let spaceName tl = tl
+                     |> TL.asHandler
+                     |> Maybe.map .spec
+                     |> Maybe.map .module_
+                     |> Maybe.andThen B.toMaybe
+                     |> Maybe.withDefault "<no space yet>"
   in
-    prefixify <|
-    List.foldr (\curr list ->
-      case list of
-        [] -> [curr]
-        prev :: rest ->
-          if prev.name == curr.name
-          then
-            let new = { prev | verbs = prev.verbs ++ curr.verbs }
-            in new :: rest
-          else
-            curr :: prev :: rest
-    ) [] asCollapsed
+  tls
+  |> List.sortBy spaceName
+  |> LE.groupWhile (\a b -> spaceName a == spaceName b)
+  |> List.map (\hs ->
+                 let space = hs
+                             |> List.head
+                             |> deMaybe "splitBySpace"
+                             |> spaceName
+                in
+                 (space, hs))
 
 
-prefixify : List Collapsed -> List Collapsed
+
+
+
+collapseHandlers : List Toplevel -> List Entry
+collapseHandlers tls =
+  tls
+  |> List.filterMap
+      (\tl ->
+        case TL.asHandler tl of
+          Just h -> Just (tl.pos, h)
+          Nothing -> Nothing)
+  |> List.map
+       (\(pos, h) ->
+         { name =
+             case B.flattenFF h.spec.name of
+               F _ s -> Just s
+               Blank _ -> Nothing
+               Flagged _ _ _ _ _ ->
+                 impossible ("FF in spec name", h.spec.name)
+         , prefix = []
+         , isHttp = SpecHeaders.isHTTP h.spec
+         , verbs =
+             case B.flattenFF h.spec.modifier of
+               F _ s -> [(s, pos)]
+               Blank _ -> []
+               Flagged _ _ _ _ _ ->
+                 impossible ("FF in spec modifier", h.spec.modifier)
+         })
+  |> List.sortBy (\c -> Maybe.withDefault "ZZZZZZ" c.name)
+  |> List.foldr (\curr list ->
+                   case list of
+                     [] -> [curr]
+                     prev :: rest ->
+                       if prev.name == curr.name
+                       then
+                         let new = { prev | verbs = prev.verbs ++ curr.verbs }
+                         in new :: rest
+                       else
+                         curr :: prev :: rest
+                ) []
+
+prefixify : List Entry -> List Entry
 prefixify hs =
   case hs of
     [] -> hs
@@ -73,7 +103,7 @@ prefixify hs =
         Nothing -> h :: prefixify rest
         Just name ->
           let len = String.length name
-              makePrefix : Collapsed -> Collapsed
+              makePrefix : Entry -> Entry
               makePrefix h2 =
                 case h2.name of
                   Just name2 ->
@@ -105,11 +135,10 @@ text class msg = span class [Html.text msg]
 div : String -> List (Html.Html Msg) -> Html.Html Msg
 div class subs = Html.div [Attrs.class class] subs
 
-viewRoutes : Model -> Html.Html Msg
-viewRoutes m =
-  let handlers = m.toplevels |> collapseHandlers
-      handlerCount = List.length handlers
-      def s = Maybe.withDefault "<not entered>" s
+viewGroup : Model -> (String, List Entry) -> Html.Html Msg
+viewGroup m (spacename, entries) =
+  let handlerCount = List.length entries
+      def s = Maybe.withDefault "<no route yet>" s
       link h =
         if List.member "GET" (List.map Tuple.first h.verbs)
         then
@@ -126,8 +155,8 @@ viewRoutes m =
         else
           Html.div [] []
 
-      internalLinks h =
-        h.verbs
+      internalLinks e =
+        e.verbs
         |> List.map
           (\(verb, pos) ->
             Html.a
@@ -138,24 +167,33 @@ viewRoutes m =
             ]
             [ Html.text verb ])
         |> List.intersperse (Html.text ",")
-      handlerHtml h =
+      entryHtml e =
         div "handler" [ div "url"
-                          (  List.map (text "prefix") h.prefix
-                          ++ [text "name" (def h.name)])
-                      , link h
-                      , span "verbs" (internalLinks h)
+                          (  List.map (text "prefix") e.prefix
+                          ++ [text "name" (def e.name)])
+                      , link e
+                      , span "verbs" (internalLinks e)
                       ]
       header = div "header"
-                 [ text "title" "HTTP"
+                 [ text "title" spacename
                  , text "parens" "("
                  , text "count" (toString handlerCount)
                  , text "parens" ")"
                  ]
-      routes = div "routes" (List.map handlerHtml handlers)
+      routes = div "routes" (List.map entryHtml entries)
   in
   Html.div
-    [ Attrs.class "http-routes"]
+    [ Attrs.class "routing-section"]
     [ header, routes]
+
+viewRoutes : Model -> Html.Html Msg
+viewRoutes m =
+  let groups = m.toplevels
+               |> splitBySpace
+               |> List.map (T2.map collapseHandlers)
+               |> List.map (T2.map prefixify)
+               |> List.map (viewGroup m)
+  in div "groups" groups
 
 
 
@@ -185,7 +223,7 @@ view404s m =
                  , text "count" (toString count)
                  , text "parens" ")"
                  ]
-      routes = div "routes" (List.map fofHtml m.f404s)
+      routes = div ".routing-section" (List.map fofHtml m.f404s)
   in Html.div
        [Attrs.class "fofs"]
        [header, routes]
