960ab6751b89228c6c75a17d5f26ec7d696381d1
Paul Biggar <paul.biggar@gmail.com>
2018-05-02 18:06:12 -0700

    Add Steve Krouse

--- server/lib/user.ml
+++ server/lib/user.ml
@@ -29,6 +29,7 @@ let users =
   (* friends trying it out *)
   ;["lee"; "lee"; "klma923wels92l{]as]"; "lee@ledwards.com"]
   ;["alexey"; "alexey"; "sd][3[mlvkm9034j09"; "me@alexey.ch"]
+  ;["steve"; "steve"; "';si83n']\spr,3idk"; "steveykrouse@gmail.com"]
 
   (* interviews *)
   ]
