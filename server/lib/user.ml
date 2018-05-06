4eacd78c51aa828f24183b47fb80cb2961902453
Paul Biggar <paul.biggar@gmail.com>
2018-05-05 17:59:15 -0700

    Add real names to user.ml, and also Tom

--- server/lib/user.ml
+++ server/lib/user.ml
@@ -14,28 +14,29 @@ let construct ~username ~domain ~password =
 
 let users =
   (* employees *)
-  [["ian"; "admin"; "look"; "ian@darklang.com"]
-  ;["paul";"admin"; "what"; "paul@darklang.com"]
-  ;["ellen"; "admin"; "you"; "ellen@darklang.com"]
-  ;["stefi"; "admin"; "made"; "stefi@darklang.com"]
-  ;["zane"; "admin"; "me do"; "zane@darklang.com"]
+  [["ian"; "admin"; "look"; "ian@darklang.com"; "Ian Connolly"]
+  ;["paul";"admin"; "what"; "paul@darklang.com"; "Paul Biggar"]
+  ;["ellen"; "admin"; "you"; "ellen@darklang.com"; "Ellen Chisa"]
+  ;["stefi"; "admin"; "made"; "stefi@darklang.com"; "Stefi Petit"]
+  ;["zane"; "admin"; "me do"; "zane@darklang.com"; "Zane Shannon"]
 
   (* testing *)
-  ;["tests"; "admin"; "fVm2CUePzGKCwoEQQdNJktUQ"; ""]
-  ;["aoife"; "dogtreats"; "notarealdog"; "ian+dog@darklang.com"]
+  ;["tests"; "admin"; "fVm2CUePzGKCwoEQQdNJktUQ"; "ops@darklang.com"; "Test Account"]
+  ;["aoife"; "dogtreats"; "notarealdog"; "ian+dog@darklang.com"; "Aoife Connolly"]
 
   (* customers *)
 
   (* friends trying it out *)
-  ;["lee"; "lee"; "klma923wels92l{]as]"; "lee@ledwards.com"]
-  ;["alexey"; "alexey"; "sd][3[mlvkm9034j09"; "me@alexey.ch"]
-  ;["steve"; "steve"; "';si83n']\spr,3idk"; "steveykrouse@gmail.com"]
+  ;["lee"; "lee"; "klma923wels92l{]as]"; "lee@ledwards.com"; "Lee Edwards"]
+  ;["alexey"; "alexey"; "sd][3[mlvkm9034j09"; "me@alexey.ch"; "Alexey Klochay"]
+  ;["steve"; "steve"; "';si83n']\\spr,3idk"; "steveykrouse@gmail.com"; "Steve Krouse"]
+  ;["tom"; "tom"; "ksef[1=s;'dier]2"; "tmrudick@gmail.com"; "Tom Rudick"]
 
   (* interviews *)
   ]
   |> List.map
        ~f:(function
-        | [username; domain; password; _] -> construct ~username ~domain ~password
+        | [username; domain; password; _email; _name] -> construct ~username ~domain ~password
         | _ -> failwith "Needs to be an array of length 3")
 
 let all_for_domain domain =
