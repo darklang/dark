4cef9edc8c6107896062ec0ac4007649483c179d
Ian Connolly <ian@connolly.io>
2018-05-11 15:12:23 -0700

    Give exceptions proper tipes I can match against

--- server/lib/exception.ml
+++ server/lib/exception.ml
@@ -5,9 +5,27 @@ type exception_info = (string * string) list [@@deriving yojson, show]
 let exception_info_to_yojson info =
   `Assoc (List.map ~f:(fun (k,v) -> (k, `String v)) info)
 
+type exception_tipe = DarkServer
+                    | DarkClient
+                    | DarkRuntime
+                    | Dependency
+                    | ExternalService
+                    | UserCode
+                    | Unknown [@@deriving show, eq, yojson, sexp]
+
+let should_log (et: exception_tipe) : bool =
+  match et with
+  | DarkServer -> true
+  | DarkClient -> true
+  | DarkRuntime -> true
+  | Dependency -> true
+  | ExternalService -> true
+  | UserCode -> false
+  | Unknown -> true
+
 type exception_data = { short : string
                       ; long : string
-                      ; tipe : string
+                      ; tipe : exception_tipe
                       ; actual : string
                       ; actual_tipe : string
                       ; expected : string (* might refer to result or actual *)
@@ -29,7 +47,7 @@ let reraise e  =
   Caml.Printexc.raise_with_backtrace e bt
 
 let raise_
-    (tipe: string)
+    (tipe: exception_tipe)
     ?(bt:Caml.Printexc.raw_backtrace option=None)
     ?(actual="")
     ?(actual_tipe="unknown tipe")
@@ -40,26 +58,28 @@ let raise_
     ?(workarounds=[])
     ?(long="")
     (short: string) =
-  let e = { short = short
-          ; long = long
-          ; tipe = tipe
-          ; actual = actual
-          ; actual_tipe = actual_tipe
-          ; expected = expected
-          ; result = result
-          ; result_tipe = result_tipe
-          ; info = info
-          ; workarounds = workarounds
+  let e = { short
+          ; long
+          ; tipe
+          ; actual
+          ; actual_tipe
+          ; expected
+          ; result
+          ; result_tipe
+          ; info
+          ; workarounds
           }
   in
-  Log.erroR "exception" e;
+  if should_log e.tipe
+  then
+    Log.erroR "exception" e;
   match bt with
   | None -> raise (DarkException e)
   | Some bt -> Caml.Printexc.raise_with_backtrace (DarkException e) bt
 
-let internal = raise_ "Dark (server)"
-let client = raise_ "Dark (editor)"
-let user = raise_ "User"
-let api = raise_ "API error"
+let internal = raise_ DarkServer
+let client = raise_ DarkClient
+let user = raise_ UserCode
+let api = raise_ ExternalService
 
 
