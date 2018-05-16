4c890dfc7446f4cf5cca98f3d155fe5e6e2bd878
Paul Biggar <paul.biggar@gmail.com>
2018-05-15 20:20:18 -0700

    Get an owner for every handler

--- server/lib/account.mli
+++ server/lib/account.mli
@@ -1,9 +1,18 @@
 open Core
 open Types
 
+type t
 type username = string
 
-val authenticate : auth_domain:string -> username:username -> password:string -> bool
-val has_access : auth_domain:string -> username:username  -> bool
+(* validate username/password of a Dark user *)
+val authenticate : username:username -> password:string -> bool
+
+(* can username edit auth_domain, in admin/ui *)
+val can_edit : auth_domain:string -> username:username -> bool
+
+(* get the id of the domain, for accounting/storage. Note that we need
+ * this even for unauthenticated access, as all data and compute need a
+ * Dark user to own them (pay for them, etc). *)
+val owner : auth_domain:string -> t option
 
 val init : unit -> unit
