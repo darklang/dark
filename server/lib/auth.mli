2d8eb4e56d4f2ccf174249756788d7d08ecce85c
Paul Biggar <paul.biggar@gmail.com>
2018-05-15 17:54:41 -0700

    Remove orgs and change users into accounts

--- server/lib/auth.mli
+++ server/lib/auth.mli
@@ -8,8 +8,8 @@ module Session : sig
   val backend : Backend.t
   val cookie_key : string
   val of_request : Cohttp_lwt_unix.Request.t -> (t option, S.error) result io
-  val new_for_username : User.username -> t io
-  val username_for : t -> User.username
+  val new_for_username : Account.username -> t io
+  val username_for : t -> Account.username
 end
 
 
