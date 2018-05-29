24100df9ce1fcf10f1cc2a3d4ffba56e584bd81a
Paul Biggar <paul.biggar@gmail.com>
2018-05-29 08:15:36 -0700

    Add DB-backed version of stored_function_results (not used yet)

--- server/lib/stored_function_result.ml
+++ server/lib/stored_function_result.ml
@@ -39,17 +39,61 @@ let filename (host, tlid, fnname, id) arglist =
 (* External *)
 (* ------------------------- *)
 
-let store (host, tlid, fnname, id) arglist result =
+let store_old (host, tlid, fnname, id) arglist result =
   mkdir host tlid;
   let filename = filename (host, tlid, fnname, id) arglist in
   Util.writejsonfile ~root ~conv:Dval.dval_to_yojson ~value:result filename
 
-let load (host, tlid, fnname, id) arglist =
+let load_old (host, tlid, fnname, id) arglist =
   let fn = filename (host, tlid, fnname, id) arglist in
   if Util.file_exists ~root fn
   then
     fn
     |> Util.readjsonfile ~root ~conv:Dval.dval_of_yojson
-    |> fun x -> Some x
+    (* only do enough to satisfy the interface for now *)
+    |> fun x -> Some (x, Time.now ())
   else None
 
+let store_new (canvas_id, tlid, fnname, id) arglist result =
+  let sql =
+    Printf.sprintf
+      "INSERT INTO function_results
+      (canvas_id, tlid, fnname, id, hash, timestamp, value)
+      VALUES ('%s':uuid, %d, '%s', %d, '%s', CURRENT_TIMESTAMP, '%s')"
+      (Db.escape canvas_id)
+      tlid
+      (Db.escape fnname)
+      id
+      (Db.escape (hash arglist))
+      (Db.escape (Dval.dval_to_json_string result))
+  in
+  Db.run_sql sql
+
+let load_new (canvas_id, tlid, fnname, id) arglist
+  : (RTT.dval * Time.t) option =
+  let sql =
+    Printf.sprintf
+      "SELECT timestamp, value
+      FROM function_results
+      WHERE ( canvas_id = '%s':uuid
+            , tlid = %d
+            , fnname = '%s'
+            , id = %d
+            , hash = '%s')"
+      (Db.escape canvas_id)
+      tlid
+      (Db.escape fnname)
+      id
+      (Db.escape (hash arglist))
+  in
+  sql
+  |> Db.fetch_via_sql
+  |> List.hd
+  |> Option.map ~f:(function
+      | [dval; ts] ->
+        (Dval.dval_of_json_string dval, Time.of_string ts)
+      | _ -> Exception.internal "Bad DB format for stored_functions")
+
+
+let store = store_old
+let load = load_old
