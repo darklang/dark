e1a51979162e2faec5c0f1bebefccad92bfd394e
Paul Biggar <paul.biggar@gmail.com>
2018-05-30 09:23:40 -0700

    Split Db into Dbconnection, Dbprim and Db
    Dbprim is for primitives that can be opened everywhere, such as higher-level
    escape fns.
    Db is for db operations, like oplists and schemas and such. DBconnection is to
    own the db connection, which unforunately needs to be passed around a bit.

---
+++ server/lib/dbconnection.ml
@@ -0,0 +1,23 @@
+open Core
+module PG = Postgresql
+
+(* globals *)
+let rec rec_con depth =
+  try
+    let db = Config.postgres_settings in
+    let c = new PG.connection ~host:db.host ~dbname:db.dbname
+      ~user:db.user ~password:db.password ()  in
+    c#set_notice_processor (Log.infO "postgres");
+    c
+  with
+  | e ->
+      Log.infO "Couldn't connect to postgres, attempt " depth;
+      if depth < 10
+      then
+        (Unix.sleep 1;
+         rec_con (depth+1))
+      else
+        raise e
+
+let conn = rec_con 0
+
---
+++ server/lib/dbprim.mli
@@ -0,0 +1,10 @@
+open Core
+open Types
+open Types.RuntimeT
+
+
+val escape : string -> string
+val escapea : string -> string
+val bytea_of_string_hex : string -> string
+
+
