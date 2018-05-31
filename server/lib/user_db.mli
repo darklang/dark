62eb971409d43d2b6c5d203ec4acc29645b5405d
Ian Connolly <ian@connolly.io>
2018-05-31 14:47:08 -0700

    WIP: Make db OPs do nothing other than mutate the TLDB object

--- server/lib/user_db.mli
+++ server/lib/user_db.mli
@@ -14,11 +14,9 @@ val delete : tables:(DbT.db list) -> DbT.db -> dval_map -> unit
 val delete_all : tables:(DbT.db list) -> DbT.db -> unit
 val update : tables:(DbT.db list) -> DbT.db -> dval_map -> unit
 val count : DbT.db -> int
-val drop : DbT.db -> unit
 
 (* DB schema modifications *)
 val create : host -> string -> tlid -> DbT.db
-val init_storage : DbT.db -> unit
 val add_col : id -> id -> DbT.db -> DbT.db
 val set_col_name : id -> string -> bool -> DbT.db -> DbT.db
 val set_col_type : id -> tipe -> bool ->  DbT.db -> DbT.db
