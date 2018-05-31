e8f2d21e87a158bccb941dc564d1e67f3ccec668
Paul Biggar <paul.biggar@gmail.com>
2018-05-30 22:49:53 -0700

    Start putting stored events in the DB

--- server/lib/stored_event.mli
+++ server/lib/stored_event.mli
@@ -7,9 +7,9 @@ type four_oh_four = event_desc * Types.RuntimeT.dval list [@@deriving show]
 (* We store a set of events for each host. The events may or may not
  * belong to a toplevel. We provide a list in advance so that they can
  * be partitioned effectively *)
-val store_event : string -> event_desc -> Types.RuntimeT.dval -> unit
-val load_events : string -> event_desc -> Types.RuntimeT.dval list
-val list_events : string -> event_desc list
-val clear_events : string -> unit
+val store_event : Uuid.t -> string -> event_desc -> Types.RuntimeT.dval -> unit
+val load_events : Uuid.t -> string -> event_desc -> Types.RuntimeT.dval list
+val list_events : Uuid.t -> string -> event_desc list
+val clear_events : Uuid.t -> string -> unit
 
 val four_oh_four_to_yojson : four_oh_four -> Yojson.Safe.json
