602cab5ac7c94e72f9b8c6b7a234dbdc491403c5
Paul Biggar <paul.biggar@gmail.com>
2018-06-02 13:52:04 -0700

    Don't run the script in question twice

--- scripts/backup_darkfiles.sh
+++ scripts/backup_darkfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-./scripts/support/assert-in-container $0 $@
+. ./scripts/support/assert-in-container $0 $@
 
 DATE=`date '+%Y_%m_%d-%H_%M_%S'`
 DEST="${DARK_CONFIG_PERSIST_DIR}/backup_appdata/${DATE}"
