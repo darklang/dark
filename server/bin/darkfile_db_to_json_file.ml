15945f5a5399556def616f3f1ca735e4ce48b184
Paul Biggar <paul.biggar@gmail.com>
2018-06-03 11:43:02 -0700

    Rename functions to be clearer whether they're touching disk or db

--- server/bin/darkfile_db_to_json_file.ml
+++ server/bin/darkfile_db_to_json_file.ml
@@ -5,9 +5,9 @@ let _ =
   Exn.initialize_module ();
   let host = Sys.argv.(1) in
   let out_file = Sys.argv.(2) in
-  match Dark.Serialize.load_from_db host with
+  match Dark.Serialize.load_binary_from_db host with
   | Some ops ->
-    Dark.Serialize.save_json ~root:Dark.Config.No_check out_file ops
+    Dark.Serialize.save_json_to_disk ~root:Dark.Config.No_check out_file ops
   | None ->
     failwith ("Can't read from DB for host: " ^ host)
 
