0124817c75927bd8afa58d9c9a66e4d9add82125
Paul Biggar <paul.biggar@gmail.com>
2018-06-03 12:59:02 -0700

    Also save to the DB as well as the disk

--- server/bin/darkfile_db_to_json_file.ml
+++ server/bin/darkfile_db_to_json_file.ml
@@ -7,7 +7,9 @@ let _ =
   let out_file = Sys.argv.(2) in
   match Dark.Serialize.load_binary_from_db host with
   | Some ops ->
-    Dark.Serialize.save_json_to_disk ~root:Dark.Config.No_check out_file ops
+    Dark.Serialize.save_json_to_db host ops;
+    Dark.Serialize.save_json_to_disk ~root:Dark.Config.No_check out_file ops;
+    ()
   | None ->
     failwith ("Can't read from DB for host: " ^ host)
 
