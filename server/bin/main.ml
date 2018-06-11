cd0648acdfb8b6e4166595a5a05a8b885449cbbb
Paul Biggar <paul.biggar@gmail.com>
2018-06-10 22:02:49 -0700

    Start splitting the backend code so that the frontend can run analysis

--- server/bin/main.ml
+++ server/bin/main.ml
@@ -4,5 +4,5 @@ let () =
   print_endline "Starting server";
   (* see https://github.com/mirage/ocaml-cohttp/issues/511 *)
   let () = Lwt.async_exception_hook := ignore in
-  Dark.Init.init ();
-  Dark.Server.run ()
+  Backend.Init.init ();
+  Backend.Server.run ()
--- server/bin/trigger_queue_workers.ml
+++ server/bin/trigger_queue_workers.ml
@@ -1,8 +1,8 @@
 open Core
 
 let _ =
-  Dark.Init.init ();
-  Dark.Queue_worker.dequeue_and_evaluate_all ()
+  Backend.Init.init ();
+  Backend.Queue_worker.dequeue_and_evaluate_all ()
   |> Out_channel.print_endline
 
 
