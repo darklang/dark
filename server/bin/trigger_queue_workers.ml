96cf349d18d1a2220fbafd66176239e58cf5d4b1
Paul Biggar <paul.biggar@gmail.com>
2018-06-11 14:35:01 -0700

    A bunch of no-op changes to splitting up the code for frontend analysis
    Changed all the libs to be called libX (eg. libbackend)
    
    Removed (native) annotation in jbuild
    
    Added js_of_ocaml flags
    
    Removed as-yet unused libraries
    
    Started trying to print

--- server/bin/trigger_queue_workers.ml
+++ server/bin/trigger_queue_workers.ml
@@ -1,8 +1,8 @@
 open Core
 
 let _ =
-  Backend.Init.init ();
-  Backend.Queue_worker.dequeue_and_evaluate_all ()
+  Libbackend.Init.init ();
+  Libbackend.Queue_worker.dequeue_and_evaluate_all ()
   |> Out_channel.print_endline
 
 
