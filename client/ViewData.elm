3a25c469b855a4b295d69290f5d74a929fbabfd6
Paul Biggar <paul.biggar@gmail.com>
2018-06-17 13:35:16 -0700

    Show requests with bad responses in a different color

--- client/ViewData.elm
+++ client/ViewData.elm
@@ -12,12 +12,15 @@ import Types exposing (..)
 import Prelude exposing (..)
 import ViewUtils exposing (..)
 import Analysis
+import Runtime as RT
 
-viewRequest : TLID -> Int -> String -> Bool -> Bool -> Html.Html Msg
-viewRequest tlid idx value isActive isHover =
+viewRequest : TLID -> Int -> String -> Bool -> Bool -> Tipe -> Html.Html Msg
+viewRequest tlid idx value isActive isHover tipe =
   let activeClass = if isActive then [Attrs.class "active"] else []
       hoverClass = if isHover then [Attrs.class "mouseovered"] else []
-      classes = activeClass ++ hoverClass
+      tipeClassName = "tipe-" ++ RT.tipe2str tipe
+      tipeClass = [Attrs.class tipeClassName]
+      classes = activeClass ++ hoverClass ++ tipeClass
       events = [ eventNoPropagation "click" (DataClick tlid idx)
                , eventNoPropagation "mouseenter" (DataMouseEnter tlid idx)
                , eventNoPropagation "mouseleave" (DataMouseLeave tlid idx)
@@ -29,22 +32,19 @@ viewRequest tlid idx value isActive isHover =
 viewRequests : ViewState -> Handler -> List (Html.Html Msg)
 viewRequests vs h =
   let resultToHtml idx result =
-    let value = if vs.handlerSpace == HSHTTP
-                then
-                  case (Dict.get "request" result.inputValues) of
-                    Just v -> v.value
-                    _ -> ""
-                else
-                  case (Dict.get "event" result.inputValues) of
-                    Just v -> v.value
-                    _ -> ""
+    let key = if vs.handlerSpace == HSHTTP
+              then "request"
+              else "event"
+        value = Dict.get key result.inputValues
+                |> Maybe.map .value
+                |> Maybe.withDefault ""
         -- Note: the following tlCursors are very different things.
         isActive = (Analysis.cursor_ vs.tlCursors vs.tl.id) == idx
         -- Note: this is not the same tlCursor as above
         hoverID = tlCursorID vs.tl.id idx
         isHover = vs.hovering == Just hoverID
     in
-    viewRequest vs.tl.id idx value isActive isHover
+    viewRequest vs.tl.id idx value isActive isHover result.astValue.tipe
   in
   List.indexedMap resultToHtml <| List.reverse vs.results
 
