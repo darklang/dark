442a764babd02d0c5cb29eb3ad54b0cd69e24205
Paul Biggar <paul.biggar@gmail.com>
2018-06-17 23:23:35 -0700

    Reenable all the functions

--- server/libexecution/init.ml
+++ server/libexecution/init.ml
@@ -3,7 +3,7 @@ open Core_kernel
 let has_inited : bool ref =
   ref false
 
-let init log_level  =
+let init log_level libs =
   if !has_inited
   then ()
   else
@@ -11,7 +11,8 @@ let init log_level  =
     Exn.initialize_module ();
 
     Log.set_level log_level;
+    Libs.init libs;
 
     Log.infO "Libexecution" "Initialization Complete";
-    has_inited := true;
+    has_inited := true
 
