6aee65b1c230fd2123d3e711f58d20c88b09d84d
Paul Biggar <paul.biggar@gmail.com>
2018-06-20 10:49:51 -0700

    Drop unused field from stored_function_result

--- server/libbackend/stored_function_result.mli
+++ server/libbackend/stored_function_result.mli
@@ -3,12 +3,12 @@ open Libexecution
 
 
 val store :
-  Uuidm.t * string * Types.tlid * string * Types.id ->
+  Uuidm.t * Types.tlid * string * Types.id ->
   Types.RuntimeT.dval list ->
   Types.RuntimeT.dval ->
   unit
 
 val load :
-  Uuidm.t * string * Types.tlid * string * Types.id ->
+  Uuidm.t * Types.tlid * string * Types.id ->
   Types.RuntimeT.dval list ->
   (Types.RuntimeT.dval * Time.t) option
