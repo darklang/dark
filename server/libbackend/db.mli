3c2d357341549af669063c2151470a848db654f9
Paul Biggar <paul.biggar@gmail.com>
2018-06-23 14:37:30 -0700

    Rename Db.sql to Db.param

--- server/libbackend/db.mli
+++ server/libbackend/db.mli
@@ -4,24 +4,24 @@ open Libexecution
 (* Low-level API *)
 val exists_via_sql : ?quiet:bool -> string -> bool
 
-type sql = Int of int
-         | String of string
-         | Uuid of Uuidm.t
-         | Binary of string
-         | Secret of string
-         | DvalJson of Types.RuntimeT.dval
-         | DvalmapJsonb of Types.RuntimeT.dval_map
-         | Null
+type param = Int of int
+           | String of string
+           | Uuid of Uuidm.t
+           | Binary of string
+           | Secret of string
+           | DvalJson of Types.RuntimeT.dval
+           | DvalmapJsonb of Types.RuntimeT.dval_map
+           | Null
 
 (* NOTE: run is not allowed to receive multiple commands. If you
  * want multiple statements, put a BEGIN/END around them. *)
-val run : params: sql list -> name:string -> string ->
+val run : params: param list -> name:string -> string ->
   unit
-val fetch : params: sql list -> name:string -> string ->
+val fetch : params: param list -> name:string -> string ->
   string list list
-val fetch_one : params: sql list -> name:string -> string ->
+val fetch_one : params: param list -> name:string -> string ->
   string list
-val fetch_one_option : params: sql list -> name:string -> string ->
+val fetch_one_option : params: param list -> name:string -> string ->
   string list option
 (* val exists_via_sql2 : ?quiet:bool -> string -> bool *)
 
