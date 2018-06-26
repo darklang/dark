06de665ca25896973aa70adddca31f85e4e63f65
Ian Connolly <ian@connolly.io>
2018-06-26 11:17:41 -0700

    Move canvas_id function creation to system migration

--- server/libbackend/canvas.ml
+++ server/libbackend/canvas.ml
@@ -105,29 +105,7 @@ let apply_op (op : Op.op) (c : canvas ref) : unit =
 let fetch_canvas_id (owner:Uuidm.t) (host:string) : Uuidm.t =
   let sql =
     Printf.sprintf
-      "CREATE OR REPLACE FUNCTION canvas_id(_new_id uuid, _account_id uuid, _name VARCHAR(40), OUT _id uuid) AS
-       $func$
-       BEGIN
-       LOOP
-         SELECT id
-         FROM   canvases
-         WHERE  name = _name
-         INTO   _id;
-
-         EXIT WHEN FOUND;
-
-         INSERT INTO canvases AS c
-         (id, account_id, name)
-         VALUES (_new_id, _account_id, _name)
-         ON     CONFLICT (name) DO NOTHING
-         RETURNING c.id
-         INTO   _id;
-
-         EXIT WHEN FOUND;
-       END LOOP;
-       END
-       $func$ LANGUAGE plpgsql;
-       SELECT canvas_id(%s, %s, %s);"
+      "SELECT canvas_id(%s, %s, %s)"
       (Db.escape (Uuid (Util.create_uuid ())))
       (Db.escape (Uuid owner))
       (Db.escape (String host))
