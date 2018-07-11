dd85c7460601a13a8d7d0fa3a3818401ba557e5e
Ian Connolly <ian@connolly.io>
2018-07-11 11:17:56 -0700

    Move Config DSL to new library: libservice

---
+++ server/libservice/config_dsl.mli
@@ -0,0 +1,9 @@
+
+
+val absolute_dir : string -> string
+val int : string -> int
+val bool : string -> bool
+val string : string -> string
+val string_option : string -> string list -> string
+val password : string -> string
+val lowercase : string -> string -> string
