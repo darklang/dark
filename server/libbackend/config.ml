e64dfd787163a044e748397391b7c81604168718
Ian Connolly <ian@connolly.io>
2018-07-11 11:17:56 -0700

    Move Config DSL to new library: libservice

--- server/libbackend/config.ml
+++ server/libbackend/config.ml
@@ -1,63 +1,9 @@
 open Core
+open Libservice.Config_dsl
 (* ------------------------- *)
 (* Note: if you add an env-var in development, you'll probably need to
  * restart the dev container. *)
 (* ------------------------- *)
-
-
-(* ------------------------- *)
-(* Envvar parsers *)
-(* ------------------------- *)
-let absolute_dir name : string =
-  let dir = Sys.getenv_exn name in
-  if not (Filename.is_absolute dir)
-  then failwith ("FAIL: " ^ name ^ " is not absolute")
-  else dir ^ "/"
-
-let int name : int =
-  Sys.getenv_exn name
-  |> int_of_string
-
-let bool name : bool =
-  let v = Sys.getenv_exn name in
-  match v with
-  | "y" -> true
-  | "n" -> false
-  | _ ->
-    failwith ("Invalid env var value for "
-              ^ name
-              ^ "="
-              ^ v
-              ^ ". Allowed values are 'n' and 'y'.")
-
-let lowercase name v =
-  if v = String.lowercase v
-  then v
-  else
-    failwith ("Env vars must be lowercased but "
-              ^ name
-              ^ "="
-              ^ v
-              ^ " is not")
-
-let string name : string =
-  Sys.getenv_exn name
-  |> lowercase name
-
-let string_option name (options: string list) : string =
-  let v = Sys.getenv_exn name |> lowercase name in
-  if List.mem ~equal:(=) options v
-  then v
-  else
-    failwith ("Envvar is not a valid option: '"
-              ^ name
-              ^ "' not in ["
-              ^ (String.concat ~sep:", " options)
-              ^ " is not")
-
-let password name : string =
-  Sys.getenv_exn name
-
 
 (* ------------------------- *)
 (* Root directories *)
