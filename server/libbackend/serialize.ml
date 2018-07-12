cc157b48ca5d55a9909df99d50addab08585c2d3
Paul Biggar <paul.biggar@gmail.com>
2018-07-12 11:46:50 -0700

    Use normal escaping for fetch_canvas_id
    We weren't able to convert this before because there was a lot more sql, but
    that has since been moved to a migration

--- server/libbackend/serialize.ml
+++ server/libbackend/serialize.ml
@@ -316,17 +316,10 @@ let search_and_load (host: string) (canvas_id: Uuidm.t) : Op.oplist =
 
 (* https://stackoverflow.com/questions/15939902/is-select-or-insert-in-a-function-prone-to-race-conditions/15950324#15950324 *)
 let fetch_canvas_id (owner:Uuidm.t) (host:string) : Uuidm.t =
-  let sql =
-    Printf.sprintf
-      "SELECT canvas_id(%s, %s, %s)"
-      (Db.escape (Uuid (Util.create_uuid ())))
-      (Db.escape (Uuid owner))
-      (Db.escape (String host))
-  in
   Db.fetch_one
     ~name:"fetch_canvas_id"
-    sql
-    ~params:[]
+    "SELECT canvas_id($1, $2, $3)"
+    ~params:[Uuid (Util.create_uuid ()); Uuid owner; String host]
   |> List.hd_exn
   |> Uuidm.of_string
   |> Option.value_exn
