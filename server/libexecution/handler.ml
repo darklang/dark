a19c44aedf02259fdc77cd64e3fd5fe7186df142
Ian Connolly <ian@connolly.io>
2018-07-13 11:16:00 -0700

    Get queue worker executing a single event per dequeue

--- server/libexecution/handler.ml
+++ server/libexecution/handler.ml
@@ -72,4 +72,9 @@ let event_desc_for (h: handler) : (string * string * string) option =
   | (Some m, Some en, Some mo) -> Some (m, en, mo)
   | _ -> None
 
+let matches_event_desc (d: (string * string * string)) (h: handler) : bool =
+  let (space, name, modifier) = d in
+  module_for h = (Some space)
+  && event_name_for h = (Some name)
+  && modifier_for h = (Some modifier)
 
