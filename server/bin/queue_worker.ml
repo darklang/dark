ad56a9c5922cb84ca30b44b093fb222d6cb3ce25
Ian Connolly <ian@connolly.io>
2018-07-25 15:32:19 -0700

    Port graceful termination to cron + queueworkers

--- server/bin/queue_worker.ml
+++ server/bin/queue_worker.ml
@@ -1,13 +1,18 @@
 open Core_kernel
 open Lwt
 
+let shutdown = ref false
+
 let () =
   Libbackend.Init.init ~run_side_effects:false;
   let execution_id = Libexecution.Util.create_id () in
   (* spin up health http server *)
   Lwt.async
     (fun () ->
-       catch (fun () -> Libservice.Health_check.run ())
+       (* We're sharing a ref across threads here, but Health_check writes
+        * `true` to that ref regardless of its value and we never write to
+        * it, so there's no sharing issue. *)
+       catch (fun () -> Libservice.Health_check.run ~shutdown ~execution_id)
          (fun e ->
             let bt = Libexecution.Exception.get_backtrace () in
             Libbackend.Rollbar.report_lwt e bt (EventQueue) (string_of_int execution_id) >>=
@@ -17,13 +22,21 @@ let () =
     let result = Libbackend.Queue_worker.run execution_id in
     match result with
     | Ok _ ->
-      (queue_worker [@tailcall]) ()
+      if not !shutdown
+      then
+        (queue_worker [@tailcall]) ()
+      else
+        Lwt.return ()
     | Error (bt, e) ->
       Libcommon.Log.erroR "queue_worker"
         ~data:"Uncaught error"
         ~params:["execution_id", string_of_int execution_id
                 ;"exn", Libexecution.Exception.exn_to_string e];
       Libbackend.Rollbar.report_lwt e bt (EventQueue) (string_of_int execution_id) >>= fun _ ->
-      (queue_worker [@tailcall]) ()
+      if not !shutdown
+      then
+        (queue_worker [@tailcall]) ()
+      else
+        Lwt.return ()
   in
   Lwt_main.run (queue_worker ())
--- server/libservice/health_check.mli
+++ server/libservice/health_check.mli
@@ -1,4 +1,9 @@
 open Core_kernel
 
-(* Spin up a HTTP server which responds to a single health check route *)
-val run : unit -> unit Lwt.t
+(* Spin up a HTTP server which responds to a single health check route
+ *
+ * Pass a boolean ref to ~shutdown and the server will write `true`
+ * to it at least once after receiving a request to /pkill
+ *
+ * *)
+val run : shutdown:(bool ref) -> execution_id:int ->unit Lwt.t
