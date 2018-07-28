30f5203c0c91853fee78eb47051b2c45da5767da
Elizabeth Dixon <_@lizzie.io>
2018-07-27 18:08:01 -0700

    New tool to generate account.ml entries.
    Has --prompt-for-password if you want to specify a password; otherwise
    it generates a random one.

---
+++ server/bin/add_user.ml
@@ -0,0 +1,52 @@
+open Core_kernel
+module Hash = Sodium.Password_hash.Bytes
+module Util = Libexecution.Util
+
+let usage () =
+  Format.printf "Usage: %s [--prompt-for-password]\n" Sys.argv.(0);
+  exit 1 ;;
+
+let prompt str =
+  print_string str;
+  Out_channel.flush Out_channel.stdout;
+  match In_channel.input_line In_channel.stdin with
+    None -> ""
+  | Some s -> s
+
+let () =
+  Random.self_init ();
+
+  (* parse args *)
+  let prompt_for_password = match (Array.length Sys.argv,
+                                   Array.to_list Sys.argv) with
+      (1, _) -> false
+    | (2, _ :: "--prompt-for-password" :: []) -> true
+    | _ -> usage () in
+
+  (* prompt command-line user for some fields... *)
+  let username = prompt "Username: " in
+  let password = if prompt_for_password
+                 then prompt "Password: "
+                 else Util.random_string 16
+  in
+  let name = prompt "Name: " in
+  let email = prompt "Email: " in
+  let hashed = password
+               |> Bytes.of_string
+               |> Hash.wipe_to_password
+               |> Hash.hash_password Sodium.Password_hash.interactive
+               |> Bytes.to_string
+               |> B64.encode in
+
+  (* print out the new entry for account.ml *)
+  Format.printf "
+  (* This user's password is as follows: %s
+     Insert everything after this into server/libbackend/account.ml *)\n
+  upsert_account
+    { username = \"%s\"
+    ; password = \"%s\"
+    ; email = \"%s\"
+    ; name = \"%s\"};\n\n"
+    password username hashed email name
+
+
