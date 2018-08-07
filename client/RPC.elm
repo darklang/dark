3341413f37327b535616d30058916e2eee10d326
Alice Q. Wong <alice@valour.me>
2018-08-07 10:44:31 -0700

    setup view code

--- client/RPC.elm
+++ client/RPC.elm
@@ -579,7 +579,7 @@ decodeHandlerSpec =
 
 decodeHandler : JSD.Decoder Handler
 decodeHandler =
-  let toHandler ast spec = {ast = ast, spec = spec } in
+  let toHandler ast spec = {ast = ast, spec = spec, isLocked = False } in
   JSDP.decode toHandler
   |> JSDP.required "ast" decodeExpr
   |> JSDP.required "spec" decodeHandlerSpec
