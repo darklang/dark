34de7bdc5e1d7e467088335847d457a15864f1f4
Paul Biggar <paul.biggar@gmail.com>
2018-08-12 19:02:00 -0700

    Start trying to call analysis

--- server/bin/darkjs.ml
+++ server/bin/darkjs.ml
@@ -1,3 +1,8 @@
 let () =
   Libfrontend.Init.init ();
+  Js.export "darkAnalysis"
+    (object%js
+       method performAnalysis tlids =
+         Libfrontend.Init.perform_analysis tlids
+     end);
   ()
--- server/libfrontend/init.ml
+++ server/libfrontend/init.ml
@@ -1,4 +1,31 @@
+open Libexecution
+
 module Analysis = Libexecution.Ast_analysis
+
 
 let init () =
   print_endline "libfrontend reporting in"
+
+let state env : Libexecution.Types.RuntimeT.exec_state =
+  { tlid = 0
+  ; host = "test"
+  ; account_id = Libexecution.Util.create_uuid ()
+  ; canvas_id = Libexecution.Util.create_uuid ()
+  ; user_fns = []
+  ; exe_fn_ids = []
+  ; env = env
+  ; fail_fn = None
+  ; input_cursor = 0
+  ; dbs = []
+  ; execution_id = 0
+  ; load_fn_result = Ast_analysis.load_no_results
+  ; store_fn_result = Ast_analysis.store_no_results
+  ; load_fn_arguments = Ast_analysis.load_no_arguments
+  ; store_fn_arguments = Ast_analysis.store_no_arguments
+  }
+
+let perform_analysis (str : string) : string =
+  let env = Libexecution.Types.RuntimeT.DvalMap.empty in
+  let _ = Libexecution.Ast_analysis.execute_ast (state env) env (Blank 0) in
+  "called performAnalysis on: " ^ str
+
