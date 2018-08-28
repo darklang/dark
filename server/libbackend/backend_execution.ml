6df8282b7d40d2dc4fb1fcdfddabeeb0144450cc
Paul Biggar <paul.biggar@gmail.com>
2018-08-27 18:20:32 -0700

    Break apart fetching the stored input_vars from combining with other input vars

--- server/libbackend/backend_execution.ml
+++ server/libbackend/backend_execution.ml
@@ -16,35 +16,35 @@ let global_vars (c: canvas) : RTT.input_vars =
   |> TL.dbs
   |> Execution.dbs_as_input_vars
 
+let saved_input_vars (c: canvas) (h: RTT.HandlerT.handler) =
+  match Handler.event_desc_for h with
+  | None -> []
+  | Some (space, path, modifier as d) ->
+    List.map (SE.load_events c.id d)
+      ~f:(fun e ->
+          match Handler.module_type h with
+          | `Http ->
+            let with_r = [("request", e)] in
+            let bound = Libexecution.Execution.http_route_input_vars h path in
+            with_r @ bound
+          | `Event ->
+            [("event", e)]
+          | `Cron  -> []
+          | `Unknown -> [] (* can't happen *)
+      )
+
 let initial_input_vars_for_handler (c: canvas) (h: RTT.HandlerT.handler)
   : RTT.input_vars list =
-  let init = global_vars c in
+  let saved = saved_input_vars c h in
   let samples = Execution.sample_input_vars h in
-  (match Handler.event_desc_for h with
-  | None -> [init @ samples]
-  | Some (space, path, modifier as d) ->
-    let events = SE.load_events c.id d in
-    if events = []
-    then [init @ samples]
-    else
-      List.map events
-        ~f:(fun e ->
-            match Handler.module_type h with
-            | `Http ->
-              let with_r = [("request", e)] in
-              let bound = Libexecution.Execution.http_route_input_vars h path in
-              init @ with_r @ bound
-            | `Event ->
-              init @ [("event", e)]
-            | `Cron  -> init
-            | `Unknown -> init (* can't happen *)
-        ))
+  if saved = []
+  then [samples]
+  else saved
 
 let initial_input_vars_for_user_fn (c: canvas) (fn: RTT.user_fn)
   : RTT.input_vars list =
-  let init = global_vars c in
   Stored_function_arguments.load (c.id, fn.tlid)
-  |> List.map ~f:(fun (m, _ts) -> init @ (RTT.DvalMap.to_alist m))
+  |> List.map ~f:(fun (m, _ts) -> RTT.DvalMap.to_alist m)
 
 
 let state_for
