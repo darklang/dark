4409258d05f86e7f5c2545e7dad17b18b12d2aa4
Paul Biggar <paul.biggar@gmail.com>
2018-09-12 00:25:59 -0700

    Do client that matches the server

---
+++ client/tests/TestRPC.elm
@@ -0,0 +1,53 @@
+module TestRPC exposing (all)
+
+-- tests
+import Test exposing (describe)
+import Expect exposing (Expectation)
+
+-- builtins
+import Nineteen.Debug as Debug
+import Json.Decode as JSD
+import Json.Encode as JSE
+import Dict
+
+-- libs
+
+-- dark
+import Types exposing (..)
+import AST
+import Blank as B
+import UtilsForTests exposing (..)
+import RPC exposing (..)
+
+id1 = ID 5
+id2 = ID 10
+
+testRoundtrip : JSD.Decoder a -> (a -> JSE.Value) -> String -> a -> Test.Test
+testRoundtrip decoder encoder name v =
+  test ("roundtrip " ++ name) <|
+    (Expect.equal (Ok v)
+       (v
+        |> encoder
+        |> JSD.decodeValue decoder))
+
+rtDval = testRoundtrip decodeDval encodeDval
+
+
+
+all : Test.Test
+all =
+  Test.describe "ast"
+    [ rtDval "int" (DInt 5)
+    , rtDval "obj" (DObj (Dict.fromList [("foo", DInt 5)]))
+    , rtDval "date" (DDate "can be anything atm")
+    , rtDval "incomplete" DIncomplete
+    , rtDval "float" (DFloat 7.2)
+    , rtDval "true" (DBool True)
+    , rtDval "false" (DBool False)
+    , rtDval "null" DNull
+    , rtDval "id" (DID "1232345346456")
+    , rtDval "title" (DTitle "some title")
+    , rtDval "errorrail" (DErrorRail (DInt 5))
+    , rtDval "db" (DDB "Visitors")
+    , rtDval "list" (DList [DDB "Visitors", DInt 4])
+    ]
