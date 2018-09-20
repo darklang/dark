faa0b71d8966814e74a6c47d24e6f1667e9f8ee1
Elizabeth Dixon <_@lizzie.io>
2018-09-20 10:13:19 -0700

    Revert "Revert "Merge pull request #172 from darklang/lizzie/split-domains""
    This reverts commit d58e185c8f9c0bb30d8f3a637c8a263cb51bbc0e.

--- client/Url.elm
+++ client/Url.elm
@@ -123,3 +123,10 @@ changeLocation m loc =
         _ -> SetPage (Fn id pos)
     Just page -> SetPage page
     _ -> NoChange
+
+parseCanvasName : Navigation.Location -> String
+parseCanvasName loc = case loc.pathname
+                           |> String.dropLeft 1 -- remove leading "/"
+                           |> String.split "/" of
+                        "a" :: canvasName :: _ -> canvasName
+                        _ -> "builtwithdark"
