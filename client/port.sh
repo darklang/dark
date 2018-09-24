3b555e43568c2ae74aab49312e29d9239bab91ae
Paul Biggar <paul.biggar@gmail.com>
2018-09-23 21:32:46 -0700

    Use .Elm.ml names, don't upgrade to 0.19

--- client/port.sh
+++ client/port.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 
 cd client
 
-for i in *.elm; do
-  name=$(basename $i .elm).ml
-  /Users/paulbiggar/.local/bin/elm-format $i --output ../client2/$name
+# FILES=$(ls *.elm)
+FILES=Main.elm
+
+for i in "$FILES"; do
+  /Users/paulbiggar/.local/bin/elm-format --elm-version 0.18 $i --output ../client2/src/$i.ml
 done
