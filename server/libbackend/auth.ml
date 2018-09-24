b04140b3167e6ac4c5237f4a753ef292cad642df
Elizabeth Dixon <_@lizzie.io>
2018-09-24 15:16:39 -0700

    Store CSRF tokens alongside usernames in Sessions.

--- server/libbackend/auth.ml
+++ server/libbackend/auth.ml
@@ -9,10 +9,45 @@ module Session = struct
   let cookie_key = "__session"
   let of_request req =
     of_header backend cookie_key (req |> Cohttp_lwt_unix.Request.headers)
+
+  (* We store two values alongside each other in the session.value:
+     one, the username; and two, the current CSRF token. These are
+     stored as a JSON map with values "username" and "csrf_token".
+
+     Because CSRF tokens exist that predate this change (previously,
+     just the username was stored, we default username_for to
+
+     We can remove this hacks one week from when this is merged,
+     since the session default cookie max-age is one week.
+   *)
   let new_for_username username =
-    generate backend username
+    generate backend
+      (Yojson.to_string
+         (`Assoc
+            [ "username", `String username
+            ; "csrf_token", `String "abc"
+            ]))
+
   let username_for session =
-    session.value
+    try
+      session.value
+      |> Yojson.Basic.from_string
+      |> Yojson.Basic.Util.member "username"
+      |> Yojson.Basic.Util.to_string
+    with e ->
+      (* If there's not valid json in the database,
+         we should assume it's just a username. *)
+      session.value
+
   let csrf_token_for session =
-    "abc"
+    try
+      session.value
+      |> Yojson.Basic.from_string
+      |> Yojson.Basic.Util.member "csrf_token"
+      |> Yojson.Basic.Util.to_string
+    with e ->
+      (* If there's not valid json in the database,
+         this user has not been issued a CSRF token.
+         So, we'll just issue a dummy token. *)
+      "DUMMY_CSRF"
 end
