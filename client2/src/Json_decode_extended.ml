f7206f7f09839e1131a072e9c2aaab869814b71e
Ian Connolly <ian@connolly.io>
2018-10-10 15:29:58 -0700

    Finish variant decoding

--- client2/src/Json_decode_extended.ml
+++ client2/src/Json_decode_extended.ml
@@ -10,8 +10,38 @@ let index i decode json =
   else
     raise @@ DecodeError ("Expected array, got " ^ _stringify json)
 
+let map2 f d1 d2 json =
+  List.map ~f [d1 json; d2 json]
+
+let map3 f d1 d2 d3 json =
+  List.map ~f [d1 json; d2 json; d3 json]
+
+let map4 f d1 d2 d3 d4 json =
+  List.map ~f [d1 json; d2 json; d3 json; d4 json]
+
 let decodeVariant0 constructor = succeed constructor
 
 let decodeVariant1 constructor d1 =
-  map constructor (index 1 d1)
+  map constructor
+    (index 1 d1)
+
+let decodeVariant2 constructor d1 d2 =
+  map2 constructor
+    (index 1 d1)
+    (index 2 d2)
+
+let decodeVariant3 constructor d1 d2 d3 =
+  map3 constructor
+    (index 1 d1)
+    (index 2 d2)
+    (index 3 d3)
+
+let decodeVariant4 constructor d1 d2 d3 d4 =
+  map4 constructor
+    (index 1 d1)
+    (index 2 d2)
+    (index 3 d3)
+    (index 4 d4)
+
+
 
