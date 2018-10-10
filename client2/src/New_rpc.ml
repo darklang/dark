a9938d767fa198a853f3e28dcb59c172eed7b2bf
Ian Connolly <ian@connolly.io>
2018-10-10 15:41:15 -0700

    Complete Tipe decoder

--- client2/src/New_rpc.ml
+++ client2/src/New_rpc.ml
@@ -7,7 +7,31 @@ module Decoders = struct
     let dv1 = decodeVariant1 in
     let dv2 = decodeVariant2 in
     decodeVariants [
-      ("TInt", dv0 Types.tint)
+      ("TInt", dv0 Types_copy.tint)
+      ("TStr", dv0 Types_copy.tstr)
+      ("TChar", dv0 Types_copy.tchar)
+      ("TBool", dv0 Types_copy.tbool)
+      ("TFloat", dv0 Types_copy.tfloat)
+      ("TObj", dv0 Types_copy.tobj)
+      ("TList", dv0 Types_copy.tlist)
+      ("TAny", dv0 Types_copy.tany)
+      ("TNull", dv0 Types_copy.tnull)
+      ("TBlock", dv0 Types_copy.tblock)
+      ("TIncomplete", dv0 Types_copy.tincomplete)
+      ("TError", dv0 Types_copy.terror)
+      ("TResp", dv0 Types_copy.tresp)
+      ("TDB", dv0 Types_copy.tdb)
+      ("TID", dv0 Types_copy.tid)
+      ("TDate", dv0 Types_copy.tdate)
+      ("TTitle", dv0 Types_copy.ttitle)
+      ("TUrl", dv0 Types_copy.turl)
+      ("TPassword", dv0 Types_copy.tpassword)
+      ("TUuid", dv0 Types_copy.tuuid)
+      ("TOption", dv0 Types_copy.toption)
+      ("TErrorRail", dv0 Types_copy.terrorrail)
+      ("TBelongsTo", dv1 Types_copy.tbelongsto string)
+      ("THasMany", dv1 Types_copy.thasmany string)
+      ("TDbList", dv1 Types_copy.tdblist tipe)
     ]
 end
 
