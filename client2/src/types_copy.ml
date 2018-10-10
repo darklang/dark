a8bcdae066cad25d63d1ee57111dd689dc80bcae
Ian Connolly <ian@connolly.io>
2018-10-10 15:41:15 -0700

    Complete Tipe decoder

---
+++ client2/src/types_copy.ml
@@ -0,0 +1,27 @@
+type tipe = Types.tipe =
+  | TInt
+  | TStr
+  | TChar
+  | TBool
+  | TFloat
+  | TObj
+  | TList
+  | TAny
+  | TNull
+  | TBlock
+  | TIncomplete
+  | TError
+  | TResp
+  | TDB
+  | TID
+  | TDate
+  | TTitle
+  | TUrl
+  | TPassword
+  | TUuid
+  | TOption
+  | TErrorRail
+  | TBelongsTo of string
+  | THasMany of string
+  | TDbList of tipe
+  [@@bs.deriving accessors]
