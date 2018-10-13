7fc217a7c006c0f7d7fbf166efad21448411fb7c
Alice Q. Wong <alice@valour.me>
2018-10-12 17:34:43 -0700

    Adding functionality that existed in elm port

---
+++ client2/src/Main_tea.ml
@@ -0,0 +1,88 @@
+open Tea
+open Tea.Mouse
+open App
+
+open Keyboard
+open Toplevel
+
+open Porting
+
+open Analysis
+open Errors
+
+(* DEFINE TYPES *)
+type model = 
+  { selected_toplevel : Toplevel.model (* TODO : make. optional *)
+  ; last_key_event: Keyboard.key_event
+  }
+
+type msg =
+  Toplevel_msg of Toplevel.msg
+  | Key_event of Keyboard.key_event
+  | Mouse_click of Mouse.position
+  | Mouse_move of Mouse.position
+  | Mouse_down of Mouse.position
+  | Mouse_up of Mouse.position
+  | Analysis_result of Analysis.analysis_result
+  | Display_error of string
+[@@bs.deriving {accessors}]
+
+(* let msg2Mod model = function
+  | GlobalClick mevt -> NoChange
+  | NothingClick mevt -> NoChange
+
+let updateMod mod m cmd = m, cmd 
+*)
+
+let update m msg =
+  match msg with
+  Toplevel_msg toplevel_msg -> m, Cmd.none
+  | Key_event key_event ->
+
+    if Keyboard.isLoc key_event
+    then Js.log (Porting.Location.hashString);
+
+    if Keyboard.isError key_event
+    then Rollbar.send "send error to rollbar";
+
+    if key_event.key_code = 65 && key_event.alt
+    then Analysis.RequestAnalysis.send [123;342;4534];
+
+    { m with last_key_event = key_event }, Cmd.none
+  | Mouse_click position ->
+    Js.log "click"; Js.log position;
+    m, Cmd.none
+  | Analysis_result analysis_result ->
+    Js.log "Analysis_result"; Js.log analysis_result; m, Cmd.none
+  | Display_error msg -> Js.log msg; m, Cmd.none
+  | _ -> m, Cmd.none
+
+let view m =
+  let open Html in
+  div
+    [ class' "canvas" ]
+    [ Toplevel.view m.selected_toplevel |> map toplevel_msg
+    ]
+
+let init () =
+  { selected_toplevel = Toplevel.new_http_handler "some stuff happening"
+  ; last_key_event = Keyboard.nullEvent ()
+  }, Cmd.none
+
+let subscriptions model = Sub.batch
+  [ Keyboard.downs key_event
+  ; Mouse.clicks mouse_click
+  ; Mouse.downs mouse_down
+  ; Mouse.ups mouse_up
+  ; Mouse.moves mouse_move
+  ; Analysis.ReceiveAnalysis.listen analysis_result
+  ; Errors.DisplayClientError.listen display_error 
+  ]
+
+let main =
+  App.standardProgram
+    { init
+    ; update
+    ; view
+    ; subscriptions = subscriptions
+    }
