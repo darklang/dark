7290eba512138843da730bf63e3396d8ba228531
Paul Biggar <paul.biggar@gmail.com>
2018-10-19 15:58:07 -0700

    Caches and Views

--- client2/src/View.ml
+++ client2/src/View.ml
@@ -4,74 +4,24 @@ module Attrs = Html.Attributes
 open Prelude
 module TL = Toplevel
 open Types
-open ViewUtils
 
-let view (m : model) : msg Html.html =
-  let attributes =
-    [ Attrs.id "grid"
-    ; Html.onWithOptions "mouseup"
-        {stopPropagation= false; preventDefault= true}
-        (decodeClickEvent GlobalClick) ]
-  in
-  let footer = [ViewScaffold.viewError m.error; ViewScaffold.viewButtons m] in
-  let routing = ViewRoutingTable.viewRoutingTable m in
-  let body = viewCanvas m in
-  let content = [routing; body] ^ footer in
-  Html.div attributes content
+type viewState = ViewUtils.viewState
+type htmlConfig = ViewBlankOr.htmlConfig
+let idConfigs = ViewBlankOr.idConfigs
+let eventNoPropagation = ViewUtils.eventNoPropagation
+let fontAwesome = ViewUtils.fontAwesome
+let viewText = ViewBlankOr.viewText
+let wc = ViewBlankOr.wc
+let text = ViewBlankOr.text
+let div = ViewBlankOr.div
+let nested = ViewBlankOr.nested
+let atom = ViewBlankOr.atom
+let keyword = ViewBlankOr.keyword
 
-let viewCanvas (m : model) : msg Html.html =
-  let entry = ViewEntry.viewEntry m in
-  let asts =
-    match m.currentPage with
-    | Toplevels _ -> List.map (viewTL m) m.toplevels
-    | Fn (tlid, _) -> (
-      match List.find (fun f -> f.ufTLID = tlid) m.userFunctions with
-      | Some func -> [viewTL m (TL.ufToTL m func)]
-      | None -> List.map (viewTL m) m.toplevels )
-  in
-  let _ = "comment" in
-  let canvasTransform =
-    let offset =
-      match m.currentPage with
-      | Toplevels _ -> m.canvas.offset
-      | Fn (_, _) -> m.canvas.fnOffset
-    in
-    let x = string_of_int (-offset.x) in
-    let y = string_of_int (-offset.y) in
-    "translate(" ^ x ^ "px, " ^ y ^ "px)"
-  in
-  let allDivs = asts ^ entry in
-  Html.div
-    [Attrs.id "canvas"; Attrs.style [("transform", canvasTransform)]]
-    allDivs
-
-let viewTL (m : model) (tl : toplevel) : msg Html.html =
-  let id = deTLID tl.id in
-  let recalc () = viewTL_ m tl.id in
-  let _ = "comment" in
-  let isDB = match tl.data with TLDB _ -> true | _ -> false in
-  let pos =
-    match m.currentPage with
-    | Toplevels _ -> tl.pos
-    | Fn (tLID, _) -> Defaults.centerPos
-  in
-  let html =
-    if Some tl.id = tlidOf m.cursorState || isDB then
-      let _ = Util.cacheClear id in
-      recalc ()
-    else
-      match Util.cacheGet id with
-      | Some html -> html
-      | None ->
-          let result = recalc () in
-          let _ = Util.cacheSet id result in
-          result
-  in
-  placeHtml m pos html
 
 let viewTL_ (m : model) (tlid : tlid) : msg Html.html =
   let tl = TL.getTL m tlid in
-  let vs = createVS m tl in
+  let vs = ViewUtils.createVS m tl in
   let body, data =
     match tl.data with
     | TLHandler h -> (ViewCode.viewHandler vs h, ViewData.viewData vs h.ast)
@@ -80,9 +30,9 @@ let viewTL_ (m : model) (tlid : tlid) : msg Html.html =
         ([ViewFunction.viewFunction vs f], ViewData.viewData vs f.ufAST)
   in
   let events =
-    [ eventNoPropagation "mousedown" (ToplevelMouseDown tl.id)
-    ; eventNoPropagation "mouseup" (ToplevelMouseUp tl.id)
-    ; eventNoPropagation "click" (ToplevelClick tl.id) ]
+    [ eventNoPropagation "mousedown" (fun x -> ToplevelMouseDown (tl.id, x))
+    ; eventNoPropagation "mouseup" (fun x -> ToplevelMouseUp (tl.id, x))
+    ; eventNoPropagation "click" (fun x -> ToplevelClick (tl.id, x)) ]
   in
   let selected =
     if Some tl.id = tlidOf m.cursorState then "selected" else ""
@@ -112,7 +62,74 @@ let viewTL_ (m : model) (tlid : tlid) : msg Html.html =
   let top = match documentation with Some doc -> doc | _ -> data in
   let html =
     Html.div
-      [Html.class' <| String.join " " (boxClasses ^ ["sidebar-box"; selected])]
-      [Html.div (Html.class' class_ :: events) (body ^ top)]
+      [Html.class' <| String.join " " (boxClasses @ ["sidebar-box"; selected])]
+      [Html.div (Html.class' class_ :: events) (body @ top)]
   in
   html
+
+let viewTL (m : model) (tl : toplevel) : msg Html.html =
+  let id = deTLID tl.id in
+  let recalc () = viewTL_ m tl.id in
+  let _ = "comment" in
+  let isDB = match tl.data with TLDB _ -> true | _ -> false in
+  let pos =
+    match m.currentPage with
+    | Toplevels _ -> tl.pos
+    | Fn (tLID, _) -> Defaults.centerPos
+  in
+  let html =
+    if Some tl.id = tlidOf m.cursorState || isDB then
+      let _ = Util.cacheClear id in
+      recalc ()
+    else
+      match Util.cacheGet id with
+      | Some html -> html
+      | None ->
+          let result = recalc () in
+          let _ = Util.cacheSet id result in
+          result
+  in
+  ViewUtils.placeHtml m pos html
+
+let viewCanvas (m : model) : msg Html.html =
+  let entry = ViewEntry.viewEntry m in
+  let asts =
+    match m.currentPage with
+    | Toplevels _ -> List.map (viewTL m) m.toplevels
+    | Fn (tlid, _) -> (
+      match List.find (fun f -> f.ufTLID = tlid) m.userFunctions with
+      | Some func -> [viewTL m (TL.ufToTL m func)]
+      | None -> List.map (viewTL m) m.toplevels )
+  in
+  let _ = "comment" in
+  let canvasTransform =
+    let offset =
+      match m.currentPage with
+      | Toplevels _ -> m.canvas.offset
+      | Fn (_, _) -> m.canvas.fnOffset
+    in
+    let x = string_of_int (-offset.x) in
+    let y = string_of_int (-offset.y) in
+    "translate(" ^ x ^ "px, " ^ y ^ "px)"
+  in
+  let allDivs = asts @ entry in
+  Html.div
+    [Html.id "canvas"; Html.style "transform" canvasTransform]
+    allDivs
+
+
+
+let view (m : model) : msg Html.html =
+  let attributes =
+    [ Html.id "grid"
+    ; Html.onWithOptions "mouseup"
+        {stopPropagation= false; preventDefault= true}
+        (Decoders.wrapDecoder (ViewUtils.decodeClickEvent (fun x -> GlobalClick x)))]
+  in
+  let footer = [ViewScaffold.viewError m.error; ViewScaffold.viewButtons m] in
+  let routing = ViewRoutingTable.viewRoutingTable m in
+  let body = viewCanvas m in
+  let content = [routing; body] @ footer in
+  Html.div attributes content
+
+
