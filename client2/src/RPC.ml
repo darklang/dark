b259ca58f043f4ea9be156ba433c718c0dadc655
Paul Biggar <paul.biggar@gmail.com>
2018-10-21 22:20:28 -0700

    Another RPC

--- client2/src/RPC.ml
+++ client2/src/RPC.ml
@@ -1,6 +1,7 @@
 open! Porting
-(* open JSON *)
 open Types
+module JSD = Json_decode_extended
+
 
 let postJson decoder (url: string) (body: Js.Json.t) =
   Tea.Http.request
@@ -13,7 +14,7 @@ let postJson decoder (url: string) (body: Js.Json.t) =
     ; withCredentials = false
     }
 
-let postEmpty decoder (url: string) =
+let postEmptyJson decoder (url: string) =
   Tea.Http.request
     { method' = "POST"
     ; headers = [Header ("Content-type", "application/json")]
@@ -24,16 +25,17 @@ let postEmpty decoder (url: string) =
     ; withCredentials = false
     }
 
-(* let postString (url : string) : string Http.request = *)
-(*   Http.request *)
-(*     { method_= "POST" *)
-(*     ; headers= [] *)
-(*     ; url *)
-(*     ; body= Http.emptyBody *)
-(*     ; expect= Http.expectString *)
-(*     ; timeout= None *)
-(*     ; withCredentials= false } *)
-(*  *)
+let postEmptyString decoder (url: string) =
+  Tea.Http.request
+    { method' = "POST"
+    ; headers = []
+    ; url
+    ; body = Web.XMLHttpRequest.EmptyBody
+    ; expect = Tea.Http.expectStringResponse (Decoders.wrapExpect decoder)
+    ; timeout = None
+    ; withCredentials = false
+    }
+
 let rpc (m : model) (canvasName : string) (focus : focus) (params : rpcParams)
     : msg Tea.Cmd.t =
   let url =
@@ -78,14 +80,14 @@ let initialLoadRPC (canvasName : string) (focus : focus) : msg Tea.Cmd.t =
   let url =
     String.concat ["/api/"; Tea.Http.encodeUri canvasName; "/initial_load"]
   in
-  let request = postEmpty Decoders.initialLoadRPC url in
+  let request = postEmptyJson Decoders.initialLoadRPC url in
   Tea.Http.send (fun x -> InitialLoadRPCCallback (focus, NoChange, x)) request
 
-(* let saveTestRPC (canvasName : string) : msg Cmd.t = *)
-(*   let url = String.concat ["/api/"; Http.encodeUri canvasName; "/save_test"] in *)
-(*   let request = postString url in *)
-(*   Http.send SaveTestRPCCallback request *)
-(*  *)
+let saveTestRPC (canvasName : string) : msg Tea.Cmd.t =
+  let url = String.concat ["/api/"; Tea.Http.encodeUri canvasName; "/save_test"] in
+  let request = postEmptyString JSD.string url in
+  Tea.Http.send (fun x -> SaveTestRPCCallback x) request
+
 (* let emptyParams : rpcParams = {ops= []} *)
 (*  *)
 let opsParams (ops : op list) : rpcParams = {ops}
@@ -95,7 +97,7 @@ let integrationRPC (m : model) (canvasName : string) (name : string) :
   let url =
     String.concat ["/api/"; Tea.Http.encodeUri canvasName; "/initial_load"]
   in
-  let request = postEmpty Decoders.initialLoadRPC url in
+  let request = postEmptyJson Decoders.initialLoadRPC url in
   Tea.Http.send
     (fun x -> InitialLoadRPCCallback (FocusNothing, TriggerIntegrationTest name, x))
     request
