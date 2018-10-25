c8ad597559b05a22299be6eb438c9a2ebecfad2b
Elizabeth Dixon <_@lizzie.io>
2018-10-25 13:40:51 -0700

    Error at startup if DARK_CONFIG_CURL_TUNNEL_URL isn't set.

--- server/libservice/config_dsl.mli
+++ server/libservice/config_dsl.mli
@@ -5,5 +5,6 @@ val int : string -> int
 val bool : string -> bool
 val string : string -> string
 val string_option : string -> string list -> string
+val maybe_string : string -> string option
 val password : string -> string
 val lowercase : string -> string -> string
