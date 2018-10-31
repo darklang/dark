e64c2bc71921a691011db2e61d383bd49356809e
Alice Q. Wong <alice@valour.me>
2018-10-31 15:52:06 -0700

    We actually don't need this script

--- server/templates/bs-glue.js
+++ server/templates/bs-glue.js
@@ -95,69 +95,6 @@ function visibilityCheck(){
   }
 }
 
-function addWheelListener(elem){
-  var prefix = "";
-  var _addEventListener;
-  var support;
-
-  // detect event model
-  if ( window.addEventListener ) {
-      _addEventListener = "addEventListener";
-  } else {
-      _addEventListener = "attachEvent";
-      prefix = "on";
-  }
-
-  // detect available wheel event
-  support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-            document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-            "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
-
-  var listener = function( elem, useCapture ) {
-      _addWheelListener( elem, support, useCapture );
-
-      // handle MozMousePixelScroll in older Firefox
-      if( support == "DOMMouseScroll" ) {
-          _addWheelListener( elem, "MozMousePixelScroll", useCapture );
-      }
-  };
-
-  function _addWheelListener( elem, eventName, useCapture ) {
-      elem[ _addEventListener ](prefix + eventName, function( originalEvent ) {
-          !originalEvent && ( originalEvent = window.event );
-
-          // create a normalized event object
-          var event = {
-              // keep a ref to the original event object
-              originalEvent: originalEvent,
-              target: originalEvent.target || originalEvent.srcElement,
-              type: "wheel",
-              deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
-              deltaX: 0,
-              deltaY: 0,
-              deltaZ: 0,
-              preventDefault: function() {
-                  originalEvent.preventDefault ?
-                      originalEvent.preventDefault() :
-                      originalEvent.returnValue = false;
-              }
-          };
-
-          // calculate deltaY (and deltaX) according to the event
-          if ( support == "mousewheel" ) {
-              event.deltaY = - 1/40 * originalEvent.wheelDelta;
-              // Webkit also support wheelDeltaX
-              originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
-          } else {
-              event.deltaY = originalEvent.deltaY || originalEvent.detail;
-          }
-
-      }, useCapture || false );
-  }
-
-  return listener(elem);
-}
-
 setTimeout(function(){
 
   const params = JSON.stringify(
@@ -182,6 +119,5 @@ setTimeout(function(){
   window.onfocus = function(evt){ windowFocusChange(true) };
   window.onblur = function(evt){ windowFocusChange(false) };
   setInterval(visibilityCheck, 2000);
-  addWheelListener(document);
 
 }, 1)
