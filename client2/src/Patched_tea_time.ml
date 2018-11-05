9db85d326a16b6f4f1abad131c51efa948caf56a
Alice Q. Wong <alice@valour.me>
2018-11-05 09:46:43 -0800

    made key required and first param

--- client2/src/Patched_tea_time.ml
+++ client2/src/Patched_tea_time.ml
@@ -5,7 +5,7 @@ open Tea
   will lead to rewrites of setInterval functions,
   if two or more share the same interval.
 *)
-let every interval tagger key =
+let every ~key interval tagger =
   let open Vdom in
   let enableCall callbacks =
   let id =
