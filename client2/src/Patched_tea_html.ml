3ee5dae2ff9b90e239de02711d9ee632aeede991
Alice Q. Wong <alice@valour.me>
2018-11-05 09:25:30 -0800

    renamed and code cleanup for tea time

--- client2/src/Patched_tea_html.ml
+++ client2/src/Patched_tea_html.ml
@@ -9,11 +9,3 @@ let onWithOptions ?(key="") eventName (options: Tea_html.options) decoder =
     |> Tea_json.Decoder.decodeEvent decoder
     |> Tea_result.result_to_option
   )
-
-let every interval tagger key =
-  let open Vdom in
-  let enableCall callbacks =
-  let id = (Web.Window.setInterval (fun () -> callbacks.enqueue (tagger (Web.Date.now ())) ) interval) in
-  fun () ->
-    Web.Window.clearTimeout id
-  in Tea_sub.registration key enableCall
