0e5548b20c2a02340962886dee92b0c0f4e0a2a1
Ian Smith <ismith@metaforgotten.org>
2018-11-06 10:36:06 -0800

    Set request analysis log level to debug (chrome devtools calls this 'verbose')

--- server/templates/bs-glue.js
+++ server/templates/bs-glue.js
@@ -7,8 +7,10 @@ window.Dark = {
   },
   analysis: {
     requestAnalysis : function (params) {
-      console.log('request analysis');
-      console.log(params);
+      // debug is not one of the default log levels in chrome devtools - to see
+      // these, set log levels to include 'Verbose'
+      console.debug('request analysis');
+      console.debug(params);
       // const bToString = (blankOr) => blankOr[2] || null;
       // const spec = params.handler.spec;
       // const route = `${bToString(spec.module)}, ${bToString(spec.name)}, ${bToString(spec.modifier)}`;
