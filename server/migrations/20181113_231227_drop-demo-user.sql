0f73b968a831060d080fa194c8094f862dfb1a35
Ian Smith <ismith@metaforgotten.org>
2018-11-14 14:16:47 -0800

    We can't remove the demo user without breaking canvases_account_id_fkey, but we can make it so they can't log in

--- server/migrations/20181113_231227_drop-demo-user.sql
+++ server/migrations/20181113_231227_drop-demo-user.sql
@@ -1,1 +1,5 @@
-DELETE FROM accounts WHERE username = 'demo'
+-- we can't delete this account without breaking canvases_account_id_fkey, but
+-- we can make it impossible to log in
+UPDATE accounts
+SET password = ''
+WHERE username = 'demo';
