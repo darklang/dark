954b8cd93320d5f5596c8fb154db36e030b8749c
Paul Biggar <paul.biggar@gmail.com>
2018-11-16 23:08:05 -0800

    Create Cache.cache2
    Ended up not using it

--- client2/src/Cache.ml
+++ client2/src/Cache.ml
@@ -14,4 +14,15 @@ let cache1 keyFn expensiveFn =
        cache := Some (cacheKey, result);
        result)
 
+let cache2 keyFn expensiveFn =
+  let cache = create () in
+  (fun arg1 arg2 ->
+     let cacheKey = keyFn arg1 arg2 in
+     match !cache with
+     | Some (key, result) when cacheKey = key -> result
+     | _ ->
+       let result = expensiveFn arg1 arg2 in
+       cache := Some (cacheKey, result);
+       result)
+
 
