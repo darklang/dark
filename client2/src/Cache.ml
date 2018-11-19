72e1fcb1e43759ed62bc284d39368c5299e599b3
Paul Biggar <paul.biggar@gmail.com>
2018-11-19 12:25:41 -0800

    Allow us to disable the cache

--- client2/src/Cache.ml
+++ client2/src/Cache.ml
@@ -3,26 +3,35 @@ type 'a t = 'a option ref
 let create () : 'a t =
   ref None
 
+let enabled = true
+
 let cache1 keyFn expensiveFn =
   let cache = create () in
   (fun arg ->
-     let cacheKey = keyFn arg in
-     match !cache with
-     | Some (key, result) when cacheKey = key -> result
-     | _ ->
-       let result = expensiveFn arg in
-       cache := Some (cacheKey, result);
-       result)
+     if enabled
+     then
+       let cacheKey = keyFn arg in
+       match !cache with
+       | Some (key, result) when cacheKey = key -> result
+       | _ ->
+         let result = expensiveFn arg in
+         cache := Some (cacheKey, result);
+         result
+     else expensiveFn arg)
+
 
 let cache2 keyFn expensiveFn =
   let cache = create () in
   (fun arg1 arg2 ->
-     let cacheKey = keyFn arg1 arg2 in
-     match !cache with
-     | Some (key, result) when cacheKey = key -> result
-     | _ ->
-       let result = expensiveFn arg1 arg2 in
-       cache := Some (cacheKey, result);
-       result)
+     if enabled
+     then
+       let cacheKey = keyFn arg1 arg2 in
+       match !cache with
+       | Some (key, result) when cacheKey = key -> result
+       | _ ->
+         let result = expensiveFn arg1 arg2 in
+         cache := Some (cacheKey, result);
+         result
+     else expensiveFn arg1 arg2)
 
 
