8678d555739525283d5ce46b5c91cb8c5541fdd4
Paul Biggar <paul.biggar@gmail.com>
2018-11-20 18:39:46 -0800

    Move stopKeys into main.ml

--- server/static/bundle.js
+++ server/static/bundle.js
@@ -155,11 +155,38 @@ const mousewheel = function(callback){
   });
 };
 
+// Allows us capture certain keys and stop them from affecting the browser.
+function stopKeys(event) {
+  if (event.keyCode == 9) { // Tab
+    event.preventDefault();
+  }
+  if (event.keyCode == 32 // Space
+      && !event.target.parentNode.className.includes("string-container")) {
+    event.preventDefault();
+  }
+  if (event.keyCode == 13 // Enter
+      && !event.target.parentNode.className.includes("large-string")) {
+    event.preventDefault();
+  }
+  if (event.keyCode == 38 // Up
+      || event.keyCode == 40) {  // Down
+    if (document.activeElement.tagName.toLowerCase() !== 'textarea')
+      event.preventDefault();
+  }
+}
+window.stopKeys = stopKeys;
+
+// ---------------------------
+// Rollbar
+// ---------------------------
 var rollbar = require('rollbar');
 
 var Rollbar = rollbar.init({});
 window.Rollbar = Rollbar;
 
+// ---------------------------
+// Exports
+// ---------------------------
 module.exports = {
   mousewheel: mousewheel
 };
