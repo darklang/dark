ff3761a8fd1fb6110709a1654d371e5e1dda2640
Paul Biggar <paul.biggar@gmail.com>
2018-11-28 19:55:06 -0800

    Don't download function results called in loops

--- server/libbackend/stored_function_result.ml
+++ server/libbackend/stored_function_result.ml
@@ -24,14 +24,19 @@ let store ~canvas_id ~trace_id (tlid, fnname, id) arglist result =
             ; DvalJson result]
 
 let load ~canvas_id ~trace_id tlid : function_result list =
+  (* Right now, we don't allow the user to see multiple results when a function
+   * is called in a loop. But, there's a lot of data when functions are called
+   * in a loop, so avoid massive responses. *)
   Db.fetch
     ~name:"sfr_load"
-    "SELECT fnname, id, hash, value, timestamp
+    "SELECT
+       DISTINCT ON (fnname, id, hash)
+       fnname, id, hash, value, timestamp
      FROM function_results_v2
      WHERE canvas_id = $1
        AND trace_id = $2
        AND tlid = $3
-     ORDER BY timestamp DESC"
+     ORDER BY fnname, id, hash, timestamp DESC"
     ~params:[ Db.Uuid canvas_id
             ; Db.Uuid trace_id
             ; Db.ID tlid
