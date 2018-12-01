f95118bdbad8b902f1fd71ca85a2ce1dd0622343
Paul Biggar <paul.biggar@gmail.com>
2018-11-30 16:55:18 -0800

    s/darkjs/analysis.js/

--- server/bin/darkjs.ml
+++ server/bin/darkjs.ml
@@ -1,14 +1,1 @@
-open Js_of_ocaml
 
-type js_string = Js.js_string Js.t
-
-let () =
-  Libfrontend.Init.init ();
-  Js.export "darkAnalysis"
-    (object%js
-       method performAnalysis (tlids: js_string) : js_string =
-         let tlids = Js.to_string tlids in
-         let result = Libfrontend.Init.perform_analysis tlids in
-         Js.string result
-     end);
-  ()
