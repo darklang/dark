216b965eaeccb26caacbba10da89947a5e5ee542
Sam Stokes <me@samstokes.co.uk>
2018-12-05 17:46:34 -0600

    avoid reconnecting to Postgres for every request

--- stroller/src/db/mod.rs
+++ stroller/src/db/mod.rs
@@ -1,19 +1,37 @@
 use diesel::pg::PgConnection;
-use diesel::Connection;
+use diesel::r2d2::{ConnectionManager, Pool};
 
 use config::*;
 
 pub mod models;
 pub mod schema;
 
-pub fn connect() -> PgConnection {
-    let db_url = format!(
+fn database_url() -> String {
+    format!(
         "postgres://{}:{}@{}/{}",
         db_user(),
         db_password(),
         db_host(),
         db_dbname()
-    );
+    )
+}
 
-    PgConnection::establish(&db_url).expect(&format!("Couldn't connect to Postgres"))
+#[cfg(test)]
+pub fn connect() -> PgConnection {
+    use diesel::Connection;
+
+    PgConnection::establish(&database_url()).expect("Couldn't connect to Postgres")
+}
+
+pub fn new_pool() -> Pool<ConnectionManager<PgConnection>> {
+    Pool::builder()
+        /*
+         * Set the number of connections the pool should maintain.
+         * This is the max, but also the min, since we don't specify min_idle.
+         * 10 is the default so this is a no-op, here for the sake of
+         * explicitness.
+         */
+        .max_size(10)
+        .build(ConnectionManager::new(database_url()))
+        .expect("couldn't establish connection pool")
 }
