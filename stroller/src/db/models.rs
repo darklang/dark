b651f26120373a41f7b5f1cfa44b5e0cf9184ff9
Sam Stokes <me@samstokes.co.uk>
2018-12-05 11:35:23 -0600

    Parse JSON (so the Pusher client can serialize it again :facepalm:)

--- stroller/src/db/models.rs
+++ stroller/src/db/models.rs
@@ -1,6 +1,7 @@
 #![allow(proc_macro_derive_resolution_fallback)]
 
 use chrono::{DateTime, Utc};
+use rustc_serialize::json;
 use uuid::Uuid;
 
 #[derive(Queryable, Debug)]
@@ -10,5 +11,12 @@ pub struct StoredEvent {
     pub timestamp: DateTime<Utc>,
     pub module: String,
     pub path: String,
-    pub value: String,
+    pub value_json: String,
+}
+
+impl StoredEvent {
+    pub fn value(&self) -> Result<json::Json, String> {
+        json::Json::from_str(&self.value_json)
+            .map_err(|e| format!("bad JSON in value field: {:?}", e).to_string())
+    }
 }
