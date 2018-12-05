a9eb91243ae2237a94567122a07bec6995117345
Sam Stokes <me@samstokes.co.uk>
2018-12-05 11:22:35 -0600

    Add Postgres connection

---
+++ stroller/src/db/mod.rs
@@ -0,0 +1,16 @@
+use diesel::pg::PgConnection;
+use diesel::Connection;
+
+use config::*;
+
+pub fn connect() -> PgConnection {
+    let db_url = format!(
+        "postgres://{}:{}@{}/{}",
+        db_user(),
+        db_password(),
+        db_host(),
+        db_dbname()
+    );
+
+    PgConnection::establish(&db_url).expect(&format!("Couldn't connect to Postgres"))
+}
