5be6a4b7e9b8629db48e1247988a9407dc40f6ee
Sam Stokes <me@samstokes.co.uk>
2018-12-06 15:57:53 -0600

    Working example Pusher call (but no TLS)

--- stroller/src/main.rs
+++ stroller/src/main.rs
@@ -1,12 +1,20 @@
 extern crate hyper;
+extern crate pusher;
+
+use std::collections::HashMap;
 
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Request, Response, Server};
+use pusher::Pusher;
 
 const GREETING: &str = "Hello Dark!";
 
 const PORT: u16 = 3000;
+
+const PUSHER_APP_ID: &str = "661887";
+const PUSHER_KEY: &str = "ee6267fa618c71d4d341";
+const PUSHER_SECRET: &str = "ce29d2033402c9f037e1";
 
 fn hello_world(_req: Request<Body>) -> Response<Body> {
     Response::new(Body::from(GREETING))
@@ -14,6 +22,32 @@ fn hello_world(_req: Request<Body>) -> Response<Body> {
 
 fn main() {
     let addr = ([0, 0, 0, 0], PORT).into();
+
+    let mut pusher = Pusher::new(PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET)
+        .host("api-us2.pusher.com")
+        /*
+         * :( we definitely should be using HTTPS but this panics with
+         * "Invalid scheme for Http".
+         * On inspection it appears .secure() just doesn't work.
+         * pusher-http-rust depends on an old version of hyper; however, TLS
+         * support was extracted out of hyper (in an even older version) into
+         * hyper-tls, the latest version of which is incompatible with the
+         * version of hyper that pusher-http-rust requires (and in particular
+         * with pusher-http-rust's .client() builder method).
+         */
+        //.secure()
+        .finalize();
+
+    let mut m = HashMap::new();
+    m.insert("message", "hey");
+
+    match pusher.trigger("my-channel", "my-event", m) {
+        Ok(events) => println!("Pushed events: {:?}", events),
+        Err(err) => eprintln!("Error pushing events: {}", err),
+    }
+    // TODO make actual Pusher call in the background without blocking the
+    // caller (since that's the whole point of having this in a separate
+    // process)
 
     let hello_svc = || service_fn_ok(hello_world);
 
