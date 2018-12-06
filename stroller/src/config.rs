a3cd602b41838d8d37167edd74c4f257edc7bfee
Sam Stokes <me@samstokes.co.uk>
2018-12-05 18:56:00 -0600

    Wrap Pusher SDK to discourage lockin, and move secrets into config

--- stroller/src/config.rs
+++ stroller/src/config.rs
@@ -19,3 +19,19 @@ pub fn db_user() -> String {
 pub fn db_password() -> String {
     require_str("DARK_CONFIG_DB_PASSWORD")
 }
+
+pub fn pusher_app_id() -> String {
+    require_str("DARK_CONFIG_PUSHER_APP_ID")
+}
+
+pub fn pusher_key() -> String {
+    require_str("DARK_CONFIG_PUSHER_KEY")
+}
+
+pub fn pusher_secret() -> String {
+    require_str("DARK_CONFIG_PUSHER_SECRET")
+}
+
+pub fn pusher_host() -> String {
+    require_str("DARK_CONFIG_PUSHER_HOST")
+}
