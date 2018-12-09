3497aace941566890cc7be9c985d9f9373512124
Paul Biggar <paul.biggar@gmail.com>
2018-12-08 17:46:18 -0800

    The ocamlformatting showed us that our init functions were incorrect

--- backend/libexecution/init.ml
+++ backend/libexecution/init.ml
@@ -4,12 +4,12 @@ open Libcommon
 let has_inited : bool ref = ref false
 
 let init log_level log_format replacements =
-  if !has_inited
-  then ()
-  else Caml.print_endline "Libexecution Initialization Begins" ;
-  Printexc.record_backtrace true ;
-  Exn.initialize_module () ;
-  Log.init ~level:log_level ~format:log_format () ;
-  Libs.init replacements ;
-  Log.infO "Libexecution" ~data:"Initialization Complete" ;
-  has_inited := true
+  if not !has_inited
+  then (
+    Caml.print_endline "Libexecution Initialization Begins" ;
+    Printexc.record_backtrace true ;
+    Exn.initialize_module () ;
+    Log.init ~level:log_level ~format:log_format () ;
+    Libs.init replacements ;
+    Log.infO "Libexecution" ~data:"Initialization Complete" ;
+    has_inited := true )
