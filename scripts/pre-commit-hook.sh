177d9d485b447454debb519dcb38bd15d1e362ee
Paul Biggar <paul.biggar@gmail.com>
2018-12-18 18:40:22 -0800

    Fix pre-commit hook

--- scripts/pre-commit-hook.sh
+++ scripts/pre-commit-hook.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+. ./scripts/support/assert-in-container $0 $@
+
+set -euo pipefail
 
 # To install:
 # - cp scripts/pre-commit.hook.sh .git/hooks/pre-commit
@@ -8,7 +11,7 @@ files=$(git diff --cached --name-only --diff-filter=ACM "*.ml" | tr '\n' ' ')
 [ -z "$files" ] && exit 0
 
 # Prettify all staged .js files
-echo "$files" | xargs scripts/ocamlformat
+echo "$files" | xargs scripts/ocamlformat --inplace
 
 # Add back the modified/prettified files to staging
 echo "$files" | xargs git add
