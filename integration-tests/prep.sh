b7177629426bb8ad43f74ddcea28f0302d04fcd9
Paul Biggar <paul.biggar@gmail.com>
2018-12-24 14:04:27 -0800

    Run the browser outside the container to get better debugging abilities.
    Can still run it the old way: run-in-docker ./integration-tests/run.sh

---
+++ integration-tests/prep.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+. ./scripts/support/assert-in-container "$0" "$@"
+
+set -euo pipefail
+
+# Prep the integration tests. This needs to be run in the container
+
+
+# Set up test reporters for CircleCI
+TEST_RESULTS_DIR="${DARK_CONFIG_RUNDIR}/test_results"
+mkdir -p "${TEST_RESULTS_DIR}"
+
+echo "Clearing old test files"
+rm -f "${DARK_CONFIG_RUNDIR}/completed_tests/*"
+rm -Rf "${DARK_CONFIG_RUNDIR}/screenshots/*"
+rm -f "${TEST_RESULTS_DIR}/integration_tests.*"
+
+# Clear DBs
+DBLOG="${DARK_CONFIG_RUNDIR}/integration_db.log"
+echo "Clearing old DB data (logs in ${DBLOG})"
+DB="${DARK_CONFIG_DB_DBNAME}"
+function run_sql { psql -d "$DB" -c "$@" >> "$DBLOG" 2>&1; }
+
+function fetch_sql { psql -d "$DB" -t -c "$@"; }
+
+CANVASES=$(fetch_sql "SELECT id FROM canvases WHERE substring(name, 0, 6)
+= 'test-';")
+SCRIPT=""
+for cid in $CANVASES; do
+  SCRIPT+="DELETE FROM events WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM function_results_v2 WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM stored_events_v2 WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM user_data WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM cron_records WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM toplevel_oplists WHERE canvas_id = '$cid';";
+  SCRIPT+="DELETE FROM canvases WHERE id = '$cid';";
+done
+run_sql "$SCRIPT";
