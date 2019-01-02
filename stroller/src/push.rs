2f5d726b7402143c8b242571cde166bda2275269
Sam Stokes <me@samstokes.co.uk>
2019-01-02 15:53:35 -0800

    Partial Pusher wrapper (incomplete)

--- stroller/src/push.rs
+++ stroller/src/push.rs
@@ -1,68 +1,84 @@
-// Thin wrapper around pusher-http-rust to discourage lock-in
+// Wrapper for the Pusher REST API (https://pusher.com/docs/rest_api). Not
+// intended to be comprehensive.
 
-use std::thread;
-
-use pusher::Pusher;
-use rustc_serialize::json;
+use futures::future;
+use hyper::client::HttpConnector;
+use hyper::header;
+use hyper::rt::{Future, Stream};
+use hyper::{Body, Client as HClient, Request as HRequest, StatusCode, Uri};
+use hyper_tls::HttpsConnector;
 
 use config::*;
 
 pub type Error = String;
 
-/*
- * A client for pushing events. Currently clients are one-use-only, due to use of a background
- * thread to avoid blocking, and the non-thread-safety of the underlying Pusher client.
- */
-pub struct Client(Pusher);
+pub struct Client {
+    pub host: String,
+    pub app_id: String,
+    pub key: String,
+    pub secret: String,
+
+    http: HClient<HttpsConnector<HttpConnector>>,
+}
+
+type BoxFut<T> = Box<Future<Item = T, Error = Error> + Send>;
 
 impl Client {
     pub fn connect() -> Self {
-        let pusher = Pusher::new(&pusher_app_id(), &pusher_key(), &pusher_secret())
-            .host(&pusher_host())
-            /*
-             * :( we definitely should be using HTTPS but this panics with
-             * "Invalid scheme for Http".
-             * On inspection it appears .secure() just doesn't work.
-             * pusher-http-rust depends on an old version of hyper; however, TLS
-             * support was extracted out of hyper (in an even older version) into
-             * hyper-tls, the latest version of which is incompatible with the
-             * version of hyper that pusher-http-rust requires (and in particular
-             * with pusher-http-rust's .client() builder method).
-             */
-            //.secure()
-            .finalize();
-        Client(pusher)
+        let mut https = HttpsConnector::new(4).unwrap();
+        https.https_only(true);
+        let http = HClient::builder().build::<_, Body>(https);
+
+        Client {
+            host: pusher_host(),
+            app_id: pusher_app_id(),
+            key: pusher_key(),
+            secret: pusher_secret(),
+
+            http,
+        }
     }
 
-    // Push an event. Consumes this Client (due to use of a background thread)
     pub fn trigger(
-        mut self,
+        &self,
         canvas_uuid: String,
         event_name: String,
         json_bytes: &[u8],
-    ) -> Result<(), Error> {
-        /*
-         * This actually parses the JSON and then immediately re-serializes it
-         * again...  which is ridiculous, but required because pusher-http-rust
-         * requires its payload to be rustc_serialize::Encodable.
-         */
+    ) -> BoxFut<()> {
+        let uri: Uri = format!("https://{}/apps/{}/events", self.host, self.app_id)
+            .parse()
+            .unwrap();
 
-        let parsed = json::Json::from_reader(&mut std::io::Cursor::new(json_bytes))
-            .map_err(|e| format!("couldn't send invalid JSON ({}): {:?}", e, json_bytes))?;
+        // TODO we need to wrap the data with params, e.g. channel, event name
+        // TODO auth
 
-        // make actual Pusher call in the background without blocking the caller
-        // (since that's the whole point of having this in a separate process)
-        thread::spawn(move || {
-            let channel = format!("canvas_{}", canvas_uuid);
-            match self.0.trigger(&channel, &event_name, parsed) {
-                Ok(_) => println!(
-                    "Pushed event \"{}\" for canvas \"{}\"",
-                    event_name, canvas_uuid
-                ),
-                Err(e) => eprintln!("Error pushing event: {:?}", e),
-            }
-        });
-
-        Ok(())
+        Box::new(
+            self.http
+                .request(
+                    HRequest::post(uri)
+                        .header(header::CONTENT_TYPE, "application/json")
+                        .body(Body::from(json_bytes.to_vec()))
+                        .unwrap(),
+                )
+                .map_err(|e| format!("Error pushing event: {:?}", e))
+                .and_then(|resp| {
+                    let result: BoxFut<()> = match resp.status() {
+                        StatusCode::OK => {
+                            println!("Pushed event");
+                            Box::new(future::ok(()))
+                        }
+                        code => Box::new(
+                            resp.into_body()
+                                .concat2()
+                                .map(move |bytes| {
+                                    eprintln!("Failed to push event: {} ({:?})", code, bytes);
+                                    ()
+                                })
+                                .map_err(|e| format!("Error reading push error: {:?}", e)),
+                        ),
+                    };
+                    result
+                }),
+        )
     }
 }
