b25e9fa2a156c78ef713b097f3ee0bf29f6a8eb2
Sam Stokes <me@samstokes.co.uk>
2019-01-07 16:10:39 -0800

    Use canvas UUID instead of subdomain for push routing

--- stroller/src/service.rs
+++ stroller/src/service.rs
@@ -7,14 +7,14 @@ use push;
 type BoxFut<T, E> = Box<Future<Item = T, Error = E> + Send>;
 
 pub trait Push {
-    fn push(canvas: &str, event_name: &str, json_bytes: &[u8]) -> Result<(), String>;
+    fn push(canvas_uuid: &str, event_name: &str, json_bytes: &[u8]) -> Result<(), String>;
 }
 
 impl Push for push::Client {
-    fn push(canvas: &str, event_name: &str, json_bytes: &[u8]) -> Result<(), String> {
+    fn push(canvas_uuid: &str, event_name: &str, json_bytes: &[u8]) -> Result<(), String> {
         // TODO reuse push client!
         let client = push::Client::connect();
-        client.trigger(canvas.to_string(), event_name.to_string(), json_bytes)
+        client.trigger(canvas_uuid.to_string(), event_name.to_string(), json_bytes)
     }
 }
 
@@ -31,20 +31,22 @@ where
 
     match (req.method(), path_segments.as_slice()) {
         (&Method::GET, ["", ""]) => {
-            *response.body_mut() = Body::from("Try POSTing to /canvas/:name/events/:event");
+            *response.body_mut() = Body::from("Try POSTing to /canvas/:uuid/events/:event");
         }
-        (&Method::POST, ["", "canvas", canvas, "events", event]) => {
-            let handled = handle_push::<PC>(canvas.to_string(), event.to_string(), req.into_body())
-                .map(|_| {
-                    *response.status_mut() = StatusCode::ACCEPTED;
-                    response
-                }).or_else(|e| {
-                    eprintln!("error trying to push trace: {}", e);
-                    Ok(Response::builder()
-                        .status(StatusCode::INTERNAL_SERVER_ERROR)
-                        .body(Body::empty())
-                        .unwrap())
-                });
+        (&Method::POST, ["", "canvas", canvas_uuid, "events", event]) => {
+            let handled =
+                handle_push::<PC>(canvas_uuid.to_string(), event.to_string(), req.into_body())
+                    .map(|_| {
+                        *response.status_mut() = StatusCode::ACCEPTED;
+                        response
+                    })
+                    .or_else(|e| {
+                        eprintln!("error trying to push trace: {}", e);
+                        Ok(Response::builder()
+                            .status(StatusCode::INTERNAL_SERVER_ERROR)
+                            .body(Body::empty())
+                            .unwrap())
+                    });
 
             return Box::new(handled);
         }
@@ -56,7 +58,11 @@ where
     Box::new(future::ok(response))
 }
 
-fn handle_push<PC>(canvas: String, event_name: String, payload_body: Body) -> BoxFut<(), String>
+fn handle_push<PC>(
+    canvas_uuid: String,
+    event_name: String,
+    payload_body: Body,
+) -> BoxFut<(), String>
 where
     PC: Push,
 {
@@ -68,11 +74,11 @@ where
                 println!(
                     "Got event \"{}\" for canvas \"{}\" ({} bytes)",
                     event_name,
-                    canvas,
+                    canvas_uuid,
                     payload_bytes.len(),
                 );
 
-                PC::push(&canvas, &event_name, &payload_bytes)
+                PC::push(&canvas_uuid, &event_name, &payload_bytes)
                     .map_err(|e| format!("failed to push event {}: {}", event_name, e))
             }),
     )
@@ -84,7 +90,7 @@ mod tests {
 
     struct FakePushClient;
     impl Push for FakePushClient {
-        fn push(_canvas: &str, _event_name: &str, _json_bytes: &[u8]) -> Result<(), String> {
+        fn push(_canvas_uuid: &str, _event_name: &str, _json_bytes: &[u8]) -> Result<(), String> {
             Ok(())
         }
     }
