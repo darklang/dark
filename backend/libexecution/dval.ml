dd465e50fb46d416d76747bbe271e14f219122de
Ian Connolly <ian@connolly.io>
2019-01-10 16:20:59 -0800

    Add quick note about Postgres rejecting 0x00

--- backend/libexecution/dval.ml
+++ backend/libexecution/dval.ml
@@ -8,6 +8,7 @@ let dstr_of_string (s : string) : dval option =
   let rec validate_string () =
     match Uutf.decode decoder with
     | `Uchar _ ->
+        (* potentially should do something with the null byte? we can probably pretend it doesn't exist for now, but Postgres doesn't allow it in text/varchar fields (presumably some C-string legacy) *)
         validate_string ()
     | `End ->
         `Ok
