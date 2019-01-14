44ecc002f9ddbbc14ece83555310b2b6e52ad574
Alice Q. Wong <alice@valour.me>
2019-01-14 13:45:24 -0800

    Place test-GET icon next to lock. & refactor code
    Moving icon:
    The test-GET icon looked misaligned directly next to get. Moved it to be near lock icon, so we can have a group of actionable icons on the right.
    
    Refactor:
    Moved all single use function definitions inside a view header function. Scoped functions locally so they are only exposed in a need to know basis

--- client/src/ViewCode.ml
+++ client/src/ViewCode.ml
@@ -454,40 +454,48 @@ and viewNExpr
             [titleBar; blockCondition; expressions] ]
 
 
-let viewEventName (vs : viewState) (v : string blankOr) : msg Html.html =
-  let configs = (enterable :: idConfigs) @ [wc "name"] in
-  viewText EventName vs configs v
-
-
-let viewEventSpace (vs : viewState) (v : string blankOr) : msg Html.html =
-  let configs = (enterable :: idConfigs) @ [wc "module"] in
-  viewText EventSpace vs configs v
-
-
-let viewEventModifier (vs : viewState) (spec : handlerSpec) : msg Html.html =
-  let externalLink =
-    match (spec.modifier, spec.name) with
-    | F (_, "GET"), F (_, name) ->
-        [ Html.a
-            [ Html.class' "external"
-            ; Html.href
-                ( "//"
-                ^ Tea.Http.encodeUri vs.canvasName
-                ^ "."
-                ^ vs.userContentHost
-                ^ name )
-            ; Html.target "_blank" ]
-            [fontAwesome "external-link-alt"] ]
-    | _ ->
-        []
+let viewEventSpec (vs : viewState) (spec : handlerSpec) : msg Html.html list =
+  let viewEventName =
+    let configs = (enterable :: idConfigs) @ [wc "name"] in
+    viewText EventName vs configs spec.name
+  and viewEventSpace =
+    let configs = (enterable :: idConfigs) @ [wc "module"] in
+    viewText EventSpace vs configs spec.module_
+  and viewEventModifier =
+    let configs = (enterable :: idConfigs) @ [wc "modifier"] in
+    if SpecHeaders.visibleModifier spec
+    then viewText EventModifier vs configs spec.modifier
+    else Html.div [] []
+  and viewEventActions =
+    let lock =
+      Html.div
+        [ Html.classList
+            [("handler-lock", true); ("is-locked", vs.handlerLocked)]
+        ; ViewUtils.eventNoPropagation
+            ~key:
+              ("lh-" ^ showTLID vs.tlid ^ "-" ^ string_of_bool vs.handlerLocked)
+            "click"
+            (fun _ -> LockHandler (vs.tlid, not vs.handlerLocked)) ]
+        [fontAwesome (if vs.handlerLocked then "lock" else "lock-open")]
+    and externalLink =
+      match (spec.modifier, spec.name) with
+      | F (_, "GET"), F (_, name) ->
+          [ Html.a
+              [ Html.class' "external"
+              ; Html.href
+                  ( "//"
+                  ^ Tea.Http.encodeUri vs.canvasName
+                  ^ "."
+                  ^ vs.userContentHost
+                  ^ name )
+              ; Html.target "_blank" ]
+              [fontAwesome "external-link-alt"] ]
+      | _ ->
+          []
+    in
+    Html.div [Html.class' "actions"] (externalLink @ [lock])
   in
-  let configs = (enterable :: idConfigs) @ [wc "modifier"] in
-  if SpecHeaders.visibleModifier spec
-  then
-    Html.div
-      [Html.class' "modifier-wrapper"]
-      ([viewText EventModifier vs configs spec.modifier] @ externalLink)
-  else Html.div [] []
+  [viewEventName; viewEventSpace; viewEventModifier; viewEventActions]
 
 
 let viewHandler (vs : viewState) (h : handler) : msg Html.html list =
@@ -499,22 +507,7 @@ let viewHandler (vs : viewState) (h : handler) : msg Html.html list =
       ; Html.div [Html.classList [("rop-rail", true); ("active", showRail)]] []
       ]
   in
-  let lock =
-    Html.div
-      [ Html.classList [("handler-lock", true); ("is-locked", vs.handlerLocked)]
-      ; ViewUtils.eventNoPropagation
-          ~key:
-            ("lh-" ^ showTLID vs.tlid ^ "-" ^ string_of_bool vs.handlerLocked)
-          "click"
-          (fun _ -> LockHandler (vs.tlid, not vs.handlerLocked)) ]
-      [fontAwesome (if vs.handlerLocked then "lock" else "lock-open")]
-  in
   let header =
-    Html.div
-      [Html.class' "spec-header"]
-      [ viewEventName vs h.spec.name
-      ; viewEventSpace vs h.spec.module_
-      ; viewEventModifier vs h.spec
-      ; lock ]
+    Html.div [Html.class' "spec-header"] (viewEventSpec vs h.spec)
   in
   [header; ast]
