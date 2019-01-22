550766a0b6449277a70f2da8301aea0f40437626
Ian Connolly <ian@connolly.io>
2019-01-22 11:04:58 -0800

    Add comments to mli

--- backend/libexecution/unicode_string.mli
+++ backend/libexecution/unicode_string.mli
@@ -1,8 +1,13 @@
 open Core_kernel
 
+(** Represents a string of user-perceivable Unicode characters.
+ ** The internal representation is deliberately opaque, and no guarantee
+ ** is given regarding the representation or encoding. **)
 type t
 
 module Character : sig
+  (** Represents a single user-perceivable Unicode character, more specifically
+   ** an 'extended grapheme cluster' in Unicoder terminology **)
   type t
 
   val compare : t -> t -> int
@@ -17,22 +22,40 @@ module Character : sig
 end
 
 val of_utf8_encoded_string : string -> t option
+(** Creates a `t` from an OCaml `string`, which is required to be encoded in UTF-8.
+ ** Returns `None` if the passed string is not valid UTF-8 **)
 
 val of_string : string -> t option
+(** alias of `of_utf8_encoded_string` **)
 
 val of_utf8_encoded_string_exn : ?message:string -> string -> t
+(** Creates a `t` from an OCaml `string`, which is required to be encoded in UTF-8
+ ** Throws an exception with `?message` as the message if the passed string
+ ** is not valid UTF-8. The exception has a default message if no message
+ ** is provided**)
 
 val of_string_exn : ?message:string -> string -> t
+(** alias of `of_utf8_encoded_string_exn` **)
 
 val to_utf8_encoded_string : t -> string
 
 val to_string : t -> string
+(** alias of `to_utf8_encoded_string` **)
 
 val uppercase : t -> t
+(** Maps the casing of the characters of the String to their defined
+ ** uppercase equivalent.
+ ** Note: `length a != length (uppercase a)` in the general case as
+ ** some characters become multiple characters when their case is changed **)
 
 val lowercase : t -> t
+(** Maps the casing of the characters of the String to their defined
+ ** lowercase equivalent.
+ ** Note: `length a != length (lowercase a)` in the general case as
+ ** some characters become multiple characters ** when their case is changed **)
 
 val append : t -> t -> t
+(** `append a b` returns the concatenation of `a` and `b` **)
 
 val map_graphemes : f:(Character.t -> 'a) -> t -> 'a list
 
