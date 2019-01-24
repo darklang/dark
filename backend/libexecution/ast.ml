da404fedd5f86b9eef3475c6feb3408d29d65669
Ian Connolly <ian@connolly.io>
2019-01-24 11:55:08 -0800

    Make `match` expressions pattern-match on Result

--- backend/libexecution/ast.ml
+++ backend/libexecution/ast.ml
@@ -388,6 +388,18 @@ let rec exec
                   matches v (p, e)
               | _ ->
                   None )
+            | Filled (_, PConstructor ("Ok", [p])) ->
+              ( match dv with
+              | DResult (ResOk v) ->
+                  matches v (p, e)
+              | _ ->
+                  None )
+            | Filled (_, PConstructor ("Error", [p])) ->
+              ( match dv with
+              | DResult (ResError v) ->
+                  matches v (p, e)
+              | _ ->
+                  None )
             | Filled (_, PConstructor ("Nothing", [])) ->
                 if dv = DOption OptNothing then Some (e, []) else None
             | _ ->
