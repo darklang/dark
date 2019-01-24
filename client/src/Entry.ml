087f4d5c0119bdc29bf29558c4bc3ad52ec89541
Alice Q. Wong <alice@valour.me>
2019-01-24 11:15:57 -0800

    ocamlformat

--- client/src/Entry.ml
+++ client/src/Entry.ml
@@ -79,7 +79,8 @@ let submitOmniAction (pos : pos) (action : omniAction) : modification =
       let next = gid () in
       let tlid = gtlid () in
       RPC
-        ( [CreateDBWithBlankOr (tlid, pos, gid (), dbname); AddDBCol (tlid, next, gid ())]
+        ( [ CreateDBWithBlankOr (tlid, pos, gid (), dbname)
+          ; AddDBCol (tlid, next, gid ()) ]
         , FocusExact (tlid, next) )
   | NewHandler name ->
       let next = gid () in
