81b9a4da456cb748ede69a59cb2131091435eb2b
Paul Biggar <paul.biggar@gmail.com>
2019-01-26 13:07:23 -0800

    Remove old code

--- client/src/ViewRoutingTable.ml
+++ client/src/ViewRoutingTable.ml
@@ -5,34 +5,6 @@ open ViewUtils
 module B = Blank
 module TL = Toplevel
 
-type viewState = ViewUtils.viewState
-
-type htmlConfig = ViewBlankOr.htmlConfig
-
-let idConfigs = ViewBlankOr.idConfigs
-
-let fontAwesome = ViewUtils.fontAwesome
-
-let wc = ViewBlankOr.wc
-
-let nested = ViewBlankOr.nested
-
-let atom = ViewBlankOr.atom
-
-let keyword = ViewBlankOr.keyword
-
-type collapseVerbs =
-  | CollapseVerbs
-  | DontCollapseVerbs
-
-and showLink =
-  | ShowLink
-  | DontShowLink
-
-and showUndo =
-  | ShowUndo
-  | DontShowUndo
-
 let missingEventSpaceDesc : string = "Undefined"
 
 let missingEventRouteDesc : string = "Undefined"
@@ -58,105 +30,6 @@ and category =
 and somename =
   | Category of category
   | Entry of newentry
-
-(* ---------------------------------- *)
-(* Html *)
-(* ---------------------------------- *)
-let span (class_ : string) (subs : msg Html.html list) : msg Html.html =
-  Html.span [Html.class' class_] subs
-
-
-let text (class_ : string) (msg : string) : msg Html.html =
-  span class_ [Html.text msg]
-
-
-let div (class_ : string) (subs : msg Html.html list) : msg Html.html =
-  Html.div [Html.class' class_] subs
-
-
-type entry =
-  { name : string option
-  ; verbs : (string * pos) list
-  ; tlid : tlid }
-
-let spaceName (tl : toplevel) : string option =
-  match tl.data with TLHandler h -> h.spec.module_ |> B.toMaybe | _ -> None
-
-
-let splitBySpace (tls : toplevel list) : (string * toplevel list) list =
-  let spaceName_ tl =
-    tl
-    |> TL.asHandler
-    |> Option.map (fun x -> x.spec)
-    |> Option.map (fun x -> x.module_)
-    |> Option.andThen B.toMaybe
-    |> Option.withDefault missingEventSpaceDesc
-  in
-  tls
-  |> List.sortBy spaceName_
-  |> List.groupWhile (fun a b -> spaceName_ a = spaceName_ b)
-  |> List.map (fun hs ->
-         let space =
-           hs |> List.head |> deOption "splitBySpace" |> spaceName_
-         in
-         (space, hs) )
-
-
-let tl2entry (tls : toplevel list) : entry list =
-  tls
-  |> List.filterMap (fun tl ->
-         match TL.asHandler tl with
-         | Some h ->
-             Some (tl.pos, tl.id, h)
-         | None ->
-             None )
-  |> List.map (fun (pos, tlid, h) ->
-         { name =
-             (match h.spec.name with F (_, s) -> Some s | Blank _ -> None)
-         ; verbs =
-             ( match h.spec.modifier with
-             | F (_, s) ->
-                 [(s, pos)]
-             | Blank _ ->
-                 [("_", pos)] )
-         ; tlid } )
-  |> List.sortBy (fun c -> Option.withDefault "ZZZZZZ" c.name)
-
-
-let collapseByVerb (es : entry list) : entry list =
-  es
-  |> List.groupWhile (fun a b -> a.name = b.name)
-  |> List.map
-       (List.foldr
-          (fun curr list ->
-            if curr.name = None
-            then curr :: list
-            else
-              match list with
-              | [] ->
-                  [curr]
-              | prev :: rest ->
-                  let new_ = {prev with verbs = prev.verbs @ curr.verbs} in
-                  new_ :: rest )
-          [])
-  |> List.concat
-
-
-let ordering (a : string) (b : string) : int =
-  match (a, b) with
-  (* to the start *)
-  | "HTTP", _ ->
-      -1
-  | _, "HTTP" ->
-      1
-  (* to the end *)
-  | _ ->
-      if a = missingEventRouteDesc
-      then 1
-      else if b = missingEventRouteDesc
-      then -1
-      else compare a b
-
 
 let openAttr (m : model) (name : string) =
   if Tc.StrSet.has m.routingTableOpenDetails ~value:name
@@ -191,14 +64,6 @@ let buttonLink
   Html.a ([event; Html.class' "button-link"] @ href) [content]
 
 
-let undoButton (tlid : tlid) (page : page) : msg Html.html =
-  buttonLink
-    ~key:("rt" ^ showTLID tlid)
-    (text "undo" "Restore")
-    (RestoreToplevel tlid)
-    (Some page)
-
-
 let tlLink (pos : pos) (class_ : string) (name : string) : msg Html.html =
   Url.linkFor (Toplevels pos) class_ [Html.text name]
 
@@ -209,118 +74,6 @@ let fnLink (fn : userFunction) (isUsed : bool) (text_ : string) : msg Html.html
     (Fn (fn.ufTLID, Defaults.centerPos))
     (if isUsed then "default-link" else "default-link unused")
     [Html.text text_]
-
-
-(* TODO: refactor `addHandler` from weird tuple to better API for passing msg
- * + the cache key for the vdom *)
-let header (name : string) (count : int) (addHandler : (string * msg) option) :
-    msg Html.html =
-  Html.summary
-    [Html.class' "header"]
-    [ text "title" name
-    ; text "parens" "("
-    ; text "count" (count |> string_of_int)
-    ; text "parens" ")"
-    ; ( match addHandler with
-      | Some (key, msg) ->
-          buttonLink ~key (fontAwesome "plus-circle") msg None
-      | None ->
-          text "" "" ) ]
-
-
-let section
-    (m : model)
-    (name : string)
-    (entries : 'a list)
-    (addHandler : (string * msg) option)
-    (routes : msg Html.html) : msg Html.html =
-  if List.length entries = 0
-  then
-    Html.div
-      [Html.class' "routing-section empty"]
-      [header name 0 addHandler; routes]
-  else
-    Html.details
-      ([Html.class' "routing-section"] @ openAttr m name)
-      [header name (List.length entries) addHandler; routes]
-
-
-let viewGroup
-    (m : model)
-    (showLink : showLink)
-    (showUndo : showUndo)
-    ((spacename, entries) : string * entry list) : msg Html.html =
-  let def s = Option.withDefault missingEventRouteDesc s in
-  let externalLink h =
-    if showLink = ShowLink && List.member "GET" (List.map Tuple.first h.verbs)
-    then
-      match h.name with
-      | Some n ->
-          Html.a
-            [ Html.class' "external"
-            ; Html.href
-                ( "//"
-                ^ Tea.Http.encodeUri m.canvasName
-                ^ "."
-                ^ m.userContentHost
-                ^ n )
-            ; Html.target "_blank" ]
-            [fontAwesome "external-link-alt"]
-      | None ->
-          Html.div [] []
-    else Html.div [] []
-  in
-  let verbs e =
-    e.verbs
-    |> List.map (fun (verb, pos) -> tlLink pos "verb-link" verb)
-    |> List.intersperse (Html.text ",")
-  in
-  let entryHtml e =
-    let pos =
-      e.verbs |> List.head |> deOption "viewGroup/entryHtml" |> Tuple.second
-    in
-    div
-      "handler"
-      ( [ div "name" [Html.text (def e.name)]
-        ; div "extra" [span "verbs" (verbs e); externalLink e] ]
-      @ if showUndo = ShowUndo then [undoButton e.tlid (Toplevels pos)] else []
-      )
-  in
-  let routes = div "routes" (List.map entryHtml entries) in
-  let distinctEntries =
-    entries |> List.map (fun x -> x.verbs) |> List.concat
-  in
-  let button =
-    if spacename = "HTTP" && showUndo <> ShowUndo
-    then Some ("crh", CreateRouteHandler)
-    else None
-  in
-  section m spacename distinctEntries button routes
-
-
-let viewRoutes
-    (m : model)
-    (tls : toplevel list)
-    (spaceName : string)
-    (filterFunc : toplevel -> bool)
-    (collapse : collapseVerbs)
-    (showLink : showLink)
-    (showUndo : showUndo) : msg Html.html list =
-  (* if the list is empty, ensure we display a routing table entry anyway *)
-  let handleEmpty entries =
-    if List.isEmpty entries then [(spaceName, [])] else entries
-  in
-  tls
-  |> List.filter filterFunc
-  |> splitBySpace
-  |> handleEmpty
-  |> List.sortWith (fun (a, _) (b, _) -> ordering a b)
-  |> List.map (Tuple.mapSecond tl2entry)
-  |> (fun entries ->
-       if collapse = CollapseVerbs
-       then List.map (Tuple.mapSecond collapseByVerb) entries
-       else entries )
-  |> List.map (viewGroup m showLink showUndo)
 
 
 let httpCategory (_m : model) (tls : toplevel list) : category =
@@ -422,12 +175,27 @@ let undefinedCategory (_m : model) (tls : toplevel list) : category =
             ; verb = None } ) }
 
 
+let splitBySpace (tls : toplevel list) : (string * toplevel list) list =
+  let spaceName_ tl =
+    tl
+    |> TL.asHandler
+    |> Option.map (fun x -> x.spec.module_)
+    |> Option.andThen B.toMaybe
+    |> Option.withDefault missingEventSpaceDesc
+  in
+  tls
+  |> List.sortBy spaceName_
+  |> List.groupWhile (fun a b -> spaceName_ a = spaceName_ b)
+  |> List.map (fun hs ->
+         let space =
+           hs |> List.head |> deOption "splitBySpace" |> spaceName_
+         in
+         (space, hs) )
+
+
 let eventCategories (_m : model) (tls : toplevel list) : category list =
   let groups =
-    tls
-    |> Tc.List.filter ~f:TL.isCustomEventSpaceHandler
-    |> splitBySpace
-    |> List.sortWith (fun (a, _) (b, _) -> ordering a b)
+    tls |> Tc.List.filter ~f:TL.isCustomEventSpaceHandler |> splitBySpace
   in
   Tc.List.map groups ~f:(fun (name, handlers) ->
       { count = List.length handlers
@@ -551,73 +319,6 @@ let deletedCategory (m : model) : category =
   ; entries = Tc.List.map cats ~f:(fun c -> Category c) }
 
 
-let view404s_ (m : model) (f404s : fourOhFour list) : msg Html.html =
-  let fofToKey fof = fof.space ^ "-" ^ fof.path ^ "-" ^ fof.modifier in
-  let theCreateLink fof =
-    buttonLink
-      ~key:(fofToKey fof)
-      (fontAwesome "plus-circle")
-      (CreateHandlerFrom404 fof)
-      None
-  in
-  let theDeleteLink fof =
-    buttonLink
-      ~key:(fofToKey fof)
-      (fontAwesome "minus-circle")
-      (Delete404 fof)
-      None
-  in
-  let fofHtml ({space; path; modifier} as fof) =
-    div
-      "fof"
-      [ text "path" path
-      ; (if space = "HTTP" then text "" "" else text "space" space)
-      ; text "modifier" modifier
-      ; theCreateLink fof
-      ; theDeleteLink fof ]
-  in
-  let routes = div "404s" (List.map fofHtml f404s) in
-  section m "404s" f404s None routes
-
-
-let view404s m =
-  Cache.cache1 (fun f404s -> (f404s, m.routingTableOpenDetails)) (view404s_ m)
-
-
-let viewRestorableDBs (m : model) (tls : toplevel list) : msg Html.html =
-  let dbs =
-    tls
-    |> List.filter (fun tl -> TL.asDB tl <> None)
-    |> List.map (fun tl -> (tl.pos, tl.id, TL.asDB tl |> deOption "asDB"))
-    |> List.sortBy (fun (_, _, db) -> db.dbName)
-  in
-  let dbHtml (pos, tlid, db) =
-    div "simple-route" [text "name" db.dbName; undoButton tlid (Toplevels pos)]
-  in
-  let routes = div "dbs" (List.map dbHtml dbs) in
-  section m "DBs" dbs None routes
-
-
-let viewDBs_ (m : model) (tls : toplevel list) : msg Html.html =
-  let dbs =
-    tls
-    |> List.filter (fun tl -> TL.asDB tl <> None)
-    |> List.map (fun tl -> (tl.pos, TL.asDB tl |> deOption "asDB"))
-    |> List.sortBy (fun (_, db) -> db.dbName)
-  in
-  let dbHtml (pos, db) =
-    div "simple-route" [span "name" [tlLink pos "default-link" db.dbName]]
-  in
-  let routes = div "dbs" (List.map dbHtml dbs) in
-  section m "DBs" dbs None routes
-
-
-let viewDBs m =
-  Cache.cache1
-    (fun tls -> (TL.dbs tls, m.routingTableOpenDetails))
-    (viewDBs_ m)
-
-
 let deletedUserFunctionsEntries (m : model) : category =
   let fns =
     m.deletedUserFunctions
@@ -644,130 +345,7 @@ let deletedUserFunctionsEntries (m : model) : category =
   ; entries }
 
 
-let viewDeletedUserFunctions (m : model) : msg Html.html =
-  let fns =
-    m.deletedUserFunctions
-    |> List.filter (fun fn -> B.isF fn.ufMetadata.ufmName)
-  in
-  let fnNamedLink fn name = [span "name" [fnLink fn false name]] in
-  let fnHtml fn =
-    div
-      "simple-route"
-      (let fnName = B.asF fn.ufMetadata.ufmName in
-       match fnName with
-       | Some name ->
-           fnNamedLink fn name
-       | None ->
-           [span "name" [fnLink fn true "should be filtered by here"]])
-  in
-  let routes = div "fns" (List.map fnHtml fns) in
-  section m "Functions" fns None routes
-
-
-let viewUserFunctions_ (m : model) : msg Html.html =
-  let fns =
-    m.userFunctions |> List.filter (fun fn -> B.isF fn.ufMetadata.ufmName)
-  in
-  let fnNamedLink fn name =
-    let useCount = Refactor.countFnUsage m name in
-    if useCount = 0
-    then
-      [ span "name" [fnLink fn false name]
-      ; buttonLink
-          ~key:("duf-" ^ showTLID fn.ufTLID)
-          (fontAwesome "minus-circle")
-          (DeleteUserFunction fn.ufTLID)
-          None ]
-    else
-      let countedName = name ^ " (" ^ string_of_int useCount ^ ")" in
-      [span "name" [fnLink fn true countedName]]
-  in
-  let fnHtml fn =
-    div
-      "simple-route"
-      (let fnName = B.asF fn.ufMetadata.ufmName in
-       match fnName with
-       | Some name ->
-           fnNamedLink fn name
-       | None ->
-           [span "name" [fnLink fn true "should be filtered by here"]])
-  in
-  let routes = div "fns" (List.map fnHtml fns) in
-  section m "Functions" fns (Some ("cf", CreateFunction)) routes
-
-
-let viewUserFunctions =
-  Cache.cache1
-    (fun m -> (m.userFunctions, m.toplevels, m.routingTableOpenDetails))
-    viewUserFunctions_
-
-
-let viewDeletedTLs_ (m : model) : msg Html.html =
-  let tls = m.deletedToplevels in
-  let httpTLs =
-    viewRoutes
-      m
-      tls
-      "HTTP"
-      TL.isHTTPHandler
-      DontCollapseVerbs
-      DontShowLink
-      ShowUndo
-  in
-  let cronTLs =
-    viewRoutes
-      m
-      tls
-      "CRON"
-      TL.isCronHandler
-      DontCollapseVerbs
-      DontShowLink
-      ShowUndo
-  in
-  let customEventSpaceTLs =
-    viewRoutes
-      m
-      tls
-      "Custom Event Space"
-      TL.isCustomEventSpaceHandler
-      DontCollapseVerbs
-      DontShowLink
-      ShowUndo
-  in
-  let undefinedEventSpaceTLs =
-    viewRoutes
-      m
-      tls
-      "Undefined"
-      TL.isUndefinedEventSpaceHandler
-      DontCollapseVerbs
-      DontShowLink
-      ShowUndo
-  in
-  let dbs = viewRestorableDBs m tls in
-  let fns = viewDeletedUserFunctions m in
-  let count = List.length m.deletedUserFunctions + List.length tls in
-  let h = header "Deleted" count None in
-  Html.details
-    ([Html.class' "routing-section deleted"] @ openAttr m "deleted")
-    ( [h]
-    @ httpTLs
-    @ [dbs; fns]
-    @ cronTLs
-    @ customEventSpaceTLs
-    @ undefinedEventSpaceTLs )
-
-
-let viewDeletedTLs =
-  Cache.cache1
-    (fun m ->
-      (m.deletedToplevels, m.deletedUserFunctions, m.routingTableOpenDetails)
-      )
-    viewDeletedTLs_
-
-
 let entry2html (m : model) (e : newentry) : msg Html.html =
-  (* TODO: restore button *)
   let ext =
     Tc.Option.map
       ~f:(fun l -> ViewCode.externalLink l m.canvasName m.userContentHost)
@@ -828,6 +406,7 @@ let rec somename2html (m : model) (s : somename) : msg Html.html =
 
 
 and category2html (m : model) (c : category) : msg Html.html =
+  let text cl t = Html.span [Html.class' cl] [Html.text t] in
   let header =
     Html.summary
       [Html.class' "header"]
@@ -851,44 +430,6 @@ and category2html (m : model) (c : category) : msg Html.html =
 
 
 let viewRoutingTable_ (m : model) : msg Html.html =
-  let sections =
-    viewRoutes
-      m
-      m.toplevels
-      "HTTP"
-      TL.isHTTPHandler
-      CollapseVerbs
-      ShowLink
-      DontShowUndo
-    @ [viewDBs m m.toplevels]
-    @ [viewUserFunctions m]
-    @ viewRoutes
-        m
-        m.toplevels
-        "CRON"
-        TL.isCronHandler
-        CollapseVerbs
-        ShowLink
-        DontShowUndo
-    @ viewRoutes
-        m
-        m.toplevels
-        "Custom Event Space"
-        TL.isCustomEventSpaceHandler
-        CollapseVerbs
-        ShowLink
-        DontShowUndo
-    @ viewRoutes
-        m
-        m.toplevels
-        "Undefined"
-        TL.isUndefinedEventSpaceHandler
-        CollapseVerbs
-        ShowLink
-        DontShowUndo
-    @ [view404s m m.f404s]
-    @ [viewDeletedTLs m]
-  in
   let tls = m.toplevels |> Tc.List.sortBy ~f:(fun tl -> TL.sortkey tl) in
   let ufns =
     m.userFunctions
@@ -911,7 +452,7 @@ let viewRoutingTable_ (m : model) : msg Html.html =
             EnablePanning false )
       ; ViewUtils.eventNoPropagation ~key:"epf" "mouseleave" (fun _ ->
             EnablePanning true ) ]
-      (Tc.List.map ~f:(category2html m) cats @ sections)
+      (Tc.List.map ~f:(category2html m) cats)
   in
   Html.div [Html.id "sidebar-left"] [html]
 
