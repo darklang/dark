b1cafb23546a4ada012656f5137a804284162718
Paul Biggar <paul.biggar@gmail.com>
2019-01-28 17:23:53 -0800

    Add many more autocomplete types

--- client/src/Types.ml
+++ client/src/Types.ml
@@ -469,11 +469,16 @@ and autocompleteItem =
   | ACConstructorName of string
   | ACField of string
   | ACVariable of varName
-  | ACExtra of string
   | ACLiteral of literal
   | ACOmniAction of omniAction
   | ACKeyword of keyword
   | ACCommand of command
+  | ACHTTPModifier of string
+  | ACCronTiming of string
+  | ACEventSpace of string
+  | ACDBColType of string
+  | ACParamTipe of string
+  | ACExtra of string
 
 and target = tlid * pointerData
 
