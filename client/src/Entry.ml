7519a0df6bd97b11413dcf9ab48759d050cb0474
Paul Biggar <paul.biggar@gmail.com>
2019-01-29 11:45:43 -0800

    Add "jump to" to autocomplete

--- client/src/Entry.ml
+++ client/src/Entry.ml
@@ -123,6 +123,8 @@ let submitOmniAction (pos : pos) (action : omniAction) : modification =
         ; tlid }
       in
       RPC ([SetHandler (tlid, pos, handler)], FocusExact (tlid, next))
+  | Goto (page, _) ->
+      SetPage page
 
 
 type nextMove =
