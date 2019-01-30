774ec0352046bdbdbeb06c4f6a663a531fa90daf
Sam Stokes <me@samstokes.co.uk>
2019-01-30 15:35:22 -0800

    avoid unnecessary string duplication

--- stroller/src/service.rs
+++ stroller/src/service.rs
@@ -14,7 +14,7 @@ impl AsyncPush for r2d2::PooledConnection<push::PusherClientManager> {
     fn push(&mut self, canvas_uuid: &str, event_name: &str, json_bytes: &[u8]) {
         let event_name_ = event_name.to_string();
         let _ = spawn(
-            self.trigger(canvas_uuid.to_string(), event_name.to_string(), json_bytes)
+            self.trigger(canvas_uuid, event_name, json_bytes)
                 .map_err(move |e| {
                     // TODO unify error handling
                     eprintln!("failed to push event {}: {}", event_name_, e);
