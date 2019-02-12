cc3cb812f272de031b7f006a508bb727903d25dd
Paul Biggar <paul.biggar@gmail.com>
2019-02-12 13:48:43 -0800

    Tidy up types around TTraceFetcher
    s/rpcContext/traceFetchContext
    remove lots of contextFromModel

--- client/src/Analysis.ml
+++ client/src/Analysis.ml
@@ -256,6 +256,6 @@ module RequestAnalysis = struct
 end
 
 module RequestTraces = struct
-  external send : rpcContext * getAnalysisParams -> unit = "fetch"
+  external send : traceFetchContext * getAnalysisParams -> unit = "fetch"
     [@@bs.val] [@@bs.scope "window", "Dark", "traceFetcher"]
 end
