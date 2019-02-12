75b67d453906904e0907fccc5942b20406656ef1
Ian Connolly <ian@connolly.io>
2019-02-12 11:48:25 -0800

    Move Stroller to own module

---
+++ backend/libbackend/stroller.mli
@@ -0,0 +1,13 @@
+open Libexecution
+
+val push :
+  execution_id:Types.id -> canvas_id:Uuidm.t -> event:string -> string -> unit
+
+val push_new_trace_id :
+  execution_id:Types.id -> canvas_id:Uuidm.t -> Types.tlid -> Uuidm.t -> unit
+
+val push_new_404 :
+     execution_id:Types.id
+  -> canvas_id:Uuidm.t
+  -> Stored_event.four_oh_four
+  -> unit
