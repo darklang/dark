6ecdd8da46dd15b3e5333c4b3734afefed109d86
Ian Connolly <ian@connolly.io>
2019-02-12 11:48:25 -0800

    Move Stroller to own module

---
+++ backend/libbackend/stroller.ml
@@ -0,0 +1,62 @@
+open Core_kernel
+open Libexecution
+open Libcommon
+module Cl = Cohttp_lwt
+module Clu = Cohttp_lwt_unix
+module CResponse = Clu.Response
+
+let push
+    ~(execution_id : Types.id)
+    ~(canvas_id : Uuidm.t)
+    ~(event : string)
+    (payload : string) =
+  let canvas_id_str = Uuidm.to_string canvas_id in
+  let log_params = [("canvas_id", canvas_id_str); ("event", event)] in
+  match Config.stroller_port with
+  | None ->
+      Log.infO "stroller not configured, skipping push" ~params:log_params
+  | Some port ->
+      Log.infO "pushing via stroller" ~params:log_params ;
+      let uri =
+        Uri.make
+          ()
+          ~scheme:"http"
+          ~host:"127.0.0.1"
+          ~port
+          ~path:(sprintf "canvas/%s/events/%s" canvas_id_str event)
+      in
+      Lwt.async (fun () ->
+          try%lwt
+                let%lwt resp, _ =
+                  Clu.Client.post uri ~body:(Cl.Body.of_string payload)
+                in
+                let code =
+                  resp |> CResponse.status |> Cohttp.Code.code_of_status
+                in
+                Log.infO
+                  "pushed via stroller"
+                  ~params:(("status", string_of_int code) :: log_params) ;
+                Lwt.return ()
+          with e ->
+            let bt = Exception.get_backtrace () in
+            let%lwt _ =
+              Rollbar.report_lwt e bt (Push event) (Types.show_id execution_id)
+            in
+            Lwt.return () )
+
+
+let push_new_trace_id
+    ~(execution_id : Types.id)
+    ~(canvas_id : Uuidm.t)
+    (tlid : Types.tlid)
+    (trace_id : Uuidm.t) =
+  let payload = Analysis.to_new_trace_frontend (tlid, trace_id) in
+  push ~execution_id ~canvas_id ~event:"new_trace" payload
+
+
+let push_new_404
+    ~(execution_id : Types.id)
+    ~(canvas_id : Uuidm.t)
+    (fof : Stored_event.four_oh_four) =
+  let payload = Analysis.to_new_404_frontend fof in
+  push ~execution_id ~canvas_id ~event:"new_404" payload
