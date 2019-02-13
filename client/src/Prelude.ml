38c6ce8b00f3a6391c271fa95038dd296c838400
Alice Q. Wong <alice@valour.me>
2019-02-13 11:40:17 -0800

    selected,filling,dragging handlers should have a visual indicator in routing table

--- client/src/Prelude.ml
+++ client/src/Prelude.ml
@@ -49,8 +49,8 @@ let tlidOf (s : cursorState) : tlid option =
         Some tlid )
   | Deselected ->
       None
-  | Dragging (_, _, _, _) ->
-      None
+  | Dragging (tlid, _, _, _) ->
+      Some tlid
   | SelectingCommand (tlid, _) ->
       Some tlid
 
