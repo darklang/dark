d5727612a255692c38935a9b18578b020f141c69
Paul Biggar <paul.biggar@gmail.com>
2019-02-15 14:27:13 -0800

    Fix migration for manual rollout
    Run the index only if it doesnt exist, so that we can manually run it concurrently

--- backend/migrations/20190215_210006_add-trace-index-to-stored-events.sql
+++ backend/migrations/20190215_210006_add-trace-index-to-stored-events.sql
@@ -1,5 +1,4 @@
-CREATE UNIQUE INDEX stored_events_v2_traceid_uniq
+CREATE INDEX IF NOT EXISTS stored_events_v2_traceid 
 ON stored_events_v2
-(canvas_id, trace_id);
+(canvas_id, trace_id)
 
-ALTER TABLE stored_events_v2 ADD constraint stored_events_v2_traceid_uniq UNIQUE USING INDEX stored_events_v2_traceid_uniq
