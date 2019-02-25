7553ef3543ea67e10eddcb9d6699957e697b23bc
Ian Connolly <ian@connolly.io>
2019-02-25 13:09:19 -0800

    Log push_canvas_event errors to stderr

--- stroller/src/worker.rs
+++ stroller/src/worker.rs
@@ -7,7 +7,7 @@ use crate::config;
 use crate::push::PusherClient;
 use crate::service;
 
-pub fn run(channel: Receiver<service::CanvasEvent>, shutting_down: &Arc<AtomicBool>) -> () {
+pub fn run(channel: Receiver<service::CanvasEvent>, shutting_down: &Arc<AtomicBool>) {
     let mut client = PusherClient::new(
         &config::pusher_cluster(),
         &config::pusher_app_id(),
@@ -19,7 +19,10 @@ pub fn run(channel: Receiver<service::CanvasEvent>, shutting_down: &Arc<AtomicBo
         match channel.recv() {
             Ok((canvas_uuid, event_name, body)) => {
                 println!("{} {} {:?}", canvas_uuid, event_name, body);
-                client.push_canvas_event(&canvas_uuid, &event_name, &body);
+                let result = client.push_canvas_event(&canvas_uuid, &event_name, &body);
+                if let Err(e) = result {
+                    eprintln!("Error pushing to Pusher: {}", e);
+                }
             }
             Err(_) => {
                 println!("All senders dropped!");
