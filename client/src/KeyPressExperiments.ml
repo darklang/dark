bcc8eec0cf5b63e591201662658ef041e503c3a7
Ian Connolly <ian@connolly.io>
2019-02-28 11:56:56 -0800

    Remove unneccessary fluid branch

--- client/src/KeyPressExperiments.ml
+++ client/src/KeyPressExperiments.ml
@@ -149,38 +149,34 @@ let arrowMoveRight (m : model) (tlid : tlid) (mId : id option) : modification =
 
 let arrowMoveHandler (event : Keyboard.keyEvent) (m : model) :
     modification option =
-  match VariantTesting.variantIsActive m FluidInputModel with
-  | false ->
-      None
-  | true ->
-    ( match m.cursorState with
-    | Entering (Filling (tlid, id)) ->
-      ( match event.keyCode with
-      | Key.Up ->
-          if m.complete.visible
-          then Some (AutocompleteMod ACSelectUp)
-          else Some (arrowMoveUp m tlid (Some id))
-      | Key.Down ->
-          if m.complete.visible
-          then Some (AutocompleteMod ACSelectDown)
-          else Some (arrowMoveDown m tlid (Some id))
-      | Key.Right ->
-          if event.metaKey && event.shiftKey
-          then Some (Select (tlid, Some id))
-          else Some (arrowMoveRight m tlid (Some id))
-      | Key.Left ->
-          if event.metaKey && event.shiftKey
-          then Some (Select (tlid, Some id))
-          else Some (arrowMoveLeft m tlid (Some id))
-      | Key.Escape ->
-          Some NoChange
-      | _ ->
-          None )
-    | Selecting (tlid, Some id) ->
-      ( match event.keyCode with
-      | Key.Escape ->
-          Some (Enter (Filling (tlid, id)))
-      | _ ->
-          None )
+  match m.cursorState with
+  | Entering (Filling (tlid, id)) ->
+    ( match event.keyCode with
+    | Key.Up ->
+        if m.complete.visible
+        then Some (AutocompleteMod ACSelectUp)
+        else Some (arrowMoveUp m tlid (Some id))
+    | Key.Down ->
+        if m.complete.visible
+        then Some (AutocompleteMod ACSelectDown)
+        else Some (arrowMoveDown m tlid (Some id))
+    | Key.Right ->
+        if event.metaKey && event.shiftKey
+        then Some (Select (tlid, Some id))
+        else Some (arrowMoveRight m tlid (Some id))
+    | Key.Left ->
+        if event.metaKey && event.shiftKey
+        then Some (Select (tlid, Some id))
+        else Some (arrowMoveLeft m tlid (Some id))
+    | Key.Escape ->
+        Some NoChange
     | _ ->
         None )
+  | Selecting (tlid, Some id) ->
+    ( match event.keyCode with
+    | Key.Escape ->
+        Some (Enter (Filling (tlid, id)))
+    | _ ->
+        None )
+  | _ ->
+      None
