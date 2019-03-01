20074f41f8e9fd7a88c5884b479630c4f0450a2c
Ian Smith <ismith@metaforgotten.org>
2019-03-01 11:12:05 -0800

    fix migration

--- backend/migrations/20190301_010734_add-canvas_id-branch-live_at-created_at-idx-to-static_asset_deploys.sql
+++ backend/migrations/20190301_010734_add-canvas_id-branch-live_at-created_at-idx-to-static_asset_deploys.sql
@@ -1,2 +1,2 @@
 CREATE INDEX idx_static_asset_deploys_canvas_id_branch_live_at_created_at
-ON idx_static_asset_deploys (canvas_id, branch, live_at, created_at);
+ON idx_static_asset_deploys (canvas_id, branch, live_at, created_at)
