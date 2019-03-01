6a4b11654091163ed3d7f9bb49229848300a2da0
Ian Smith <ismith@metaforgotten.org>
2019-02-28 16:58:04 -0800

    wrap in DResult

--- backend/libbackend/libstatic_assets.ml
+++ backend/libbackend/libstatic_assets.ml
@@ -34,81 +34,118 @@ let replacements =
     , InProcess
         (function
         | state, [DStr deploy_hash; DStr file] ->
-            let url =
-              url_for
-                state.canvas_id
-                (Unicode_string.to_string deploy_hash)
-                `Short
-                (Unicode_string.to_string file)
-            in
-            Httpclient.call url Httpclient.GET [] "" |> Dval.dstr_of_string_exn
+          ( try
+              let url =
+                url_for
+                  state.canvas_id
+                  (Unicode_string.to_string deploy_hash)
+                  `Short
+                  (Unicode_string.to_string file)
+              in
+              Httpclient.call url Httpclient.GET [] ""
+              |> Dval.dstr_of_string_exn
+              |> ResOk
+              |> DResult
+            with e ->
+              DResult
+                (ResError
+                   ( Dval.dstr_of_string "Response was not
+UTF-8 safe"
+                   |> Core_kernel.Option.value_exn )) )
         | args ->
             Libexecution.Lib.fail args) )
   ; ( "StaticAssets::fetchLatest"
     , InProcess
         (function
         | state, [DStr file] ->
-            let url =
-              url_for
-                state.canvas_id
-                (latest_deploy_hash state.canvas_id)
-                `Short
-                (Unicode_string.to_string file)
-            in
-            Httpclient.call url Httpclient.GET [] "" |> Dval.dstr_of_string_exn
+          ( try
+              let url =
+                url_for
+                  state.canvas_id
+                  (latest_deploy_hash state.canvas_id)
+                  `Short
+                  (Unicode_string.to_string file)
+              in
+              Httpclient.call url Httpclient.GET [] ""
+              |> Dval.dstr_of_string_exn
+              |> ResOk
+              |> DResult
+            with e ->
+              DResult
+                (ResError
+                   ( Dval.dstr_of_string "Response was not UTF-8 safe"
+                   |> Core_kernel.Option.value_exn )) )
         | args ->
             Libexecution.Lib.fail args) )
   ; ( "StaticAssets::serve"
     , InProcess
         (function
         | state, [DStr deploy_hash; DStr file] ->
-            let url =
-              url_for
-                state.canvas_id
-                (Unicode_string.to_string deploy_hash)
-                `Short
-                (Unicode_string.to_string file)
-            in
-            let body, code, headers =
-              Httpclient.http_call_with_code url [] Httpclient.GET [] ""
-            in
-            let headers =
-              headers
-              |> List.map (fun (k, v) -> (k, String.trim v))
-              |> List.filter (fun (k, v) ->
-                     not
-                       (Core_kernel.String.is_substring
-                          k
-                          ~substring:"Transfer-Encoding") )
-              |> List.filter (fun (k, v) -> not (String.trim k = ""))
-            in
-            DResp (Response (code, headers), Dval.dstr_of_string_exn body)
+          ( try
+              let url =
+                url_for
+                  state.canvas_id
+                  (Unicode_string.to_string deploy_hash)
+                  `Short
+                  (Unicode_string.to_string file)
+              in
+              let body, code, headers =
+                Httpclient.http_call_with_code url [] Httpclient.GET [] ""
+              in
+              let headers =
+                headers
+                |> List.map (fun (k, v) -> (k, String.trim v))
+                |> List.filter (fun (k, v) ->
+                       not
+                         (Core_kernel.String.is_substring
+                            k
+                            ~substring:"Transfer-Encoding") )
+                |> List.filter (fun (k, v) -> not (String.trim k = ""))
+              in
+              DResult
+                (ResOk
+                   (DResp
+                      (Response (code, headers), Dval.dstr_of_string_exn body)))
+            with e ->
+              DResult
+                (ResError
+                   ( Dval.dstr_of_string "Response was not UTF-8 safe"
+                   |> Core_kernel.Option.value_exn )) )
         | args ->
             Libexecution.Lib.fail args) )
   ; ( "StaticAssets::serveLatest"
     , InProcess
         (function
         | state, [DStr file] ->
-            let url =
-              url_for
-                state.canvas_id
-                (latest_deploy_hash state.canvas_id)
-                `Short
-                (Unicode_string.to_string file)
-            in
-            let body, code, headers =
-              Httpclient.http_call_with_code url [] Httpclient.GET [] ""
-            in
-            let headers =
-              headers
-              |> List.map (fun (k, v) -> (k, String.trim v))
-              |> List.filter (fun (k, v) ->
-                     not
-                       (Core_kernel.String.is_substring
-                          k
-                          ~substring:"Transfer-Encoding") )
-              |> List.filter (fun (k, v) -> not (String.trim k = ""))
-            in
-            DResp (Response (code, headers), Dval.dstr_of_string_exn body)
+          ( try
+              let url =
+                url_for
+                  state.canvas_id
+                  (latest_deploy_hash state.canvas_id)
+                  `Short
+                  (Unicode_string.to_string file)
+              in
+              let body, code, headers =
+                Httpclient.http_call_with_code url [] Httpclient.GET [] ""
+              in
+              let headers =
+                headers
+                |> List.map (fun (k, v) -> (k, String.trim v))
+                |> List.filter (fun (k, v) ->
+                       not
+                         (Core_kernel.String.is_substring
+                            k
+                            ~substring:"Transfer-Encoding") )
+                |> List.filter (fun (k, v) -> not (String.trim k = ""))
+              in
+              DResult
+                (ResOk
+                   (DResp
+                      (Response (code, headers), Dval.dstr_of_string_exn body)))
+            with e ->
+              DResult
+                (ResError
+                   ( Dval.dstr_of_string "Response was not UTF-8 safe"
+                   |> Core_kernel.Option.value_exn )) )
         | args ->
             Libexecution.Lib.fail args) ) ]
