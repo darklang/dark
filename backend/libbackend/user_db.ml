ed9b22e72de4f97c6e674ee4e058d0d63f1696a8
Paul Biggar <paul.biggar@gmail.com>
2019-03-03 19:32:56 -0800

    Remove unsafe_dval_of_json_string

--- backend/libbackend/user_db.ml
+++ backend/libbackend/user_db.ml
@@ -159,7 +159,7 @@ and (* PG returns lists of strings. This converts them to types using the
   match db_strings with
   | [obj] ->
       let p_obj =
-        match Dval.unsafe_dval_of_json_string obj with
+        match Dval.of_internal_queryable_v0 obj with
         | DObj o ->
             (* <HACK>: some legacy objects were allowed to be saved with `id` keys _in_ the
          * data object itself. they got in the database on the `update` of
