57a94c9fe37477aaf35a1e7fa7a700e2dc2aef5b
Paul Biggar <paul.biggar@gmail.com>
2019-03-07 23:16:04 -0800

    Better names for legacy functions

--- backend/libexecution/legacy.mli
+++ backend/libexecution/legacy.mli
@@ -1,13 +1,13 @@
-module PrettyJsonV0 : sig
+module PrettyResponseJsonV0 : sig
   (* Original function for sending json back to the user. This uses an object
     * format for dates. *)
-  val to_pretty_machine_json_v0 : Types.RuntimeT.dval -> string
+  val to_pretty_response_json_v0 : Types.RuntimeT.dval -> string
 end
 
-module PrettyJsonV0a : sig
+module PrettyRequestJsonV0 : sig
   (* Original function for making JSON HTTP requests. This uses an odd (and
    * invalid) format for dates ("<Date: datestr>"), as well as DBs, Errros,
    * UUIDs and IDs,wraps characters in single quotes (invalid json), errors
    * on results, includes the string "Nothing/Just" for options *)
-  val to_pretty_machine_json_v0a : Types.RuntimeT.dval -> string
+  val to_pretty_request_json_v0 : Types.RuntimeT.dval -> string
 end
