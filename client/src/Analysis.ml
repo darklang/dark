1dfb0a8a2d68418ff389036ba60151bd9932a2c4
Ian Connolly <ian@connolly.io>
2019-03-19 14:52:07 -0700

    Don't request traces for user tipes

--- client/src/Analysis.ml
+++ client/src/Analysis.ml
@@ -296,9 +296,9 @@ let contextFromModel (m : model) : traceFetchContext =
 
 let requestTrace ?(force = false) m tlid traceID : model * msg Cmd.t =
   let should =
-    (* DBs dont have traces *)
+    (* DBs + Types dont have traces *)
     TL.get m tlid
-    |> Option.map ~f:(not << TL.isDB)
+    |> Option.map ~f:(fun tl -> not (TL.isDB tl || TL.isUserTipe tl))
     |> Option.withDefault ~default:false
   in
   if should
