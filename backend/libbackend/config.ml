f1b0bfb66f089dc041f13011f4dc149e3162d20d
Ian Smith <ismith@metaforgotten.org>
2019-03-20 13:05:02 -0700

    Remove unused logging formats - stackdriver and regular

--- backend/libbackend/config.ml
+++ backend/libbackend/config.ml
@@ -114,17 +114,11 @@ let create_accounts = bool "DARK_CONFIG_CREATE_ACCOUNTS"
 (* ------------------------- *)
 (* Logs *)
 (* ------------------------- *)
-let log_format : [`Stackdriver | `Regular | `Json | `Decorated] =
+let log_format : [`Json | `Decorated] =
   let as_str =
-    string_choice
-      "DARK_CONFIG_LOGGING_FORMAT"
-      ["stackdriver"; "regular"; "json"; "decorated"]
+    string_choice "DARK_CONFIG_LOGGING_FORMAT" ["json"; "decorated"]
   in
   match as_str with
-  | "stackdriver" ->
-      `Stackdriver
-  | "regular" ->
-      `Regular
   | "json" ->
       `Json
   | "decorated" ->
