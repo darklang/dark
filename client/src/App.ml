6fe9db1a3c0be1b090beb938182d7a296b972bc3
Paul Biggar <paul.biggar@gmail.com>
2019-03-22 21:32:04 -0700

    Don't show timeouts for UnlockedDBs call

--- client/src/App.ml
+++ client/src/App.ml
@@ -369,7 +369,7 @@ let rec updateMod (mod_ : modification) ((m, cmd) : model * msg Cmd.t) :
           | Http.BadUrl _ ->
               true
           | Http.Timeout ->
-              true
+              not ignoreCommon
           | Http.NetworkError ->
               not ignoreCommon
           | Http.BadStatus response ->
