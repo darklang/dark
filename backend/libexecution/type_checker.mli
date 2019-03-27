b9e0933525dff1ea08da886c9cf5a4b492cfadbf
Ian Connolly <ian@connolly.io>
2019-03-27 15:34:37 -0700

    Functioning but hacky type-checker

--- backend/libexecution/type_checker.mli
+++ backend/libexecution/type_checker.mli
@@ -7,8 +7,5 @@ type type_error =
   { expected : tipe
   ; actual : tipe }
 
-val unify :
-  type_env:user_tipe list -> tipe -> dval -> (unit, type_error) Result.t
-
 val check_function_call :
   type_env:user_tipe list -> fn -> dval_map -> (unit, type_error list) Result.t
