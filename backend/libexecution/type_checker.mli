05381893313dc1e887436c6796463f08ddd8c84a
Ian Connolly <ian@connolly.io>
2019-03-28 11:14:52 -0700

    Add error module

--- backend/libexecution/type_checker.mli
+++ backend/libexecution/type_checker.mli
@@ -3,12 +3,15 @@ open Libcommon
 open Types
 open Types.RuntimeT
 
-type type_error =
-  { expected : tipe
-  ; actual : tipe }
+module Error : sig
+  type t =
+    | TypeLookupFailure of string * int
+    | TypeUnificationFailure of {expected_tipe : tipe; actual_value : dval}
+    | MismatchedRecordFields of
+        { expected_fields : String.Set.t
+        ; actual_fields : String.Set.t }
+    | InvalidDefinition of user_record_field list
+end
 
 val check_function_call :
-     user_tipes:user_tipe list
-  -> fn
-  -> dval_map
-  -> (unit, type_error list) Result.t
+  user_tipes:user_tipe list -> fn -> dval_map -> (unit, Error.t list) Result.t
