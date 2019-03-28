79a3b68e2ce037a0c7d4ccd3ddf6128d8d80f2de
Ian Connolly <ian@connolly.io>
2019-03-28 12:06:18 -0700

    Add comments to error variants

--- backend/libexecution/type_checker.mli
+++ backend/libexecution/type_checker.mli
@@ -5,8 +5,16 @@ open Types.RuntimeT
 
 module Error : sig
   type t =
+    (* Failed to find a TUserType (name, version) in our type environment.
+     * Potentially a type was deleted and we didn't clean-up every use
+     * (or a race condition! *)
     | TypeLookupFailure of string * int
+    (* Type error in _values_, ie. expected a string but got an Int *)
     | TypeUnificationFailure of {expected_tipe : tipe; actual_value : dval}
+    (* Type error between a user record definition and the actual object
+      * received, specifically in its keys. ie expected {a : *, b : * } but
+      * got { a : *, b : *, c : *} -- note we currently expect an _exact_
+      * match and do not support structural subtyping. *)
     | MismatchedRecordFields of
         { expected_fields : String.Set.t
         ; actual_fields : String.Set.t }
