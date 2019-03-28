dd6cadd36f569ac3a855ee6046a1162b566024a0
Alice Q. Wong <alice@valour.me>
2019-03-28 09:53:43 -0700

    rm helper code

--- client/src/View.ml
+++ client/src/View.ml
@@ -53,11 +53,7 @@ let viewTL_ (m : model) (tl : toplevel) : msg Html.html =
     ; ViewUtils.eventNoPropagation
         ~key:("tlc-" ^ showTLID tl.id)
         "click"
-        (fun x -> ToplevelClick (tl.id, x))
-    ; ViewUtils.eventNoPropagation
-        ~key:("tldblc-" ^ showTLID vs.tl.id)
-        "dblclick"
-        (fun _ -> AnalyzeCode vs.tl.id) ]
+        (fun x -> ToplevelClick (tl.id, x)) ]
   in
   let selected = Some tl.id = tlidOf m.cursorState in
   let boxClasses =
