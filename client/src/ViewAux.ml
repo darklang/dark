42a54c7c52bfcd8d003af8765c74a1f5460b1afb
Alice Q. Wong <alice@valour.me>
2019-04-02 15:41:12 -0700

    dont overlap

--- client/src/ViewAux.ml
+++ client/src/ViewAux.ml
@@ -6,11 +6,13 @@ let rDBColView (cols : dBColumn list) : msg Html.html =
   let colView col =
     match col with
     | F (_, nm), F (_, ty) ->
-        Some
-          (Html.div
-             [Html.class' "col"]
-             [ Html.div [Html.class' "name"] [Html.text nm]
-             ; Html.div [Html.class' "type"] [Html.text ty] ])
+        let html =
+          Html.div
+            [Html.class' "col"]
+            [ Html.div [Html.class' "name"] [Html.text nm]
+            ; Html.div [Html.class' "type"] [Html.text ty] ]
+        in
+        Some html
     | _ ->
         None
   in
@@ -18,41 +20,46 @@ let rDBColView (cols : dBColumn list) : msg Html.html =
 
 
 let referenceViews (refs : tlReference list) : msg Html.html list =
-  let linkToDB tlid =
-    ViewUtils.eventNoPropagation
-      ~key:("ref-db-link" ^ showTLID tlid)
-      "click"
-      (fun _ -> GoTo (FocusedDB tlid))
-  and linkToEmit tlid =
-    ViewUtils.eventNoPropagation
-      ~key:("ref-emit-link" ^ showTLID tlid)
-      "click"
-      (fun _ -> GoTo (FocusedHandler tlid))
-  and calcTopOffset id =
-    match Native.Ext.querySelector (".id-" ^ showID id) with
-    | Some e ->
-        Native.Ext.offsetTop e
+  let topOffset =
+    match List.head refs with
+    | Some r ->
+        let id =
+          match r with
+          | OutReferenceDB (_, _, _, id) ->
+              id
+          | OutReferenceHandler (_, _, _, _, id) ->
+              id
+        in
+        let el = Native.Ext.querySelector (".id-" ^ showID id) in
+        (match el with Some e -> Native.Ext.offsetTop e | None -> 0)
     | None ->
         0
-  in
-  let renderDB tlid id name cols =
+  and renderDB tlid name cols =
     Html.div
       [ Html.class' "tlref db"
-      ; Html.styles [("top", string_of_int (calcTopOffset id) ^ "px")]
-      ; linkToDB tlid ]
+      ; ViewUtils.eventNoPropagation
+          ~key:("ref-db-link" ^ showTLID tlid)
+          "click"
+          (fun _ -> GoTo (FocusedDB tlid)) ]
       [Html.span [Html.class' "dbtitle"] [Html.text name]; rDBColView cols]
-  and renderEvent tlid id space name =
+  and renderEvent tlid space name =
     Html.div
       [ Html.class' "tlref emit"
-      ; Html.styles [("top", string_of_int (calcTopOffset id) ^ "px")]
-      ; linkToEmit tlid ]
+      ; ViewUtils.eventNoPropagation
+          ~key:("ref-emit-link" ^ showTLID tlid)
+          "click"
+          (fun _ -> GoTo (FocusedHandler tlid)) ]
       [ Html.div [Html.class' "spec"] [Html.text space]
       ; Html.div [Html.class' "spec"] [Html.text name] ]
   in
-  refs
-  |> List.map ~f:(fun r ->
-         match r with
-         | OutReferenceDB (tlid, name, cols, id) ->
-             renderDB tlid id name cols
-         | OutReferenceHandler (tlid, space, _, name, id) ->
-             renderEvent tlid id space name )
+  let renderView r =
+    match r with
+    | OutReferenceDB (tlid, name, cols, _) ->
+        renderDB tlid name cols
+    | OutReferenceHandler (tlid, space, _, name, _) ->
+        renderEvent tlid space name
+  in
+  [ Html.div
+      [ Html.class' "references"
+      ; Html.styles [("top", string_of_int topOffset ^ "px")] ]
+      (List.map ~f:renderView refs) ]
