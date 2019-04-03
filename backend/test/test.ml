88357d28bae29ca95b2b393aafa731cd0ac47bd2
Ian Smith <ismith@metaforgotten.org>
2019-04-03 11:49:48 -0700

    Remove case instead of changing it

--- backend/test/test.ml
+++ backend/test/test.ml
@@ -221,7 +221,6 @@ let sample_dvals =
   ; ("true", DBool true)
   ; ("false", DBool false)
   ; ("null", DNull)
-  ; ("char", DCharacter (Unicode_string.Character.unsafe_of_string "c"))
   ; ("string", Dval.dstr_of_string_exn "incredibly this was broken")
   ; ("list", DList [DDB "Visitors"; DInt 4])
   ; ("obj", DObj (DvalMap.of_alist_exn [("foo", DInt 5)]))
