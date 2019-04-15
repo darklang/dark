a23426f1683bd4ea7eb5d6fd8c156fe8c9416113
Korede Aderele <kaderele@gmail.com>
2019-04-15 16:13:41 -0700

    disable single record field delete button

--- client/src/ViewUserType.ml
+++ client/src/ViewUserType.ml
@@ -51,12 +51,16 @@ let viewKillFieldBtn (t : userTipe) (field : userRecordField) : msg Html.html =
     [fontAwesome "times-circle"]
 
 
-let viewTipeField (vs : viewState) (t : userTipe) (field : userRecordField) :
+let viewTipeField
+    (vs : viewState) (t : userTipe) (fieldCount : int) (field : userRecordField) :
     msg Html.html =
+  let button =
+    if fieldCount > 1 then viewKillFieldBtn t field else Vdom.noNode
+  in
   let row =
     [ viewFieldName vs [wc "name"] field.urfName
     ; viewFieldType vs [wc "type"] field.urfTipe
-    ; viewKillFieldBtn t field ]
+    ; button ]
   in
   Html.div [Html.class' "field"] row
 
@@ -65,6 +69,9 @@ let viewUserTipe (vs : viewState) (t : userTipe) : msg Html.html =
   match t.utDefinition with UTRecord fields ->
     let nameDiv = viewTipeName vs t in
     let fieldDivs =
-      Html.div [Html.class' "fields"] (List.map ~f:(viewTipeField vs t) fields)
+      let fieldCount = List.length fields in
+      Html.div
+        [Html.class' "fields"]
+        (List.map ~f:(viewTipeField vs t fieldCount) fields)
     in
     Html.div [Html.class' "user-type"] [nameDiv; fieldDivs]
