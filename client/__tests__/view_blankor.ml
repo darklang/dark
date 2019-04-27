1c3e59f9ef0a5040d2590707498f394441034277
Korede Aderele <kaderele@gmail.com>
2019-04-26 17:30:00 -0700

    add basic tests

---
+++ client/__tests__/view_blankor.ml
@@ -0,0 +1,61 @@
+open! Tc
+open Prelude
+open Types
+open Jest
+open Expect
+open ViewBlankOr
+
+let () =
+  describe "placeholdersDisplayed" (fun () ->
+      test "shows placeholders in user functions" (fun () ->
+        let id = gid () in
+        let ast = Blank.newF (FnCall (F (id,"Int::add_v0"), [], NoRail)) in
+        let tlFunc = TLFunc { ufAST = ast; ufTLID = gtlid (); ufMetadata = {
+            ufmName = Blank (gid ())
+            ; ufmParameters = []
+            ; ufmDescription = ""
+            ; ufmReturnTipe = Blank (gid ())
+            ; ufmInfix = false
+        } } in
+        let tl = { id = gtlid (); data = tlFunc; pos = {x = 0; y = 0} } in
+        let vs : ViewUtils.viewState = 
+        { tl = tl
+        ; cursorState = Deselected
+        ; tlid = gtlid () 
+        ; hovering = None
+        ; ac = { functions = []
+          ; admin = false
+          ; completions = []
+          ; invalidCompletions = []
+          ; allCompletions = []
+          ; index = -1
+          ; value = ""
+          ; prevValue = ""
+          ; target = None
+          ; targetDval = None
+          ; isCommandMode = false
+          ; visible = true }        
+        ; handlerSpace = Toplevel.spaceOf tl |> Option.withDefault ~default:HSOther
+        ; showEntry = false
+        ; showLivevalue = false
+        ; dbLocked = false
+        ; currentResults = { liveValues = StrDict.empty } 
+        ; traces = [] 
+        ; analyses = StrDict.empty
+        ; ufns = [] 
+        ; relatedBlankOrs = []
+        ; tooWide = false 
+        ; executingFunctions = []
+        ; tlCursors = StrDict.empty
+        ; testVariants = [] 
+        ; featureFlags = StrDict.empty
+        ; handlerProp = None
+        ; canvasName = ""
+        ; userContentHost = ""
+        ; inReferences = []
+        ; toReferences = [] 
+        ; usagesOfHoveredReference = [] } in
+
+          expect (placeHolderFor vs id FnName) |> toBe "function name") ;
+      () ) ;
+  ()
