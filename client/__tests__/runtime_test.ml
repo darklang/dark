ecb5cd9748c8bff9e5a7b7130c96473b21c7b398
Paul Biggar <paul.biggar@gmail.com>
2019-04-30 10:44:21 -0700

    Refactor tests

--- client/__tests__/runtime_test.ml
+++ client/__tests__/runtime_test.ml
@@ -11,61 +11,48 @@ type ('a, 'b) transformation_test_result =
 
 let () =
   describe "validStringLiteral" (fun () ->
-      test "string with newline is valid" (fun () ->
-          expect (isValidStringLiteral "\\n") |> toEqual true ) ;
-      test "string with carriage return is valid" (fun () ->
-          expect (isValidStringLiteral "\\t") |> toEqual true ) ;
-      test "string with tab is valid" (fun () ->
-          expect (isValidStringLiteral "\\r") |> toEqual true ) ;
-      test "string with escaped backslash is valid" (fun () ->
-          expect (isValidStringLiteral "\\\\") |> toEqual true ) ;
-      test "string with naked backslash is invalid" (fun () ->
-          expect (isValidStringLiteral "\\") |> toEqual false ) ;
-      test "string with invalid backslashed char is invalid" (fun () ->
-          expect (isValidStringLiteral "\\6") |> toEqual false ) ;
+      let t name subject expected =
+        test name (fun () ->
+            expect (isValidStringLiteral subject) |> toEqual expected )
+      in
+      t "newline" "\\n" true ;
+      t "carriage return" "\\t" true ;
+      t "tab" "\\r" true ;
+      t "escaped backslash" "\\\\" true ;
+      t "escaped quote" "\\\"" true ;
+      t "naked backslash" "\\" false ;
+      t "invalid backslashed char" "\\6" false ;
       () ) ;
-  describe "convertLiteralStringToDisplay " (fun () ->
-      test "string with newline is valid" (fun () ->
-          expect (convertLiteralStringToDisplay "\\n") |> toEqual "\\\\n" ) ;
-      test "string with newline is valid2" (fun () ->
-          expect (convertLiteralStringToDisplay "asd\\nqwe")
-          |> toEqual "asd\\\\nqwe" ) ;
-      test "string with carriage return is valid" (fun () ->
-          expect (convertLiteralStringToDisplay "\\t") |> toEqual "\\\\t" ) ;
-      test "string with carriage return is valid2" (fun () ->
-          expect (convertLiteralStringToDisplay "asd\\tqwe")
-          |> toEqual "asd\\\\tqwe" ) ;
-      test "string with tab is valid" (fun () ->
-          expect (convertLiteralStringToDisplay "\\r") |> toEqual "\\\\r" ) ;
-      test "string with tab is valid2" (fun () ->
-          expect (convertLiteralStringToDisplay "asd\\rqwe")
-          |> toEqual "asd\\\\rqwe" ) ;
-      test "string with escaped backslash is valid" (fun () ->
-          expect (convertLiteralStringToDisplay "\\") |> toEqual "\\\\" ) ;
-      test "string with escaped backslash is valid2" (fun () ->
-          expect (convertLiteralStringToDisplay "asd\\qwe")
-          |> toEqual "asd\\\\qwe" ) ;
+  describe "convertLiteralStringToDisplay" (fun () ->
+      let t name subject expected =
+        test name (fun () ->
+            expect (convertLiteralStringToDisplay subject) |> toEqual expected
+        )
+      in
+      t "newline" "\\n" "\\\\n" ;
+      t "newline2" "asd\\nqwe" "asd\\\\nqwe" ;
+      t "carriage return" "\\t" "\\\\t" ;
+      t "carriage return2" "asd\\tqwe" "asd\\\\tqwe" ;
+      t "tab" "\\r" "\\\\r" ;
+      t "tab2" "asd\\rqwe" "asd\\\\rqwe" ;
+      t "escaped backslash" "\\" "\\\\" ;
+      t "escaped backslash2" "asd\\qwe" "asd\\\\qwe" ;
+      t "escaped quote" "\"" "\\\"" ;
+      t "escaped quote2" "asd\"qwe" "asd\\\"qwe" ;
       () ) ;
   describe "convertDisplayStringToLiteral " (fun () ->
-      test "string with newline is valid" (fun () ->
-          expect (convertDisplayStringToLiteral "\\n") |> toEqual "\n" ) ;
-      test "string with newline is valid2" (fun () ->
-          expect (convertDisplayStringToLiteral "asd\\nqwe")
-          |> toEqual "asd\nqwe" ) ;
-      test "string with carriage return is valid" (fun () ->
-          expect (convertDisplayStringToLiteral "\\t") |> toEqual "\t" ) ;
-      test "string with carriage return is valid2" (fun () ->
-          expect (convertDisplayStringToLiteral "asd\\tqwe")
-          |> toEqual "asd\tqwe" ) ;
-      test "string with tab is valid" (fun () ->
-          expect (convertDisplayStringToLiteral "\\r") |> toEqual "\r" ) ;
-      test "string with tab is valid2" (fun () ->
-          expect (convertDisplayStringToLiteral "asd\\rqwe")
-          |> toEqual "asd\rqwe" ) ;
-      test "string with escaped backslash is valid" (fun () ->
-          expect (convertDisplayStringToLiteral "\\\\") |> toEqual "\\" ) ;
-      test "string with escaped backslash is valid2" (fun () ->
-          expect (convertDisplayStringToLiteral "asd\\\\qwe")
-          |> toEqual "asd\\qwe" ) ;
+      let t name subject expected =
+        test name (fun () ->
+            expect (convertDisplayStringToLiteral subject) |> toEqual expected
+        )
+      in
+      t "newline" "\\n" "\n" ;
+      t "newline2" "asd\\nqwe" "asd\nqwe" ;
+      t "carriage return" "\\t" "\t" ;
+      t "carriage return2" "asd\\tqwe" "asd\tqwe" ;
+      t "tab2" "\\r" "\r" ;
+      t "tab2 " "asd\\rqwe" "asd\rqwe" ;
+      t "escaped backslash" "\\\\" "\\" ;
+      t "escaped backslash2" "asd\\\\qwe" "asd\\qwe" ;
       () ) ;
   ()
