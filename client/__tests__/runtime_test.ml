73b3ec6d280304eb5c28de76fd03444c2bead0a4
Paul Biggar <paul.biggar@gmail.com>
2019-04-29 22:18:28 -0700

    Add functions to convert between literal strings and display strings
    Slash hell

--- client/__tests__/runtime_test.ml
+++ client/__tests__/runtime_test.ml
@@ -10,7 +10,7 @@ type ('a, 'b) transformation_test_result =
   | Fail of 'a * 'b
 
 let () =
-  describe "runtime" (fun () ->
+  describe "validStringLiteral" (fun () ->
       test "string with newline is valid" (fun () ->
           expect (isValidStringLiteral "\\n") |> toEqual true ) ;
       test "string with carriage return is valid" (fun () ->
@@ -23,5 +23,49 @@ let () =
           expect (isValidStringLiteral "\\") |> toEqual false ) ;
       test "string with invalid backslashed char is invalid" (fun () ->
           expect (isValidStringLiteral "\\6") |> toEqual false ) ;
+      () ) ;
+  describe "convertLiteralStringToDisplay " (fun () ->
+      test "string with newline is valid" (fun () ->
+          expect (convertLiteralStringToDisplay "\\n") |> toEqual "\\\\n" ) ;
+      test "string with newline is valid2" (fun () ->
+          expect (convertLiteralStringToDisplay "asd\\nqwe")
+          |> toEqual "asd\\\\nqwe" ) ;
+      test "string with carriage return is valid" (fun () ->
+          expect (convertLiteralStringToDisplay "\\t") |> toEqual "\\\\t" ) ;
+      test "string with carriage return is valid2" (fun () ->
+          expect (convertLiteralStringToDisplay "asd\\tqwe")
+          |> toEqual "asd\\\\tqwe" ) ;
+      test "string with tab is valid" (fun () ->
+          expect (convertLiteralStringToDisplay "\\r") |> toEqual "\\\\r" ) ;
+      test "string with tab is valid2" (fun () ->
+          expect (convertLiteralStringToDisplay "asd\\rqwe")
+          |> toEqual "asd\\\\rqwe" ) ;
+      test "string with escaped backslash is valid" (fun () ->
+          expect (convertLiteralStringToDisplay "\\") |> toEqual "\\\\" ) ;
+      test "string with escaped backslash is valid2" (fun () ->
+          expect (convertLiteralStringToDisplay "asd\\qwe")
+          |> toEqual "asd\\\\qwe" ) ;
+      () ) ;
+  describe "convertDisplayStringToLiteral " (fun () ->
+      test "string with newline is valid" (fun () ->
+          expect (convertDisplayStringToLiteral "\\n") |> toEqual "\n" ) ;
+      test "string with newline is valid2" (fun () ->
+          expect (convertDisplayStringToLiteral "asd\\nqwe")
+          |> toEqual "asd\nqwe" ) ;
+      test "string with carriage return is valid" (fun () ->
+          expect (convertDisplayStringToLiteral "\\t") |> toEqual "\t" ) ;
+      test "string with carriage return is valid2" (fun () ->
+          expect (convertDisplayStringToLiteral "asd\\tqwe")
+          |> toEqual "asd\tqwe" ) ;
+      test "string with tab is valid" (fun () ->
+          expect (convertDisplayStringToLiteral "\\r") |> toEqual "\r" ) ;
+      test "string with tab is valid2" (fun () ->
+          expect (convertDisplayStringToLiteral "asd\\rqwe")
+          |> toEqual "asd\rqwe" ) ;
+      test "string with escaped backslash is valid" (fun () ->
+          expect (convertDisplayStringToLiteral "\\\\") |> toEqual "\\" ) ;
+      test "string with escaped backslash is valid2" (fun () ->
+          expect (convertDisplayStringToLiteral "asd\\\\qwe")
+          |> toEqual "asd\\qwe" ) ;
       () ) ;
   ()
