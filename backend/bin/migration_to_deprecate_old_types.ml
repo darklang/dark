2853cbedb8dfd3a3264ecc09e8e7272046bf17af
Ian Smith <ismith@metaforgotten.org>
2019-05-03 17:00:21 -0700

    Actually migrate ids to strings in user_data

--- backend/bin/migration_to_deprecate_old_types.ml
+++ backend/bin/migration_to_deprecate_old_types.ml
@@ -160,4 +160,48 @@ let () =
      FROM toplevel_oplists
      JOIN canvases ON canvases.id = canvas_id"
     ~f:migrate_oplist_row ;
+  Db.iter_with_cursor
+    ~name:"migrate id to string"
+    ~params:[]
+    "SELECT user_data.key, data FROM user_data CROSS JOIN LATERAL jsonb_each(data) sub WHERE
+value @> '{\"type\": \"id\"}'"
+    ~f:(function
+      | [key; data] ->
+          let parsed_data = data |> Yojson.Safe.from_string in
+          (* if a pair has an id value = `{"type": "id", ...}` - then make it a
+           * string instead *)
+          let transform_pair (pair : string * Yojson.Safe.json) :
+              string * Yojson.Safe.json =
+            let key, value = pair in
+            let new_value =
+              match value with
+              | `Assoc [("type", `String "id"); ("value", `String value)] ->
+                  `String value
+              | _ ->
+                  value
+            in
+            (key, new_value)
+          in
+          let transformed_data =
+            match parsed_data with
+            | `Assoc pairs ->
+                `Assoc (pairs |> List.map ~f:transform_pair)
+            | _ ->
+                parsed_data
+          in
+          if parsed_data <> transformed_data
+          then Log.infO "update_id_to_string" ~params:[("key", key)] ;
+          Db.run
+            ~name:"update ids in user_data to be strings"
+            ~params:
+              [ Db.Uuid
+                  ( key
+                  |> Uuidm.of_string
+                  |> (Option.value_exn : Uuidm.t option -> Uuidm.t) )
+              ; Db.String data
+              ; Db.String (transformed_data |> Yojson.Safe.to_string) ]
+            (* AND data = $2 to guard against race conditions *)
+            "UPDATE user_data SET data = $3 WHERE key = $1 AND data = $2"
+      | _ ->
+          Exception.internal "Bad db result" ) ;
   ()
