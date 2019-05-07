ca58b83a9549b74b580f8f2a515984c128d32e9b
Ian Smith <ismith@metaforgotten.org>
2019-05-07 09:48:26 -0700

    ocamlformat

--- backend/libbackend/libdb.ml
+++ backend/libbackend/libdb.ml
@@ -37,14 +37,24 @@ let fetch_by_field ~state fieldname fieldvalue db =
 
 
 let replacements =
-  [ ("DB::insert", InProcess (fun _ -> Exception.user "DB::insert is DEPRECATED"))
-  ; ("DB::delete", InProcess (fun _ -> Exception.user "DB::delete is DEPRECATED"))
-  ; ("DB::deleteAll", InProcess (fun _ -> Exception.user "DB::deleteAll is DEPRECATED"))
-  ; ("DB::update", InProcess (fun _ -> Exception.user "DB::update is DEPRECATED"))
-  ; ("DB::fetchBy", InProcess (fun _ -> Exception.user "DB::fetchBy is DEPRECATED"))
-  ; ("DB::fetchOneBy", InProcess (fun _ -> Exception.user "DB::fetchOneBy is DEPRECATED"))
-  ; ("DB::fetchByMany", InProcess (fun _ -> Exception.user "DB::fetchByMany is DEPRECATED"))
-  ; ("DB::fetchOneByMany", InProcess (fun _ -> Exception.user "DB::fetchOneByMany is DEPRECATED"))
-  ; ("DB::fetchAll", InProcess (fun _ -> Exception.user "DB::fetchAll is DEPRECATED"))
+  [ ( "DB::insert"
+    , InProcess (fun _ -> Exception.user "DB::insert is DEPRECATED") )
+  ; ( "DB::delete"
+    , InProcess (fun _ -> Exception.user "DB::delete is DEPRECATED") )
+  ; ( "DB::deleteAll"
+    , InProcess (fun _ -> Exception.user "DB::deleteAll is DEPRECATED") )
+  ; ( "DB::update"
+    , InProcess (fun _ -> Exception.user "DB::update is DEPRECATED") )
+  ; ( "DB::fetchBy"
+    , InProcess (fun _ -> Exception.user "DB::fetchBy is DEPRECATED") )
+  ; ( "DB::fetchOneBy"
+    , InProcess (fun _ -> Exception.user "DB::fetchOneBy is DEPRECATED") )
+  ; ( "DB::fetchByMany"
+    , InProcess (fun _ -> Exception.user "DB::fetchByMany is DEPRECATED") )
+  ; ( "DB::fetchOneByMany"
+    , InProcess (fun _ -> Exception.user "DB::fetchOneByMany is DEPRECATED") )
+  ; ( "DB::fetchAll"
+    , InProcess (fun _ -> Exception.user "DB::fetchAll is DEPRECATED") )
   ; ("DB::keys", InProcess (fun _ -> Exception.user "DB::keys is DEPRECATED"))
-  ; ("DB::schema", InProcess (fun _ -> Exception.user "DB::schema is DEPRECATED")) ]
+  ; ( "DB::schema"
+    , InProcess (fun _ -> Exception.user "DB::schema is DEPRECATED") ) ]
