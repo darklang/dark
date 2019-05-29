69fc7650e47aedc00f9ca72b25ae1a5cbdc3be71
Korede Aderele <kaderele@gmail.com>
2019-05-29 14:51:50 -0700

    show input data timestamp as relative time

--- client/src/ViewData.ml
+++ client/src/ViewData.ml
@@ -5,6 +5,56 @@ open Types
 (* Dark *)
 module B = Blank
 
+let timeDifference (time : float) : string =
+  let msPerMinute = 60.0 *. 1000.0 in
+  let msPerHour = msPerMinute *. 60.0 in
+  let msPerDay = msPerHour *. 24.0 in
+  let msPerMonth = msPerDay *. 30.0 in
+  let msPerYear = msPerDay *. 365.0 in
+  let rec f time =
+    if time /. msPerYear > 1.0
+    then
+      let suffix = if time /. msPerYear > 2.0 then "years" else "year" in
+      ( (time /. msPerYear |> int_of_float |> string_of_int)
+      ^ " "
+      ^ suffix
+      ^ ", " )
+      ^ f (mod_float time msPerYear)
+    else if time /. msPerMonth > 1.0
+    then
+      let suffix = if time /. msPerMonth > 2.0 then "months" else "month" in
+      ( (time /. msPerMonth |> int_of_float |> string_of_int)
+      ^ " "
+      ^ suffix
+      ^ ", " )
+      ^ f (mod_float time msPerMonth)
+    else if time /. msPerDay > 1.0
+    then
+      let suffix = if time /. msPerDay > 2.0 then "days" else "day" in
+      ( (time /. msPerDay |> int_of_float |> string_of_int)
+      ^ " "
+      ^ suffix
+      ^ ", " )
+      ^ f (mod_float time msPerDay)
+    else if time /. msPerHour > 1.0
+    then
+      let suffix = if time /. msPerHour > 2.0 then "hours" else "hour" in
+      ( (time /. msPerHour |> int_of_float |> string_of_int)
+      ^ " "
+      ^ suffix
+      ^ ", " )
+      ^ f (mod_float time msPerHour)
+    else if time /. msPerMinute > 1.0
+    then
+      let suffix = if time /. msPerMinute > 2.0 then "minutes" else "minute" in
+      ((time /. msPerMinute |> int_of_float |> string_of_int) ^ " " ^ suffix)
+      ^ f (mod_float time msPerMinute)
+    else ""
+  in
+  let diff = f time in
+  if diff = "" then "less than a minute" else diff
+
+
 let viewInput
     (tlid : tlid)
     (traceID : traceID)
@@ -30,7 +80,10 @@ let viewInput
           TraceMouseLeave (tlid, traceID, x) ) ]
   in
   Html.li
-    ( [Vdom.attribute "" "data-content" (value ^ "\nMade at: " ^ timestamp)]
+    ( [ Vdom.attribute
+          ""
+          "data-content"
+          (value ^ "\nMade at: " ^ timestamp ^ " ago") ]
     @ [classes |> String.join ~sep:" " |> Html.class']
     @ events )
     [Html.text {js|•|js}]
@@ -50,6 +103,8 @@ let viewInputs (vs : ViewUtils.viewState) (ID astID : id) : msg Html.html list
     in
     let timestamp =
       Option.map ~f:(fun (td : traceData) -> td.timestamp) traceData
+      |> Option.map ~f:(fun tstr ->
+             Js.Date.now () -. Js.Date.parseAsFloat tstr |> timeDifference )
       |> Option.withDefault ~default:""
     in
     (* Note: the isActive and hoverID tlcursors are very different things *)
