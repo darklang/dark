928b0e84a77fb857db477b5ef5b073cadada751b
Paul Biggar <paul.biggar@gmail.com>
2019-06-07 15:55:19 -0700

    Move libstaticassets to own file in libexecution

--- backend/libexecution/libs.ml
+++ backend/libexecution/libs.ml
@@ -81,6 +81,7 @@ let init (replacements : fn_replacement list) : unit =
     @ Libstd.fns
     @ Libhttp.fns
     @ Libdarkinternal.fns
+    @ Libstaticassets.fns
     (* @ Libtwitter.fns  *)
   in
   List.iter ~f:add_short_fn libs ;
