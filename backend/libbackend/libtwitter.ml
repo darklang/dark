82a10c327e2a79e3c54610f430f8600d83e159ff
Paul Biggar <paul.biggar@gmail.com>
2019-06-27 14:13:29 -0700

    Pull dval-related things out of twitter.ml
    Also call POST methods with POST not GET

--- backend/libbackend/libtwitter.ml
+++ backend/libbackend/libtwitter.ml
@@ -4,9 +4,32 @@ open Lib
 open Types
 open Types.RuntimeT
 
-let schema = Swagger.parse "twitter.json"
+let call (endpoint : string) (verb : string) (args : dval_map) : dval =
+  let prefix = "https://api.twitter.com" in
+  let url = prefix ^ endpoint in
+  let authargs =
+    args
+    |> DvalMap.filter ~f:(( <> ) Types.RuntimeT.DNull)
+    |> DvalMap.map ~f:(fun v -> Dval.to_url_string_exn v)
+    |> DvalMap.to_list
+  in
+  let result =
+    match verb with
+    | "GET" ->
+        let query = Dval.to_form_encoding (DObj args) in
+        let header = Twitter.authorization_header url "GET" authargs in
+        Httpclient.call (url ^ "?" ^ query) GET [header] ""
+    | "POST" ->
+        let body = "" in
+        let header = Twitter.authorization_header url "POST" authargs in
+        Httpclient.call url POST [header] body
+    | _ ->
+        Exception.internal ("Invalid Twitter httpMethod: " ^ verb)
+  in
+  Dval.of_unknown_json_v0 result
 
-let call_twitter path (args : dval_map) : dval = Twitter.get path args
+
+let schema = Swagger.parse "twitter.json"
 
 let sw_type2dark tipe =
   match tipe with
@@ -39,12 +62,12 @@ let fns =
   |> List.filter_map ~f:(fun (api : Swagger.api) ->
          api.operations
          |> List.hd
-         |> Option.map ~f:(fun (get : Swagger.operation) ->
+         |> Option.map ~f:(fun (op : Swagger.operation) ->
                 { pns = ["Twitter::" ^ twurl2name api.path]
                 ; ins = []
                 ; r = TAny
-                ; f = API (call_twitter api.path)
-                ; p = List.map ~f:param2param get.parameters
-                ; d = Option.value ~default:"" get.summary
+                ; f = API (call op.httpMethod api.path)
+                ; p = List.map ~f:param2param op.parameters
+                ; d = Option.value ~default:"" op.summary
                 ; ps = false
                 ; dep = false } ) )
--- backend/libbackend/twitter.ml
+++ backend/libbackend/twitter.ml
@@ -1,13 +1,4 @@
 open Core_kernel
-open Libexecution
-module RTT = Types.RuntimeT
-module RT = Runtime
-
-type dval = RTT.dval
-
-type dval_map = RTT.dval_map
-
-module DvalMap = RTT.DvalMap
 
 let pct_encode_key = Uri.pct_encode ~component:`Userinfo
 
@@ -47,7 +38,7 @@ let sign consumer_secret access_token_secret uri verb params =
   |> B64.encode
 
 
-let nonce () = Util.random_string 42 |> Mock.get_string "nonce"
+let nonce () = Libexecution.Util.random_string 42 |> Mock.get_string "nonce"
 
 let ts () =
   Unix.gettimeofday () |> Float.to_int |> Int.to_string |> Mock.get_string "ts"
@@ -57,7 +48,8 @@ let param_to_string (key, value) : string =
   pct_encode key ^ "=\"" ^ pct_encode value ^ "\""
 
 
-let oauth_params (secret : Secret.twitter_secret) url verb (args : dval_map) :
+let oauth_params
+    (secret : Secret.twitter_secret) url verb (args : (string * string) list) :
     (string * string) list =
   let initial_params =
     [ ("oauth_consumer_key", secret.consumer_key)
@@ -67,56 +59,26 @@ let oauth_params (secret : Secret.twitter_secret) url verb (args : dval_map) :
     ; ("oauth_timestamp", ts ())
     ; ("oauth_token", secret.access_token) ]
   in
-  let argparams =
-    args
-    |> DvalMap.filter ~f:(( <> ) Types.RuntimeT.DNull)
-    |> DvalMap.map ~f:(fun v -> Dval.to_url_string_exn v)
-    |> DvalMap.to_list
-  in
   let signature =
     sign
       secret.consumer_secret
       secret.access_token_secret
       url
       verb
-      (List.append initial_params argparams)
+      (List.append initial_params args)
   in
   [("oauth_signature", signature)]
   |> List.append initial_params
   |> List.sort ~compare:(fun (a, _) (b, _) -> compare a b)
 
 
-let oauth_header secret url verb (args : dval_map) : string =
+let oauth_header secret url verb (args : (string * string) list) : string =
   oauth_params secret url verb args
   |> List.map ~f:param_to_string
   |> String.concat ~sep:", "
   |> ( ^ ) "OAuth "
 
 
-let authorization_header url verb (args : dval_map) : string * string =
+let authorization_header url verb (args : (string * string) list) :
+    string * string =
   ("Authorization", oauth_header Secret.twitter url verb args)
-
-
-let call (endpoint : string) (verb : Httpclient.verb) (args : dval_map) : dval
-    =
-  let prefix = "https://api.twitter.com" in
-  let url = prefix ^ endpoint in
-  let result =
-    match verb with
-    | GET ->
-        let query = Dval.to_form_encoding (DObj args) in
-        let header = authorization_header url "GET" args in
-        Httpclient.call (url ^ "?" ^ query) verb [header] ""
-    | POST ->
-        let body = "" in
-        let header = authorization_header url "POST" args in
-        Httpclient.call url verb [header] body
-    | _ ->
-        Exception.internal "not implemented yet"
-  in
-  Dval.of_unknown_json_v0 result
-
-
-let get (url : string) (args : dval_map) : dval = call url GET args
-
-let post (url : string) (args : dval_map) : dval = call url POST args
