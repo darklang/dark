5fc5b45f57e2c055d52c8fdbf3ece2a31f99954c
Ian Connolly <ian@connolly.io>
2019-07-03 11:03:01 -0700

    External link on GET handlers interpolates variables + query params

--- client/__tests__/runtime_test.ml
+++ client/__tests__/runtime_test.ml
@@ -58,4 +58,33 @@ let () =
       t "escaped quote" "\\\"" "\"" ;
       t "escaped quote2" "asd\\\"qwe" "asd\"qwe" ;
       () ) ;
+  describe "pathFromInputVars" (fun () ->
+      let noRequest = StrDict.empty in
+      let noURL = StrDict.fromList [("request", Types.DObj StrDict.empty)] in
+      let generate url =
+        StrDict.fromList
+          [("request", Types.DObj (StrDict.fromList [("url", Types.DStr url)]))]
+      in
+      test "returns None if no request object" (fun () ->
+          expect (pathFromInputVars noRequest) |> toEqual None ) ;
+      test "returns None if no url object" (fun () ->
+          expect (pathFromInputVars noURL) |> toEqual None ) ;
+      test "returns None if no url is parseable - numbers" (fun () ->
+          expect (pathFromInputVars (generate "123456")) |> toEqual None ) ;
+      test "returns None if no url is parseable - no slashes" (fun () ->
+          expect (pathFromInputVars (generate "localhost")) |> toEqual None ) ;
+      test "returns None if no url is parseable - no scheme" (fun () ->
+          expect (pathFromInputVars (generate "//foobar.builwithdark.com"))
+          |> toEqual None ) ;
+      test "returns path with no query string" (fun () ->
+          expect
+            (pathFromInputVars
+               (generate "https://foobar.builwithdark.com/hello"))
+          |> toEqual (Some "/hello") ) ;
+      test "returns path with query string" (fun () ->
+          expect
+            (pathFromInputVars
+               (generate
+                  "https://foobar.builwithdark.com/hello?foo=bar&baz=quux"))
+          |> toEqual (Some "/hello?foo=bar&baz=quux") ) ) ;
   ()
