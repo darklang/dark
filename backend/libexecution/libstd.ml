50fdb3b22e531493d0b0ac824da989d08747358e
Korede Aderele <kaderele@gmail.com>
2019-07-08 10:14:07 -0700

    move Object:: functions to Dict::

--- backend/libexecution/libstd.ml
+++ backend/libexecution/libstd.ml
@@ -103,11 +103,24 @@ let fns : Lib.shortfn list =
           | args ->
               fail args)
     ; ps = true
+    ; dep = true }
+  ; { pns = ["Dict::map"]
+    ; ins = []
+    ; p = [par "dict" TObj; func ["value"]]
+    ; r = TObj
+    ; d =
+        "Iterates each `value` in object `dict` and mutates it according to the provided lambda"
+    ; f =
+        InProcess
+          (function
+          | _, [DObj o; DBlock fn] ->
+              let f (dv : dval) : dval = fn [dv] in
+              DObj (Map.map ~f o)
+          | args ->
+              fail args)
+    ; ps = true
     ; dep = false }
-  ; (* ====================================== *)
-    (* Objects *)
-    (* ====================================== *)
-    { pns = ["Object::empty"]
+  ; { pns = ["Dict::empty"]
     ; ins = []
     ; p = []
     ; r = TObj
@@ -116,7 +129,7 @@ let fns : Lib.shortfn list =
         InProcess (function _, [] -> DObj DvalMap.empty | args -> fail args)
     ; ps = true
     ; dep = false }
-  ; { pns = ["Object::merge"]
+  ; { pns = ["Dict::merge"]
     ; ins = []
     ; p = [par "left" TObj; par "right" TObj]
     ; r = TObj
@@ -131,6 +144,65 @@ let fns : Lib.shortfn list =
               fail args)
     ; ps = true
     ; dep = false }
+  ; { pns = ["Dict::toJSON"]
+    ; ins = []
+    ; p = [par "obj" TObj]
+    ; r = TStr
+    ; d = "Dumps `obj` to a JSON string"
+    ; f =
+        InProcess
+          (function
+          | _, [DObj o] ->
+              DObj o
+              |> Legacy.PrettyResponseJsonV0.to_pretty_response_json_v0
+              |> Dval.dstr_of_string_exn
+          | args ->
+              fail args)
+    ; ps = true
+    ; dep = true }
+  ; { pns = ["Dict::toJSON_v1"]
+    ; ins = []
+    ; p = [par "obj" TObj]
+    ; r = TStr
+    ; d = "Dumps `obj` to a JSON string"
+    ; f =
+        InProcess
+          (function
+          | _, [DObj o] ->
+              DObj o
+              |> Dval.to_pretty_machine_json_v1
+              |> Dval.dstr_of_string_exn
+          | args ->
+              fail args)
+    ; ps = true
+    ; dep = false }
+  ; (* ====================================== *)
+    (* Objects *)
+    (* ====================================== *)
+    { pns = ["Object::empty"]
+    ; ins = []
+    ; p = []
+    ; r = TObj
+    ; d = "Return an empty object"
+    ; f =
+        InProcess (function _, [] -> DObj DvalMap.empty | args -> fail args)
+    ; ps = true
+    ; dep = true }
+  ; { pns = ["Object::merge"]
+    ; ins = []
+    ; p = [par "left" TObj; par "right" TObj]
+    ; r = TObj
+    ; d =
+        "Return a combined object with both objects' keys and values. If the same key exists in both `left` and `right`, then use the value from `right`"
+    ; f =
+        InProcess
+          (function
+          | _, [DObj l; DObj r] ->
+              DObj (Util.merge_right l r)
+          | args ->
+              fail args)
+    ; ps = true
+    ; dep = true }
   ; { pns = ["assoc"]
     ; ins = []
     ; p = [par "obj" TObj; par "key" TStr; par "val" TAny]
@@ -228,7 +300,7 @@ let fns : Lib.shortfn list =
           | args ->
               fail args)
     ; ps = true
-    ; dep = false }
+    ; dep = true }
   ; (* ====================================== *)
     (* Int *)
     (* ====================================== *)
