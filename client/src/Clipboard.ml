208071c199982d28b44e9c99cba87a333ec9d6e8
Paul Biggar <paul.biggar@gmail.com>
2019-07-25 13:08:18 -0700

    Make paste work in fluid

--- client/src/Clipboard.ml
+++ client/src/Clipboard.ml
@@ -1,5 +1,6 @@
 open Tc
 open Types
+open Prelude
 
 (* Dark *)
 module P = Pointer
@@ -10,10 +11,42 @@ type copyData =
   | `Json of Js.Json.t
   | `None ]
 
+let getCurrent (m : model) : (toplevel * pointerData) option =
+  let myIdOf (m : model) : id option =
+    match unwrapCursorState m.cursorState with
+    | FluidEntering tlid ->
+        (* TODO: call getTokens once it merges *)
+        let s = m.fluidState in
+        let neighbours =
+          TL.get m tlid
+          |> Option.andThen ~f:TL.getAST
+          |> Option.map ~f:(Fluid.fromExpr s)
+          |> Option.map ~f:(Fluid.toTokens s)
+          |> Option.map ~f:(Fluid.getNeighbours ~pos:s.newPos)
+        in
+        ( match neighbours with
+        | Some (Fluid.L (_, ti), _, _) when FluidToken.isTextToken ti.token ->
+            Some (FluidToken.tid ti.token)
+        | Some (_, Fluid.R (_, ti), _) when FluidToken.isTextToken ti.token ->
+            Some (FluidToken.tid ti.token)
+        | _ ->
+            failwith "no id" )
+    | _ ->
+        idOf m.cursorState
+  in
+  match (tlidOf m.cursorState, myIdOf m) with
+  | Some tlid, Some id ->
+      TL.get m tlid
+      |> Option.andThen ~f:(fun tl ->
+             Option.map (TL.find tl id) ~f:(fun pd -> (tl, pd)) )
+  | _ ->
+      None
+
+
 let copy (m : model) : copyData =
   match m.cursorState with
   | Selecting _ ->
-    ( match TL.getCurrent m with
+    ( match getCurrent m with
     | Some (_, (PExpr _ as pd))
     | Some (_, (PPattern _ as pd))
     | Some (_, (PParamTipe _ as pd)) ->
@@ -31,7 +64,7 @@ let copy (m : model) : copyData =
 let cut (m : model) : copyData * modification =
   match m.cursorState with
   | Selecting _ ->
-    ( match TL.getCurrent m with
+    ( match getCurrent m with
     | None ->
         (`None, NoChange)
     | Some (tl, pd) ->
@@ -42,7 +75,7 @@ let cut (m : model) : copyData * modification =
 
 
 let paste (m : model) (data : copyData) : modification =
-  match TL.getCurrent m with
+  match getCurrent m with
   | Some (tl, currentPd) ->
     ( match data with
     | `Json j ->
