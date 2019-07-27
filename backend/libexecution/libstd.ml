cd731222535e57b88575b101726bebae9d847451
Paul Biggar <paul.biggar@gmail.com>
2019-07-26 20:09:39 -0700

    Deprecate old get function

--- backend/libexecution/libstd.ml
+++ backend/libexecution/libstd.ml
@@ -69,7 +69,7 @@ let fns : Lib.shortfn list =
           | args ->
               fail args)
     ; ps = true
-    ; dep = false }
+    ; dep = true }
   ; { pns = ["Dict::get_v1"]
     ; ins = []
     ; p = [par "dict" TObj; par "key" TStr]
