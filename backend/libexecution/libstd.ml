ecae1169847424386ddbfbe66dfb6748d02a8ae9
Paul Biggar <paul.biggar@gmail.com>
2019-07-28 15:29:21 -0700

    Also deprecate List::last_v0

--- backend/libexecution/libstd.ml
+++ backend/libexecution/libstd.ml
@@ -1466,7 +1466,7 @@ let fns : Lib.shortfn list =
           | args ->
               fail args)
     ; ps = true
-    ; dep = false }
+    ; dep = true }
   ; { pns = ["List::last_v1"]
     ; ins = []
     ; p = [par "list" TList]
