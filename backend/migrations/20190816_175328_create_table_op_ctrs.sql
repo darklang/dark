57f9344f4554c07dbb2fe1973473ab6d72fa40da
Ian Smith <ismith@metaforgotten.org>
2019-08-21 13:54:46 -0700

    Address the write-write race problem
    Described by Paul in #1299 as:
    ```
    Given two requests r1 and r2, r1 happens first and so we hope for r2 to be the winner.
    
    Theoretically, r1 processes and then r2 processes, and they dont conflict and so r1 is saved, then r2 is loaded and r2 is saved.
    
    However, r1 can complete the ctr check before r2 does, allowing both to pass. But r1 could be delayed allowing r2 to save before r1 saves. So we could have:
    
    r1 loads
    r2 loads
    r2 saves
    r1 saves
    ```
    
    This:
    - checks if there's an existing lock more than 1s old
    - tries to grab a lock
      - if we succeed, return true (is_latest_op_request is true)
      - if we fail, and
        - op_ctr is less than the db value, return false
        - op_ctr is greater than the db value, it might be the
          latest_op_request, but we can't grab the lock yet - loop
    - before we respond (whether `Ok or `Error), release the lock

--- backend/migrations/20190816_175328_create_table_op_ctrs.sql
+++ backend/migrations/20190816_175328_create_table_op_ctrs.sql
@@ -3,7 +3,8 @@ op_ctrs
 (
  browser_id UUID NOT NULL UNIQUE
 , ctr INTEGER NOT NULL DEFAULT 0
-, timestamp TIMESTAMP NOT NULL DEFAULT NOW());
+, timestamp TIMESTAMP NOT NULL DEFAULT NOW()
+, locked BOOLEAN NOT NULL);
 
 CREATE INDEX IF NOT EXISTS idx_op_ctrs_browser_id_ctr ON op_ctrs (browser_id, ctr DESC);
 CREATE INDEX IF NOT EXISTS idx_op_ctrs_timestamp ON op_ctrs (timestamp ASC)
