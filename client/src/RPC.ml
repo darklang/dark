34c37987add68d4f797f2cedd02877197cf7a13d
Ian Smith <ismith@metaforgotten.org>
2019-08-22 15:33:01 -0700

    Move filterOpsAndResult from App.ml to RPC.ml

--- client/src/RPC.ml
+++ client/src/RPC.ml
@@ -157,3 +157,95 @@ let sendPresence (m : model) (av : avatarModelMessage) : msg Tea.Cmd.t =
   if m.origin = "https://darklang.com"
   then Tea.Http.send (fun x -> TriggerSendPresenceCallback x) request
   else Tea.Cmd.none
+
+
+(* We do some dropping of ops based on browserId+opCtr to preserve ordering.
+ * (opCtr is per-browserId, and inc'd client-side; thus we know, when processing
+ * a set of ops whether this is the latest seen so far from a given client, or
+ * has come in out of order.) This is initially done server-side, to guard
+ * against ops being processed there out of order; but we also need to do this
+ * client-side, since messages coming in from Pusher (and stroller) are not
+ * guaranteed to be delivered in order.
+ *
+ * Ordering is determined by model.opCtrs, and we return a model so we can also
+ * update the opCtrs map.
+ * *)
+let filterOpsAndResult
+    (m : model) (params : addOpRPCParams) (result : addOpRPCResult option) :
+    model * op list * addOpRPCResult option =
+  let newOpCtrs =
+    (* if the opCtr in params is greater than the one in the map, we'll create
+     * an updated map *)
+    StrDict.update m.opCtrs ~key:params.browserId ~f:(fun oldCtr ->
+        match (oldCtr, params.opCtr) with
+        | Some oldCtr, Some paramsOpCtr ->
+            Some (max oldCtr paramsOpCtr)
+        | _ ->
+            params.opCtr )
+  in
+  let m2 = {m with opCtrs = newOpCtrs} in
+  (* if the new opCtrs map was updated by params.opCtr, then this msg was the
+   * latest; otherwise, we need to filter out some ops from params *)
+  if StrDict.get m2.opCtrs ~key:params.browserId = params.opCtr
+  then (m2, params.ops, result)
+  else
+    (* filter down to only those ops which can be applied out of order without
+       * overwriting previous ops' state - eg, if we have SetHandler1 setting a
+       * handler's value to "aaa", and then SetHandler2's value is "aa",
+       * applying them out of order (SH2, SH1) will result in SH2's update being
+       * overwritten *)
+    (* NOTE: DO NOT UPDATE WITHOUT UPDATING THE SERVER-SIDE LIST *)
+    let filter_ops_received_out_of_order =
+      List.filter ~f:(fun op ->
+          match op with
+          | SetHandler _
+          | SetFunction _
+          | SetType _
+          | MoveTL _
+          | SetDBColName _
+          | ChangeDBColName _
+          | ChangeDBColType _
+          | SetExpr _
+          | CreateDBMigration _
+          | SetDBColNameInDBMigration _
+          | SetDBColTypeInDBMigration _
+          | UndoTL _
+          | RedoTL _
+          | RenameDBname _ ->
+              false
+          | CreateDB _
+          | AddDBCol _
+          | SetDBColType _
+          | DeleteTL _
+          | DeprecatedInitDbm _
+          | TLSavepoint _
+          | DeleteFunction _
+          | AddDBColToDBMigration _
+          | AbandonDBMigration _
+          | DeleteColInDBMigration _
+          | DeleteDBCol _
+          | CreateDBWithBlankOr _
+          | DeleteTLForever _
+          | DeleteFunctionForever _
+          | DeleteType _
+          | DeleteTypeForever _ ->
+              true )
+    in
+    let ops = params.ops |> filter_ops_received_out_of_order in
+    let opTlids = ops |> List.map ~f:(fun op -> Encoders.tlidOf op) in
+    (* We also want to ignore the result of ops we ignored *)
+    let result =
+      Option.map result ~f:(fun (result : addOpRPCResult) ->
+          { result with
+            handlers =
+              result.handlers
+              |> List.filter ~f:(fun h -> List.member ~value:h.hTLID opTlids)
+          ; userFunctions =
+              result.userFunctions
+              |> List.filter ~f:(fun uf -> List.member ~value:uf.ufTLID opTlids)
+          ; userTipes =
+              result.userTipes
+              |> List.filter ~f:(fun ut -> List.member ~value:ut.utTLID opTlids)
+          } )
+    in
+    (m2, ops, result)
