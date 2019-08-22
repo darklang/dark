47d40274c2d32bd05271a4c85e442d15b8a4cb0a
Ian Smith <ismith@metaforgotten.org>
2019-08-22 15:48:57 -0700

    Move topbar code into its own ViewTopbar.ml
    Doing this under the assumption that we might reuse topbar for other
    opt-in feature flags.

--- client/src/View.ml
+++ client/src/View.ml
@@ -29,11 +29,6 @@ let atom = ViewBlankOr.atom
 
 let keyword = ViewBlankOr.keyword
 
-let msgLink ~(key : string) (content : msg Html.html) (handler : msg) :
-    msg Html.html =
-  let event = ViewUtils.eventNeither ~key "click" (fun _ -> handler) in
-  Html.a [event; Html.class' ""] [content]
-
 
 let viewTL_ (m : model) (tl : toplevel) : msg Html.html =
   let tlid = TL.id tl in
@@ -403,44 +398,8 @@ let view (m : model) : msg Html.html =
     then [Fluid.viewStatus (Fluid.fromExpr m.fluidState ast) m.fluidState]
     else []
   in
-  let topbar =
-    if m.showTopbar = false
-    then []
-    else if VariantTesting.isFluidV2 m.tests
-    then
-      let fluid = VariantTesting.isFluidForCustomers m.tests in
-      let fluidUrl =
-        let qp =
-          ("fluid", not fluid) :: Url.queryParams
-          |> List.uniqueBy ~f:(fun (k, _) -> k)
-          |> List.filter ~f:(fun (_, v) -> v)
-          |> List.map ~f:(fun (k, _) -> k ^ "=1")
-          |> String.join ~sep:"&"
-          |> fun s -> "?" ^ s
-        in
-        let loc = {(Tea.Navigation.getLocation ()) with search = qp} in
-        loc.protocol ^ "//" ^ loc.host ^ loc.pathname ^ loc.search ^ loc.hash
-      in
-      [ Html.div
-          [Html.styles []; Html.classList [("topbar", true)]]
-          [ Html.a
-              [Html.href fluidUrl]
-              [ Html.text
-                  (* NB: right now, this "old editor" link will take you
-                       * from ?fluidv2=1&fluid=1 -> ?fluidv2=1, so it won't take
-                       * you to the structured editor, it'll just put the
-                       * status/debug box back.  Once we remove the fluidv2
-                       * feature flag - that is, ship it to customers - then
-                       * it'll take you from fluid to not-fluid *)
-                  ( if fluid
-                  then "Back to the old editor"
-                  else "Try our new editor!" ) ]
-          ; Html.text " "
-          ; msgLink ~key:"hide-topbar" (Html.text "(hide)") HideTopbar ] ]
-    else []
-  in
   let content =
-    topbar
+    ViewTopbar.html m
     @ [sidebar; body; activeAvatars; viewToast m.toast]
     @ fluidStatus
     @ footer
