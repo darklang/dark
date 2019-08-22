ba73ac8b85a624d7ee3b61b43ea7c985676da724
Ian Connolly <ian@connolly.io>
2019-08-22 15:28:58 -0700

    Don't wrap DError in ErrorRail

--- backend/libexecution/ast.ml
+++ backend/libexecution/ast.ml
@@ -564,6 +564,8 @@ and call_fn
             v
         | DIncomplete ->
             DIncomplete
+        | DError e ->
+            DError e
         (* There should only be DOptions and DResults here, but hypothetically we got
         * something else, they would go on the error rail too.  *)
         | other ->
