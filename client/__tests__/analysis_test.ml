a4b5ac2dabeb90203048fe25d6464ca7e4308886
Alice Q. Wong <alice@valour.me>
2019-09-17 15:01:46 -0700

    unit tests

---
+++ client/__tests__/analysis_test.ml
@@ -0,0 +1,15 @@
+open Types
+open Analysis
+open Jest
+open Expect
+module B = Blank
+
+let () =
+  describe "requestAnalysis" (fun () ->
+      test "on tlid not found" (fun () ->
+          let m =
+            {Defaults.defaultModel with deletedUserFunctions = TLIDDict.empty}
+          in
+          expect (requestAnalysis m (TLID "123") "abc") |> toEqual Cmd.none )
+  ) ;
+  ()
