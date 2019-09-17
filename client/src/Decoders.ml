e54d3c3260caed041590e205c4b05b88d6c12bdf
Paul Biggar <paul.biggar@gmail.com>
2019-09-17 08:26:05 -0700

    Improve error messages on bad decode

--- client/src/Decoders.ml
+++ client/src/Decoders.ml
@@ -668,19 +668,23 @@ let exception_ j : exception_ =
 (* Wrap JSON decoders using bs-json's format, into TEA's HTTP expectation format *)
 let wrapExpect (fn : Js.Json.t -> 'a) : string -> ('ok, string) Tea.Result.t =
  fun j ->
-  try Ok (fn (Json.parseOrRaise j)) with
-  | DecodeError e ->
-      Error e
-  | e ->
-      Error (Printexc.to_string e)
+  try Ok (fn (Json.parseOrRaise j)) with e ->
+    Js.log2 "unexpected json" j ;
+    ( match e with
+    | DecodeError e | Json.ParseError e ->
+        Error e
+    | e ->
+        Error (Printexc.to_string e) )
 
 
 (* Wrap JSON decoders using bs-json's format, into TEA's JSON decoder format *)
 let wrapDecoder (fn : Js.Json.t -> 'a) : (Js.Json.t, 'a) Tea.Json.Decoder.t =
   Decoder
     (fun value ->
-      try Tea_result.Ok (fn value) with
-      | DecodeError e ->
-          Tea_result.Error e
-      | e ->
-          Tea_result.Error ("Json error: " ^ Printexc.to_string e) )
+      try Tea_result.Ok (fn value) with e ->
+        Js.log2 "undecodable json" value ;
+        ( match e with
+        | DecodeError e | Json.ParseError e ->
+            Tea_result.Error e
+        | e ->
+            Tea_result.Error ("Json error: " ^ Printexc.to_string e) ) )
