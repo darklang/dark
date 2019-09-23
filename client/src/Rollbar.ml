bad49a887f2a7718f4cb216074304ad2855d9441
Paul Biggar <paul.biggar@gmail.com>
2019-09-23 07:27:15 -0700

    Send rollbar for recoverable errors

--- client/src/Rollbar.ml
+++ client/src/Rollbar.ml
@@ -1,36 +1,9 @@
 open Tc
 open Types
 
-type rollbar_mod = < init : Js.Json.t -> unit [@bs.meth] > Js.t
+let send = Native.Rollbar.send
 
-type rollbar_instance =
-  < error :
-         string
-      -> string Js.nullable
-      -> int Js.null
-      -> string Js.null
-      -> Js.Json.t
-      -> unit [@bs.meth] >
-  Js.t
-
-external rollbar : rollbar_mod = "rollbar" [@@bs.module]
-
-let init (rollbarConfig : Js.Json.t) =
-  rollbar##init rollbarConfig ;
-  Js.log "Inited rollbar"
-
-
-let send (msg : string) (url : string option) (custom : Js.Json.t) : unit =
-  let url = Js.Nullable.fromOption url in
-  let (rb : rollbar_instance) =
-    (* There's a better way of doing this but I couldn't get it to work:
-     * https://bucklescript.github.io/docs/en/embed-raw-javascript#detect-global-variables
-     * *)
-    [%raw
-      {| (typeof window === 'undefined') ? self.rollbar : window.rollbar |}]
-  in
-  rb##error msg url Js.null Js.null custom
-
+let init = Native.Rollbar.init
 
 let customContext (e : apiError) (state : cursorState) : Js.Json.t =
   let parameters = Option.withDefault ~default:Js.Json.null e.requestParams in
