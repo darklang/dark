a0f7640f65d846142d9fff05dc45699cedba33ff
Ian Smith <ismith@metaforgotten.org>
2019-09-24 13:06:24 -0700

    Put QW and cron worker health check onto a separate OS thread
    With Maxim's old canvas, we ran into a problem where long-running jobs
    could keep the health check from executing, so the qw would be killed by
    k8s as unhealthy; this fixes that.
    
    Also: add net-tools to Dockerfile, netstat is helpful for local
    debugging.
    
    https://trello.com/c/RMQ08KlD/1769-separate-http-healthchecks-for-queue-and-cron-workers-into-their-own-threads-not-shaerd-with-events

--- backend/bin/cron_checker.ml
+++ backend/bin/cron_checker.ml
@@ -4,9 +4,8 @@ open Libcommon
 
 let shutdown = ref false
 
-let () =
+let health_check execution_id =
   Libbackend.Init.init ~run_side_effects:false ;
-  let execution_id = Libexecution.Util.create_id () in
   (* spin up health http server *)
   let run_health_check_server =
     if Array.length Sys.argv >= 2
@@ -15,28 +14,38 @@ let () =
   in
   if run_health_check_server
   then
-    Lwt.async (fun () ->
-        (* We're sharing a ref across threads here, but Health_check writes
+    Lwt_main.run
+      (* We're sharing a ref across threads here, but Health_check writes
         * `true` to that ref regardless of its value and we never write to
         * it, so there's no sharing issue. *)
-        try%lwt
-              Libcommon.Log.infO
-                "cron_checker"
-                ~data:"Spinning up health check service" ;
-              Libservice.Health_check.run ~shutdown ~execution_id
-        with e ->
-          Libcommon.Log.erroR
-            "cron_checker"
-            ~data:"Health check service threw error"
-            ~params:[("exn", Libcommon.Log.dump e)] ;
-          let bt = Libexecution.Exception.get_backtrace () in
-          Lwt.async (fun () ->
-              Libbackend.Rollbar.report_lwt
-                e
-                bt
-                CronChecker
-                (Libexecution.Types.string_of_id execution_id) ) ;
-          fail e ) ;
+        ( try%lwt
+                Libcommon.Log.infO
+                  "queue_worker"
+                  ~data:"Spinning up health check service"
+                  ~params:
+                    [ ( "execution_id"
+                      , Libexecution.Types.string_of_id execution_id ) ] ;
+                Libservice.Health_check.run ~shutdown ~execution_id
+          with e ->
+            let execution_id = Libexecution.Util.create_id () in
+            Libcommon.Log.erroR
+              "queue_worker"
+              ~data:"Health check service threw error"
+              ~params:
+                [ ("execution_id", Libexecution.Types.string_of_id execution_id)
+                ; ("exn", Libcommon.Log.dump e) ] ;
+            let bt = Libexecution.Exception.get_backtrace () in
+            Lwt.async (fun () ->
+                Libbackend.Rollbar.report_lwt
+                  e
+                  bt
+                  CronChecker
+                  (Libexecution.Types.string_of_id execution_id) ) ;
+            fail e )
+
+
+let cron_checker execution_id =
+  Libbackend.Init.init ~run_side_effects:false ;
   let rec cron_checker () =
     let%lwt () = Lwt_unix.sleep 1.0 in
     let result = Libbackend.Cron.check_all_canvases execution_id in
@@ -61,3 +70,13 @@ let () =
        [ ( "execution_id"
          , `String (Libexecution.Types.string_of_id execution_id) ) ]
        (fun _ -> Nocrypto_entropy_lwt.initialize () >>= cron_checker))
+
+
+let () =
+  let execution_id = Libexecution.Util.create_id () in
+  let health_check_thread = Thread.create health_check execution_id in
+  let cron_checker_thread = Thread.create cron_checker execution_id in
+  (* If either thread sets the shutdown ref, the other will see it and
+   * terminate; block until both have terminated. *)
+  Thread.join cron_checker_thread ;
+  Thread.join health_check_thread
