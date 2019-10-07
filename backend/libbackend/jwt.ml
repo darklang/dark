7556c6e3ec984792a88f27aebfad683de00c6b95
Ian Smith <ismith@metaforgotten.org>
2019-10-07 16:06:09 -0700

    If user tries to put iss in the headers, return an error
    (We don't have a complete list of allowed headers, but --iss is an easy
    mistake to make, it belongs in the payload)

--- backend/libbackend/jwt.ml
+++ backend/libbackend/jwt.ml
@@ -129,67 +129,80 @@ let encode_es256
     ~(pkey : string)
     ~(extra_headers : (string * Yojson.Safe.t) list)
     ~(payload : (string * Yojson.Safe.t) list) : (string, string) Result.t =
-  (* the jwt cmd line program only allows setting the --kid header,
+  let disallowed_extra_headers =
+    extra_headers
+    |> List.filter ~f:(fun (k, _) -> List.mem ["iss"] k ~equal:( = ))
+    |> List.map ~f:(fun (k, _) -> k)
+  in
+  (* I bet Result.combine_errors would be cleaner here *)
+  if not (List.is_empty disallowed_extra_headers)
+  then
+    Error
+      ( "You provided one or more invalid headers (possibly they belong in the payload?): "
+      ^ String.concat disallowed_extra_headers ~sep:"," )
+  else
+    (* the jwt cmd line program only allows setting the --kid header,
    * so we only extract that right now, even though there are theoretically
    * other valid headers in a JWT *)
-  let headers_to_args (headers : (string * Yojson.Safe.t) list) : string list =
-    match Map.of_alist (module String) extra_headers with
-    | `Duplicate_key _ ->
-        []
-    | `Ok headers_map ->
-      ( match Map.find headers_map "kid" with
-      | None ->
+    let headers_to_args (headers : (string * Yojson.Safe.t) list) : string list
+        =
+      match Map.of_alist (module String) extra_headers with
+      | `Duplicate_key _ ->
           []
-      | Some (`String kid) ->
-          ["--kid"; kid]
-      | Some (`Int kid) ->
-          ["--kid"; string_of_int kid]
-      | Some _ ->
-          [] )
-  in
-  let payload_to_args (payload : (string * Yojson.Safe.t) list) : string list =
-    List.concat_map payload ~f:(fun (k, v) ->
-        match v with
-        | `String v ->
-            ["-P"; k ^ "=" ^ v]
-        | `Int v ->
-            ["-P"; k ^ "=" ^ string_of_int v]
-        | _ ->
+      | `Ok headers_map ->
+        ( match Map.find headers_map "kid" with
+        | None ->
+            []
+        | Some (`String kid) ->
+            ["--kid"; kid]
+        | Some (`Int kid) ->
+            ["--kid"; string_of_int kid]
+        | Some _ ->
             [] )
-  in
-  let pkey64 = String.substr_replace_all ~pattern:"\n" ~with_:"" pkey in
-  (* !!!! FIXME !!!! this does no escaping and allows malicious actors full RCE *)
-  let args =
-    ["encode"]
-    @ headers_to_args extra_headers
-    @ payload_to_args payload
-    @ ["-A"; "ES256"; "--secret-b64"; pkey64]
-  in
-  let command =
-    "timeout 1 /home/dark/bin/jwt " ^ String.concat args ~sep:" "
-  in
-  Log.infO command ;
-  let inchan = Unix.open_process_in command in
-  let buf = Buffer.create 256 in
-  ( try
-      while true do
-        Buffer.add_channel buf inchan 64
-      done
-    with End_of_file -> () ) ;
-  let trim buf = buf |> Buffer.contents |> String.rstrip in
-  try
-    let status = Unix.close_process_in inchan in
-    match status with
-    | Unix.WEXITED 0 ->
-        Ok (trim buf)
-    | Unix.WEXITED n ->
-        Error (Printf.sprintf "failed to encode JWT (exit=%d)" n)
-    | Unix.WSTOPPED n | Unix.WSIGNALED n ->
-        Error (Printf.sprintf "failed to encode JWT (signal=%d)" n)
-  with Unix.Unix_error (Unix.ECHILD, b, c) ->
-    (* the child is already gone, which is probably fine, given we have something in buffer *)
-    ( match Buffer.length buf with
-    | 0 ->
-        Error "failed to encode JWT (unknown)"
-    | _ ->
-        Ok (trim buf) )
+    in
+    let payload_to_args (payload : (string * Yojson.Safe.t) list) : string list
+        =
+      List.concat_map payload ~f:(fun (k, v) ->
+          match v with
+          | `String v ->
+              ["-P"; k ^ "=" ^ v]
+          | `Int v ->
+              ["-P"; k ^ "=" ^ string_of_int v]
+          | _ ->
+              [] )
+    in
+    let pkey64 = String.substr_replace_all ~pattern:"\n" ~with_:"" pkey in
+    (* !!!! FIXME !!!! this does no escaping and allows malicious actors full RCE *)
+    let args =
+      ["encode"]
+      @ headers_to_args extra_headers
+      @ payload_to_args payload
+      @ ["-A"; "ES256"; "--secret-b64"; pkey64]
+    in
+    let command =
+      "timeout 1 /home/dark/bin/jwt " ^ String.concat args ~sep:" "
+    in
+    let inchan = Unix.open_process_in command in
+    let buf = Buffer.create 256 in
+    ( try
+        while true do
+          Buffer.add_channel buf inchan 64
+        done
+      with End_of_file -> () ) ;
+    let trim buf = buf |> Buffer.contents |> String.rstrip in
+    try
+      let status = Unix.close_process_in inchan in
+      match status with
+      | Unix.WEXITED 0 ->
+          Ok (trim buf)
+      | Unix.WEXITED n ->
+          Error (Printf.sprintf "failed to encode JWT (exit=%d)" n)
+      | Unix.WSTOPPED n | Unix.WSIGNALED n ->
+          Error (Printf.sprintf "failed to encode JWT (signal=%d)" n)
+    with Unix.Unix_error (Unix.ECHILD, b, c) ->
+      (* the child is already gone, which is probably fine, given we have something in buffer *)
+      ( match Buffer.length buf with
+      | 0 ->
+          Error "failed to encode JWT (unknown)"
+      | _ ->
+          Ok (trim buf) )
