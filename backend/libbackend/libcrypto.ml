024d9ba20a02b7f16291e0311aebcfedca2ee29c
Julian Ceipek <julian@darklang.com>
2019-10-14 14:49:38 -0700

    SHA256HMAC Dark function I'm a bit wary of blitting the digest to bytes since it discards type information, but in practical usecases (such as AWS signing) you need to use the result of one HMAC as the key for the next one in a chain.

--- backend/libbackend/libcrypto.ml
+++ backend/libbackend/libcrypto.ml
@@ -5,6 +5,11 @@ open Runtime
 open Types.RuntimeT
 module Hash = Sodium.Password_hash.Bytes
 
+let digest_to_bytes (digest: Nocrypto.Hash.digest) : Libexecution.Types.RuntimeT.RawBytes.t =
+  let len = Cstruct.len digest in
+  let bytes = Bytes.create len in
+  (Cstruct.blit_to_bytes digest 0 bytes 0 len); bytes
+
 let fns : Lib.shortfn list =
   [ (* ====================================== *)
     (* Password *)
@@ -68,4 +73,24 @@ let fns : Lib.shortfn list =
           | args ->
               fail args)
     ; ps = false
-    ; dep = false } ]
+    ; dep = false } 
+  ; { pns = ["Crypto::sha256hmac"]
+    ; ins = []
+    ; p = [par "key" TBytes; par "data" TBytes]
+    ; r = TBytes
+    ; d =
+        "Computes the 256 HMAC (hash-based message authentication code) digest of the given `key` and `data`."
+    ; f =
+        InProcess
+          (function
+          | _, [DBytes key; DBytes data] ->
+            let key = Cstruct.of_bytes key in
+            let data = Cstruct.of_bytes data in
+            (Nocrypto.Hash.SHA256.hmac ~key data)
+            |> digest_to_bytes
+            |> DBytes
+          | args ->
+              fail args)
+    ; ps = false
+    ; dep = false }
+    ]
