9bb0f42ebf8cf7599e373943d0c2909cab222a02
Julian Ceipek <julian@darklang.com>
2019-10-17 23:19:04 -0700

    (FIXME) Prep work part 2 for fixing fe/be `DBytes` divergence and also enable B64 encoding of bytes TODO: naming needs to be improved and we should ideally make `js_uint8Array` be the primary type instead of also using `js_arrayBuffer` as an input + Implement B64<>Bytes.t on frontend and backend via `Libtarget.base64_bytes` and `Libtarget.bytes_from_base64`

--- backend/libtarget/libtarget.mli
+++ backend/libtarget/libtarget.mli
@@ -3,6 +3,13 @@ val digest384 : string -> string
 
 (* Returns base64 encoding of the sha384 of the bytes *)
 val digest384_bytes : Bytes.t -> string
+
+(* Returns base64 encoding of the bytes *)
+val base64_bytes : Bytes.t -> string
+
+(* Decodes the provided base64 string, getting bytes from it *)
+val bytes_from_base64 : string -> Bytes.t
+
 (* Returns base64 encoding of the sha256 of the string *)
 val digest256 : string -> string
 
