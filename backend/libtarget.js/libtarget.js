19a7e491c893dccd9a8baeb4c0a1e1b93c910f8a
Julian Ceipek <julian@darklang.com>
2019-10-17 22:41:41 -0700

    (FIXME) Prep work for `Libtarget` `digest384_bytes`, `base64_bytes`, `bytes_from_base64` needed to fix fe/be `DBytes` divergence + Implement (very bad implementation) javascript functionality for b64 encoding and decoding array buffers TODO: This needs to be seriously rewritten, possibly with an npm dependency (see comments). It would also probably be better if the functions returned/accepted `Uint8Array` instead of `ArrayBuffer`

--- backend/libtarget.js/libtarget.js
+++ backend/libtarget.js/libtarget.js
@@ -1,3 +1,71 @@
+//Provides: dark_arrayBuffer_from_b64
+function dark_arrayBuffer_from_b64(base64) {
+  // TODO(JULIAN): Actually import https://github.com/niklasvh/base64-arraybuffer/blob/master/lib/base64-arraybuffer.js as a lib and use decode here
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+  // Use a lookup table to find the index.
+  var lookup = new Uint8Array(256);
+  for (var i = 0; i < chars.length; i++) {
+    lookup[chars.charCodeAt(i)] = i;
+  }
+
+
+  var bufferLength = base64.length * 0.75, len = base64.length, i, p = 0, encoded1, encoded2, encoded3, encoded4;
+
+  if (base64[base64.length - 1] === "=") {
+    bufferLength--;
+    if (base64[base64.length - 2] === "=") {
+      bufferLength--;
+    }
+  }
+
+  var arraybuffer = new ArrayBuffer(bufferLength),
+  bytes = new Uint8Array(arraybuffer);
+
+  for (i = 0; i < len; i+=4) {
+    encoded1 = lookup[base64.charCodeAt(i)];
+    encoded2 = lookup[base64.charCodeAt(i+1)];
+    encoded3 = lookup[base64.charCodeAt(i+2)];
+    encoded4 = lookup[base64.charCodeAt(i+3)];
+
+    bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
+    bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+    bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+  }
+
+  return arraybuffer;
+}
+
+//Provides: dark_arrayBuffer_to_b64
+function dark_arrayBuffer_to_b64(arraybuffer) {
+  // TODO(JULIAN): Actually import https://github.com/niklasvh/base64-arraybuffer/blob/master/lib/base64-arraybuffer.js as a lib and use encode here
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+  // Use a lookup table to find the index.
+  var lookup = new Uint8Array(256);
+  for (var i = 0; i < chars.length; i++) {
+    lookup[chars.charCodeAt(i)] = i;
+  }
+
+    var bytes = new Uint8Array(arraybuffer),
+    i, len = bytes.length, base64 = "";
+
+    for (i = 0; i < len; i+=3) {
+      base64 += chars[bytes[i] >> 2];
+      base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+      base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+      base64 += chars[bytes[i + 2] & 63];
+    }
+
+    if ((len % 3) === 2) {
+      base64 = base64.substring(0, base64.length - 1) + "=";
+    } else if (len % 3 === 1) {
+      base64 = base64.substring(0, base64.length - 2) + "==";
+    }
+
+    return base64;
+}
+
 //Provides: dark_targetjs_digest384
 function dark_targetjs_digest384(s) {
   var b64 = analysiswrapper.sha2.SHA384(s).toString("base64");
