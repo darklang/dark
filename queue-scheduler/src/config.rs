4a0776822090cb86197f166cba44dc53374b08e2
Dean Strelau <dean@darklang.com>
2019-10-24 12:32:48 -0400

    [scheduler] Add Rollbar and error handling

--- queue-scheduler/src/config.rs
+++ queue-scheduler/src/config.rs
@@ -11,8 +11,16 @@ pub struct Database {
 }
 
 #[derive(Debug)]
+pub struct Rollbar {
+    pub enabled: bool,
+    pub environment: String,
+    pub token: String,
+}
+
+#[derive(Debug)]
 pub struct AppConfig {
     pub database: Database,
+    pub rollbar: Rollbar,
 }
 
 pub fn load() -> Result<AppConfig, ConfigError> {
@@ -20,6 +28,11 @@ pub fn load() -> Result<AppConfig, ConfigError> {
     cfg.merge(Environment::with_prefix("DARK_CONFIG"))?;
 
     Ok(AppConfig {
+        rollbar: Rollbar {
+            enabled: cfg.get_str("ROLLBAR_ENABLED").map(|s| s == "y")?,
+            environment: cfg.get_str("ROLLBAR_ENVIRONMENT")?,
+            token: cfg.get_str("ROLLBAR_POST_SERVER_ITEM")?,
+        },
         database: Database {
             user: cfg.get_str("DB_USER")?,
             password: cfg.get_str("DB_PASSWORD")?,
