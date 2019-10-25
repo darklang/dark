e2642ccba4b8aac2aba02abddc68bc62cc38ec66
Julian Ceipek <julian@darklang.com>
2019-10-25 11:16:40 -0700

    Implement Bytes::hexEncode needed for AWS signing

--- backend/libexecution/libbytes.ml
+++ backend/libexecution/libbytes.ml
@@ -19,4 +19,26 @@ let fns : Lib.shortfn list =
               fail args)
     ; ps = true
     ; dep = false }
+  ; { pns = ["Bytes::hexEncode"]
+    ; ins = []
+    ; p = [par "bytes" TBytes]
+    ; r = TStr
+    ; d = "Hex (Base16) encodes `bytes` using an uppercase alphabet. Complies with RFC 4648 section 8."
+    ; f =
+        InProcess
+          (function
+          | _, [DBytes bytes] ->     
+              let hexUppercaseLookup = "0123456789ABCDEF" in
+              let len = Bytes.length bytes in
+              let buf = Buffer.create (len * 2) in
+              for i = 0 to len-1 do
+                let byte = Bytes.unsafe_get bytes i |> int_of_char in
+                Buffer.add_char buf (String.unsafe_get hexUppercaseLookup ((byte lsr 4) land 0xF));
+                Buffer.add_char buf (String.unsafe_get hexUppercaseLookup (byte land 0xF));
+              done;
+              Buffer.contents buf |> Dval.dstr_of_string_exn
+          | args ->
+              fail args)
+    ; ps = true
+    ; dep = false }
   ]
