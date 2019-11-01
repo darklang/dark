6873a09d1dca512e51a604871242d4834605999f
Julian Ceipek <julian@darklang.com>
2019-11-01 10:08:48 -0700

    [cleanup] Simplify `fromChar`

--- client/src/FluidKeyboard.ml
+++ client/src/FluidKeyboard.ml
@@ -613,60 +613,9 @@ let fromChar (char : char) : key =
       QuestionMark
   | '@' ->
       At
-  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' ->
+  | '0'..'9' ->
       Number char
-  | 'A'
-  | 'B'
-  | 'C'
-  | 'D'
-  | 'E'
-  | 'F'
-  | 'G'
-  | 'H'
-  | 'I'
-  | 'J'
-  | 'K'
-  | 'L'
-  | 'M'
-  | 'N'
-  | 'O'
-  | 'P'
-  | 'Q'
-  | 'R'
-  | 'S'
-  | 'T'
-  | 'U'
-  | 'V'
-  | 'W'
-  | 'X'
-  | 'Y'
-  | 'Z'
-  | 'a'
-  | 'b'
-  | 'c'
-  | 'd'
-  | 'e'
-  | 'f'
-  | 'g'
-  | 'h'
-  | 'i'
-  | 'j'
-  | 'k'
-  | 'l'
-  | 'm'
-  | 'n'
-  | 'o'
-  | 'p'
-  | 'q'
-  | 'r'
-  | 's'
-  | 't'
-  | 'u'
-  | 'v'
-  | 'w'
-  | 'x'
-  | 'y'
-  | 'z' ->
+  | 'A'..'Z' | 'a'..'z' ->
       Letter char
   | _ ->
       Unknown (String.fromChar char)
