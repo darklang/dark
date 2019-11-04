a2c405bac1cf09816f927e3d2182fe0104545ce1
Dean Strelau <dean@darklang.com>
2019-11-04 16:00:20 -0500

    [scheduler] Pause a handler by adding a rule.

--- queue-scheduler/tests/scheduler.rs
+++ queue-scheduler/tests/scheduler.rs
@@ -1,46 +1,81 @@
 mod helpers;
 
-use slog::o;
-use std::sync::Arc;
-
+use failure::{ensure, Error};
 use helpers::*;
-use scheduler::Looper;
 
 #[test]
-fn schedules_new_events() {
-    let log = Arc::new(slog::Logger::root(slog::Discard {}, o!()));
+fn schedules_new_events() -> Result<(), Error> {
+    let (conn, mut looper) = helpers::setup()?;
+    let (canvas_id, account_id) = helpers::setup_canvas(&conn)?;
 
-    let lconn = helpers::setup_database(log.clone()).unwrap();
-    let mut looper = Looper::new(lconn, log.clone());
-
-    let conn = helpers::setup_database(log.clone()).unwrap();
-    let (canvas_id, account_id) = helpers::setup_canvas(&conn).unwrap();
-
-    let current_event_id =
-        insert_event(&conn, &canvas_id, &account_id, "CURRENT_TIMESTAMP").unwrap();
+    let current_event_id = insert_event(
+        &conn,
+        &canvas_id,
+        &account_id,
+        "worker-1",
+        "CURRENT_TIMESTAMP",
+    )?;
 
     let delayed_event_id = insert_event(
         &conn,
         &canvas_id,
         &account_id,
+        "worker-name",
         "CURRENT_TIMESTAMP + INTERVAL '1 MINUTE'",
-    )
-    .unwrap();
+    )?;
 
-    // run one iteration of scheduling
-    looper.tick().unwrap();
+    looper.tick()?;
 
-    assert_eq!(
-        "scheduled",
-        get_event_status(&conn, current_event_id).unwrap(),
+    ensure!(
+        "scheduled" == get_event_status(&conn, current_event_id)?,
         "schedules the new, undelayed event (event = {})",
         current_event_id
     );
 
-    assert_eq!(
-        "new",
-        get_event_status(&conn, delayed_event_id).unwrap(),
+    ensure!(
+        "new" == get_event_status(&conn, delayed_event_id)?,
         "does not schedule the delayed event (event = {})",
         delayed_event_id
     );
+
+    Ok(())
+}
+
+#[test]
+fn does_not_schedule_paused_events() -> Result<(), Error> {
+    let (conn, mut looper) = helpers::setup()?;
+    let (canvas_id, account_id) = helpers::setup_canvas(&conn)?;
+
+    insert_pause_rule(&conn, &canvas_id, "worker-2")?;
+
+    let evt1 = insert_event(
+        &conn,
+        &canvas_id,
+        &account_id,
+        "worker-1",
+        "CURRENT_TIMESTAMP",
+    )?;
+    let evt2 = insert_event(
+        &conn,
+        &canvas_id,
+        &account_id,
+        "worker-2",
+        "CURRENT_TIMESTAMP",
+    )?;
+
+    looper.tick()?;
+
+    ensure!(
+        "scheduled" == get_event_status(&conn, evt1)?,
+        "schedules the un-paused event (event = {})",
+        evt1
+    );
+
+    ensure!(
+        "new" == get_event_status(&conn, evt2)?,
+        "does not schedule the paused event (event = {})",
+        evt1
+    );
+
+    Ok(())
 }
