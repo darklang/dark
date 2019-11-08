d05b5372057ed14142abb722764c8567b4398c67
Ian Smith <ismith@metaforgotten.org>
2019-11-08 00:51:50 -0500

    No arbitrary length limits; it doesn't bring us performance benefits anyway
    Cites:
    https://www.depesz.com/2010/03/02/charx-vs-varcharx-vs-varchar-vs-text/
    
    Trello mentions canvases.name, but I also searched:
    ```
    select table_catalog, table_schema, table_name, column_name from in
    formation_schema.columns where data_type = 'character varying' and
    table_schema != 'information_schema';
    ```
    and got access.permission, accounts.{username,name,email,password},
    static_asset_deploys.{deploy_hash,branch}.
    
    Since the representation of varchar and text is the same under the hood
    (see cite above), I expect conversion to be quick. For confirmation,
    `ALTER TABLE canvases ALTER COLUMN name TYPE text` took 0.011s on
    prodclone. Other tables in the list above took similar lengths of time.
    
    https://trello.com/c/YUADvYE6/1936-we-leak-sql-errors-when-using-a-canvas-name-that-is-too-long

---
+++ backend/migrations/20191108_055938_replace-varchars-with-text.sql
@@ -0,0 +1,9 @@
+alter table canvases alter column name type text;
+alter table static_asset_deploys alter column branch type text;
+alter table static_asset_deploys alter column deploy_hash type text;
+alter table access alter column permission type text;
+alter table accounts alter column username type text;
+alter table accounts alter column name type text;
+alter table accounts alter column email type text;
+alter table accounts alter column password type text
+
