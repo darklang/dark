65815ac179ae9ac77cdd15fa4df62a98f400e660
Julian Ceipek <julian@darklang.com>
2019-12-04 14:09:27 -0800

    [Needs improvement] First pass at making explicit backspace on TFieldOp I'm worried about the precedent this sets in terms of: - using a ref in a function that is passed around - relying on knowledge of what parents a type can have, bypassing the   type system and setting the stage for future bugs

--- client/src/Fluid.ml
+++ client/src/Fluid.ml
@@ -1819,6 +1819,20 @@ let removeField (id : id) (ast : ast) : ast =
       | _ ->
           recover "not a fieldAccess in removeField" e e )
 
+(* removeFieldProvidingParent takes an ast and the id of a field access
+   and produces the "parent" that was accessed and a newAST without that field access.
+   For eample, given the id of .baz in ((foo).bar).baz, it returns ((foo).bar), newAST without .baz *)
+let removeFieldProvidingParent (id : id) (ast : ast) : (fluidExpr * ast) =
+  let parentExpr = ref ast in
+  let newAst = (updateExpr id ast ~f:(fun e ->
+      match e with
+      | EFieldAccess (_, faExpr, _, _) ->
+          parentExpr := faExpr;
+          faExpr
+      | _ ->
+          (parentExpr := e;
+          recover "not a fieldAccess in removeFieldProvidingParent" e e ))) in
+  (!parentExpr, newAst)
 
 (* ---------------- *)
 (* Lambdas *)
@@ -2851,6 +2865,14 @@ let posFromCaretTarget (s : state) (ast : ast) (ct : caretTarget) : int =
           , ARRecord (targetId, RPFieldname fieldIndex) )
           when id = targetId && tIndex = fieldIndex ->
             true
+        | ( TFieldName (id, _, _)
+          , ARFieldAccess (targetId, FAPFieldname) )
+          when id = targetId ->
+            true
+        | ( TVariable (id, _)
+          , ARVariable targetId )
+          when id = targetId ->
+            true
         | _ ->
             false )
   in
@@ -2863,6 +2885,14 @@ let posFromCaretTarget (s : state) (ast : ast) (ct : caretTarget) : int =
         ct
         s.newPos
 
+(* getCaretTargetForLastPartOfExpr takes a fluidExpr and produces a caretTarget corresponding to the "very end"
+  of that fluidExpr. The concept of "very end" is related to an understanding of the tokenization of the expr,
+  even though this function doesn't explicitly depend on any tokenization functions. *)
+let getCaretTargetForLastPartOfExpr (expr: fluidExpr) : caretTarget = 
+  match expr with
+    | EVariable (id, str) -> {astRef=ARVariable id; offset=String.length str}
+    | EFieldAccess (id, _, _, fieldName) -> {astRef=ARFieldAccess (id, FAPFieldname); offset=String.length fieldName}
+    | _ -> recover "we don't yet support getCaretTargetForLastPartOfExpr for this" expr { astRef=ARNull (ID "unhandled"); offset=0 }
 
 type newPosition =
   | RightOne
@@ -3012,7 +3042,8 @@ let doBackspace ~(pos : int) (ti : tokenInfo) (ast : ast) (s : state) :
     | TNewline _ ->
         (ast, Exactly ti.startPos)
     | TFieldOp id ->
-        (removeField id ast, LeftOne)
+        let (parent, newAst) = removeFieldProvidingParent id ast in
+        (newAst, AtTarget (getCaretTargetForLastPartOfExpr parent))
     | TFloatPoint id ->
         (removePointFromFloat id ast, LeftOne)
     | TPatternFloatPoint (mID, id) ->
