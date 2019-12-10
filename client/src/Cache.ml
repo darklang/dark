6a3f5ed6c40c7b274a6a5af269a1a8e2739df1f5
Ian Smith <ismith@metaforgotten.org>
2019-12-10 09:37:14 -0800

    Add comment explaining Cache.ml
    Also, move enabled into a match instead of an if, so we have a single
    conditional for "do I check the cache or just run expensiveFunction".
    And rename enabled -> is_caching_enabled, I think it makes it clearer.

--- client/src/Cache.ml
+++ client/src/Cache.ml
@@ -1,15 +1,27 @@
 open Tc
 
-let enabled = true
+let is_caching_enabled = true
+
+(* Explanatory comments:
+ * cache funcs take a function that returns an option cache key. If the cache
+ * key has changed, or is None, then we run the expensive function (that renders
+ * some vdom); if it is unchanged from the last time (as stored by
+ * Tea_html.lazy1), then use the cached value.
+ *
+ * Note: one use of None as a cache key is "this TL is where the cursor is,
+ * don't use the cached value". (At the time this comment is being written, we
+ * only use this cache functionality to wrap TL rendering.) *)
 
 let cache1m
     (keyFn : 'a -> 'b option) (expensiveFn : 'a -> 'msg Vdom.t) (arg1 : 'a) :
     'msg Vdom.t =
   let eFn () = expensiveFn arg1 in
   let keyFn k = keyFn k |> Option.andThen ~f:Js.Json.stringifyAny in
-  if not enabled
-  then eFn ()
-  else match keyFn arg1 with Some k -> Tea_html.lazy1 k eFn | _ -> eFn ()
+  match (is_caching_enabled, keyFn arg1) with
+  | true, Some k ->
+      Tea_html.lazy1 k eFn
+  | _ ->
+      eFn ()
 
 
 let cache1 (keyFn : 'a -> 'b) (expensiveFn : 'a -> 'msg Vdom.t) (arg1 : 'a) :
@@ -24,10 +36,11 @@ let cache2m
     (arg2 : 'b) : 'msg Vdom.t =
   let keyFn k1 k2 = keyFn k1 k2 |> Option.andThen ~f:Js.Json.stringifyAny in
   let eFn () = expensiveFn arg1 arg2 in
-  if not enabled
-  then eFn ()
-  else
-    match keyFn arg1 arg2 with Some k -> Tea_html.lazy1 k eFn | _ -> eFn ()
+  match (is_caching_enabled, keyFn arg1 arg2) with
+  | true, Some k ->
+      Tea_html.lazy1 k eFn
+  | _ ->
+      eFn ()
 
 
 let cache2
