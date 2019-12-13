39f96fc4b94fcbcb83d3c4fdc100263a184d9e51
Dean Strelau <dean@darklang.com>
2019-12-13 14:39:02 -0500

    fix all keypress handling to use key

--- client/src/FluidKeyboard.ml
+++ client/src/FluidKeyboard.ml
@@ -414,6 +414,11 @@ let fromKeyboardEvent
   (****************
    * Single Chars *
    ****************)
+  (* special-case alt-x on mac, which comes in like this *)
+  (* for now, you cannot type non-ASCII chars, but when you can we can't just
+   * magically make this an 'x'. *)
+  | {js|≈|js} ->
+      Letter 'x'
   | _ when String.length key = 1 ->
       Char.fromString key
       |> Option.map ~f:fromChar
