5c5c3afc518dd21854d50a2639618c39833c5781
Paul Biggar <paul.biggar@gmail.com>
2019-12-29 19:02:32 -0800

    Delete old feature flag code
    None of it is salvageable in fluid, so just delete it

--- client/src/FeatureFlags.ml
+++ client/src/FeatureFlags.ml
@@ -23,55 +23,19 @@ let fromFlagged (pick : pick) (expr : expr) : expr =
       recover "cant convert flagged to flagged" ~debug:expr expr
 
 
-let wrap (_m : model) (tl : toplevel) (pd : blankOrData) : modification =
-  let msgId = gid () in
-  let newPd = P.exprmap (toFlagged msgId) pd in
-  let newTL = TL.replace pd newPd tl in
-  let focus = FocusExact (TL.id tl, msgId) in
-  match newTL with
-  | TLHandler h ->
-      RPC ([SetHandler (h.hTLID, h.pos, h)], focus)
-  | TLFunc f ->
-      RPC ([SetFunction f], focus)
-  | _ ->
-      NoChange
+let wrap (_m : model) (_tl : toplevel) (_pd : blankOrData) : modification =
+  (* TODO: needs to be reimplmented in fluid *)
+  NoChange
 
 
-let start (m : model) : modification =
-  match unwrapCursorState m.cursorState with
-  | Selecting (tlid, Some id) ->
-      let tl = TL.get m tlid in
-      let pd = Option.andThen ~f:(fun tl -> TL.find tl id) tl in
-      ( match (tl, pd) with
-      | Some tl, Some pd ->
-          wrap m tl pd
-      | _ ->
-          recover
-            "invalid tl and id for starting feature flag"
-            ~debug:(tl, pd)
-            NoChange )
-  | _ ->
-      NoChange
+let start (_m : model) : modification =
+  (* TODO: needs to be reimplmented in fluid *)
+  NoChange
 
 
-let end_ (m : model) (id : id) (pick : pick) : modification =
-  match
-    tlidOf (unwrapCursorState m.cursorState) |> Option.andThen ~f:(TL.get m)
-  with
-  | None ->
-      NoChange
-  | Some tl ->
-      let pd = TL.find tl id |> AST.recoverPD "FF.end" in
-      let newPd = P.exprmap (fromFlagged pick) pd in
-      let newTL = TL.replace pd newPd tl in
-      let focus = FocusExact (TL.id tl, P.toID newPd) in
-      ( match newTL with
-      | TLHandler h ->
-          RPC ([SetHandler (h.hTLID, h.pos, h)], focus)
-      | TLFunc f ->
-          RPC ([SetFunction f], focus)
-      | _ ->
-          recover "ending FF on invalid handler" ~debug:(tl, id) NoChange )
+let end_ (_m : model) (_id : id) (_pick : pick) : modification =
+  (* TODO: needs to be reimplmented in fluid *)
+  NoChange
 
 
 let toggle (id : id) (isExpanded : bool) : modification =
