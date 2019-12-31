45ded339d4d7546a6b28c90e5920fe491bbfdd26
Paul Biggar <paul.biggar@gmail.com>
2019-12-30 18:02:13 -0800

    Remove the word fluid from some places, now that all Exprs are fluid

--- client/__tests__/autocomplete_test.ml
+++ client/__tests__/autocomplete_test.ml
@@ -28,9 +28,9 @@ let defaultID = gid ()
 
 let defaultID2 = gid ()
 
-let defaultExpr = Blank defaultID
+let defaultBlankOr = Blank defaultID
 
-let defaultFluidExpr = EBlank defaultID
+let defaultExpr = EBlank defaultID
 
 let fillingCS ?(tlid = defaultTLID) ?(id = defaultID) () : cursorState =
   Entering (Filling (tlid, id))
@@ -61,7 +61,7 @@ let defaultModel
 
 let aHandler
     ?(tlid = defaultTLID)
-    ?(expr = defaultFluidExpr)
+    ?(expr = defaultExpr)
     ?(space : string option = None)
     ?(name : string option = None)
     ?(modifier : string option = None)
@@ -76,7 +76,7 @@ let aHandler
 
 let aFunction
     ?(tlid = defaultTLID)
-    ?(expr = defaultFluidExpr)
+    ?(expr = defaultExpr)
     ?(params = [])
     ?(name = "myFunc")
     () : userFunction =
