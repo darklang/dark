d6a819620283e764cbc6c8e1ebd79ae8585cad5f
Paul Biggar <paul.biggar@gmail.com>
2020-01-01 20:28:08 -0800

    Put WebURL into Native

--- client/src/util/Native.ml
+++ client/src/util/Native.ml
@@ -357,6 +357,26 @@ module Decoder = struct
             Ok (Js.Json.stringify j))
 end
 
+module Url = struct
+  type t =
+    < hash : string
+    ; host : string
+    ; hostname : string
+    ; href : string
+    ; origin : string
+    ; password : string
+    ; pathname : string
+    ; port : string
+    ; protocol : string
+    ; search : string
+    ; username : string >
+    Js.t
+
+  external make_internal : string -> t = "URL" [@@bs.new]
+
+  let make s = try Some (make_internal s) with _ -> None
+end
+
 module Rollbar = struct
   type rollbar_mod = < init : Js.Json.t -> unit [@bs.meth] > Js.t
 
