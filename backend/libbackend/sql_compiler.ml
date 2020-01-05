c35e29b15a3e68d35ac1c58ae5fd14e25036f7e2
Paul Biggar <paul.biggar@gmail.com>
2020-01-04 18:05:39 -0800

    Use Prelude, not Tablecloth

--- backend/libbackend/sql_compiler.ml
+++ backend/libbackend/sql_compiler.ml
@@ -64,12 +64,12 @@ let tipe_to_sql_tipe (t : tipe_) : string =
       Exception.internal ("unsupported DB field tipe" ^ show_tipe_ t)
 
 
-let rec inline (symtable : expr Tablecloth.StrDict.t) (expr : expr) : expr =
+let rec inline (symtable : expr Prelude.StrDict.t) (expr : expr) : expr =
   match expr with
   | Filled (_, Let (Filled (_, name), expr, body)) ->
-      inline (Tablecloth.StrDict.insert ~key:name ~value:expr symtable) body
+      inline (Prelude.StrDict.insert ~key:name ~value:expr symtable) body
   | Filled (_, Variable name) when name <> "value" ->
-    ( match Tablecloth.StrDict.get ~key:name symtable with
+    ( match Prelude.StrDict.get ~key:name symtable with
     | Some expr ->
         expr
     | None ->
@@ -83,7 +83,7 @@ let rec canonicalize expr =
   | Filled (id, Thread []) ->
       Blank id
   | Filled (id, Thread (head :: tail)) ->
-      Tablecloth.List.foldl tail ~init:head ~f:(fun expr arg ->
+      Prelude.List.foldl tail ~init:head ~f:(fun expr arg ->
           match expr with
           | Filled (id, FnCall (name, args)) ->
               Filled (id, FnCall (name, arg :: args))
@@ -99,7 +99,7 @@ let dval_to_sql (dval : dval) : string = "null"
 let rec lambda_to_sql_inner
     (symtable : dval_map)
     (paramName : string)
-    (dbFields : tipe_ Tablecloth.StrDict.t)
+    (dbFields : tipe_ Prelude.StrDict.t)
     (expr : expr) : string =
   let lts e = lambda_to_sql_inner symtable paramName dbFields e in
   match expr with
@@ -125,7 +125,7 @@ let rec lambda_to_sql_inner
   | Filled (_, FieldAccess (Filled (_, Variable v), Filled (_, fieldname)))
     when v = paramName ->
       let tipe =
-        match Tablecloth.StrDict.get dbFields ~key:fieldname with
+        match Prelude.StrDict.get dbFields ~key:fieldname with
         | Some v ->
             v
         | None ->
@@ -144,9 +144,9 @@ let rec lambda_to_sql_inner
 let compile_lambda
     (symtable : dval_map)
     (paramName : string)
-    (dbFields : tipe_ Tablecloth.StrDict.t)
+    (dbFields : tipe_ Prelude.StrDict.t)
     (body : expr) =
   body
   |> canonicalize
-  |> inline Tablecloth.StrDict.empty
+  |> inline Prelude.StrDict.empty
   |> lambda_to_sql_inner symtable paramName dbFields
