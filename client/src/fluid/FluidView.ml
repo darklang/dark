aa37ae7d3d72379f63a67f782156b6c8a50d2cd8
Paul Biggar <paul.biggar@gmail.com>
2020-01-08 11:41:55 -0800

    Revert "Merge pull request #1791 from darklang/paul/fix-omnibox-click"
    This reverts commit a74ddb5f2ac56371e8928340eb7b3b8990bc862a, reversing
    changes made to 65c84f180f5e399b6424bac9eb47e248b9dc7ccf.

--- client/src/fluid/FluidView.ml
+++ client/src/fluid/FluidView.ml
@@ -572,10 +572,8 @@ let viewStatus (m : model) (ast : ast) (s : state) : Types.msg Html.html =
     [dtText "cursorState"; ddText (show_cursorState m.cursorState)]
   in
   let lastMod = [dtText "lastMod"; ddText (show_modification m.lastMod)] in
-  let lastMsg = [dtText "lastMsg"; ddText (show_msg m.lastMsg)] in
   let status =
-    List.concat
-      [posData; error; tokenData; actions; cursorState; lastMod; lastMsg]
+    List.concat [posData; error; tokenData; actions; cursorState; lastMod]
   in
   Html.div [Attrs.id "fluid-status"] [Html.dl [] status]
 
