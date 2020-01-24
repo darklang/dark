5b180d0feb44ac726a877d89e9da40e61e219f48
Julian Ceipek <julian@darklang.com>
2020-01-24 13:28:19 -0800

    [Autoformat]

--- integration-tests/tests.js
+++ integration-tests/tests.js
@@ -243,27 +243,34 @@ test("enter_changes_state", async t => {
 test("field_access_closes", async t => {
   await createHTTPHandler(t);
   await gotoAST(t);
-  await t.typeText("#fluid-editor", "req")
-    .expect(fluidAcHighlightedText("requestdict")).ok()
+  await t
+    .typeText("#fluid-editor", "req")
+    .expect(fluidAcHighlightedText("requestdict"))
+    .ok()
     .typeText("#fluid-editor", ".bo")
-    .expect(fluidAcHighlightedText("bodyfield")).ok()
+    .expect(fluidAcHighlightedText("bodyfield"))
+    .ok()
     .pressKey("enter");
 });
 
 test("field_access_pipes", async t => {
   await createHTTPHandler(t);
   await gotoAST(t);
-  await t.typeText("#fluid-editor", "req")
-    .expect(fluidAcHighlightedText()).contains("request")
+  await t
+    .typeText("#fluid-editor", "req")
+    .expect(fluidAcHighlightedText())
+    .contains("request")
     .typeText("#fluid-editor", ".bo")
-    .expect(fluidAcHighlightedText()) .eql("bodyfield")
+    .expect(fluidAcHighlightedText())
+    .eql("bodyfield")
     .pressKey("shift+enter");
 });
 
 test("tabbing_works", async t => {
   await createRepl(t);
   // Fill in "then" box in if stmt
-  await t.typeText("#fluid-editor", "if")
+  await t
+    .typeText("#fluid-editor", "if")
     .pressKey("space tab")
     .typeText("#fluid-editor", "5");
 });
@@ -271,16 +278,20 @@ test("tabbing_works", async t => {
 test("autocomplete_highlights_on_partial_match", async t => {
   await createRepl(t);
   await gotoAST(t);
-  await t.typeText("#fluid-editor", "nt::add")
-    .expect(fluidAcHighlightedText("Int::add")).ok()
+  await t
+    .typeText("#fluid-editor", "nt::add")
+    .expect(fluidAcHighlightedText("Int::add"))
+    .ok()
     .pressKey("enter");
 });
 
 test("no_request_global_in_non_http_space", async t => {
   await createWorkerHandler(t);
   await gotoAST(t);
-  await t.typeText("#fluid-editor", "request")
-    .expect(fluidAcHighlightedText("Http::badRequest")).ok()
+  await t
+    .typeText("#fluid-editor", "request")
+    .expect(fluidAcHighlightedText("Http::badRequest"))
+    .ok()
     .pressKey("enter");
 });
 
@@ -352,7 +363,8 @@ test("switching_from_default_repl_space_removes_name", async t => {
 test("tabbing_through_let", async t => {
   await createRepl(t);
   await gotoAST(t);
-  await t.typeText("#fluid-editor", "let")
+  await t
+    .typeText("#fluid-editor", "let")
     .pressKey("enter")
     // round trip through the let blanks once
     .pressKey("tab tab tab")
@@ -528,8 +540,10 @@ test("rename_function", async t => {
 
 test("execute_function_works", async t => {
   await createRepl(t);
-  await t.typeText("#fluid-editor", "Uuid::gen")
-    .pressKey("enter").click(Selector(".execution-button-needed"));
+  await t
+    .typeText("#fluid-editor", "Uuid::gen")
+    .pressKey("enter")
+    .click(Selector(".execution-button-needed"));
 
   let v1 = await Selector(".selected .live-value").innerText;
 
@@ -555,7 +569,8 @@ test("correct_field_livevalue", async t => {
 
 test("function_version_renders", async t => {
   await createRepl(t);
-  await t.typeText("#fluid-editor", "DB::del")
+  await t
+    .typeText("#fluid-editor", "DB::del")
     .expect(Selector(".autocomplete-item.fluid-selected .version").withText("v1"))
     .ok();
 });
