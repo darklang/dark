d6f233e0defa0189a78479f15ebd121fb7c0252b
Alice Q. Wong <alice@valour.me>
2020-01-27 16:11:31 -0800

    bring btn code in

--- client/src/toplevels/ViewUserFunction.ml
+++ client/src/toplevels/ViewUserFunction.ml
@@ -67,6 +67,57 @@ let viewKillParameterBtn (uf : userFunction) (p : userFunctionParameter) :
   | _ ->
       buttonContent true
 
+let viewExecuteBtn (vs : viewState) (fn : userFunction) : msg Html.html =
+  let readyClass = match vs.analysisStore with
+    | LoadableSuccess _ -> "ready"
+    | LoadableNotInitialized -> "no-data"
+    | LoadableLoading _ -> "wait"
+    | LoadableError _ -> "error"
+  in
+  let events =
+    ViewUtils.eventNoPropagation
+      ~key:("run-fun" ^ "-" ^ showTLID fn.ufTLID)
+      "click"
+      (fun _ -> Debug.loG "exe fun" vs.analysisStore; IgnoreMsg )
+  in
+  Html.div [Html.class' ("execution-button " ^ readyClass); events] [fontAwesome "redo"]
+  (*
+  if vs.permission = Some ReadWrite
+  then
+    let hasData =
+      Analysis.selectedTrace vs.tlTraceIDs vs.traces vs.tlid
+      |> Option.andThen ~f:(fun trace_id ->
+              List.find ~f:(fun (id, _) -> id = trace_id) vs.traces
+              |> Option.andThen ~f:(fun (_, data) -> data |> Result.toOption))
+      |> Option.is_some
+    in
+    let classes =
+      Html.classList
+        [ ("handler-trigger", true)
+        ; ("is-executing", handlerIsExecuting vs)
+        ; ("inactive", not hasData)
+        ; ("complete", handlerIsExeComplete vs)
+        ; ("failed", handlerIsExeFail vs) ]
+    in
+    let attrs =
+      if hasData
+      then
+        [ Html.title "Replay this execution"
+        ; ViewUtils.eventNoPropagation
+            ~key:("lh" ^ "-" ^ showTLID vs.tlid)
+            "click"
+            (fun _ -> TriggerHandler vs.tlid)
+        ; ViewUtils.onAnimationEnd
+            ~key:("exe" ^ "-" ^ showTLID vs.tlid)
+            ~listener:(fun name ->
+              if name = "fadeIn"
+              then SetHandlerExeIdle vs.tlid
+              else IgnoreMsg) ]
+      else [Html.title "Need input data to replay execution"]
+    in
+    Html.div (classes :: attrs) [fontAwesome "redo"]
+  else Vdom.noNode
+  *)
 
 let viewParam (fn : userFunction) (vs : viewState) (p : userFunctionParameter) :
     msg Html.html =
@@ -120,7 +171,7 @@ let viewMetadata (vs : viewState) (fn : userFunction) : msg Html.html =
       [Html.class' "spec-header"]
       [ ViewUtils.svgIconFn "#7dcac0"
       ; viewUserFnName vs [wc "fn-name-content"] fn.ufMetadata.ufmName
-      ; Html.div [Html.class' "fn-actions"] [menuView] ]
+      ; Html.div [Html.class' "fn-actions"] [viewExecuteBtn vs fn; menuView] ]
   in
   let paramRows =
     let params = fn.ufMetadata.ufmParameters |> List.map ~f:(viewParam fn vs) in
