6b370c3216fdf423cde859f4eaaf70366c4c147e
Paul Biggar <paul.biggar@gmail.com>
2020-01-28 22:16:20 -0800

    Support rollbar sigs on the server

--- backend/libunshared/Unshared.ml
+++ backend/libunshared/Unshared.ml
@@ -1,1 +1,13 @@
 let gid () : Base.Int63.t = Base.Int63.random Base.Int63.max_value
+
+type jsonType = Yojson.Safe.t
+
+module Rollbar = struct
+  (* Do nothing for now *)
+  let init _ = ()
+
+  let send (msg : string) (url : string option) _custom : unit = ()
+end
+
+let reportError (msg : string) (msgVal : 'm) : unit =
+  Rollbar.send msg None `Null
--- backend/libunshared/Unshared.mli
+++ backend/libunshared/Unshared.mli
@@ -1,1 +1,11 @@
 val gid : unit -> UnsharedTypes.id
+
+type jsonType = Yojson.Safe.t
+
+module Rollbar : sig
+  val init : jsonType -> unit
+
+  val send : string -> string option -> jsonType -> unit
+end
+
+val reportError : string -> 'm -> unit
