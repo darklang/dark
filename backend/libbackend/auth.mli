a81de43f594325a110f2e21f77fab2a9b782ecc8
Ian Smith <ismith@metaforgotten.org>
2020-01-29 11:29:44 -0800

    Rename Auth.Session Auth.SessionLwt, and add a non-lwt Auth.SessionSync

--- backend/libbackend/auth.mli
+++ backend/libbackend/auth.mli
@@ -1,6 +1,6 @@
 open Core
 
-module Session : sig
+module SessionLwt : sig
   module Backend = Session_postgresql_lwt
 
   include module type of Session
@@ -15,15 +15,21 @@ module Session : sig
 
   val new_for_username : Account.username -> t io
 
-  val new_for_username_sync : Account.username -> (string, exn) Result.t
-
-  val username_for : t -> Account.username
-
-  val csrf_token_for : t -> string
-
   val username_of_key : string -> string option
 
   val random_string : int -> string
 
   val session_data : string -> string
+
+  val username_for : t -> Account.username
+
+  val csrf_token_for : t -> string
+end
+
+module SessionSync : sig
+  module Backend = Session_postgresql
+
+  val new_for_username : Account.username -> string
+
+  val username_of_key : string -> string option
 end
--- backend/libbackend/libdarkinternal.ml
+++ backend/libbackend/libdarkinternal.ml
@@ -830,7 +830,7 @@ that's already taken, returns an error."
         internal_fn (function
             | _, [DStr sessionKey] ->
                 let sessionKey = sessionKey |> Unicode_string.to_string in
-                ( match Auth.Session.username_of_key sessionKey with
+                ( match Auth.SessionSync.username_of_key sessionKey with
                 | None ->
                     DResult
                       (ResError
@@ -1290,33 +1290,39 @@ that's already taken, returns an error."
                          (Dval.dstr_of_string_exn
                             ("No user '" ^ username ^ "'")))
                 | Some _user_id ->
-                  ( match Auth.Session.new_for_username_sync username with
-                  | Ok session_key ->
-                      DResult (ResOk (Dval.dstr_of_string_exn session_key))
-                  | Error e ->
-                      (* If session creation fails, log and rollbar *)
-                      let err = Libexecution.Exception.exn_to_string e in
-                      Log.erroR
-                        "DarkInternal::newSessionForUsername"
-                        ~params:[("username", username); ("exception", err)] ;
-                      let bt = Libexecution.Exception.get_backtrace () in
-                      ( match
-                          Rollbar.report
-                            e
-                            bt
-                            (Other "Darklang")
-                            (exec_state.execution_id |> Types.string_of_id)
-                        with
-                      | `Success | `Disabled ->
-                          ()
-                      | `Failure ->
-                          Log.erroR
-                            "rollbar.report at DarkInternal::newSessionForUsername"
-                      ) ;
-                      DResult
-                        (ResError
-                           (Dval.dstr_of_string_exn "Failed to create session"))
-                  ) )
+                    let session_key =
+                      try
+                        Auth.SessionSync.new_for_username username
+                        |> Result.return
+                      with e -> Result.fail e
+                    in
+                    ( match session_key with
+                    | Ok session_key ->
+                        DResult (ResOk (Dval.dstr_of_string_exn session_key))
+                    | Error e ->
+                        (* If session creation fails, log and rollbar *)
+                        let err = Libexecution.Exception.exn_to_string e in
+                        Log.erroR
+                          "DarkInternal::newSessionForUsername"
+                          ~params:[("username", username); ("exception", err)] ;
+                        let bt = Libexecution.Exception.get_backtrace () in
+                        ( match
+                            Rollbar.report
+                              e
+                              bt
+                              (Other "Darklang")
+                              (exec_state.execution_id |> Types.string_of_id)
+                          with
+                        | `Success | `Disabled ->
+                            ()
+                        | `Failure ->
+                            Log.erroR
+                              "rollbar.report at DarkInternal::newSessionForUsername"
+                        ) ;
+                        DResult
+                          (ResError
+                             (Dval.dstr_of_string_exn
+                                "Failed to create session")) ) )
             | args ->
                 fail args)
     ; ps = false
