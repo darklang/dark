9209365fa5101e6c014fca3609a337b2a170f26f
Julian Ceipek <julian@darklang.com>
2020-01-28 16:03:12 -0800

    Fix pluralization

--- client/src/canvas/ViewModal.ml
+++ client/src/canvas/ViewModal.ml
@@ -11,7 +11,7 @@ let viewMessage : msg Html.html =
     [ Html.p
         [Html.class' "title"]
         [ Html.text
-            "Unfortunately we only support Dark on desktop Chrome right now. Between browser different input models, difference in scripting and rendering performance, and differing web platform support, we don't have the capacity to support other browsers at the moment. We hope to support Firefox, Safari, and mobile use once we've really nailed the experience on Chrome. Thanks for understanding!"
+            "Unfortunately we only support Dark on desktop Chrome right now. Between browser different input models, differences in scripting and rendering performance, and differing web platform support, we don't have the capacity to support other browsers at the moment. We hope to support Firefox, Safari, and mobile use once we've really nailed the experience on Chrome. Thanks for understanding!"
         ] ]
 
 
