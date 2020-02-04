8c437488f7480f0cce9877f5c1773178f4c4eb95
Ian Smith <ismith@metaforgotten.org>
2020-02-04 14:02:28 -0800

    Add comment

--- backend/libbackend/config.mli
+++ backend/libbackend/config.mli
@@ -64,4 +64,6 @@ val static_assets_bucket : string option
 
 val build_hash : string
 
+(* Do we use the new login service - login.darklang.com - or depend on local
+ * sessions in postgres only *)
 val use_login_darklang_com_for_login : bool
