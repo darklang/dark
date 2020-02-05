8ab0f64180b761795737f348481941c4bdf653ab
Julian Ceipek <julian@darklang.com>
2020-02-04 17:13:38 -0800

    Modularize

--- client/src/core/Types.ml
+++ client/src/core/Types.ml
@@ -483,10 +483,60 @@ type caretTarget =
   ; offset : int }
 [@@deriving show {with_path = false}]
 
+module CaretTarget = struct
+  type t = caretTarget
+
+  (** [forARStringOpenQuote id offset] produces an ARString caretTarget
+  * pointing to an [offset] into the open " of the string with [id].
+  * [offset] may NOT be negative as it cannot represent something out of string bounds. *)
+  let forARStringOpenQuote (id : id) (offset : int) : t =
+    {astRef = ARString (id, SPOpenQuote); offset}
+
+
+  (** [forARStringText id offset] produces an ARString caretTarget
+  * pointing to an [offset] into the text of the string with [id].
+  * [offset] may be negative but cannot represent something out of string bounds. *)
+  let forARStringText (id : id) (offset : int) : t =
+    {astRef = ARString (id, SPOpenQuote); offset = 1 + offset}
+
+
+  (** [forARStringCloseQuote id offset] produces an ARString caretTarget
+  * pointing to an [offset] into the close " of the string with [id]. It uses the
+  * [fullStr] of the string (excluding visual quotes) to compute the target.
+  * [offset] may be negative but cannot represent something out of string bounds. *)
+  let forARStringCloseQuote (id : id) (offset : int) (fullStr : string)
+      : t =
+    let lenPlusOpenQuote = 1 + String.length fullStr in
+    {astRef = ARString (id, SPOpenQuote); offset = lenPlusOpenQuote + offset}
+
+
+  (** [forPPStringOpenQuote id offset] produces an ARPattern PPString caretTarget
+  * pointing to an [offset] into the open " of the pattern string with [id].
+  * [offset] may NOT be negative as it cannot represent something out of string bounds. *)
+  let forPPStringOpenQuote (id : id) (offset : int) : t =
+    {astRef = ARPattern (id, PPString SPOpenQuote); offset}
+
+  (** [forPPStringText id offset] produces an ARPattern PPString caretTarget
+  * pointing to an [offset] into the text of the pattern string with [id].
+  * [offset] may be negative but cannot represent something out of string bounds. *)
+  let forPPStringText (id : id) (offset : int) : t =
+    {astRef = ARPattern (id, PPString SPOpenQuote); offset = 1 + offset}
+
+  (** [forARStringCloseQuote id offset] produces an ARPattern PPString caretTarget
+  * pointing to an [offset] into the close " of the pattern string with [id]. It uses the
+  * [fullStr] of the string (excluding visual quotes) to compute the target.
+  * [offset] may be negative but cannot represent something out of string bounds. *)
+  let forPPStringCloseQuote (id : id) (offset : int) (fullStr : string)
+      : t =
+    let lenPlusOpenQuote = 1 + String.length fullStr in
+    { astRef = ARPattern (id, PPString SPOpenQuote)
+    ; offset = lenPlusOpenQuote + offset }
+end
+
 (* ----------------------------- *)
 (* Mouse *)
 (* ----------------------------- *)
-and mouseEvent =
+type mouseEvent =
   { mePos : vPos
   ; button : int
   ; altKey : bool
