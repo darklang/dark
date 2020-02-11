3255ac370d9c1d0958fcc3b9531e8acecd7d4061
Paul Biggar <paul.biggar@gmail.com>
2020-02-11 11:54:52 -0800

    Better names for what we're trying to do now

--- backend/libexecution/analysis_types.ml
+++ backend/libexecution/analysis_types.ml
@@ -24,16 +24,19 @@ let ht_to_json_dict ds ~f =
   `Assoc (List.map ~f:(fun (id, v) -> (string_of_id id, f v)) alist)
 
 
-type dval_store = dval IDTable.t
+type execution_info = dval
 
-let dval_store_to_yojson (ds : dval_store) : Yojson.Safe.t =
+type intermediate_result_store = execution_info IDTable.t
+
+let intermediate_result_store_to_yojson (ds : intermediate_result_store) :
+    Yojson.Safe.t =
   ht_to_json_dict ds ~f:dval_to_yojson
 
 
 (* -------------------- *)
 (* Analysis result *)
 (* -------------------- *)
-type analysis = dval_store [@@deriving to_yojson]
+type analysis = intermediate_result_store [@@deriving to_yojson]
 
 type input_vars = (string * dval) list [@@deriving eq, show, yojson]
 
