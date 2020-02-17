f007428c2df520a421b4b6e2ed23d51da545f25d
sydNoteboom <sydney@darklang.com>
2020-02-17 13:56:37 -0800

    Enable tab switching and create view for invite a user

--- client/src/components/SettingsView.ml
+++ client/src/components/SettingsView.ml
@@ -7,8 +7,7 @@ module TD = TLIDDict
 
 let fontAwesome = ViewUtils.fontAwesome
 
-
-let allTabs = [UserSettings; InviteUser] 
+let allTabs = [UserSettings; InviteUser]
 
 let update (m : model) (msg : settingsMsg) : model =
   match msg with
@@ -17,10 +16,11 @@ let update (m : model) (msg : settingsMsg) : model =
       { m with
         settingsView = {m.settingsView with opened}
       ; canvasProps = {m.canvasProps with enablePan} }
+  | SwitchSettingsTabs tab ->
+      {m with settingsView = {m.settingsView with tab}}
 
 
 let openSettingView (m : model) : model = update m (ToggleSettingsView false)
-
 
 (* View functions *)
 
@@ -56,26 +56,54 @@ let viewUserCanvases (acc : settingsViewState) : msg Html.html list =
   orgView @ canvasView
 
 
+let viewInviteUserToDark : msg Html.html list =
+  let introText =
+    [ Html.h2 [] [Html.text "Invite a friend to try dark!"]
+    ; Html.p
+        []
+        [ Html.text
+            "Friends dont let friends stay on waitlists. Help your friends skip the line by inviting them to join us. Note: This does not add the user to an org or to your canvas, it allows your friends to sign up for their own dark accounts"
+        ] ]
+  in
+  let inviteform =
+    [ Html.div
+        [Html.class' "invite-form"]
+        [ Html.div
+            [Html.class' "form-field"]
+            [ Html.h3 [] [Html.text "Email:"]
+            ; Html.input' [Vdom.attribute "" "spellcheck" "false"] [] ]
+        ; Html.h3 [Html.class' "submit-btn"] [Html.text "Submit"] ] ]
+  in
+  introText @ inviteform
+
+
 let settingsTabToHtml (acc : settingsViewState) : msg Html.html list =
   let tab = acc.tab in
-  match tab with UserSettings -> viewUserCanvases acc | InviteUser -> []
+  match tab with
+  | UserSettings ->
+      viewUserCanvases acc
+  | InviteUser ->
+      viewInviteUserToDark
 
 
 let tabTitleView (tab : settingsTab) : msg Html.html =
-  let tabTitle (t : settingsTab) = Html.h3
-  [Html.classList [("tab-title", true); ("selected", tab == t)]]
-  [Html.text (settingsTabToText t)]
+  let tabTitle (t : settingsTab) =
+    Html.h3
+      [ Html.classList [("tab-title", true); ("selected", tab == t)]
+      ; ViewUtils.eventNoPropagation
+          ~key:"close-settings-modal"
+          "click"
+          (fun _ -> SettingsViewMsg (SwitchSettingsTabs t)) ]
+      [Html.text (settingsTabToText t)]
   in
-  Html.div
-    [Html.class' "settings-tab-titles"]
-    (List.map allTabs ~f:tabTitle)
+  Html.div [Html.class' "settings-tab-titles"] (List.map allTabs ~f:tabTitle)
 
 
 let settingViewWrapper (acc : settingsViewState) : msg Html.html =
   let tabView = settingsTabToHtml acc in
   Html.div
     [Html.class' "settings-tab-wrapper"]
-    ([Html.h2 [] [Html.text "Account"]; tabTitleView acc.tab] @ tabView)
+    ([Html.h1 [] [Html.text "Account"]; tabTitleView acc.tab] @ tabView)
 
 
 let html (m : model) : msg Html.html =
