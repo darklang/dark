3b7cd32d0642b80cc83c8b8379a8d86a9758d029
Alice Q. Wong <alice@valour.me>
2020-02-19 10:39:44 -0800

    put content in dom instead of css

--- client/src/fluid/FluidView.ml
+++ client/src/fluid/FluidView.ml
@@ -447,12 +447,22 @@ let viewReturnValue (vs : ViewUtils.viewState) (ast : ast) : Types.msg Html.html
           then match dval with DIncomplete _ -> true | _ -> false
           else false
         in
+        let auxText =
+          if isIncomplete
+          then
+            [ Html.span
+                [Html.class' "msg"]
+                [ Html.text
+                    "Your code needs to return a value in the last expression"
+                ] ]
+          else [Vdom.noNode]
+        in
         Html.div
           [ Html.classList
               [ ("return-value", true)
               ; ("refreshed", isRefreshed)
               ; ("incomplete", isIncomplete) ] ]
-          (viewDval vs.tlid dval ~canCopy:true)
+          (viewDval vs.tlid dval ~canCopy:true @ auxText)
     | _ ->
         Vdom.noNode
   else Vdom.noNode
