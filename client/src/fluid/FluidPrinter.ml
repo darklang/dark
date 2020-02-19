5d4ff45daa74b8cf0a83927631cfc9c400f4526c
Julian Ceipek <julian@darklang.com>
2020-02-19 13:18:28 -0800

    Change binop tokenization for things that probably shouldn't happen

--- client/src/fluid/FluidPrinter.ml
+++ client/src/fluid/FluidPrinter.ml
@@ -275,10 +275,28 @@ let rec toTokens' (e : E.t) (b : Builder.t) : Builder.t =
         match lexpr with
         | EPipeTarget _ ->
             b
-        | _ ->
+        | EInteger _
+        | EBool _
+        | EString _
+        | EFloat _
+        | ENull _
+        | EBlank _
+        | EFieldAccess _
+        | EVariable _
+        | EPartial _
+        | ERightPartial _
+        | EList _
+        | ERecord _
+        | EConstructor _
+        | EBinOp _
+        | EFnCall _ ->
             b
             |> nest ~indent:0 ~placeholderFor:(Some (id, op, 0)) lexpr
             |> add (TSep (E.toID lexpr))
+        | EPipe _ | EMatch _ | ELambda _ | ELet _ | EIf _ | EFeatureFlag _ ->
+            b
+            |> nest ~indent:0 ~placeholderFor:(Some (id, op, 0)) lexpr
+            |> addNewlineIfNeeded (Some (E.toID lexpr, id, None))
       in
       b
       |> start
