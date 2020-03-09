8bc2feb360ca0b2f9a13c4730737a317d57e71eb
Julian Ceipek <julian@darklang.com>
2020-03-09 15:17:47 -0700

    Specific and generic cases

--- client/src/canvas/ViewErrorRailHint.ml
+++ client/src/canvas/ViewErrorRailHint.ml
@@ -5,15 +5,6 @@ module E = FluidExpression
 
 let viewFunction (fn : Prelude.function_) (sendToRail : E.sendToRail option) :
     'a Vdom.t =
-  let handleRailMsg =
-    match sendToRail with
-    | Some Rail ->
-        Html.text "Use `take-function-off-rail` to handle the `Nothing` case."
-    | Some NoRail ->
-        Html.text "Use `put-function-on-rail` to restore this behavior."
-    | None ->
-        Html.noNode
-  in
   let errorRail =
     Html.a
       [ Html.class' "link"
@@ -22,22 +13,77 @@ let viewFunction (fn : Prelude.function_) (sendToRail : E.sendToRail option) :
       ; Html.target "_blank" ]
       [Html.text "error rail"]
   in
-  match fn.fnReturnTipe with
-  | TOption ->
-      Html.p
-        []
-        [ Html.text "By default, this function goes to the "
-        ; errorRail
-        ; Html.text
-            " on `Nothing` and returns the unwrapped value in `Just value` otherwise. "
-        ; handleRailMsg ]
-  | TResult ->
-      Html.p
-        []
-        [ Html.text "By default, this function goes to the "
-        ; errorRail
-        ; Html.text
-            " on `Error` and returns the unwrapped value in `Ok value` otherwise. "
-        ; handleRailMsg ]
-  | _ ->
-      Html.noNode
+  Option.withDefault
+    ~default:Html.noNode
+    ( match sendToRail with
+    | None ->
+      (* If we don't know if the function is on the rail, return a generic message: *)
+      ( match fn.fnReturnTipe with
+      | TOption ->
+          Some
+            (Html.p
+               []
+               [ Html.text "By default, this function goes to the "
+               ; errorRail
+               ; Html.text
+                   " on `Nothing` and returns the unwrapped value in `Just value` otherwise. "
+               ])
+      | TResult ->
+          Some
+            (Html.p
+               []
+               [ Html.text "By default, this function goes to the "
+               ; errorRail
+               ; Html.text
+                   " on `Error` and returns the unwrapped value in `Ok value` otherwise. "
+               ])
+      | _ ->
+          None )
+    | Some sendToRail ->
+      (* If we know if the function is on the rail, return a specific message: *)
+      ( match (fn.fnReturnTipe, sendToRail) with
+      | TOption, Rail ->
+          Some
+            (Html.p
+               []
+               [ Html.text "This function goes to the "
+               ; errorRail
+               ; Html.text
+                   " on `Nothing` and returns the unwrapped value in `Just value` otherwise. "
+               ; Html.text
+                   "Use `take-function-off-rail` to handle the `Nothing` case."
+               ])
+      | TOption, NoRail ->
+          Some
+            (Html.p
+               []
+               [ Html.text "This function is not on the "
+               ; errorRail
+               ; Html.text
+                   " so you need to handle `Just value` and `Nothing` manually. "
+               ; Html.text
+                   "Alternatively, use `put-function-on-rail`."
+               ])
+      | TResult, Rail ->
+          Some
+            (Html.p
+               []
+               [ Html.text "This function goes to the "
+               ; errorRail
+               ; Html.text
+                   " on `Error` and returns the unwrapped value in `Ok value` otherwise. "
+               ; Html.text
+                   "Use `take-function-off-rail` to handle the `Error` case." ])
+      | TResult, NoRail ->
+          Some
+            (Html.p
+               []
+               [ Html.text "This function is not on the "
+               ; errorRail
+               ; Html.text
+                   " so you need to handle `Error error` and `Ok value` manually. "
+               ; Html.text
+                   "Alternatively, use `put-function-on-rail`."
+               ])
+      | _, (Rail | NoRail) ->
+          None ) )
