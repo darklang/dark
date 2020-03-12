0ac44977857e4cac67c4c584041d4ffaee815afb
Dean Strelau <dean@darklang.com>
2020-03-12 13:30:46 -0400

    Enable FF automatically for admins

--- client/src/canvas/View.ml
+++ client/src/canvas/View.ml
@@ -85,7 +85,7 @@ let viewTL_ (m : model) (tl : toplevel) : msg Html.html =
     ; ("group", match tl with TLGroup _ -> true | _ -> false) ]
   in
   let id =
-    Fluid.getToken' m.fluidState vs.tokens
+    Fluid.getToken vs.ast vs.fluidState
     |> Option.map ~f:(fun ti -> FluidToken.tid ti.token)
     |> Option.orElse (CursorState.idOf m.cursorState)
   in
