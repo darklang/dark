d3a2507ab44347cf24158656c89b1576cae363c2
Julian Ceipek <julian@darklang.com>
2020-03-16 11:39:09 -0700

    Reorder float tests to have expected and actual in correct order

--- backend/test/test_other_libs.ml
+++ backend/test/test_other_libs.ml
@@ -568,18 +568,18 @@ let t_url_encode () =
 
 
 let t_float_stdlibs () =
-  check_dval "Float::sum works" (exec_ast "(Float::sum (1.0 0.2))") (DFloat 1.2) ;
+  check_dval "Float::sum works" (DFloat 1.2) (exec_ast "(Float::sum (1.0 0.2))") ;
   AT.check
     AT.bool
     "Float::sum fails on list elements that are not floats"
-    (match exec_ast "(Float::sum (1.0 2))" with DError _ -> true | _ -> false)
-    true ;
+    true
+    (match exec_ast "(Float::sum (1.0 2))" with DError _ -> true | _ -> false) ;
   check_dval
     "Float::ceiling works"
-    (exec_ast "(Float::ceiling 1.3)")
-    (Dval.dint 2) ;
-  check_dval "Float::floor works" (exec_ast "(Float::floor 1.8)") (Dval.dint 1) ;
-  check_dval "Float::round works" (exec_ast "(Float::round 1.5)") (Dval.dint 2) ;
+    (Dval.dint 2)
+    (exec_ast "(Float::ceiling 1.3)") ;
+  check_dval "Float::floor works" (Dval.dint 1) (exec_ast "(Float::floor 1.8)") ;
+  check_dval "Float::round works" (Dval.dint 2) (exec_ast "(Float::round 1.5)") ;
   check_dval
     "Float::truncate works"
     (Dval.dint (-2367))
@@ -589,36 +589,36 @@ let t_float_stdlibs () =
     (DFloat (-10.0))
     (exec_ast "(Float::min -10.0 1.0)") ;
   check_dval "Float::max works" (DFloat 1.0) (exec_ast "(Float::max -10.0 1.0)") ;
-  check_dval "Float::sqrt works" (exec_ast "(Float::sqrt 25.0)") (DFloat 5.0) ;
+  check_dval "Float::sqrt works" (DFloat 5.0) (exec_ast "(Float::sqrt 25.0)") ;
   check_dval
     "Float::divide works"
-    (exec_ast "(Float::divide 9.0 2.0)")
-    (DFloat 4.5) ;
-  check_dval "Float::add works" (exec_ast "(Float::add 1.2 1.3)") (DFloat 2.5) ;
+    (DFloat 4.5)
+    (exec_ast "(Float::divide 9.0 2.0)") ;
+  check_dval "Float::add works" (DFloat 2.5) (exec_ast "(Float::add 1.2 1.3)") ;
   check_dval
     "Float::multiply works"
-    (exec_ast "(Float::multiply 26.0 0.5)")
-    (DFloat 13.0) ;
+    (DFloat 13.0)
+    (exec_ast "(Float::multiply 26.0 0.5)") ;
   check_dval
     "Float::subtract works"
-    (exec_ast "(Float::subtract 1.0 0.2)")
-    (DFloat 0.8) ;
+    (DFloat 0.8)
+    (exec_ast "(Float::subtract 1.0 0.2)") ;
   check_dval
     "Float::greaterThan works"
-    (exec_ast "(Float::greaterThan 0.2 0.1)")
-    (DBool true) ;
+    (DBool true)
+    (exec_ast "(Float::greaterThan 0.2 0.1)") ;
   check_dval
     "Float::greaterThanOrEqualTo works"
-    (exec_ast "(Float::greaterThanOrEqualTo 0.1 0.1)")
-    (DBool true) ;
+    (DBool true)
+    (exec_ast "(Float::greaterThanOrEqualTo 0.1 0.1)") ;
   check_dval
     "Float::lessThan works"
-    (exec_ast "(Float::lessThan 0.2 0.1)")
-    (DBool false) ;
+    (DBool false)
+    (exec_ast "(Float::lessThan 0.2 0.1)") ;
   check_dval
     "Float::lessThanOrEqualTo works"
-    (exec_ast "(Float::lessThanOrEqualTo 0.1 0.1)")
-    (DBool true) ;
+    (DBool true)
+    (exec_ast "(Float::lessThanOrEqualTo 0.1 0.1)") ;
   ()
 
 
