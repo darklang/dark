20d2853fcf0015b485c82368d0547406c495d3d3
Ian Smith <ismith@metaforgotten.org>
2020-03-23 15:01:05 -0700

    remove no-longer-relevant TODO list

--- backend/bin/clone_canvas.ml
+++ backend/bin/clone_canvas.ml
@@ -12,26 +12,6 @@ let usage () : unit =
   exit 1
 
 
-(* TODO
- *
- * [x] usage: two params, FROM-CANVAS, TO-CANVAS
- * [x] if FROM doesn't exist, or TO does, you've got your inputs wrong. Most
- *     likely swapped
- * [x] load FROM, change canvas name, write to TO
- * [x] edit all ops - remove history (everything since the last savepoint?)
- * [x] walk the ops' asts and look for strings matching FROM.builtwithdark.com?
- *     Rewrite to TO.builtwithdark.com and/or warn
- * [ ] a script to print all string literals for a canvas would be nice (see
- * previous security audit) and might be useful here in prepping a canvas
- * ===================
- * [x] If FROM's auth_domain isn't 'sample', require a --force flag (slightly safer)
- * [ ] do all of this on account creation with FROM and TO canvases specified in
- * config? Or hardcoded?
- * [x] hmm, maybe we should have a savepoint so ellen can edit while account
- * creation is happening; maybe don't block shipping on this but at least warn +
- * file a trello - discussed, decided wasn't worth it right now
- * *)
-
 let () =
   let clone_canvas_or_fail from_canvas_name to_canvas_name : unit =
     match Canvas.Clone.clone_canvas from_canvas_name to_canvas_name with
