ed1b852bf50b8558749e0de02183fdae6781464a
Julian Ceipek <julian@darklang.com>
2020-03-25 16:12:15 -0700

    Document and test duplicate key behavior for Dict::fromList

--- backend/test/test_other_libs.ml
+++ backend/test/test_other_libs.ml
@@ -314,6 +314,13 @@ let t_dict_stdlibs_work () =
               [ list [str "a"; int 1]
               ; list [str "b"; int 2]
               ; list [str "c"; int 3] ] ])) ;
+  check_dval
+    "Dict::fromList works (overwrites duplicates correctly)"
+    (DObj (DvalMap.from_list [("a", dint 1); ("b", dint 2); ("a", dint 3)]))
+    (exec_ast'
+       (fn
+          "Dict::fromList"
+          [list [list [str "a"; int 3]; list [str "b"; int 2]]])) ;
   check_error_contains
     "Dict::fromList works (wrong length)"
     (exec_ast'
