6c48722d84328ba4f367279446b290debeb0582a
Dean Strelau <dean@darklang.com>
2020-03-24 20:42:25 -0400

    use structural equality for FF tests

--- client/test/feature_flag_test.ml
+++ client/test/feature_flag_test.ml
@@ -1,4 +1,3 @@
-open Tc
 open Tester
 open FluidShortcuts
 module FF = FeatureFlags
@@ -13,35 +12,43 @@ module FF = FeatureFlags
  * That means that in order to target a specific expression to be wrapped, use
  * the passed [id] as the ID of that expression when constructing it. For
  * example, to wrap a simple string:
- *   (fun id -> FluidShortcuts.str ~id "a")
+ *   (fun id -> str ~id "a")
  *
- * The [expected] string is in eToTestcase format. (That is, using an
- * S-expression-like syntax with the FluidShortcuts helpers.)
+ * The [expected] value is compared with equalIgnoringIds, so should be
+ * constructed with FluidShortcuts helpers.
  *
- * [testUnwrap] * works the same way, but has two expectations: the result when
+ * [testUnwrap] works the same way, but has two expectations: the result when
  * keeping the old code and the result when keeping the new code. *)
 let testWrap
-    (name : string) (exprFn : ID.t -> FluidExpression.t) (expected : string) =
+    (name : string)
+    (exprFn : ID.t -> FluidExpression.t)
+    (expected : FluidExpression.t) =
   test name (fun () ->
       let id = Shared.gid () in
       let ast = FluidAST.ofExpr (exprFn id) in
-      let newAST = FF.wrap ast id in
-      expect (FluidAST.toExpr newAST |> FluidPrinter.eToTestcase)
+      let newAST = FF.wrap ast id |> FluidAST.toExpr in
+      expect newAST
+      |> withEquality FluidExpression.equalIgnoringIds
       |> toEqual expected)
 
 
 let testUnwrap
     (name : string)
     (exprFn : ID.t -> FluidExpression.t)
-    ~(keepOld : string)
-    ~(keepNew : string) =
-  test name (fun () ->
-      let id = Shared.gid () in
-      let ast = FluidAST.ofExpr (exprFn id) in
-      let keepOldAST = FF.unwrap FF.KeepOld ast id |> FluidAST.toExpr in
-      let keepNewAST = FF.unwrap FF.KeepNew ast id |> FluidAST.toExpr in
-      expect (List.map [keepOldAST; keepNewAST] ~f:FluidPrinter.eToTestcase)
-      |> toEqual [keepOld; keepNew])
+    ~(keepOld : FluidExpression.t)
+    ~(keepNew : FluidExpression.t) =
+  let id = Shared.gid () in
+  let ast = FluidAST.ofExpr (exprFn id) in
+  test (name ^ "- KeepOld") (fun () ->
+      let actualOld = FF.unwrap FF.KeepOld ast id |> FluidAST.toExpr in
+      expect actualOld
+      |> withEquality FluidExpression.equalIgnoringIds
+      |> toEqual keepOld) ;
+  test (name ^ "- KeepNew") (fun () ->
+      let actualNew = FF.unwrap FF.KeepNew ast id |> FluidAST.toExpr in
+      expect actualNew
+      |> withEquality FluidExpression.equalIgnoringIds
+      |> toEqual keepNew)
 
 
 let run () =
@@ -49,36 +56,38 @@ let run () =
       testWrap
         "wrapping a simple expression puts expression in the old code"
         (fun id -> str ~id "a")
-        {|(ff (b) (str "a") (b))|} ;
+        (flag (blank ()) (str "a") (blank ())) ;
       testWrap
         "wrapping a let puts the RHS in the old code"
         (fun id -> let' ~id "a" (int 1) (var "a"))
-        {|(let' "a" (ff (b) (int 1) (b)) (var "a"))|} ;
+        (let' "a" (flag (blank ()) (int 1) (blank ())) (var "a")) ;
       testWrap
         "does not wrap an expr inside FF condition"
         (fun id -> flag (bool ~id true) (str "old") (str "new"))
-        {|(ff (bool true) (str "old") (str "new"))|} ;
+        (flag (bool true) (str "old") (str "new")) ;
       testWrap
         "does not wrap an expr inside FF oldCode"
         (fun id -> flag (bool true) (str ~id "old") (str "new"))
-        {|(ff (bool true) (str "old") (str "new"))|} ;
+        (flag (bool true) (str "old") (str "new")) ;
       testWrap
         "does not wrap an expr inside FF oldCode"
         (fun id -> flag (bool true) (str "old") (str ~id "new"))
-        {|(ff (bool true) (str "old") (str "new"))|}) ;
+        (flag (bool true) (str "old") (str "new")) ;
+      ()) ;
   describe "FeatureFlag.unwrap" (fun () ->
       testUnwrap
         "unwrapping a simple expression leaves old code"
         (fun id -> flag ~id (blank ()) (str "old") (str "new"))
-        ~keepOld:{|(str "old")|}
-        ~keepNew:{|(str "new")|} ;
+        ~keepOld:(str "old")
+        ~keepNew:(str "new") ;
       testUnwrap
         "unwrapping from within the old leaves the old code"
         (fun id -> flag (blank ()) (str ~id "old") (str "new"))
-        ~keepOld:{|(str "old")|}
-        ~keepNew:{|(str "new")|} ;
+        ~keepOld:(str "old")
+        ~keepNew:(str "new") ;
       testUnwrap
         "unwrapping from within the new code leaves the old code"
         (fun id -> flag (blank ()) (str "old") (str ~id "new"))
-        ~keepOld:{|(str "old")|}
-        ~keepNew:{|(str "new")|})
+        ~keepOld:(str "old")
+        ~keepNew:(str "new") ;
+      ())
