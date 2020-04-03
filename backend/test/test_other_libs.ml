9265aae1d4f189dd5d0908a77fe10364dd46e549
Julian Ceipek <julian@darklang.com>
2020-04-02 23:09:18 -0700

    Test String::trimStart and String::trimEnd

--- backend/test/test_other_libs.ml
+++ backend/test/test_other_libs.ml
@@ -910,6 +910,80 @@ let t_string_stdlibs_work () =
     "String::padEnd works (> 1 EGC errors)"
     (exec_ast' (fn "String::padEnd" [str "123"; str "_-"; int 4]))
     "Expected the argument `padWith` passed to `String::padEnd` to be one Character long. However, `\"_-\"` is 2 Characters long." ;
+  check_dval
+    "String::trimStart works (Noop)"
+    (exec_ast' (fn "String::trimStart" [str "foo"]))
+    (dstr "foo") ;
+  check_dval
+    "String::trimEnd works (Noop)"
+    (exec_ast' (fn "String::trimEnd" [str "foo"]))
+    (dstr "foo") ;
+  check_dval
+    "String::trimStart works (Start Trivial)"
+    (exec_ast' (fn "String::trimStart" [str "  foo"]))
+    (dstr "foo") ;
+  check_dval
+    "String::trimEnd works (Start Trivial)"
+    (exec_ast' (fn "String::trimEnd" [str "  foo"]))
+    (dstr "  foo") ;
+  check_dval
+    "String::trimStart works (End Trivial)"
+    (exec_ast' (fn "String::trimStart" [str "foo  "]))
+    (dstr "foo  ") ;
+  check_dval
+    "String::trimEnd works (End Trivial)"
+    (exec_ast' (fn "String::trimEnd" [str "foo  "]))
+    (dstr "foo") ;
+  check_dval
+    "String::trimStart works (Both Trivial)"
+    (exec_ast' (fn "String::trimStart" [str "  foo  "]))
+    (dstr "foo  ") ;
+  check_dval
+    "String::trimEnd works (Both Trivial)"
+    (exec_ast' (fn "String::trimEnd" [str "  foo  "]))
+    (dstr "  foo") ;
+  check_dval
+    "String::trimStart works (BothNotInner Trivial)"
+    (exec_ast' (fn "String::trimStart" [str "  foo bar  "]))
+    (dstr "foo bar  ") ;
+  check_dval
+    "String::trimEnd works (BothNotInner Trivial)"
+    (exec_ast' (fn "String::trimEnd" [str "  foo bar  "]))
+    (dstr "  foo bar") ;
+  check_dval
+    "String::trimStart works (Both Unicode)"
+    (* Leading em-space, inner thin space, trailing space *)
+    (exec_ast'
+       (fn
+          "String::trimStart"
+          [str " \xe2\x80\x83foo\xe2\x80\x83bar\xe2\x80\x83 "]))
+    (dstr "foo\xe2\x80\x83bar\xe2\x80\x83 ") ;
+  check_dval
+    "String::trimEnd works (Both Unicode)"
+    (* Leading em-space, inner thin space, trailing space *)
+    (exec_ast'
+       (fn
+          "String::trimEnd"
+          [str " \xe2\x80\x83foo\xe2\x80\x83bar\xe2\x80\x83 "]))
+    (dstr " \xe2\x80\x83foo\xe2\x80\x83bar") ;
+  check_dval
+    "String::trimStart works (All)"
+    (exec_ast' (fn "String::trimStart" [str "      "]))
+    (dstr "") ;
+  check_dval
+    "String::trimEnd works (All)"
+    (exec_ast' (fn "String::trimEnd" [str "      "]))
+    (dstr "") ;
+  check_dval
+    "String::trimStart works (PreservesEmoji)"
+    (exec_ast'
+       (fn "String::trimStart" [str " \xf0\x9f\x98\x84foobar\xf0\x9f\x98\x84 "]))
+    (dstr "\xf0\x9f\x98\x84foobar\xf0\x9f\x98\x84 ") ;
+  check_dval
+    "String::trimEnd works (PreservesEmoji)"
+    (exec_ast'
+       (fn "String::trimEnd" [str " \xf0\x9f\x98\x84foobar\xf0\x9f\x98\x84 "]))
+    (dstr " \xf0\x9f\x98\x84foobar\xf0\x9f\x98\x84") ;
   ()
 
 
