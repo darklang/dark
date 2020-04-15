0b6e83e67e094a668a2d791b21503dd7894fc97c
Ian Smith <ismith@metaforgotten.org>
2020-04-15 10:08:01 -0700

    took Dean's suggestions to log less

--- backend/libbackend/curl_logging.ml
+++ backend/libbackend/curl_logging.ml
@@ -41,6 +41,14 @@ let debugfn (bufs : bufs) (c : Curl.t) (cdt : Curl.curlDebugType) (s : string) :
       ()
 
 
+type request_or_response =
+  | Request
+  | Response
+
+let string_of_request_or_response ror : string =
+  match ror with Request -> "request" | Response -> "response"
+
+
 let log_debug_info (bufs : bufs) =
   let ( debugbuf_text
       , debugbuf_data_in
@@ -63,18 +71,7 @@ let log_debug_info (bufs : bufs) =
                ~params:[("line", line)] ;
              Some (line, ""))
       |> Option.map ~f:(fun (header, value) ->
-             (header |> String.strip, value |> String.strip))
-      |> Option.map ~f:(fun (header, value) ->
-             let headers_to_redact = ["authorization"; "set-cookie"] in
-             if List.mem
-                  ~equal:( = )
-                  headers_to_redact
-                  (header |> String.lowercase)
-             then
-               ( header
-               , Printf.sprintf "[REDACTED: %i bytes]" (value |> String.length)
-               )
-             else (header, value))
+             (header |> String.strip |> String.lowercase, value |> String.strip))
   in
   let line_and_headers (buffer : string ref) : string * (string * string) list =
     (* The first 'header' line of a request or response is not really a
@@ -91,35 +88,78 @@ let log_debug_info (bufs : bufs) =
         ("", [])
   in
   let request_line, request_headers = line_and_headers debugbuf_header_out in
+  (* Query params may contain sensitive data, so remove anything after a ?
+   * before logging *)
+  let request_line =
+    request_line
+    |> String.split ~on:'?'
+    |> List.hd
+    |> Tc.Option.withDefault ~default:""
+  in
   let response_line, response_headers = line_and_headers debugbuf_header_in in
+  (* Takes a list of headers (k,v pairs) and returns a string of just the header
+   * keys, comma-separated, with an additional comma on either end; that way we
+   * can filter in honeycomb by header name - for instance,
+   * `curl.request_headers contains ,set-cookie,` *)
+  let header_names (headers : (string * string) list) : string =
+    headers
+    |> List.map ~f:fst
+    |> String.concat ~sep:","
+    |> fun s -> "," ^ s ^ ","
+  in
+  let extract_headers
+      (headers : (string * string) list)
+      ~(keys : string list)
+      request_or_response : (string * string) list =
+    headers
+    |> List.filter ~f:(fun (k, _) -> List.mem ~equal:( = ) keys k)
+    |> List.map ~f:(fun (k, v) ->
+           ( Printf.sprintf
+               "curl.%s_%s"
+               (string_of_request_or_response request_or_response)
+               k
+           , v ))
+  in
+  (* For this and selected_response_headers, I looked at
+   * https://en.wikipedia.org/wiki/List_of_HTTP_header_fields *)
+  let selected_request_headers =
+    extract_headers
+      request_headers
+      ~keys:
+        [ "content-type"
+        ; "host"
+          (* https://aws.amazon.com/premiumsupport/knowledge-center/s3-request-id-values/ *)
+        ; "x-amz-request-id"
+        ; "x-amz-id-2"
+        ; "x-request-id" ]
+      Request
+  in
+  let selected_response_headers =
+    extract_headers
+      response_headers
+      ~keys:
+        [ "content-type"
+        ; "server"
+          (* https://aws.amazon.com/premiumsupport/knowledge-center/s3-request-id-values/ *)
+        ; "x-amz-request-id"
+        ; "x-amz-id-2"
+        ; "x-request-id" ]
+      Response
+  in
   Log.infO
     "libcurl"
     ~params:
-      (* raw bodies are not useful to log - they may be large, or may contain
-       * non-human-readable data (esp binary data, but minimized js, etc can
-       * also be this). If we decided it was useful, we could plausibly have a
-       * feature-flag-esque construct to turn up/down log verbosity, and make a
-       * decision to log the whole thing - for certain requests, or up to a
-       * certain size - based on that. *)
-      [ ("informational_text", !debugbuf_text)
-      ; ("request_line", request_line)
-      ; ("response_line", response_line) ]
+      (* raw bodies and headers are deliberately not logged - they can be large,
+       * and may contain sensitive information *)
+      ( [ ("curl.informational_text", !debugbuf_text)
+        ; ("curl.request_line", request_line)
+        ; ("curl.response_line", response_line)
+        ; ("curl.request_headers", request_headers |> header_names)
+        ; ("curl.response_headers", response_headers |> header_names) ]
+      @ selected_request_headers
+      @ selected_response_headers )
     ~jsonparams:
-      [ ("informational_text_size", `Int (!debugbuf_text |> String.length))
-      ; ("response_headers_count", `Int (response_headers |> List.length))
-      ; ("request_headers_count", `Int (request_headers |> List.length))
-      ; ("response_headers_size", `Int (!debugbuf_header_in |> String.length))
-      ; ("request_headers_size", `Int (!debugbuf_header_out |> String.length))
-      ; ("response_body_size", `Int (!debugbuf_data_in |> String.length))
-      ; ("request_body_size", `Int (!debugbuf_data_out |> String.length))
-        (* Honeycomb will not, by default, unpack this into columns - it's a
-         * raw string. We don't want HC to auto-unpack it - there's too many
-         * possible headers, our schema would be ridiculous.  However, now we
-         * have a raw thing to poke at for debugging, and if we wanted to
-         * extract known/common headers into a column, we could. *)
-      ; ( "request_headers"
-        , `Assoc (request_headers |> List.map ~f:(fun (k, v) -> (k, `String v)))
-        )
-      ; ( "response_headers"
-        , `Assoc (response_headers |> List.map ~f:(fun (k, v) -> (k, `String v)))
-        ) ]
+      [ ("curl.informational_text_size", `Int (!debugbuf_text |> String.length))
+      ; ("curl.response_body_size", `Int (!debugbuf_data_in |> String.length))
+      ; ("curl.request_body_size", `Int (!debugbuf_data_out |> String.length))
+      ]
