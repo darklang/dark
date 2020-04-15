b47f8480ec58cdc1e5d5c988ed4e018a31434e77
Dean Strelau <dean@darklang.com>
2020-04-15 09:32:39 -0400

    Add List::randomElement

--- backend/libexecution/liblist.ml
+++ backend/libexecution/liblist.ml
@@ -914,4 +914,23 @@ let fns =
           | args ->
               fail args)
     ; preview_safety = Safe
+    ; deprecated = false }
+  ; { prefix_names = ["List::randomElement"]
+    ; infix_names = []
+    ; parameters = [par "list" TList]
+    ; return_type = TOption
+    ; description =
+        "If the list contains at least one value, returns {{Just}} a randomly selected element. Returns {{Nothing}} if the list is empty."
+    ; func =
+        InProcess
+          (function
+          | _, [DList []] ->
+              DOption OptNothing
+          | _, [DList l] ->
+              List.nth l (Random.int (List.length l))
+              |> Option.map ~f:Dval.to_opt_just
+              |> Option.value_exn
+          | args ->
+              fail args)
+    ; preview_safety = Unsafe
     ; deprecated = false } ]
