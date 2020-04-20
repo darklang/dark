8fa60d81c52d42b19d196fd3b658a742254cb3f5
Ian Smith <ismith@metaforgotten.org>
2020-04-20 10:14:05 -0700

    Rename from buildhash to client-buildhash - it's _not_ necessarily the server's buildhash

--- client/src/api/API.ml
+++ client/src/api/API.ml
@@ -12,7 +12,7 @@ let apiCallNoParams
       ; headers =
           [ Header ("Content-type", "application/json")
           ; Header ("X-CSRF-Token", m.csrfToken)
-          ; Header ("x-darklang-buildhash", m.buildHash) ]
+          ; Header ("x-darklang-client-buildhash", m.buildHash) ]
       ; url
       ; body = Web.XMLHttpRequest.EmptyBody
       ; expect = Tea.Http.expectStringResponse (Decoders.wrapExpect decoder)
