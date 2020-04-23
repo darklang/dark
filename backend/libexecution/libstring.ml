581490a378bc58397cbdec3cf776a7bbe522283f
Julian Ceipek <julian@darklang.com>
2020-04-23 15:23:59 -0700

    Add stdlib_util for all files with a lib* prefix

--- backend/libexecution/libstring.ml
+++ backend/libexecution/libstring.ml
@@ -602,7 +602,8 @@ let fns : expr fn list =
               if l < Dint.zero
               then Exception.code "l should be a positive integer"
               else
-                Dval.dstr_of_string_exn (Util.random_string (Dint.to_int_exn l))
+                Dval.dstr_of_string_exn
+                  (Stdlib_util.random_string (Dint.to_int_exn l))
           | args ->
               fail args)
     ; preview_safety = Unsafe
@@ -623,7 +624,7 @@ let fns : expr fn list =
                 DResult
                   (ResOk
                      (Dval.dstr_of_string_exn
-                        (Util.random_string (Dint.to_int_exn l))))
+                        (Stdlib_util.random_string (Dint.to_int_exn l))))
           | args ->
               fail args)
     ; preview_safety = Unsafe
@@ -643,7 +644,7 @@ let fns : expr fn list =
               else
                 Dval.to_res_ok
                   (Dval.dstr_of_string_exn
-                     (Util.random_string (Dint.to_int_exn l)))
+                     (Stdlib_util.random_string (Dint.to_int_exn l)))
           | args ->
               fail args)
     ; preview_safety = Unsafe
@@ -659,7 +660,7 @@ let fns : expr fn list =
           (function
           | _, [DStr s] ->
               Dval.dstr_of_string_exn
-                (Util.html_escape (Unicode_string.to_string s))
+                (Stdlib_util.html_escape (Unicode_string.to_string s))
           | args ->
               fail args)
     ; preview_safety = Unsafe
