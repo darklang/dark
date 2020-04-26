e14416798b7b3e780381ce0339ba54938731de88
Ian Smith <ismith@metaforgotten.org>
2020-04-26 00:45:13 -0700

    remove debug ls

--- scripts/integration-test-results-to-honeycomb.sh
+++ scripts/integration-test-results-to-honeycomb.sh
@@ -15,7 +15,6 @@ if [[ ! -f honeytail ]]; then
     wget -q -O honeytail https://honeycomb.io/download/honeytail/linux/1.762 && \
       echo '00e24441316c7ae24665b1aaea4cbb77e2ee52c83397bf67d70f3ffe14a1e341  honeytail' | sha256sum -c && \
       chmod 755 ./honeytail
-    ls
 fi
 
 test_results=rundir/test_results/integration_tests.json
