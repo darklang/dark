6d2037c01ee72416a78b310631584feafcdb8829
sydNoteboom <sydney@darklang.com>
2020-04-27 11:07:48 -0700

    Creating a page for the package manager functions

--- client/src/package/PackageManager.ml
+++ client/src/package/PackageManager.ml
@@ -1,5 +1,20 @@
 open Prelude
 
+let paramData (pfp : packageFnParameter) : blankOrData list =
+  let paramName = BlankOr.newF pfp.name in
+  let paramTipe = BlankOr.newF pfp.tipe in
+  [PParamName paramName; PParamTipe paramTipe]
+
+
+let allParamData (pmf : packageFn) : blankOrData list =
+  List.concat (List.map ~f:paramData pmf.parameters)
+
+
+let blankOrData (pmf : packageFn) : blankOrData list =
+  let fnname = BlankOr.newF pmf.fnname in
+  [PFnName fnname] @ allParamData pmf
+
+
 let fn_of_packageFn (pkgFn : packageFn) : function_ =
   let paramOfPkgFnParam (pkgFnParam : packageFnParameter) : parameter =
     { paramName = pkgFnParam.name
