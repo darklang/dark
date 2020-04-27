1edf128499619d0f371eb39aad4170ebd6a7f07e
Ian Smith <ismith@metaforgotten.org>
2020-04-27 10:25:37 -0700

    improve documentation of test

--- integration-tests/output_processing/test_process_integration_tests.sh
+++ integration-tests/output_processing/test_process_integration_tests.sh
@@ -3,20 +3,25 @@
 # Not an automated test, this is a manual test to verify the script before
 # pushing to circle
 #
-# If you pipe the output through `jq -s group_by(.error) | .[] | length'`, you
-# should get "14\n1" - 14 non-error, 1 error (group_by sorts lexically, false is
-# before true)
+# If you run this script with SUMMARY=1, you should get "1\n1", indicating one
+# error case and one success.
 #
-# (I have not put that in this script because this script is also useful for
-# looking at what's in the output - the right keys, single- not multi- line
-# json, etc.)
+# (That is not the default because this script is also useful for looking at
+# what's in the output - the right keys, single- not multi- line json, etc.)
 
 DIR=$(dirname "$0")
 
 export CIRCLE_BRANCH="this is a test, lol"
 export CIRCLE_BUILD_URL='this is a test of what happens with a "double quote"'
 
+if [[ -z $SUMMARY ]]; then
+    SUMMARY_CMD="cat"
+else
+    SUMMARY_CMD=(jq -s 'group_by(.error) | .[] | length')
+fi
+
 # SC2002 is "useless use of cat"
 # shellcheck disable=SC2002
 cat "${DIR}"/sample_integration_tests_with_failure.json\
-    | scripts/support/process-integration-test-results.sh
+    | scripts/support/process-integration-test-results.sh \
+    | "${SUMMARY_CMD[@]}"
