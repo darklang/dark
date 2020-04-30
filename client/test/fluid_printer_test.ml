7972656800134622d8c8491dfb50143ebc7e2674
Alice Q. Wong <alice@valour.me>
2020-04-30 13:12:03 -0700

    clean code

--- client/test/fluid_printer_test.ml
+++ client/test/fluid_printer_test.ml
@@ -5,58 +5,50 @@ open Fluid_test_data
 module E = FluidExpression
 
 let run () =
-  let hasTokenMatch ~(f : fluidToken -> bool) (tokens : fluidToken list) =
+  let hasTokenMatch ~(f : fluidToken -> bool) (e : E.t) =
+    let tokens = (toTokens' e Builder.empty).tokens in
     expect (List.any tokens ~f) |> toEqual true
   in
   describe "toTokens' converts expressions to tokens" (fun () ->
       test "field access keeps parentBlockID" (fun () ->
           let parentID = Some (gid ()) in
           let tokens = (toTokens' ~parentID aField Builder.empty).tokens in
-          tokens
-          |> hasTokenMatch ~f:(fun t ->
-                 match t with
-                 | TVariable (_, _, pbid) when pbid = parentID ->
-                     true
-                 | _ ->
-                     false)) ;
+          expect
+            (List.any tokens ~f:(function
+                | TVariable (_, _, pbid) when pbid = parentID ->
+                    true
+                | _ ->
+                    false))
+          |> toEqual true) ;
       test "empty object literal does not have parentBlockID" (fun () ->
-          let tokens = (toTokens' emptyRecord Builder.empty).tokens in
-          tokens
-          |> hasTokenMatch ~f:(fun t ->
-                 match t with
+          emptyRecord
+          |> hasTokenMatch ~f:(function
                  | TRecordOpen (_, None) | TRecordClose (_, None) ->
                      true
                  | _ ->
                      false)) ;
       test "object literal keeps parentBlockID in fields" (fun () ->
           let parentBlockID = gid () in
-          let expr = E.ERecord (parentBlockID, [recordRow1]) in
-          let tokens = (toTokens' expr Builder.empty).tokens in
-          tokens
-          |> hasTokenMatch ~f:(fun t ->
-                 match t with
-                 | TRecordFieldname _ ->
-                     FluidToken.parentBlockID t = Some parentBlockID
+          E.ERecord (parentBlockID, [recordRow1])
+          |> hasTokenMatch ~f:(function
+                 | TRecordFieldname d when d.parentBlockID = Some parentBlockID
+                   ->
+                     true
                  | _ ->
                      false)) ;
       test "empty list literal does not have parentBlockID" (fun () ->
-          let tokens = (toTokens' emptyList Builder.empty).tokens in
-          tokens
-          |> hasTokenMatch ~f:(fun t ->
-                 match t with
+          emptyList
+          |> hasTokenMatch ~f:(function
                  | TListOpen (_, None) | TListClose (_, None) ->
                      true
                  | _ ->
                      false)) ;
       test "list literal keeps parentBlockID in the items" (fun () ->
           let parentBlockID = gid () in
-          let expr = E.EList (parentBlockID, [fiftySix; seventyEight]) in
-          let tokens = (toTokens' expr Builder.empty).tokens in
-          tokens
-          |> hasTokenMatch ~f:(fun t ->
-                 match t with
-                 | TInteger _ ->
-                     FluidToken.parentBlockID t = Some parentBlockID
+          E.EList (parentBlockID, [fiftySix; seventyEight])
+          |> hasTokenMatch ~f:(function
+                 | TInteger (_, _, pid) when pid = Some parentBlockID ->
+                     true
                  | _ ->
                      false))) ;
   ()
