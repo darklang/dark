ce035f1cd471b19bffd902a9d77bef37910fb2c0
sydNoteboom <sydney@darklang.com>
2020-04-30 16:16:03 -0700

    Append to html body instead of replacing

--- client/src/unsupportedBrowser.js
+++ client/src/unsupportedBrowser.js
@@ -10,7 +10,7 @@ function unsupportedBrowser() {
 }
 document.addEventListener("DOMContentLoaded", event => {
   if (unsupportedBrowser()) {
-    document.body.innerHTML =
+    document.body.innerHTML +=
       "<div class='modal-overlay' id='unsupportedBrowser'><div class='modal'><div class='warning'><p class='title'>Unfortunately we only support Dark on desktop Chrome right now. Between browser different input models, differences in scripting and rendering performance, and differing web platform support, we don't have the capacity to support other browsers at the moment. We hope to support Firefox, Safari, and mobile use once we've really nailed the experience on Chrome. Thanks for understanding!</p></div></div></div>";
   }
 });
