e4d2e1075d9ef768c7de105760522a48b54923fc
sydNoteboom <sydney@darklang.com>
2020-05-01 11:04:01 -0700

    append node

--- client/src/unsupportedBrowser.js
+++ client/src/unsupportedBrowser.js
@@ -10,7 +10,10 @@ function unsupportedBrowser() {
 }
 document.addEventListener("DOMContentLoaded", event => {
   if (unsupportedBrowser()) {
-    document.body.innerHTML +=
+    let htmlString =
       "<div class='modal-overlay' id='unsupportedBrowser'><div class='modal'><div class='warning'><p class='title'>Unfortunately we only support Dark on desktop Chrome right now. Between browser different input models, differences in scripting and rendering performance, and differing web platform support, we don't have the capacity to support other browsers at the moment. We hope to support Firefox, Safari, and mobile use once we've really nailed the experience on Chrome. Thanks for understanding!</p></div></div></div>";
+    var div = document.createElement("div");
+    div.innerHTML = htmlString;
+    document.body.append(div);
   }
 });
