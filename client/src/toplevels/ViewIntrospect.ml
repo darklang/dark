77e954e761fccd8472928f8967b8d324617044e5
Julian Ceipek <julian@darklang.com>
2020-06-02 17:22:36 -0700

    First pass at visualizing the package manager function references - this is mostly a copy of the code for existing function references - still need the appropriate color - needs to link to the right place - need to decide whether to use the same classes or not - still need to show which handlers reference the function!

--- client/src/toplevels/ViewIntrospect.ml
+++ client/src/toplevels/ViewIntrospect.ml
@@ -42,6 +42,24 @@ let fnParamsView (params : userFunctionParameter list) : msg Html.html =
   in
   Html.div [Html.class' "fields"] (List.map ~f:paramView params)
 
+let packageFnParamsView (params : packageFnParameter list) : msg Html.html =
+  let paramView (p:packageFnParameter) =
+    let name =
+      Html.span
+        [ Html.classList
+            [("name", true)] ]
+        [Html.text p.name]
+    in
+    let ptype =
+      Html.span
+        [ Html.classList
+            [("type", true)] ]
+        [ Html.text (Runtime.tipe2str p.tipe) ]
+    in
+    Html.div [Html.class' "field"] [name; ptype]
+  in
+  Html.div [Html.class' "fields"] (List.map ~f:paramView params)
+
 
 let hoveringRefProps
     (originTLID : TLID.t) (originIDs : ID.t list) ~(key : string) =
@@ -122,6 +140,28 @@ let fnView
     @ hoveringRefProps originTLID originIDs ~key:"ref-fn-hover" )
     [Html.div [Html.class' "fnheader"] header; fnParamsView params]
 
+let packageFnView
+    (originTLID : TLID.t)
+    (originIDs : ID.t list)
+    (tlid : TLID.t)
+    (name : string)
+    (params : packageFnParameter list)
+    (direction : string) : msg Html.html =
+  (* Spec is here: https://www.notion.so/darklang/PM-Function-References-793d95469dfd40d5b01c2271cb8f4a0f *)
+  let header =
+    [ViewUtils.fontAwesome "box-open"; Html.span [Html.class' "fnname"] [Html.text name]]
+  in
+  Html.div
+    (* TODO(JULIAN): Make this link to the correct function when you click on it! *)
+    (* TODO(JULIAN): Make the icon color correct based on if you can edit it! *)
+    ( [ Html.class' ("ref-block fn " ^ direction)
+      ; ViewUtils.eventNoPropagation
+          ~key:("ref-fn-link" ^ TLID.toString tlid)
+          "click"
+          (fun _ -> GoTo (FocusedFn (tlid, None))) ]
+    @ hoveringRefProps originTLID originIDs ~key:"ref-fn-hover" )
+    [Html.div [Html.class' "fnheader"] header; packageFnParamsView params]
+
 
 let tipeView
     (originTLID : TLID.t)
@@ -162,6 +202,9 @@ let renderView originalTLID direction (tl, originalIDs) =
       {ufTLID; ufMetadata = {ufmName = F (_, name); ufmParameters; _}; ufAST = _}
     ->
       fnView originalTLID originalIDs ufTLID name ufmParameters direction
+  | TLPmFunc pFn ->
+    let name = pFn |> PackageManager.extendedName in
+    packageFnView originalTLID originalIDs pFn.pfTLID name pFn.parameters direction
   | TLTipe {utTLID; utName = F (_, name); utVersion; utDefinition = _} ->
       tipeView originalTLID originalIDs utTLID name utVersion direction
   | _ ->
