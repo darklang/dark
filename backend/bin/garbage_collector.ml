8f0b61965c97f9c8d22a7248e271aab71b83c5a8
Ian Smith <ismith@metaforgotten.org>
2020-06-09 21:16:47 -0700

    update garbage_collector.exe to loop over all canvases
    Running in 'count' (not delete) mode across all canvases:
    - took 29m16s (long, but tolerable - and I expect that to go down as db size does)
    - found 1995755 rows to delete (2M, out of 85M)
    - generated 113k traces (`cat gc.log | grep trace.trace_id | wc -l`),
      because we have 102k handlers worth of traces (note: not all traces
      have a corresponding handler - 404s, for instance, have no handler)
    
    Unclear how long it'll take in delete mode; doing this per-canvas we saw
    it take 3-4x as long. And even if we have to kill the script partway
    through, that's still progress - each handler gets its own transaction,
    so it's not all-or-nothing.
    
    https://trello.com/c/UhpUHJcj/3186-fix-garbage-collection-of-old-traces

--- backend/bin/garbage_collector.ml
+++ backend/bin/garbage_collector.ml
@@ -10,40 +10,68 @@ open Libcommon
  * 43e58c86-2580-45e8-a569-b02740ec189b is paulshen-animalcrossing
  * *)
 let usage () : unit =
-  Format.printf "Usage: %s [count|delete] <limit> <canvas_id>" Sys.argv.(0) ;
+  Format.printf
+    "Usage: %s [count|delete] <limit> [all|<canvas_id>]\n\nIf given 'all', it will iterate across all canvases; if given a specific canvas_id,\nit will only gc that canvas\n"
+    Sys.argv.(0) ;
   exit 1
 
 
 let () =
+  (* parse args *)
+  let (action, action_arg, limit, canvas_arg)
+        : Stored_event.trim_events_action * string * int * string =
+    match Sys.argv with
+    | [|_argv0; action_arg; limit; canvas_arg|] ->
+        let action : Stored_event.trim_events_action =
+          match action_arg with
+          | "count" ->
+              Count
+          | "delete" ->
+              Delete
+          | _ ->
+              usage () ;
+              Exception.internal "Can't happen, unreachable code"
+        in
+        let limit = limit |> int_of_string in
+        (action, action_arg, limit, canvas_arg)
+    | _ ->
+        usage () ;
+        Exception.internal "Can't happen, unreachable code"
+  in
+  (* Outer span - one per script invocation *)
   Telemetry.with_root "garbage_collector" (fun span ->
-      let (action, action_arg, limit, canvas_id)
-            : Stored_event.trim_events_action * string * int * Uuidm.t =
-        match Sys.argv with
-        | [|_argv0; action_arg; limit; canvas_id|] ->
-            let action : Stored_event.trim_events_action =
-              match action_arg with
-              | "count" ->
-                  Count
-              | "delete" ->
-                  Delete
-              | _ ->
-                  usage () ;
-                  Exception.internal "Can't happen, unreachable code"
-            in
-            ( action
-            , action_arg
-            , limit |> int_of_string
-            , canvas_id
-              |> Uuidm.of_string
-              |> (Option.value_exn : Uuidm.t option -> Uuidm.t) )
-        | _ ->
-            usage () ;
-            Exception.internal "Can't happen, unreachable code"
+      (* Get all canvas ids *)
+      let canvas_ids : Uuidm.t list =
+        ( match canvas_arg with
+        | "all" ->
+            Telemetry.with_span span "get_canvases_for_gc" (fun span ->
+                let canvas_ids : string list =
+                  Db.fetch ~name:"canvases" "SELECT id FROM canvases" ~params:[]
+                  (* List.map over all the rows; then List.hd_exn each row, it's is a single field, canvases.id *)
+                  |> List.map ~f:List.hd_exn
+                in
+                Telemetry.Span.set_attr
+                  span
+                  "canvas_count"
+                  (`Int (List.length canvas_ids)) ;
+                canvas_ids)
+        | canvas_arg ->
+            [canvas_arg] )
+        |> List.map ~f:(fun cid ->
+               cid |> Uuidm.of_string |> Tc.Option.value_exn)
       in
-      Telemetry.Span.set_attrs
+      Telemetry.Span.set_attr
         span
-        [ ("limit", `Int limit)
-        ; ("action_arg", `String action_arg)
-        ; ("canvas_id", `String (canvas_id |> Uuidm.to_string)) ] ;
-      Stored_event.trim_events_for_canvas ~span ~action canvas_id limit
-      |> ignore)
+        "canvas_count"
+        (`Int (List.length canvas_ids)) ;
+      (* Map over canvases *)
+      let row_count =
+        canvas_ids
+        |> List.map ~f:(fun canvas_id ->
+               Stored_event.trim_events_for_canvas ~span ~action canvas_id limit)
+        (* Sum the row_count returned for each canvas and put it in this outer
+         * span *)
+        |> Tc.List.sum
+      in
+      Telemetry.Span.set_attr span "row_count" (`Int row_count))
+  |> ignore
