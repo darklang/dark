07806e7d1013c3b0dc9a628c86ea996f617f1905
aydinbahadir <mehmet.aydin.bahadir@gmail.com>
2020-06-29 22:53:57 +0200

    Updated existing patterns instead of adding new ones

--- client/src/fluid/Fluid.ml
+++ client/src/fluid/Fluid.ml
@@ -4315,31 +4315,24 @@ let rec updateKey
       when false (* disable for now *) && pos - ti.startPos != 0 ->
         startEscapingString pos ti astInfo
     (* comma - add another of the thing *)
-    | InsertText ",", L (TPartial _, ti), R (TListClose (id, _), _)
-    | InsertText ",", L (TPartial _, ti), R (TListComma (id, _), _)
-      when isAutocompleting ti astInfo.state ->
-        let astInfo = acEnter ti K.Enter astInfo in
-        let bID = gid () in
-        let newExpr = E.EBlank bID in
-        astInfo
-        |> ASTInfo.setAST (insertAtListEnd id ~newExpr astInfo.ast)
-        |> moveToCaretTarget {astRef = ARBlank bID; offset = 0}
     | InsertText ",", L (TListOpen (id, _), _), _ when onEdge ->
         let bID = gid () in
         let newExpr = E.EBlank bID (* new separators *) in
         astInfo
         |> ASTInfo.setAST (insertInList ~index:0 id ~newExpr astInfo.ast)
         |> moveToCaretTarget {astRef = ARBlank bID; offset = 0}
-    | InsertText ",", L (TListComma (id, index), _), _
-    | InsertText ",", _, R (TListComma (id, index), _)
+    | InsertText ",", L (TListComma (id, index), _), R (_, ti)
+    | InsertText ",", L (_, ti), R (TListComma (id, index), _)
       when onEdge ->
+        let astInfo = acEnter ti K.Enter astInfo in
         let bID = gid () in
         let newExpr = E.EBlank bID (* new separators *) in
         astInfo
         |> ASTInfo.setAST
              (insertInList ~index:(index + 1) id ~newExpr astInfo.ast)
         |> moveToCaretTarget {astRef = ARBlank bID; offset = 0}
-    | InsertText ",", _, R (TListClose (id, _), _) when onEdge ->
+    | InsertText ",", L (_, ti), R (TListClose (id, _), _) when onEdge ->
+        let astInfo = acEnter ti K.Enter astInfo in
         let bID = gid () in
         let newExpr = E.EBlank bID (* new separators *) in
         astInfo
