d47a220963feca921b7aabff4f1f08f8359463e0
Paul Biggar <paul.biggar@gmail.com>
2020-10-10 09:46:27 -0400

    Split Server.fs into ApiServer and BwdServer

---
+++ fsharp-backend/src/BwdServer/BwdServer.fs
@@ -0,0 +1,23 @@
+module Server
+
+open Saturn
+open Giraffe.Core
+open Giraffe.ResponseWriters
+
+open LibExecution
+
+module Route =
+  let builder typeName methodName = sprintf "/api/%s/%s" typeName methodName
+
+let webApp =
+  router { not_found_handler (setStatusCode 404 >=> text "Api 404") }
+
+let app =
+  application {
+    url "http://0.0.0.0:9000"
+    use_router webApp
+  (* use_static "public" *)
+  (* use_gzip *)
+  }
+
+run app
