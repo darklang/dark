9cb05f4c70358cab9bde9297b53b4bd812ace410
Paul Biggar <paul.biggar@gmail.com>
2020-10-14 16:24:06 -0400

    Get something callable from JS

--- fsharp-backend/src/Wasm/Startup.fs
+++ fsharp-backend/src/Wasm/Startup.fs
@@ -1,13 +1,16 @@
-namespace MyApp.Client
+namespace Wasm
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
-open Bolero.Remoting.Client
 
 module Program =
 
+  // call this from JS with DotNet.invokeMethod('Wasm', 'run', 7)
+  // or DotNet.invokeMethodAsync('Wasm', 'run', 8)
+  [<Microsoft.JSInterop.JSInvokable>]
+  let run (arg : int) = 5 + arg
+
   [<EntryPoint>]
   let Main args =
-      printf("tset")
-      let builder = WebAssemblyHostBuilder.CreateDefault(args)
+      let builder = WebAssemblyHostBuilder.CreateDefault([||])
       builder.Build().RunAsync() |> ignore
       0
