0b96d67be6b711086b67f07a40d7f5cef56c28ea
Paul Biggar <paul.biggar@gmail.com>
2020-10-16 14:00:49 -0400

    Tiny fixup, formatting, etc

--- fsharp-backend/src/LibExecution/Execution.fs
+++ fsharp-backend/src/LibExecution/Execution.fs
@@ -5,24 +5,21 @@ open FSharp.Control.Tasks
 open Runtime
 
 
-let run (e: Expr): Task<Dval> =
-  let functions =
-    StdLib.fns
-    |> List.map (fun fn -> (fn.name, fn))
-    |> Map
+let run (e : Expr) : Task<Dval> =
+  let functions = StdLib.fns |> List.map (fun fn -> (fn.name, fn)) |> Map
 
   let state = { functions = functions }
   (Interpreter.eval state Symtable.empty e).toTask()
 
 
-let runString (e: Expr): Task<string> =
+let runString (e : Expr) : Task<string> =
   task {
     let! result = run e
     return result.ToString()
   }
 
 
-let runJSON (e: Expr): Task<string> =
+let runJSON (e : Expr) : Task<string> =
   task {
     let! result = run e
     return result.toJSON().ToString()
