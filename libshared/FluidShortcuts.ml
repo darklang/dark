a4ce6f5d2a4381d4ce28481eb45be390453b31cc
Paul Biggar <paul.biggar@gmail.com>
2020-10-19 11:02:03 -0400

    Fix incorrect merge

--- libshared/FluidShortcuts.ml
+++ libshared/FluidShortcuts.ml
@@ -2,100 +2,100 @@ open Tc
 open FluidExpression
 open Shared
 
-let blank () : t = EBlank id
+let blank ?(id = gid ()) () : t = EBlank id
 
-let str (str : string) : t = EString (id, str)
+let str ?(id = gid ()) (str : string) : t = EString (id, str)
 
-let int (int : int) : t = EInteger (id, string_of_int int)
+let int ?(id = gid ()) (int : int) : t = EInteger (id, string_of_int int)
 
-let intStr (int : string) : t = EInteger (id, int)
+let intStr ?(id = gid ()) (int : string) : t = EInteger (id, int)
 
-let bool (b : bool) : t = EBool (id, b)
+let bool ?(id = gid ()) (b : bool) : t = EBool (id, b)
 
-let float' (whole : int) (fraction : int) : t =
+let float' ?(id = gid ()) (whole : int) (fraction : int) : t =
   EFloat (id, string_of_int whole, string_of_int fraction)
 
 
-let floatStr (whole : string) (fraction : string) : t =
+let floatStr ?(id = gid ()) (whole : string) (fraction : string) : t =
   EFloat (id, whole, fraction)
 
 
 let null : t = ENull (gid ())
 
-let record (rows : (string * t) list) : t =
+let record ?(id = gid ()) (rows : (string * t) list) : t =
   ERecord (id, List.map rows ~f:(fun (k, v) -> (k, v)))
 
 
-let list (elems : t list) : t = EList (id, elems)
+let list ?(id = gid ()) (elems : t list) : t = EList (id, elems)
 
 let pipeTarget = EPipeTarget (gid ())
 
-let fn ?(ster = NoRail) (name : string) (args : t list) =
+let fn ?(id = gid ()) ?(ster = NoRail) (name : string) (args : t list) =
   EFnCall (id, name, args, ster)
 
 
-let binop ?(ster = NoRail) (name : string) (arg0 : t) (arg1 : t)
+let binop ?(id = gid ()) ?(ster = NoRail) (name : string) (arg0 : t) (arg1 : t)
     =
   EBinOp (id, name, arg0, arg1, ster)
 
 
-let partial (str : string) (e : t) : t = EPartial (id, str, e)
+let partial ?(id = gid ()) (str : string) (e : t) : t = EPartial (id, str, e)
 
-let rightPartial (str : string) (e : t) : t =
+let rightPartial ?(id = gid ()) (str : string) (e : t) : t =
   ERightPartial (id, str, e)
 
 
-let leftPartial (str : string) (e : t) : t =
+let leftPartial ?(id = gid ()) (str : string) (e : t) : t =
   ELeftPartial (id, str, e)
 
 
-let var (name : string) : t = EVariable (id, name)
+let var ?(id = gid ()) (name : string) : t = EVariable (id, name)
 
-let fieldAccess (expr : t) (fieldName : string) : t =
+let fieldAccess ?(id = gid ()) (expr : t) (fieldName : string) : t =
   EFieldAccess (id, expr, fieldName)
 
 
-let if' (cond : t) (then' : t) (else' : t) : t =
+let if' ?(id = gid ()) (cond : t) (then' : t) (else' : t) : t =
   EIf (id, cond, then', else')
 
 
-let let' (varName : string) (rhs : t) (body : t) : t =
+let let' ?(id = gid ()) (varName : string) (rhs : t) (body : t) : t =
   ELet (id, varName, rhs, body)
 
 
-let lambda (varNames : string list) (body : t) : t =
+let lambda ?(id = gid ()) (varNames : string list) (body : t) : t =
   ELambda (id, List.map varNames ~f:(fun name -> (gid (), name)), body)
 
 
-let pipe (first : t) (rest : t list) : t =
+let pipe ?(id = gid ()) (first : t) (rest : t list) : t =
   EPipe (id, first :: rest)
 
 
-let constructor (name : string) (args : t list) : t =
+let constructor ?(id = gid ()) (name : string) (args : t list) : t =
   EConstructor (id, name, args)
 
 
-let just (arg : t) : t = EConstructor (id, "Just", [arg])
+let just ?(id = gid ()) (arg : t) : t = EConstructor (id, "Just", [arg])
 
-let nothing () : t = EConstructor (id, "Nothing", [])
+let nothing ?(id = gid ()) () : t = EConstructor (id, "Nothing", [])
 
-let error (arg : t) : t = EConstructor (id, "Error", [arg])
+let error ?(id = gid ()) (arg : t) : t = EConstructor (id, "Error", [arg])
 
-let ok (arg : t) : t = EConstructor (id, "Ok", [arg])
+let ok ?(id = gid ()) (arg : t) : t = EConstructor (id, "Ok", [arg])
 
-let match' (cond : t) (matches : (FluidPattern.t * t) list) : t =
+let match' ?(id = gid ()) (cond : t) (matches : (FluidPattern.t * t) list) : t =
   EMatch (id, cond, matches)
 
 
-let pInt (int : int) : FluidPattern.t =
+let pInt ?(mid = gid ()) ?(id = gid ()) (int : int) : FluidPattern.t =
   FPInteger (mid, id, string_of_int int)
 
 
-let pIntStr (int : string) : FluidPattern.t =
+let pIntStr ?(mid = gid ()) ?(id = gid ()) (int : string) : FluidPattern.t =
   FPInteger (mid, id, int)
 
 
-let pVar (name : string) : FluidPattern.t =
+let pVar ?(mid = gid ()) ?(id = gid ()) (name : string) : FluidPattern.t =
   FPVariable (mid, id, name)
 
 
@@ -107,46 +107,46 @@ let pConstructor
   FPConstructor (mid, id, name, patterns)
 
 
-let pJust (arg : FluidPattern.t) : FluidPattern.t
+let pJust ?(mid = gid ()) ?(id = gid ()) (arg : FluidPattern.t) : FluidPattern.t
     =
   FPConstructor (mid, id, "Just", [arg])
 
 
-let pNothing () : FluidPattern.t =
+let pNothing ?(mid = gid ()) ?(id = gid ()) () : FluidPattern.t =
   FPConstructor (mid, id, "Nothing", [])
 
 
-let pError (arg : FluidPattern.t) :
+let pError ?(mid = gid ()) ?(id = gid ()) (arg : FluidPattern.t) :
     FluidPattern.t =
   FPConstructor (mid, id, "Error", [arg])
 
 
-let pOk (arg : FluidPattern.t) : FluidPattern.t =
+let pOk ?(mid = gid ()) ?(id = gid ()) (arg : FluidPattern.t) : FluidPattern.t =
   FPConstructor (mid, id, "Ok", [arg])
 
 
-let pBool (b : bool) : FluidPattern.t =
+let pBool ?(mid = gid ()) ?(id = gid ()) (b : bool) : FluidPattern.t =
   FPBool (mid, id, b)
 
 
-let pString (str : string) : FluidPattern.t =
+let pString ?(mid = gid ()) ?(id = gid ()) (str : string) : FluidPattern.t =
   FPString {matchID = mid; patternID = id; str}
 
 
 let pFloatStr
-    (whole : string) (fraction : string) :
+    ?(mid = gid ()) ?(id = gid ()) (whole : string) (fraction : string) :
     FluidPattern.t =
   FPFloat (mid, id, whole, fraction)
 
 
-let pFloat (whole : int) (fraction : int) :
+let pFloat ?(mid = gid ()) ?(id = gid ()) (whole : int) (fraction : int) :
     FluidPattern.t =
   FPFloat (mid, id, string_of_int whole, string_of_int fraction)
 
 
-let pNull () : FluidPattern.t = FPNull (mid, id)
+let pNull ?(mid = gid ()) ?(id = gid ()) () : FluidPattern.t = FPNull (mid, id)
 
-let pBlank () : FluidPattern.t = FPBlank (mid, id)
+let pBlank ?(mid = gid ()) ?(id = gid ()) () : FluidPattern.t = FPBlank (mid, id)
 
-let flag ?(name = "flag-1") cond oldCode newCode =
+let flag ?(id = gid ()) ?(name = "flag-1") cond oldCode newCode =
   EFeatureFlag (id, name, cond, oldCode, newCode)
