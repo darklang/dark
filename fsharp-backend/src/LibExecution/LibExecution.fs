13fe5e630918b02b620f6021a2b09d329f8b823f
Paul Biggar <paul.biggar@gmail.com>
2020-11-20 12:17:52 -0500

    Formatting

--- fsharp-backend/src/LibExecution/LibExecution.fs
+++ fsharp-backend/src/LibExecution/LibExecution.fs
@@ -2,19 +2,19 @@ module Execution
 
 open Runtime
 
-let run (e: Expr): Task<Dval> =
+let run (e : Expr) : Task<Dval> =
   let env = Environment.envWith (StdLib.functions)
   (eval env Symtable.empty e).toTask()
 
 
-let runString (e: Expr): Task<string> =
+let runString (e : Expr) : Task<string> =
   task {
     let! result = run e
     return result.ToString()
   }
 
 
-let runJSON (e: Expr): Task<string> =
+let runJSON (e : Expr) : Task<string> =
   task {
     let! result = run e
     return result.toJSON().ToString()
