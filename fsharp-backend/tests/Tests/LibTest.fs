39a1a8196d33b159ef3b8a35cb50d1b6eb6f4028
Paul Biggar <paul.biggar@gmail.com>
2020-12-02 11:50:57 -0500

    Add a test counting execution steps

--- fsharp-backend/tests/Tests/LibTest.fs
+++ fsharp-backend/tests/Tests/LibTest.fs
@@ -14,6 +14,9 @@ let fn = FnDesc.stdFnDesc
 let varA = TVariable "a"
 let varB = TVariable "b"
 
+// FSTODO: this is going cause race conditions - we should move this into state
+let sideEffectCount : int ref = ref 0
+
 let fns : List<BuiltInFn> =
   [ { name = fn "Test" "errorRailNothing" 0
       parameters = []
@@ -22,6 +25,32 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | state, [] -> Value(DFakeVal(DErrorRail(DOption None)))
+        | args -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+    { name = fn "Test" "incrementSideEffectCounter" 0
+      parameters =
+        [ Param.make "passThru" (TVariable "a") "Value which will be returned" ]
+      returnType = TVariable "a"
+      description =
+        "Increases the side effect counter by one, to test real-world side-effects. Returns its argument."
+      fn =
+        (function
+        | state, [ arg ] ->
+            sideEffectCount := !sideEffectCount + 1
+            Value(arg)
+        | args -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+    { name = fn "Test" "sideEffectCount" 0
+      parameters = []
+      returnType = TInt
+      description = "Return the value of the side-effect counter"
+      fn =
+        (function
+        | state, [] -> Value(Dval.int !sideEffectCount)
         | args -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
