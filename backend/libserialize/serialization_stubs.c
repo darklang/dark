2fb376c360c126b237156dace03772c14c055faf
Paul Biggar <paul.biggar@gmail.com>
2020-12-03 23:56:28 -0500

    Fix segfaults when calling ocaml code
    When calling OCaml, your thread needs the runtime lock as OCaml is not reentrant

--- backend/libserialize/serialization_stubs.c
+++ backend/libserialize/serialization_stubs.c
@@ -4,44 +4,51 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
+#include <caml/threads.h>
 
 /* --------------------
  * Initialize
  * -------------------- */
 
-static const value* user_fn_bin2json_closure = NULL;
-static const value* user_tipe_bin2json_closure = NULL;
-static const value* handler_bin2json_closure = NULL;
-static const value* db_bin2json_closure = NULL;
-static const value* oplist_bin2json_closure = NULL;
-static const value* pos_bin2json_closure = NULL;
-static const value* user_fn_json2bin_closure = NULL;
-static const value* user_tipe_json2bin_closure = NULL;
-static const value* handler_json2bin_closure = NULL;
-static const value* db_json2bin_closure = NULL;
-static const value* oplist_json2bin_closure = NULL;
-static const value* pos_json2bin_closure = NULL;
-static const value* digest_value = NULL;
+// C code has to get a lock to the runtime before calling it.
+// https://caml.inria.fr/pub/docs/manual-ocaml/intfc.html#s%3AC-multithreading
 
+void lock() {
+  printf("registering thread\n");
+  fflush(stdout);
+  caml_c_thread_register();
+  printf("acquiring lock\n");
+  fflush(stdout);
+  caml_acquire_runtime_system();
+  printf("lock acquired\n");
+  fflush(stdout);
+}
 
-int check_string(value v) {
+void unlock() {
+  printf("releasing lock\n");
+  caml_release_runtime_system();
+  printf("lock released\n");
+}
+
+void check_string(value v) {
   if (Is_exception_result(v)) {
     printf("Value is exception!\n");
-    return 0;
+    fflush(stdout);
+    unlock();
+    exit(1);
   }
   if (Tag_val(v) != String_tag) {
     printf("Value is expected to be a string but isn't!\n");
-    return 0;
+    unlock();
+    fflush(stdout);
+    exit(1);
   }
-  return 1;
 }
 
 char* copy_string_outside_runtime(value v) {
-  if (check_string(v)) {
-    return strndup(String_val(v), caml_string_length(v));
-  }
-  printf("Attempting to copy a non-string");
-  exit(1);
+  check_string(v);
+  // OCaml strings can have NULL bytes in them
+  return strndup(String_val(v), caml_string_length(v));
 }
 
 extern char* dark_init_ocaml() {
@@ -53,20 +60,12 @@ extern char* dark_init_ocaml() {
   if (Is_exception_result(res)) {
     printf("OCAML exception !\n");
   }
-  user_fn_bin2json_closure = caml_named_value("user_fn_bin2json");
-  user_tipe_bin2json_closure = caml_named_value("user_tipe_bin2json");
-  handler_bin2json_closure = caml_named_value("handler_bin2json");
-  db_bin2json_closure = caml_named_value("db_bin2json");
-  oplist_bin2json_closure = caml_named_value("oplist_bin2json");
-  pos_bin2json_closure = caml_named_value("pos_bin2json");
-  user_fn_json2bin_closure = caml_named_value("user_fn_json2bin");
-  user_tipe_json2bin_closure = caml_named_value("user_tipe_json2bin");
-  handler_json2bin_closure = caml_named_value("handler_json2bin");
-  db_json2bin_closure = caml_named_value("db_json2bin");
-  oplist_json2bin_closure = caml_named_value("oplist_json2bin");
-  pos_json2bin_closure = caml_named_value("pos_json2bin");
-  digest_value = caml_named_value("digest");
-  check_string(*digest_value);
+  printf("Registering main thread!\n");
+  caml_c_thread_register();
+  printf("Registered main thread!\n");
+  printf("releasing lock from main thread\n");
+  caml_release_runtime_system();
+  printf("released lock from main thread\n");
   return strdup("loaded");
 }
 
@@ -74,35 +73,39 @@ extern char* dark_init_ocaml() {
  * Convert from json to binary strings
  * //FSTODO check for memory leaks in these functions
  * -------------------- */
-char* call_bin2json(const value* closure, void* bytes, int length) {
+char* call_bin2json(const char* callback_name, void* bytes, int length) {
+  lock();
+  value v = caml_alloc_initialized_string(length, bytes);
+  check_string(v);
+  value* closure = caml_named_value(callback_name);
   if (Is_exception_result(*closure)) {
     printf("Closure is exception!\n");
   }
-  value v = caml_alloc_initialized_string(length, bytes);
-  check_string(v);
-  value result = caml_callback(*closure, v);
+  value result = caml_callback_exn(*closure, v);
   if (Is_exception_result(result)) {
     printf("result is exception!\n");
   }
-  return copy_string_outside_runtime(result);
+  char* retval = copy_string_outside_runtime(result);
+  unlock();
+  return retval;
 }
 extern char* user_fn_bin2json(void* bytes, int length) {
-  return call_bin2json(user_fn_bin2json_closure, bytes, length);
+  return call_bin2json("user_fn_bin2json", bytes, length);
 }
 extern char* user_tipe_bin2json(void* bytes, int length) {
-  return call_bin2json(user_tipe_bin2json_closure, bytes, length);
+  return call_bin2json("user_tipe_bin2json", bytes, length);
 }
 extern char* handler_bin2json(void* bytes, int length) {
-  return call_bin2json(handler_bin2json_closure, bytes, length);
+  return call_bin2json("handler_bin2json", bytes, length);
 }
 extern char* db_bin2json(void* bytes, int length) {
-  return call_bin2json(db_bin2json_closure, bytes, length);
+  return call_bin2json("db_bin2json", bytes, length);
 }
 extern char* oplist_bin2json(void* bytes, int length) {
-  return call_bin2json(oplist_bin2json_closure, bytes, length);
+  return call_bin2json("oplist_bin2json", bytes, length);
 }
 extern char* pos_bin2json(void* bytes, int length) {
-  return call_bin2json(pos_bin2json_closure, bytes, length);
+  return call_bin2json("pos_bin2json", bytes, length);
 }
 
 /* --------------------
@@ -111,35 +114,38 @@ extern char* pos_bin2json(void* bytes, int length) {
  * -------------------- */
 
 // out_bytes is an out parameter, it passes a pointer to a memory location. We allocate memory then write the location into which we insert
-int call_json2bin(const value* closure, char* json, void** out_bytes) {
+int call_json2bin(const char* callback_name, char* json, void** out_bytes) {
+  lock();
+  value* closure = caml_named_value(callback_name);
   if (Is_exception_result(*closure)) {
     printf("call_json2bin: Closure is exception!\n");
   }
   value v = caml_copy_string(json);
   check_string(v);
 
-  value result = caml_callback(*closure, v);
+  value result = caml_callback_exn(*closure, v);
   int length = caml_string_length(result);
   *out_bytes = copy_string_outside_runtime(result);
+  unlock();
   return length;
 }
 extern int user_fn_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(user_fn_json2bin_closure, json, out_bytes);
+  return call_json2bin("user_fn_json2bin", json, out_bytes);
 }
 extern int user_tipe_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(user_tipe_json2bin_closure, json, out_bytes);
+  return call_json2bin("user_tipe_json2bin", json, out_bytes);
 }
 extern int handler_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(handler_json2bin_closure, json, out_bytes);
+  return call_json2bin("handler_json2bin", json, out_bytes);
 }
 extern int db_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(db_json2bin_closure, json, out_bytes);
+  return call_json2bin("db_json2bin", json, out_bytes);
 }
 extern int oplist_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(oplist_json2bin_closure, json, out_bytes);
+  return call_json2bin("oplist_json2bin", json, out_bytes);
 }
 extern int pos_json2bin(char* json, void** out_bytes) {
-  return call_json2bin(pos_json2bin_closure, json, out_bytes);
+  return call_json2bin("pos_json2bin", json, out_bytes);
 }
 
 
@@ -147,5 +153,9 @@ extern int pos_json2bin(char* json, void** out_bytes) {
  * OCaml values
  * -------------------- */
 extern char* digest () {
-  return copy_string_outside_runtime(*digest_value);
+  lock();
+  value* digest_value = caml_named_value("digest");
+  char* result = copy_string_outside_runtime(*digest_value);
+  unlock();
+  return result;
 }
