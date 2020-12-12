e2c78c3240c0d4a5526247d684e321d0c86bb407
Paul Biggar <paul.biggar@gmail.com>
2020-12-12 17:35:00 -0500

    Add the first part of the API server
    Everything is disabled, commented out, etc

--- fsharp-backend/src/ApiServer/Ui.fs
+++ fsharp-backend/src/ApiServer/Ui.fs
@@ -1,3 +1,87 @@
 module ApiServer.Ui
 
-let ui (canvasName) : string = $"text: {canvasName}"
+module Config = LibBackend.Config
+
+let adminUiTemplate = "tdo"
+// File.readfile Templates "ui.html"
+
+// FSTODO: clickjacking/ CSP/ frame-ancestors
+let ui (canvasName) : string =
+  // let username = Auth.SessionLwt.username_for session
+  // let user = Account.get_user username
+  // let accountCreated = Account.getUserCreatedAt username
+
+  // let accountCreatedMsTs =
+  //   accountCreated
+  //   |> Core_kernel.Time.to_span_since_epoch
+  //   |> Core_kernel.Time.Span.to_ms
+  //   |> Float.iround_exn
+
+  // let staticHost =
+  //   match local with
+  //   (* TODO: can add other people to this for easier debugging *)
+  //   | Some username -> $"darklang-{username}.ngrok.io"
+  //   | _ -> Config.staticHost
+
+  // let hashStaticFilenames =
+  //   if local = None then Config.hashStaticFilenames else false
+
+  // let liveReloadJs =
+  //   (if Config.browser_reload_enabled then
+  //     "<script type=\"text/javascript\" src=\"//localhost:35729/livereload.js\"> </script>"
+  //    else
+  //      "")
+
+  let hashedStaticFilenames source =
+    // FSTODO
+    ""
+  // if not hash_static_filenames
+  // then "{{HASH_REPLACEMENTS}}" "{}" x
+  // else
+  //   let etags_str = File.readfile ~root:Webroot "etags.json" in
+  //   let etags_json = Yojson.Safe.from_string etags_str in
+  //   let etag_assoc_list =
+  //     to_assoc_list etags_json
+  //     |> List.filter ~f:(fun (file, _) -> not (String.equal "__date" file))
+  //     |> List.filter (* Only hash our assets, not vendored assets *)
+  //          ~f:(fun (file, _) ->
+  //            not (String.is_substring ~substring:"vendor/" file))
+  //   in
+  //   x
+  //   |> fun instr ->
+  //   etag_assoc_list
+  //   |> List.fold ~init:instr ~f:(fun acc (file, hash) ->
+  //          (Util.string_replace file (hashed_filename file hash)) acc)
+  //   |> fun instr ->
+  //   Util.string_replace
+  //     "{{HASH_REPLACEMENTS}}"
+  //     ( etag_assoc_list
+  //     |> List.map ~f:(fun (k, v) ->
+  //            ("/" ^ k, `String ("/" ^ hashed_filename k v)))
+  //     |> (fun x -> `Assoc x)
+  //     |> Yojson.Safe.to_string )
+  //     instr)
+  //
+
+  (* TODO: allow APPSUPPORT in here *)
+  let t = System.Text.StringBuilder(adminUiTemplate)
+  t.Replace("{{ENVIRONMENT_NAME}}", Config.envDisplayName)
+   // .Replace("{{ALLFUNCTIONS}}", Api.functions user.username)
+   // .Replace("{{LIVERELOADJS}}", liveReloadJs).Replace("{{STATIC}}", static_host)
+   // .Replace("{{HEAPIO_ID}}", Config.heapio_id)
+   // .Replace("{{ROLLBARCONFIG}}", Config.rollbar_js)
+   // .Replace("{{PUSHERCONFIG}}", Config.pusher_js)
+   // .Replace("{{USER_CONTENT_HOST}}", Config.user_content_host)
+   // .Replace("{{USER_USERNAME}}", user.username).Replace("{{USER_EMAIL}}", user.email)
+   // .Replace("{{USER_FULLNAME}}", user.name)
+   // .Replace("{{USER_CREATED_AT_UNIX_MSTS}}", (string_of_int account_created_msts))
+   // .Replace("{{USER_IS_ADMIN}}", (string_of_bool user.admin))
+   // .Replace("{{USER_ID}}", (Uuidm.to_string user.id))
+   // .Replace("{{CANVAS_ID}}", (Uuidm.to_string canvas_id))
+   // .Replace("{{CANVAS_NAME}}", canvas)
+   // .Replace("{{APPSUPPORT}}", (File.readfile Webroot "appsupport.js"))
+   // .Replace("{{STATIC}}", static_host)
+   // .Replace("{{HASH_REPLACEMENTS}}", hash_replacements)
+   // .Replace("{{CSRF_TOKEN}}", csrf_token)
+   // .Replace("{{BUILD_HASH}}", Config.build_hash)
+   .ToString()
---
+++ fsharp-backend/src/LibBackend/Config.fs
@@ -0,0 +1,211 @@
+module LibBackend.Config
+
+open LibService.ConfigDsl
+
+// -------------------------
+// Note: if you add an env-var in development, you'll probably need to
+// restart the dev container.
+// -------------------------
+
+// -------------------------
+// Root directories - see File.fs
+// -------------------------
+
+let runDir = absoluteDir "DARK_CONFIG_RUNDIR"
+
+let rootDir = absoluteDir "DARK_CONFIG_ROOT_DIR"
+
+let backendDir = $"{rootDir}backend/"
+
+let testdataDir = $"{backendDir}test_appdata/"
+
+let testResultDir = $"{runDir}test_results/"
+
+let logDir = $"{runDir}logs/"
+
+let serializationDir = "${backendDir}serialization/"
+
+let completedTestDir = $"{runDir}completed_tests/"
+
+// -------------------------
+// Configurable dirs *)
+// -------------------------
+let templatesDir = absoluteDir "DARK_CONFIG_TEMPLATES_DIR"
+
+let webrootDir = absoluteDir "DARK_CONFIG_WEBROOT_DIR"
+
+let swaggerDir = absoluteDir "DARK_CONFIG_SWAGGER_DIR"
+
+let migrationsDir = absoluteDir "DARK_CONFIG_MIGRATIONS_DIR"
+
+let binRootDir = absoluteDir "DARK_CONFIG_BIN_ROOT_DIR"
+
+let __unused_bin_scripts_dir = absoluteDir "DARK_CONFIG_SCRIPTS_DIR"
+
+// -------------------------
+// Web configuration *)
+// -------------------------
+let staticHost = string "DARK_CONFIG_STATIC_HOST"
+
+let cookieDomain = string "DARK_CONFIG_COOKIE_DOMAIN"
+
+let userContentHost = string "DARK_CONFIG_USER_CONTENT_HOST"
+
+let envDisplayName = LibService.Config.envDisplayName
+
+// -------------------------
+// Kubernetes *)
+// -------------------------
+
+let curlTunnelUrl = string "DARK_CONFIG_CURL_TUNNEL_URL"
+
+// --------------------
+// For use in Util
+// --------------------
+
+type root =
+  | Log
+  | Serialization
+  | Templates
+  | Webroot
+  | CompletedTest
+  | Testdata
+  | TestResults
+  | BinRoot
+  | Migrations
+  | NoCheck
+
+let dir root =
+  match root with
+  | Log -> logDir
+  | Serialization -> serializationDir
+  | Templates -> templatesDir
+  | Webroot -> webrootDir
+  | CompletedTest -> completedTestDir
+  | BinRoot -> binRootDir
+  | Testdata -> testdataDir
+  | TestResults -> testResultDir
+  | Migrations -> migrationsDir
+  | NoCheck -> ""
+
+
+(* ------------------------- *)
+(* Running the server *)
+(* ------------------------- *)
+let port = int "DARK_CONFIG_HTTP_PORT"
+
+let allowTestRoutes = bool "DARK_CONFIG_ALLOW_TEST_ROUTES"
+
+let __unusedTriggerQueueWorkers = bool "DARK_CONFIG_TRIGGER_QUEUE_WORKERS"
+
+let createAccounts = bool "DARK_CONFIG_CREATE_ACCOUNTS"
+
+(* ------------------------- *)
+(* Logs *)
+(* ------------------------- *)
+// let logFormat : [`Json | `DecoratedJson] =
+//   let asStr =
+//     stringChoice "DARK_CONFIG_LOGGING_FORMAT" ["json"; "decorated_json"]
+//   in
+//   match asStr with
+//   | "json" ->
+//       `Json
+//   | "decorated_json" ->
+//       `DecoratedJson
+//   | _ ->
+//       failwith $"Invalid logging format: {asStr}"
+//
+
+// let logLevel =
+//   let asStr =
+//     stringChoice
+//       "DARK_CONFIG_LOGLEVEL"
+//       [ "off"
+//         "inspect"
+//         "fatal"
+//         "error"
+//         "warn"
+//         "info"
+//         "success"
+//         "debug"
+//         "all" ]
+//
+//   match asStr with
+//   | "off" -> Off
+//   | "inspect" -> Inspect
+//   | "fatal" -> Fatal
+//   | "error" -> Error
+//   | "warn" -> Warn
+//   | "info" -> Info
+//   | "success" -> Success
+//   | "debug" -> Debug
+//   | "all" -> All
+//   | _ -> failwith $"Invalid level name: {asStr}"
+//
+// FSTODO
+let shouldWriteShapeData = bool "DARK_CONFIG_SAVE_SERIALIZATION_DIGEST"
+
+let showStacktrace = bool "DARK_CONFIG_SHOW_STACKTRACE"
+
+(* ------------------------- *)
+(* Rollbar *)
+(* ------------------------- *)
+
+let rollbarClientAccessToken =
+  (* This is what the rollbar UI calls it *)
+  match string "DARK_CONFIG_ROLLBAR_POST_CLIENT_ITEM" with
+  | "none" -> None
+  | item -> Some item
+
+
+let rollbarEnabled = LibService.Config.rollbarEnabled
+
+let rollbarEnvironment = LibService.Config.rollbarEnvironment
+
+let rollbarJs =
+  match rollbarClientAccessToken with
+  | Some token ->
+      Printf.sprintf
+        "{captureUncaught:true,verbose:true,enabled:%s,accessToken:'%s',payload:{environment: '%s'}}"
+        (rollbarEnabled.ToString())  // FSTODO: is this true/false?
+        token
+        rollbarEnvironment
+  | _ -> "{enabled:false}"
+
+
+let pusherKey = stringOption "DARK_CONFIG_PUSHER_KEY"
+
+let pusherCluster = string "DARK_CONFIG_PUSHER_CLUSTER"
+
+let pusherJs =
+  match pusherKey with
+  | Some key ->
+      Printf.sprintf "{enabled: true, key: '%s', cluster: '%s'}" key pusherCluster
+  | _ -> "{enabled: false}"
+
+
+let heapioId = string "DARK_CONFIG_HEAPIO_ID"
+
+let publicDomain = string "DARK_CONFIG_PUBLIC_DOMAIN"
+
+let browserReloadEnabled = bool "DARK_CONFIG_BROWSER_RELOAD_ENABLED"
+
+let hashStaticFilenames = bool "DARK_CONFIG_HASH_STATIC_FILENAMES"
+
+let checkTierOneHosts = bool "DARK_CONFIG_CHECK_TIER_ONE_HOSTS"
+
+let staticAssetsBucket = stringOption "DARK_CONFIG_STATIC_ASSETS_BUCKET"
+
+// If the GIT_COMMIT is in the environment, use that as the build hash.
+// Otherwise, set it to the env name so that it's constant.
+//
+// We intentionally bypass our DSL here as `GIT_COMMIT` is not set by the
+// config _files_ but as part of the production container build process.
+//
+let buildHash : string =
+  try
+    getEnv "GIT_COMMIT"
+  with _ -> envDisplayName
+
+
+let useLoginDarklangComForLogin = bool "DARK_CONFIG_USE_LOGIN_DARKLANG_COM_FOR_LOGIN"
