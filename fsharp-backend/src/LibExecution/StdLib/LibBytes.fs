b9379543f731a946f1b35cc786737537b54bb68d
Araceli Sánchez <aracelisanchezisc@gmail.com>
2021-02-03 22:11:05 -0800

    hexEncode implemented.

--- fsharp-backend/src/LibExecution/StdLib/LibBytes.fs
+++ fsharp-backend/src/LibExecution/StdLib/LibBytes.fs
@@ -2,6 +2,7 @@ module LibExecution.StdLib.LibBytes
 
 open LibExecution.RuntimeTypes
 open Prelude
+open System.Text
 
 let fn = FQFnName.stdlibName
 
@@ -21,32 +22,30 @@ let fns : List<BuiltInFn> =
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
       deprecated = NotDeprecated }
-    // { name = fn "Bytes" "hexEncode" 0
-    //
-    //   parameters = [ Param.make "bytes" TBytes ]
-    //   returnType = TStr
-    //   description =
-    //     "Hex (Base16) encodes `bytes` using an uppercase alphabet. Complies with RFC 4648 section 8."
-    //   fn =
-    //
-    //     (function
-    //     | _, [ DBytes bytes ] ->
-    //         let hexUppercaseLookup = "0123456789ABCDEF"
-    //         let len = Bytes.length bytes
-    //         let buf = Buffer.create (len * 2)
-    //         for i = 0 to len - 1 do
-    //           let byte = Bytes.unsafe_get bytes i |> int_of_char
-    //           Buffer.add_char
-    //             buf
-    //             (String.unsafe_get hexUppercaseLookup ((byte ``lsr`` 4) ``land`` 0xF))
-    //           Buffer.add_char
-    //             buf
-    //             (String.unsafe_get hexUppercaseLookup (byte ``land`` 0xF))
-    //         Buffer.contents buf |> Dval.dstr_of_string_exn
-    //     | args -> incorrectArgs ())
-    //   sqlSpec = NotYetImplementedTODO
-    //   previewable = Pure
-    //   deprecated = NotDeprecated }
+    { name = fn "Bytes" "hexEncode" 0
+
+      parameters = [ Param.make "bytes" TBytes "" ]
+      returnType = TStr
+      description =
+        "Hex (Base16) encodes `bytes` using an uppercase alphabet. Complies with RFC 4648 section 8."
+      fn =
+
+        (function
+        | _, [ DBytes bytes ] ->
+            let hexUppercaseLookup = "0123456789ABCDEF"
+            let len = bytes.Length
+            let buf = new StringBuilder(len * 2)
+
+            for i = 0 to len - 1 do
+              let byte = bytes.[i] |> int
+              buf.Append(hexUppercaseLookup.[((byte >>> 4) &&& 0xF)])
+              buf.Append(hexUppercaseLookup.[(byte &&& 0xF)])
+
+            buf.ToString() |> DStr |> Value
+        | args -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
     { name = fn "Bytes" "length" 0
       parameters = [ Param.make "bytes" TBytes "" ]
       returnType = TInt
