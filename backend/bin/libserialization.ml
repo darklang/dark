ace95ba4f53465084bffa19d8b0d243ffde9f874
Paul Biggar <paul.biggar@gmail.com>
2021-02-26 21:19:56 -0500

    Make ofInternalQueryable available

--- backend/bin/libserialization.ml
+++ backend/bin/libserialization.ml
@@ -25,6 +25,8 @@ let () =
   Callback.register
     "to_internal_roundtrippable_v0"
     Fuzzing.to_internal_roundtrippable_v0 ;
+  Callback.register "of_internal_queryable_v0" Fuzzing.of_internal_queryable_v0 ;
+  Callback.register "of_internal_queryable_v1" Fuzzing.of_internal_queryable_v1 ;
   Callback.register "to_internal_queryable_v0" Fuzzing.to_internal_queryable_v0 ;
   Callback.register "to_internal_queryable_v1" Fuzzing.to_internal_queryable_v1 ;
   Callback.register "to_developer_repr_v0" Fuzzing.to_developer_repr_v0 ;
