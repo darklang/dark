a8b54572b631762ee7bde869c4821e5afde7e86b
Paul Biggar <paul.biggar@gmail.com>
2021-03-03 16:07:51 -0500

    Move tests into new framework

--- fsharp-backend/tests/Tests/test_user_db.ml
+++ fsharp-backend/tests/Tests/test_user_db.ml
@@ -42,36 +42,6 @@ let t_case_insensitive_db_roundtrip () =
         false
 
 
-let t_nulls_allowed_in_db () =
-  clear_test_data () ;
-  let ops =
-    [ CreateDB (dbid, pos, "MyDB")
-    ; AddDBCol (dbid, colnameid, coltypeid)
-    ; SetDBColName (dbid, colnameid, "x")
-    ; SetDBColType (dbid, coltypeid, "Str") ]
-  in
-  let ast =
-    let'
-      "old"
-      (fn "DB::set_v1" [record [("x", null)]; str "hello"; var "MyDB"])
-      (let'
-         "new"
-         (fn "DB::get_v1" ~ster:Rail [str "hello"; var "MyDB"])
-         (binop "==" (var "old") (var "new")))
-  in
-  check_dval "equal_after_roundtrip" (DBool true) (exec_handler ~ops ast)
-
-
-let t_inserting_object_to_missing_col_gives_good_error () =
-  clear_test_data () ;
-  check_error_contains
-    "error is expected"
-    (exec_handler
-       (fn "DB::add_v0" [record [("col", record [])]; var "TestDB"])
-       ~ops:[CreateDB (dbid, pos, "TestDB")])
-    "Found but did not expect: [col]"
-
-
 let t_nulls_added_to_missing_column () =
   (* Test for the hack that columns get null in all rows to start *)
   clear_test_data () ;
@@ -134,57 +104,3 @@ let t_uuid_db_roundtrip () =
         compare_dval p1 p2
     | _ ->
         1 )
-
-
-let t_password_hash_db_roundtrip () =
-  clear_test_data () ;
-  let ops =
-    [ CreateDB (dbid, pos, "Passwords")
-    ; AddDBCol (dbid, colnameid, coltypeid)
-    ; SetDBColName (dbid, colnameid, "password")
-    ; SetDBColType (dbid, coltypeid, "Password") ]
-  in
-  let ast =
-    let'
-      "pw"
-      (fn "Password::hash" [str "password"])
-      (let'
-         "_"
-         (fn "DB::add_v0" [record [("password", var "pw")]; var "Passwords"])
-         (let'
-            "fetched"
-            (fieldAccess
-               (fn "List::head" [fn "DB::getAll_v2" [var "Passwords"]])
-               "password")
-            (list [var "pw"; var "fetched"])))
-  in
-  AT.check
-    AT.int
-    "A Password::hash'd string can get stored in and retrieved from a user datastore."
-    0
-    ( match exec_handler ~ops ast with
-    | DList [p1; p2] ->
-        compare_dval p1 p2
-    | _ ->
-        1 )
-
-
-let t_escape_pg_escaping () =
-  AT.check AT.string "no quotes" "asdd" (Db.escape_string "asdd") ;
-  AT.check AT.string "single" "as''dd" (Db.escape_string "as'dd") ;
-  AT.check AT.string "double" "as\"dd" (Db.escape_string "as\"dd") ;
-  ()
-
-
-let suite =
-  [ ("DB case-insensitive roundtrip", `Quick, t_case_insensitive_db_roundtrip)
-  ; ( "Good error when inserting badly"
-    , `Quick
-    , t_inserting_object_to_missing_col_gives_good_error )
-  ; ("Nulls allowed in DB", `Quick, t_nulls_allowed_in_db)
-  ; ("UUIDs round-trip to the DB", `Quick, t_uuid_db_roundtrip)
-  ; ("Nulls for missing column", `Quick, t_nulls_added_to_missing_column)
-  ; ( "Password hashes can be stored in and retrieved from the DB"
-    , `Quick
-    , t_password_hash_db_roundtrip )
-  ; ("Test postgres escaping", `Quick, t_escape_pg_escaping) ]
