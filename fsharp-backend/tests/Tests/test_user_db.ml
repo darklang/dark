d24a8088fb2ac3318a3aa1ec815fe959c0160cb0
Paul Biggar <paul.biggar@gmail.com>
2021-03-04 00:39:09 -0500

    Port uuid test, add Uuid.generate

--- fsharp-backend/tests/Tests/test_user_db.ml
+++ fsharp-backend/tests/Tests/test_user_db.ml
@@ -27,36 +27,3 @@ let t_nulls_added_to_missing_column () =
     (exec_handler
        ~ops
        (fn "List::head" [fn "DB::getAllWithKeys_v1" [var "MyDB"]]))
-
-
-let t_uuid_db_roundtrip () =
-  clear_test_data () ;
-  let ops =
-    [ CreateDB (dbid, pos, "Ids")
-    ; AddDBCol (dbid, colnameid, coltypeid)
-    ; SetDBColName (dbid, colnameid, "uu")
-    ; SetDBColType (dbid, coltypeid, "UUID") ]
-  in
-  let ast =
-    let'
-      "i"
-      (fn "Uuid::generate" [])
-      (let'
-         "_"
-         (fn "DB::add_v0" [record [("uu", var "i")]; var "Ids"])
-         (let'
-            "fetched"
-            (fieldAccess
-               (fn "List::head" [fn "DB::getAll_v2" [var "Ids"]])
-               "uu")
-            (list [var "i"; var "fetched"])))
-  in
-  AT.check
-    AT.int
-    "A generated UUID can round-trip from the DB"
-    0
-    ( match exec_handler ~ops ast with
-    | DList [p1; p2] ->
-        compare_dval p1 p2
-    | _ ->
-        1 )
