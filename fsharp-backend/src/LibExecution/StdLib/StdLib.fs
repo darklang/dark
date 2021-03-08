2ebcaade62f1ffc91dbf46c18d4f767950fd6e78
Paul Biggar <paul.biggar@gmail.com>
2021-03-08 12:55:31 -0500

    Remove dead code

--- fsharp-backend/src/LibExecution/StdLib/StdLib.fs
+++ fsharp-backend/src/LibExecution/StdLib/StdLib.fs
@@ -74,35 +74,3 @@ let infixFns : List<BuiltInFn> =
   fns
 
 let fns = infixFns @ prefixFns
-
-// [ { name = FQFnName.stdlibName "Int" "range" 0
-//     parameters =
-//       [ param "list" (TList(TVariable("a"))) "The list to be operated on"
-//         param "fn" (TFn([ TVariable("a") ], TVariable("b"))) "Function to be called on each member" ]
-//     returnType = retVal (TList(TInt)) "List of ints between lowerBound and upperBound"
-//     fn =
-//       (function
-//       | _, [ DInt lower; DInt upper ] ->
-//           List.map DInt [ lower .. upper ]
-//           |> DList
-//           |> Value
-//
-//       | _ -> Error()) }
-//   { name = (FQFnName.stdlibName "HttpClient" "get" 0)
-//     parameters = [ param "url" TString "URL to fetch" ]
-//     returnType = (retVal TString "Body of response")
-//     fn =
-//       (function
-//       | env, [ DStr url ] ->
-//           try
-//             Ok
-//               (Task
-//                 (task {
-//                   let! response = FSharp.Data.Http.AsyncRequestString(url)
-//                   return DStr(response)
-//                  }))
-//           with e ->
-//             printfn "error in HttpClient::get: %s" (e.ToString())
-//             Error()
-//       | _ -> Error()) } ]
-//
