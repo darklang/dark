173cd0f5d762352c46cb04b91b6f2160cfaf7b01
Paul Biggar <paul.biggar@gmail.com>
2021-03-12 11:29:01 -0500

    More FSI support

---
+++ fsharp-backend/tests/Tests/FSI.fs
@@ -0,0 +1,63 @@
+module Tests.FSI
+
+// This isn't really for tests, it's for utilities to tests things in FSI. I
+// didn't have a better place for this.
+
+
+open System.Threading.Tasks
+open FSharp.Control.Tasks
+
+module RT = LibExecution.RuntimeTypes
+module PT = LibBackend.ProgramTypes
+module DvalRepr = LibExecution.DvalRepr
+module OCamlInterop = LibBackend.OCamlInterop
+module Exe = LibExecution.Execution
+
+open Prelude
+
+let execute (code : string) : RT.Dval =
+  let t =
+    task {
+      let! state =
+        TestUtils.executionStateFor
+          "fsi"
+          Map.empty
+          Map.empty
+          (Tests.LibExecution.fns.Force())
+
+      let prog = FSharpToExpr.parseRTExpr code
+
+      return! Exe.run state Map.empty prog
+    }
+
+  Task.WaitAll [| t :> Task |]
+  t.Result
+
+let executeOCaml (code : string) : RT.Dval =
+  let t =
+    task {
+      let prog = FSharpToExpr.parsePTExpr code
+
+      let! state =
+        TestUtils.executionStateFor
+          "fsi"
+          Map.empty
+          Map.empty
+          (Tests.LibExecution.fns.Force())
+
+      return
+        LibBackend.OCamlInterop.execute
+          state.accountID
+          state.canvasID
+          prog
+          Map.empty
+          []
+          []
+    }
+
+  Task.WaitAll [| t :> Task |]
+  t.Result
+
+
+let toBytes (dv : RT.Dval) : string =
+  dv |> toString |> toBytes |> System.BitConverter.ToString
