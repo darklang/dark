2c4fc0bcca212e7e074da39f5320dc0f6cbc6834
Paul Biggar <paul.biggar@gmail.com>
2021-03-17 10:22:23 -0400

    Fix test date equality.
    Like NaN, two dates that seem the same aren't necessarily the same. For testing,
    we want to test that you get a _specific_ date, so we need to allow dates that
    look the same to be equal

--- fsharp-backend/tests/TestUtils/TestUtils.fs
+++ fsharp-backend/tests/TestUtils/TestUtils.fs
@@ -209,8 +209,10 @@ module Expect =
     | DResult (Error l), DResult (Error r) -> de l r
     | DOption (Some l), DOption (Some r) -> de l r
     | DDate l, DDate r ->
-        // There shouldn't be milliseconds by the time we get here
-        Expect.equal l r $"{msg}: {l} <> {r}"
+        // Two dates can be the same millisecond and not be equal if they don't
+        // have the same number of ticks. For testing, we shall consider them
+        // equal if they print the same string.
+        Expect.equal (l.ToString()) (r.ToString()) $"{msg}: {l} <> {r}"
     | DList ls, DList rs ->
         let lLength = List.length ls
         let rLength = List.length rs
@@ -273,6 +275,7 @@ let rec dvalEquality (left : Dval) (right : Dval) : bool =
   match left, right with
   | DFloat l, DFloat r ->
       if System.Double.IsNaN l && System.Double.IsNaN r then
+        // This isn't "true" equality, it's just for tests
         true
       else if System.Double.IsPositiveInfinity l
               && System.Double.IsPositiveInfinity r then
@@ -285,7 +288,11 @@ let rec dvalEquality (left : Dval) (right : Dval) : bool =
   | DResult (Ok l), DResult (Ok r) -> de l r
   | DResult (Error l), DResult (Error r) -> de l r
   | DOption (Some l), DOption (Some r) -> de l r
-  | DDate l, DDate r -> l = r
+  | DDate l, DDate r ->
+      // Two dates can be the same millisecond and not be equal if they don't
+      // have the same number of ticks. For testing, we shall consider them
+      // equal if they print the same string.
+      l.ToString() = r.ToString()
   | DList ls, DList rs -> List.map2 de ls rs |> List.all (fun x -> x)
   | DObj ls, DObj rs ->
       List.map2
