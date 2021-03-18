858285667fccf365cd30bc6f7482a6e7f61159d2
Paul Biggar <paul.biggar@gmail.com>
2021-03-18 15:50:50 -0400

    Port nearly all sqlCompiler tests

--- fsharp-backend/tests/Tests/SqlCompiler.Tests.fs
+++ fsharp-backend/tests/Tests/SqlCompiler.Tests.fs
@@ -4,30 +4,21 @@ open Expecto
 open Prelude
 open TestUtils
 
+open System.Threading.Tasks
+open FSharp.Control.Tasks
+
 module PT = LibBackend.ProgramTypes
-module RT = LibExecution.RuntimeTypes
+open LibExecution.RuntimeTypes
 
 module C = LibBackend.SqlCompiler
+module S = LibExecution.Shortcuts
 
 
 // let t_sql_compiler_works () =
-//   let open Types in
-//   let open Prelude in
-//   let _, state, _ = Utils.test_execution_data [] in
-//   let check
-//       (msg : string)
-//       ?(paramName = "value")
-//       ?(dbFields = [])
-//       ?(symtable = [])
-//       (body : fluid_expr)
-//       (generated : string) : unit =
-//     let dbFields = StrDict.from_list dbFields in
-//     let symtable = StrDict.from_list symtable in
-//     let result =
-//       Sql_compiler.compile_lambda ~state symtable paramName dbFields body
-//     in
-//     AT.check AT.string msg result generated
-//   in
+//   checkError
+//     "no field gives error"
+//     field
+//     "The datastore does not have a field named: myfield" ;
 //   let checkError
 //       (msg : string)
 //       ?(paramName = "value")
@@ -37,60 +28,101 @@ module C = LibBackend.SqlCompiler
 //       (expectedError : string) : unit =
 //     try check msg ~paramName ~dbFields ~symtable body "<error expected>"
 //     with Db.DBQueryException e -> AT.check AT.string msg e expectedError
-//   in
 
-let compileTrue =
-  testTask "compile true" {
-    let state = []
-
+let compile
+  (symtable : DvalMap)
+  (paramName : string)
+  (dbFields : List<string * DType>)
+  (expr : Expr)
+  : Task<string * Map<string, SqlValue>> =
+  task {
     let! state =
       executionStateFor "test" Map.empty Map.empty (Tests.LibExecution.fns.Force())
 
-    let! sql, args =
-      C.compileLambda
-        state
-        Map.empty
-        "value"
-        Map.empty
-        (LibExecution.Shortcuts.eBool true)
+    printfn $"expr: {expr}"
 
-    let args = Map.ofList args
-    printfn $"args: {args}"
-    printfn $"sql: {sql}"
+    try
+      let! sql, args =
+        C.compileLambda state symtable paramName (Map.ofList dbFields) expr
 
-    Expect.isMatchGroups
-      sql
-      @"\(@([A-Z]+)\)"
-      (fun g -> Map.find (debug "key" g.[1].Value) args = Sql.bool true)
-      ""
+      let args = Map.ofList args
+      printfn $"sql: {sql}"
+      printfn $"args: {args}"
+      return sql, args
+    with LibExecution.Errors.DBQueryException msg as e ->
+      printfn $"Exception {msg}"
+      raise e
+      return ("", Map.empty)
   }
 
-//   let true' = bool true in
-//   check "true is true" true' "(true)" ;
-//   let field = fieldAccess (var "value") "myfield" in
-//   check
-//     "correct SQL for field access"
-//     ~dbFields:[("myfield", TBool)]
-//     field
-//     "(CAST(data::jsonb->>'myfield' as bool))" ;
-//   checkError
-//     "no field gives error"
-//     field
-//     "The datastore does not have a field named: myfield" ;
-//   let injection = "'; select * from user_data ;'field" in
-//   let field = binop "==" (fieldAccess (var "value") injection) (str "x") in
-//   check
-//     "field accesses are escaped"
-//     ~dbFields:[(injection, TStr)]
-//     field
-//     "((CAST(data::jsonb->>'''; select * from user_data ;''field' as text)) = ('x'))" ;
-//   let variable = binop "==" (var "var") (fieldAccess (var "value") "name") in
-//   check
-//     "symtable escapes correctly"
-//     ~dbFields:[("name", TStr)]
-//     ~symtable:[("var", Dval.dstr_of_string_exn "';select * from user_data;'")]
-//     variable
-//     "((''';select * from user_data;''') = (CAST(data::jsonb->>'name' as text)))" ;
+let matchSql
+  (sql : string)
+  (pattern : string)
+  (args : Map<string, SqlValue>)
+  (expected : List<SqlValue>)
+  =
+  Expect.isMatchGroups
+    sql
+    pattern
+    (fun g ->
+      match g.Count with // implicit full match counts for 1
+      | 1 -> true
+      | 2 -> Map.find g.[1].Value args = expected.[0]
+      | _ -> failwith "not supported yet")
+    "compare sql"
+
+let compileTests =
+
+  let p code = FSharpToExpr.parseRTExpr code
+
+  testList
+    "compile tests"
+    [ testTask "compile true" {
+        let! sql, args = compile Map.empty "value" [] (p "true")
+        matchSql sql @"\(@([A-Z]+)\)" args [ Sql.bool true ]
+      }
+      testTask "compile field" {
+        let! sql, args =
+          compile Map.empty "value" [ "myfield", TBool ] (p "value.myfield")
+
+        matchSql sql @"\(CAST\(data::jsonb->>'myfield' as bool\)\)" args []
+      }
+      testTask "check escaped fields" {
+        let injection = "'; select * from user_data ;'field"
+
+        let expr =
+          S.eApply
+            (S.eStdFnVal "" "==" 0)
+            [ S.eFieldAccess (S.eVar "value") injection; (S.eStr "x") ]
+
+        let! sql, args = compile Map.empty "value" [ injection, TStr ] expr
+
+        matchSql
+          sql
+          @"\(CAST\(data::jsonb->>'''; select * from user_data ;''field' as text\)\) = \('x'\)\)"
+          args
+          []
+      }
+      testTask "symtable values escaped" {
+        let injection = "';select * from user_data;'"
+
+        let expr =
+          S.eApply
+            (S.eStdFnVal "" "==" 0)
+            [ S.eVar "var"; S.eFieldAccess (S.eVar "value") "name" ]
+
+        let symtable = Map.ofList [ "var", DStr injection ]
+
+        let! sql, args = compile symtable "value" [ "name", TStr ] expr
+
+        matchSql
+          sql
+          @"\(\(''';select * from user_data;'''\) = \(CAST\(data::jsonb->>'name' as text\)\)\)"
+          args
+          []
+      } ]
+
+
 //   let thread =
 //     pipe
 //       (fieldAccess (var "value") "age")
@@ -126,4 +158,4 @@ let inlineWorksWithNested =
 
 
 let tests =
-  testList "SqlCompiler" [ inlineWorksAtRoot; inlineWorksWithNested; compileTrue ]
+  testList "SqlCompiler" [ inlineWorksAtRoot; inlineWorksWithNested; compileTests ]
