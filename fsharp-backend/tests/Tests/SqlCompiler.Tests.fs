55aeffa2a0dbcc91fe10b03d5c9f0d364d0fc3b1
Paul Biggar <paul.biggar@gmail.com>
2021-03-25 18:50:06 -0400

    Better error messages

--- fsharp-backend/tests/Tests/SqlCompiler.Tests.fs
+++ fsharp-backend/tests/Tests/SqlCompiler.Tests.fs
@@ -45,7 +45,7 @@ let compile
       let args = Map.ofList args
       return sql, args
     with LibExecution.Errors.DBQueryException msg as e ->
-      raise e
+      failwith msg
       return ("", Map.empty)
   }
 
