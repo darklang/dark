69c146a765a919628030a7dd4f60be72ae41200f
Paul Biggar <paul.biggar@gmail.com>
2021-03-31 11:33:06 -0400

    Port another analysis test

--- fsharp-backend/tests/Tests/Analysis.Tests.fs
+++ fsharp-backend/tests/Tests/Analysis.Tests.fs
@@ -10,7 +10,6 @@ open Tablecloth
 open TestUtils
 
 open LibExecution.RuntimeTypes
-open LibExecution.Shortcuts
 
 module Analysis = LibBackend.Analysis
 module Canvas = LibBackend.Canvas
@@ -46,7 +45,6 @@ let testRouteVariablesWorkWithStoredEvents : Test =
 
     // set up test
     let httpRoute = "/some/:vars/:and/such"
-    let route = ("HTTP", httpRoute, "GET")
     let handler = testHttpRouteHandler httpRoute "GET" (PT.EBlank 0UL)
     let! meta = testCanvasInfo canvasName
     let! (c : Canvas.T) = canvasForTLs meta [ PT.TLHandler handler ]
@@ -54,12 +52,11 @@ let testRouteVariablesWorkWithStoredEvents : Test =
     // store an event and check it comes out
     let t1 = System.Guid.NewGuid()
     let httpRequestPath = "/some/vars/and/such"
-    let data = (DStr "1")
     let desc = ("HTTP", httpRequestPath, "GET")
-    let! (_ : System.DateTime) = TI.storeEvent c.meta.id t1 desc data
+    let! (_ : System.DateTime) = TI.storeEvent c.meta.id t1 desc (DStr "1")
 
     // check we get back the data we put into it
-    let! events = TI.loadEvents c.meta.id route
+    let! events = TI.loadEvents c.meta.id ("HTTP", httpRoute, "GET")
 
     let (loadedPaths, loadedDvals) =
       events
@@ -67,7 +64,7 @@ let testRouteVariablesWorkWithStoredEvents : Test =
       |> List.unzip
 
     Expect.equal loadedPaths [ httpRequestPath ] "path is the same"
-    Expect.equal loadedDvals [ data ] "data is the same"
+    Expect.equal loadedDvals [ (DStr "1") ] "data is the same"
 
     // check that the event is not in the 404s
     let! f404s = TI.getRecent404s c.meta.id
@@ -75,32 +72,36 @@ let testRouteVariablesWorkWithStoredEvents : Test =
   }
 
 
-// let t_route_variables_work_with_stored_events_and_wildcards () =
-//   (* set up test *)
-//   clear_test_data () ;
-//   let host = "test-route_variables_works_with_wildcards" in
-//   let route = "/api/create_token" in
-//   let request_path = "/api/create-token" in
-//   (* note hyphen vs undeerscore *)
-//   let oplist = [SetHandler (tlid, pos, http_route_handler ~route ())] in
-//   let c = ops2c_exn host oplist in
-//   Canvas.serialize_only [tlid] !c ;
-//   let t1 = Util.create_uuid () in
-//   let desc = ("HTTP", request_path, "GET") in
-//   let route = ("HTTP", route, "GET") in
-//   (* store an event and check it comes out *)
-//   ignore
-//     (SE.store_event
-//        ~canvas_id:!c.id
-//        ~trace_id:t1
-//        desc
-//        (Dval.dstr_of_string_exn "1")) ;
-//   (* check we get back the path for a route with a variable in it *)
-//   let loaded1 = SE.load_events ~canvas_id:!c.id route in
-//   check_dval_list "load GET events" [] (loaded1 |> List.map ~f:t4_get4th) ;
-//   ()
-//
+let testRouteVariablesWorkWithTraceInputsAndWildcards : Test =
+  testTask "route variables work with trace inputs and wildcards" {
+    let canvasName = "test-route_variables_works_with_withcards"
+    do! clearCanvasData (CanvasName.create canvasName)
+
+    // note hyphen vs undeerscore
+    let route = "/api/create_token"
+    let requestPath = "/api/create-token"
+
+    // set up test
+    let handler = testHttpRouteHandler route "GET" (PT.EBlank 0UL)
+    let! meta = testCanvasInfo canvasName
+    let! (c : Canvas.T) = canvasForTLs meta [ PT.TLHandler handler ]
+
+    // store an event and check it comes out
+    let t1 = System.Guid.NewGuid()
+    let desc = ("HTTP", requestPath, "GET")
+    let! (_ : System.DateTime) = TI.storeEvent c.meta.id t1 desc (DStr "1")
+
+    // check we get back the path for a route with a variable in it
+    let! events = TI.loadEvents c.meta.id ("HTTP", route, "GET")
+
+    Expect.equal [] events ""
+  }
+
 
 
 let tests =
-  testList "Analysis" [ testFilterSlash; testRouteVariablesWorkWithStoredEvents ]
+  testList
+    "Analysis"
+    [ testFilterSlash
+      testRouteVariablesWorkWithStoredEvents
+      testRouteVariablesWorkWithTraceInputsAndWildcards ]
