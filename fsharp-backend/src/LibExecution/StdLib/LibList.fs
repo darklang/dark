bac7feab0a43e35e42566f57f15d81b839af7f32
Maria Jose Davila Briceno <mariajdab@gmail.com>
2021-04-03 13:40:51 -0400

    Fixed part of List::getAt_v0 and List::getAt_v1

--- fsharp-backend/src/LibExecution/StdLib/LibList.fs
+++ fsharp-backend/src/LibExecution/StdLib/LibList.fs
@@ -1092,12 +1092,12 @@ let fns : List<BuiltInFn> =
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
-      deprecated = NotDeprecated }
+      deprecated = ReplacedBy(fn "List" "getAt" 1) }
     { name = fn "List" "getAt" 1
       parameters = [Param.make "list" (TList varA) ""; Param.make "index" TInt ""]
       returnType = TOption varA
       description =
-        "Returns `Just value` at `index` in `list` if `index` is less than the length of the list. Otherwise returns `Nothing`."
+      "Returns `Just value` at `index` in `list` if `index` is less than the length of the list otherwise returns `Nothing`."
       fn =
         (function
         | _, [ DList l; DInt index ] -> (List.tryItem (int index) l) |> Dval.option |> Value
