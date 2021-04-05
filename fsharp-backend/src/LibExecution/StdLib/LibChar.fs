e169fe35ec7490225b601d5203de3ceb302a7f5c
Joakim Waltersson <joakim.waltersson@gmail.com>
2021-04-05 14:19:39 +0200

    Port Char to F#

--- fsharp-backend/src/LibExecution/StdLib/LibChar.fs
+++ fsharp-backend/src/LibExecution/StdLib/LibChar.fs
@@ -19,41 +19,37 @@ let incorrectArgs = LibExecution.Errors.incorrectArgs
 let varA = TVariable "a"
 let varB = TVariable "b"
 
-let fns : List<BuiltInFn> = []
-  // [ { name = fn "Char" "toASCIICode" 0
-  //
-  //   ; parameters = [Param.make "c" TCharacter ""]
-  //   ; returnType = TInt
-  //   ; description = "Return `c`'s ASCII code"
-  //   ; fn =
-  //        (fun _ -> Exception.code "This function no longer exists.")
-  //   ; sqlSpec = NotYetImplementedTODO
-  //   ; previewable = Pure
-  //   ; deprecated = ReplacedBy(fn "" "" 0) }
-  // ; { name = fn "Char" "toASCIIChar" 0
-  //   ; parameters = [Param.make "i" TInt ""]
-  //   ; returnType = TCharacter
-  //   ; description = "convert an int to an ASCII character"
-  //   ; fn =
-  //        (fun _ -> Exception.code "This function no longer exists.")
-  //   ; sqlSpec = NotYetImplementedTODO
-  //   ; previewable = Pure
-  //   ; deprecated = ReplacedBy(fn "" "" 0) }
-  // ; { name = fn "Char" "toLowercase" 0
-  //   ; parameters = [Param.make "c" TCharacter ""]
-  //   ; returnType = TCharacter
-  //   ; description = "Return the lowercase value of `c`"
-  //   ; fn =
-  //        (fun _ -> Exception.code "This function no longer exists.")
-  //   ; sqlSpec = NotYetImplementedTODO
-  //   ; previewable = Pure
-  //   ; deprecated = ReplacedBy(fn "" "" 0) }
-  // ; { name = fn "Char" "toUppercase" 0
-  //   ; parameters = [Param.make "c" TCharacter ""]
-  //   ; returnType = TCharacter
-  //   ; description = "Return the uppercase value of `c`"
-  //   ; fn =
-  //        (fun _ -> Exception.code "This function no longer exists.")
-  //   ; sqlSpec = NotYetImplementedTODO
-  //   ; previewable = Pure
-  //   ; deprecated = ReplacedBy(fn "" "" 0) } ]
+let fns : List<BuiltInFn> = 
+  [ { name = fn "Char" "toASCIICode" 0
+      parameters = [Param.make "c" TChar ""]
+      returnType = TInt
+      description = "Return `c`'s ASCII code"
+      fn = Errors.removedFunction
+      sqlSpec = NotQueryable
+      previewable = Pure
+      deprecated = DeprecatedBecause("Dark does not support Characters yet") }
+    { name = fn "Char" "toASCIIChar" 0
+      parameters = [Param.make "i" TInt ""]
+      returnType = TChar
+      description = "convert an int to an ASCII character"
+      fn = Errors.removedFunction
+      sqlSpec = NotQueryable
+      previewable = Pure
+      deprecated = DeprecatedBecause("Dark does not support Characters yet") }
+    { name = fn "Char" "toLowercase" 0
+      parameters = [Param.make "c" TChar ""]
+      returnType = TChar
+      description = "Return the lowercase value of `c`"
+      fn = Errors.removedFunction
+      sqlSpec = NotQueryable
+      previewable = Pure
+      deprecated = DeprecatedBecause("Dark does not support Characters yet") }
+    { name = fn "Char" "toUppercase" 0
+      parameters = [Param.make "c" TChar ""]
+      returnType = TChar
+      description = "Return the uppercase value of `c`"
+      fn = Errors.removedFunction
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = DeprecatedBecause("Dark does not support Characters yet") }
+  ]
