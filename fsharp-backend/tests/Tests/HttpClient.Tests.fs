d4e04f6e0301cfbd7c4be80a6dd2f4a58b7684d4
Paul Biggar <paul.biggar@gmail.com>
2021-05-09 00:13:56 -0400

    Mostly get the HttpClient tests working

--- fsharp-backend/tests/Tests/HttpClient.Tests.fs
+++ fsharp-backend/tests/Tests/HttpClient.Tests.fs
@@ -19,17 +19,18 @@ module Exe = LibExecution.Execution
 
 open TestUtils
 
-type TestCase = { expected : byte array; result : byte array }
+type TestCase = { expected : Http.T; result : Http.T }
 
 let testCases : Dictionary.T<string, TestCase> = Dictionary.empty ()
+
+let toBytes (str : string) = System.Text.Encoding.ASCII.GetBytes str
+let toStr (bytes : byte array) = System.Text.Encoding.ASCII.GetString bytes
 
 let t filename =
   testTask $"HttpClient files: {filename}" {
     let skip = String.startsWith "_" filename
     let name = if skip then String.dropLeft 1 filename else filename
     let testName = $"test-{name}"
-    let toBytes (str : string) = System.Text.Encoding.ASCII.GetBytes str
-    let toStr (bytes : byte array) = System.Text.Encoding.ASCII.GetString bytes
 
     let filename = $"tests/httpclienttestfiles/{filename}"
     let! contents = System.IO.File.ReadAllBytesAsync filename
@@ -39,22 +40,39 @@ let t filename =
       let m =
         Regex.Match(
           contents,
-          "^((\[expected-request\]\n.*\n)+)\[response\]\n(.*)\[test\]\n(.*)$",
+          "^(\[expected-request\]\n(.*)\n)\[response\]\n(.*)\[test\]\n(.*)$",
           RegexOptions.Singleline
         )
 
       if not m.Success then failwith $"incorrect format in {name}"
       let g = m.Groups
 
-      (g.[2].Value, g.[3].Value, g.[4].Value)
+      (toBytes g.[2].Value, toBytes g.[3].Value, g.[4].Value)
 
+    // debuG "expectedRequest" (toStr expectedRequest)
+    // debuG "response" (toStr response)
+    // debuG "code" code
+
+    let host = $"test.builtwithdark.localhost:10005"
+
+    let normalizeHeaders
+      (headers : (string * string) list)
+      : (string * string) list =
+      List.map
+        (function
+        | ("Host", _) -> ("Host", host)
+        | other -> other)
+        headers
 
     // Add the expectedRequest and response to the server
-    let host = $"test.builtwithdark.localhost:10005/{testName}"
+    let expected = expectedRequest |> Http.setHeadersToCRLF |> Http.split
+    let expected = { expected with headers = normalizeHeaders expected.headers }
+    let response = response |> Http.setHeadersToCRLF |> Http.split
+    let response = { response with headers = normalizeHeaders response.headers }
 
-    let testCase = { expected = toBytes expectedRequest; result = toBytes response }
+    let testCase = { expected = expected; result = response }
 
-    testCases.Add(testName, testCase)
+    testCases.Add(name, testCase)
 
     if skip then
       skiptest $"underscore test - {name}"
@@ -62,7 +80,7 @@ let t filename =
       // Parse the code
       let shouldEqual, actualProg, expectedResult =
         code
-        |> String.replace "HOST" host
+        |> String.replace "URL" $"{host}/{name}"
         |> FSharpToExpr.parse
         |> FSharpToExpr.convertToTest
 
@@ -120,47 +138,37 @@ open Microsoft.AspNetCore.Server.Kestrel.Core
 
 let runTestHandler (ctx : HttpContext) : Task<HttpContext> =
   task {
-    // let request =
-    //   request |> String.replace "HOST" host |> toBytes |> Http.setHeadersToCRLF
+    let testName =
+      ctx.Request.Path.Value
+      |> String.dropLeft 1
+      |> String.dropRight 2
+      |> debug "testName"
 
+    debuG "testCases" (Dictionary.toList testCases)
+    let testCase = Dictionary.tryGetValue testName testCases |> Option.unwrapUnsafe
 
-    // get host name
-    // get the response
-    // check if the request is as expected
-    // return the response (headers, body, statuscode)
-    // let actual = Http.split response
-    // let expected = Http.split expectedResponse
-    // let eHeaders = normalizeExpectedHeaders expected.headers actual.body.Length
-    // let aHeaders = normalizeHeaders server actual.headers expected.body
+    let headers = BwdServer.getHeaders ctx
+    let! requestBody = BwdServer.getBody ctx
+    debuG "requestBody" (toStr requestBody)
 
-    // let normalizeHeaders
-    //   (server : Server)
-    //   (hs : string list)
-    //   (body : byte array)
-    //   : string list =
-    //   List.map
-    //     (FsRegEx.replace
-    //       "Date: ..., .. ... .... ..:..:.. ..."
-    //       "Date: xxx, xx xxx xxxx xx:xx:xx xxx")
-    //     hs
-    //
-    // let response =
-    //   response
-    //   |> String.splitOnNewline
-    //   |> List.filterMap
-    //        (fun line ->
-    //          if String.includes "// " line then
-    //            line
-    //            |> String.split "// "
-    //            |> List.head
-    //            |> Option.map String.trim_right
-    //          else
-    //            Some line)
-    //   |> String.concat "\n"
-    //   |> String.replace "HOST" host
-    //   |> toBytes
-    //   |> Http.setHeadersToCRLF
-    printfn "Getting a http request"
+    let expectedHeaders = Map testCase.expected.headers
+    let expectedBody = testCase.expected.body
+
+    if (headers, requestBody) = (expectedHeaders, testCase.expected.body) then
+      ctx.Response.StatusCode <- 200
+      Map.iter (fun k v -> BwdServer.setHeader ctx k v) expectedHeaders
+      do! ctx.Response.Body.WriteAsync(expectedBody, 0, expectedBody.Length)
+    else
+      let body =
+        $"Expected\n{expectedHeaders}\n{toStr expectedBody}\n"
+        + $"Got:\n{headers}\n{toStr requestBody}"
+
+      printfn "%s" body
+
+      ctx.Response.StatusCode <- 400
+      ctx.Response.ContentLength <- int64 body.Length
+      do! ctx.Response.Body.WriteAsync(toBytes body, 0, body.Length)
+
     return ctx
   }
 
