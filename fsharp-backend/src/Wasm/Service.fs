4f85a8b87235240bcac939b62b7ab84d290582ca
Paul Biggar <paul.biggar@gmail.com>
2021-07-01 15:07:30 +0000

    Remove unused code

--- fsharp-backend/src/Wasm/Service.fs
+++ fsharp-backend/src/Wasm/Service.fs
@@ -3,11 +3,10 @@ namespace Wasm
 open BlazorWorker.WorkerCore
 open FSharp.Control.Tasks
 
+#nowarn "988"
+
 type EvalService(messageService : IWorkerMessageService) as this =
   do messageService.IncomingMessage.Add(this.OnMessage) |> ignore
-  member this.Eval expr = ()
-  static member EventsEval = $"Events.EvalRequested"
-  static member ResultMessage = $"EvalResult"
 
   member this.OnMessage(message : string) =
     task {
