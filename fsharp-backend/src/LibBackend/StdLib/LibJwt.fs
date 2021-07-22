fac7ef6f840cce1f0d071283ae341ae96d710740
Paul Biggar <paul.biggar@gmail.com>
2021-07-22 15:45:40 +0000

    Minor refactors

--- fsharp-backend/src/LibBackend/StdLib/LibJwt.fs
+++ fsharp-backend/src/LibBackend/StdLib/LibJwt.fs
@@ -19,86 +19,85 @@ let varA = TVariable "a"
 let varB = TVariable "b"
 let varErr = TVariable "err"
 
-(* Here's how JWT with RS256, and this library, work:
+// Here's how JWT with RS256, and this library, work:
+//
+//   Users provide a private key, some headers, and a payload.
+//
+//   We add fields "type" "JWT" and "alg" "RS256" to the header.
+//
+//   We create the body by base64-encoding the header and the payload,
+//   and joining them with a period:
+//
+//   body = (b64encode header) ^ "." ^ (b64encode payload)
+//
+//   We use the private key to sign the body:
+//
+//   signature = sign body
+//
+//   Then we join the body with the signature with a period.
+//
+//   token = body ^ "." ^ signature
+//
+//   We verify by splitting the parts, checking the signature against the body,
+//   and then de-base-64-ing the body and parsing the JSON.
+//
+//   https://jwt.io/ is helpful for validating this!
 
-   Users provide a private key, some headers, and a payload.
-
-   We add fields "type" "JWT" and "alg" "RS256" to the header.
-
-   We create the body by base64-encoding the header and the payload,
-   and joining them with a period:
-
-   body = (b64encode header) ^ "." ^ (b64encode payload)
-
-   We use the private key to sign the body:
-
-   signature = sign body
-
-   Then we join the body with the signature with a period.
-
-   token = body ^ "." ^ signature
-
-   We verify by splitting the parts, checking the signature against the body,
-   and then de-base-64-ing the body and parsing the JSON.
-
-   https://jwt.io/ is helpful for validating this!
- *)
 
 let signAndEncode (key : RSA) (extraHeaders : DvalMap) (payload : string) : string =
   let header =
     extraHeaders
     |> Map.add "alg" (DStr "RS256")
     |> Map.add "typ" (DStr "JWT")
-    |> DObj
-    |> DvalRepr.toPrettyMachineJsonStringV1
+    |> Json.Vanilla.serialize
     |> toBytes
     |> base64Encode
     |> base64ToUrlEncoded
 
   let payload = payload |> toBytes |> base64Encode |> base64ToUrlEncoded
   let body = header + "." + payload
-  let sha256 = SHA256.Create()
-
-  let RSAFormatter = RSAPKCS1SignatureFormatter key
-  RSAFormatter.SetHashAlgorithm "SHA256"
 
   let signature =
-    RSAFormatter.CreateSignature(body |> toBytes |> sha256.ComputeHash)
+    let RSAFormatter = RSAPKCS1SignatureFormatter key
+    RSAFormatter.SetHashAlgorithm "SHA256"
+    let sha256 = SHA256.Create()
+
+    body
+    |> toBytes
+    |> sha256.ComputeHash
+    |> RSAFormatter.CreateSignature
     |> base64Encode
     |> base64ToUrlEncoded
 
   body + "." + signature
 
 let verifyAndExtractV0 (key : RSA) (token : string) : (string * string) option =
-
   match Seq.toList (String.split [| "." |] token) with
   | [ header; payload; signature ] ->
-      // do the minimum of parsing and decoding before verifying signature.
-      // c.f. "cryptographic doom principle".
-      try
-        let sha256 = SHA256.Create()
-        let hash = (header + "." + payload) |> toBytes |> sha256.ComputeHash
+    // do the minimum of parsing and decoding before verifying signature.
+    // c.f. "cryptographic doom principle".
+    try
+      let signature = signature |> base64FromUrlEncoded |> base64DecodeOpt
 
-        let RSADeformatter = RSAPKCS1SignatureDeformatter key
-        RSADeformatter.SetHashAlgorithm "SHA256"
+      match signature with
+      | None -> None
+      | Some signature ->
+        let hash = (header + "." + payload) |> toBytes |> SHA256.Create().ComputeHash
 
-        let signature = signature |> base64FromUrlEncoded |> base64DecodeOpt
+        let rsaDeformatter = RSAPKCS1SignatureDeformatter key
+        rsaDeformatter.SetHashAlgorithm "SHA256"
 
-        match signature with
-        | None -> None
-        | Some signature ->
-            let header = header |> base64FromUrlEncoded |> base64DecodeOpt
-            let payload = payload |> base64FromUrlEncoded |> base64DecodeOpt
+        if rsaDeformatter.VerifySignature(hash, signature) then
+          let header = header |> base64FromUrlEncoded |> base64DecodeOpt
+          let payload = payload |> base64FromUrlEncoded |> base64DecodeOpt
 
-            if RSADeformatter.VerifySignature(hash, signature) then
-              match (header, payload) with
-              | Some header, Some payload ->
-                  Some(header |> ofBytes, payload |> ofBytes)
-              | _ -> None
-            else
-              None
-
-      with e -> Errors.throw (e.ToString())
+          match (header, payload) with
+          | Some header, Some payload -> Some(ofBytes header, ofBytes payload)
+          | _ -> None
+        else
+          None
+    with
+    | e -> None
   | _ -> None
 
 let verifyAndExtractV1
@@ -108,33 +107,32 @@ let verifyAndExtractV1
 
   match Seq.toList (String.split [| "." |] token) with
   | [ header; payload; signature ] ->
-      //do the minimum of parsing and decoding before verifying signature.
-      //c.f. "cryptographic doom principle".
-      try
-        let sha256 = SHA256.Create()
-        let hash = (header + "." + payload) |> toBytes |> sha256.ComputeHash
+    //do the minimum of parsing and decoding before verifying signature.
+    //c.f. "cryptographic doom principle".
+    try
+      let signature = signature |> base64FromUrlEncoded |> base64DecodeOpt
 
-        let RSADeformatter = RSAPKCS1SignatureDeformatter key
-        RSADeformatter.SetHashAlgorithm "SHA256"
+      match signature with
+      | None -> Error "Unable to base64-decode signature"
+      | Some signature ->
+        let hash = (header + "." + payload) |> toBytes |> SHA256.Create().ComputeHash
 
-        let signature = signature |> base64FromUrlEncoded |> base64DecodeOpt
+        let rsaDeformatter = RSAPKCS1SignatureDeformatter key
+        rsaDeformatter.SetHashAlgorithm "SHA256"
 
-        match signature with
-        | None -> Error "Unable to base64-decode signature"
-        | Some signature ->
-            let header = header |> base64FromUrlEncoded |> base64DecodeOpt
-            let payload = payload |> base64FromUrlEncoded |> base64DecodeOpt
+        if rsaDeformatter.VerifySignature(hash, signature) then
+          let header = header |> base64FromUrlEncoded |> base64DecodeOpt
+          let payload = payload |> base64FromUrlEncoded |> base64DecodeOpt
 
-            if RSADeformatter.VerifySignature(hash, signature) then
-              match (header, payload) with
-              | Some header, Some payload ->
-                  Ok(header |> ofBytes, payload |> ofBytes)
-              | Some _, None -> Error "Unable to base64-decode header"
-              | _ -> Error "Unable to base64-decode payload"
-            else
-              Error "Unable to verify signature"
+          match (header, payload) with
+          | Some header, Some payload -> Ok(ofBytes header, ofBytes payload)
+          | Some _, None -> Error "Unable to base64-decode header"
+          | _ -> Error "Unable to base64-decode payload"
+        else
+          Error "Unable to verify signature"
 
-      with e -> Error e.Message
+    with
+    | e -> Error e.Message
   | _ -> Error "Invalid token format"
 
 let fns : List<BuiltInFn> =
@@ -146,12 +144,12 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; payload ] ->
-            let rsa = RSA.Create()
-            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          let rsa = RSA.Create()
+          rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-            let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
+          let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
 
-            signAndEncode rsa Map.empty payload |> DStr |> Value
+          signAndEncode rsa Map.empty payload |> DStr |> Value
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
@@ -167,12 +165,12 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; DObj headers; payload ] ->
-            let rsa = RSA.Create()
-            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          let rsa = RSA.Create()
+          rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-            let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
+          let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
 
-            signAndEncode rsa headers payload |> DStr |> Value
+          signAndEncode rsa headers payload |> DStr |> Value
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
@@ -185,14 +183,15 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; payload ] ->
-            try
-              let rsa = RSA.Create()
-              rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          try
+            let rsa = RSA.Create()
+            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-              let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
+            let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
 
-              signAndEncode rsa Map.empty payload |> DStr |> Ok |> DResult |> Value
-            with e -> Value(DResult(Error(DStr e.Message)))
+            signAndEncode rsa Map.empty payload |> DStr |> Ok |> DResult |> Value
+          with
+          | e -> Value(DResult(Error(DStr e.Message)))
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
@@ -208,12 +207,12 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; DObj headers; payload ] ->
-            let rsa = RSA.Create()
-            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          let rsa = RSA.Create()
+          rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-            let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
+          let payload = DvalRepr.toPrettyMachineJsonStringV1 payload
 
-            signAndEncode rsa headers payload |> DStr |> Value
+          signAndEncode rsa headers payload |> DStr |> Value
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
@@ -226,23 +225,24 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; DStr token ] ->
-            try
-              let rsa = RSA.Create()
-              rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          try
+            let rsa = RSA.Create()
+            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-              match verifyAndExtractV0 rsa token with
-              | Some (headers, payload) ->
-                  [ ("header", DvalRepr.ofUnknownJsonV1 headers)
-                    ("payload", DvalRepr.ofUnknownJsonV1 payload) ]
-                  |> Map.ofList
-                  |> DObj
-                  |> Some
-                  |> DOption
-                  |> Value
-              | None -> Value(DOption None)
-            with _ ->
-              Errors.throw
-                "No supported key formats were found. Check that the input represents the contents of a PEM-encoded key file, not the path to such a file."
+            match verifyAndExtractV0 rsa token with
+            | Some (headers, payload) ->
+              [ ("header", DvalRepr.ofUnknownJsonV1 headers)
+                ("payload", DvalRepr.ofUnknownJsonV1 payload) ]
+              |> Map.ofList
+              |> DObj
+              |> Some
+              |> DOption
+              |> Value
+            | None -> Value(DOption None)
+          with
+          | _ ->
+            Errors.throw
+              "No supported key formats were found. Check that the input represents the contents of a PEM-encoded key file, not the path to such a file."
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
@@ -255,21 +255,22 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr key; DStr token ] ->
-            try
-              let rsa = RSA.Create()
-              rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
+          try
+            let rsa = RSA.Create()
+            rsa.ImportFromPem(System.ReadOnlySpan(key.ToCharArray()))
 
-              match verifyAndExtractV1 rsa token with
-              | Ok (headers, payload) ->
-                  [ ("header", DvalRepr.ofUnknownJsonV1 headers)
-                    ("payload", DvalRepr.ofUnknownJsonV1 payload) ]
-                  |> Map.ofList
-                  |> DObj
-                  |> Ok
-                  |> DResult
-                  |> Value
-              | Error msg -> Value(DResult(Error(DStr msg)))
-            with _ -> Value(DResult(Error(DStr "Invalid public key")))
+            match verifyAndExtractV1 rsa token with
+            | Ok (headers, payload) ->
+              [ ("header", DvalRepr.ofUnknownJsonV1 headers)
+                ("payload", DvalRepr.ofUnknownJsonV1 payload) ]
+              |> Map.ofList
+              |> DObj
+              |> Ok
+              |> DResult
+              |> Value
+            | Error msg -> Value(DResult(Error(DStr msg)))
+          with
+          | _ -> Value(DResult(Error(DStr "Invalid public key")))
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Impure
