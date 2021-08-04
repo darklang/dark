c64ffd8db62ad15c62bb635b479106e08c8bf5f6
Paul Biggar <paul.biggar@gmail.com>
2021-08-04 23:20:57 +0000

    Add Libx509
    Doesn't work yet though

--- fsharp-backend/src/LibBackend/StdLib/LibX509.fs
+++ fsharp-backend/src/LibBackend/StdLib/LibX509.fs
@@ -4,6 +4,7 @@ open System.Threading.Tasks
 open System.Numerics
 open FSharp.Control.Tasks
 open FSharpPlus
+open System.Security.Cryptography
 
 open LibExecution.RuntimeTypes
 open Prelude
@@ -19,32 +20,26 @@ let incorrectArgs = LibExecution.Errors.incorrectArgs
 let varA = TVariable "a"
 let varB = TVariable "b"
 
-let fns : List<BuiltInFn> = []
-// [ { name = fn "X509" "pemCertificatePublicKey" 0
-//
-//   ; parameters = [Param.make "pemCert" TStr ""]
-//   ; returnType = TResult
-//   ; description =
-//       "Extract the public key from a PEM encoded certificate and return the key in PEM format."
-//   ; fn =
-//         (function
-//         | _, [DStr cert] ->
-//           ( try
-//               cert
-//               |> U.to_string
-//               |> Cstruct.of_string
-//               |> X509.Encoding.Pem.Certificate.of_pem_cstruct1
-//               |> X509.public_key
-//               |> X509.Encoding.Pem.Public_key.to_pem_cstruct1
-//               |> Cstruct.to_string
-//               |> DStr
-//               |> ResOk
-//               |> DResult
-//             with Invalid_argument msg ->
-//               DResult (ResError (DStr msg)) )
-//         | _ ->
-//             incorrectArgs ())
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = NotDeprecated } ]
-//
+let fns : List<BuiltInFn> =
+  [ { name = fn "X509" "pemCertificatePublicKey" 0
+      parameters = [ Param.make "pemCert" TStr "" ]
+      returnType = TResult(TStr, TStr)
+      description =
+        "Extract the public key from a PEM encoded certificate and return the key in PEM format."
+      fn =
+        (function
+        | _, [ DStr cert ] ->
+          (try
+            let x509Cert = new X509Certificates.X509Certificate2(toBytes cert)
+            let publicKeyBytes = x509Cert.GetPublicKey()
+            let label = System.ReadOnlySpan<char>("PUBLIC KEY".ToCharArray())
+            let data = System.ReadOnlySpan<byte> publicKeyBytes
+            let chars = PemEncoding.Write(label, data)
+
+            (new System.String(chars)) |> DStr |> Ok |> DResult |> Value
+           with
+           | e -> Value(DResult(Error(DStr e.Message))))
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Impure
+      deprecated = NotDeprecated } ]
