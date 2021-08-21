cfc148793e0b4da475c871fef027112466e50bf6
Paul Biggar <paul.biggar@gmail.com>
2021-08-21 19:10:52 +0000

    Also benchmark OCaml as a baseline

--- fsharp-backend/src/Benchmark/Benchmark.fs
+++ fsharp-backend/src/Benchmark/Benchmark.fs
@@ -17,7 +17,19 @@ module Interpreter = LibExecution.Interpreter
 module Account = LibBackend.Account
 module Canvas = LibBackend.Canvas
 
-let runCode
+let programContext () : Task<RT.ProgramContext> =
+  task {
+    let! c = TestUtils.testCanvasInfo "benchmark"
+    return
+      { canvasID = c.id
+        accountID = c.owner
+        dbs = Map.empty
+        userFns = Map.empty
+        secrets = []
+        userTypes = Map.empty }
+  }
+
+let runFSharp
   (program : RT.ProgramContext)
   (traceID : System.Guid)
   (expr : RT.Expr)
@@ -28,34 +40,77 @@ let runCode
     return! (Interpreter.eval state symtable expr) |> TaskOrValue.toTask
   }
 
+let runFSharpBenchmark (expr : RT.Expr) (warmUpCount : uint) : Task<unit> =
+  task {
+    print "Running F# Benchmark"
+    let traceID = System.Guid.NewGuid()
+    let! program = programContext ()
+
+    print "  Running warmups"
+    for i = 1u to warmUpCount do
+      let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+      do! runFSharp program traceID expr |> Task.map (ignore<RT.Dval>)
+      stopWatch.Stop()
+      print $"    Warmup {i}: {stopWatch.ElapsedMilliseconds}ms"
+
+    print "  Starting benchmark"
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let! dval = runFSharp program traceID expr
+    stopWatch.Stop()
+    print $"{dval}"
+    print $"  Done: {stopWatch.ElapsedMilliseconds}ms"
+    return ()
+  }
+
+
+
+let runOCaml (expr : PT.Expr) : Task<RT.Dval> =
+  task {
+    let! program = programContext ()
+    let! result =
+      // FSTODO: Even though this goes through serialization and a HTTP call, this is
+      // still massively faster than the F# version, but we should remove that at some point.
+      LibBackend.OCamlInterop.execute
+        program.accountID
+        program.canvasID
+        expr
+        Map.empty
+        []
+        []
+    return result
+  }
+
+let runOCamlBenchmark (expr : PT.Expr) (warmUpCount : uint) : Task<unit> =
+  task {
+    print "Running OCaml Benchmark"
+    print "  Running warmups"
+    for i = 1u to warmUpCount do
+      let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+      do! runOCaml expr |> Task.map (ignore<RT.Dval>)
+      stopWatch.Stop()
+      print $"    Warmup {i}: {stopWatch.ElapsedMilliseconds}ms"
+
+    print "  Starting benchmark"
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let! dval = runOCaml expr
+    stopWatch.Stop()
+    print $"{dval}"
+    print $"  Done: {stopWatch.ElapsedMilliseconds}ms"
+    return ()
+  }
+
+
+
+
 let runBenchmark (filename : string) (warmUpCount : uint) : Task<unit> =
   task {
     let filename = if filename.EndsWith(".fs") then filename else filename + ".fs"
-    let traceID = System.Guid.NewGuid()
-    let! c = TestUtils.testCanvasInfo "benchmark"
-    let program : RT.ProgramContext =
-      { canvasID = c.id
-        accountID = c.owner
-        dbs = Map.empty
-        userFns = Map.empty
-        secrets = []
-        userTypes = Map.empty }
-    print "Running warmups"
     let! code =
       System.IO.File.ReadAllTextAsync $"src/Benchmark/benchmarks/{filename}"
-    let expr = FSharpToExpr.parseRTExpr code
-    for i = 1u to warmUpCount do
-      let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-      do! runCode program traceID expr |> Task.map (ignore<RT.Dval>)
-      stopWatch.Stop()
-      print $"  Warmup {i}: {stopWatch.ElapsedMilliseconds}ms"
-
-    print "Starting benchmark"
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    let! dval = runCode program traceID expr
-    stopWatch.Stop()
-    print $"{dval}"
-    print $"Done: {stopWatch.ElapsedMilliseconds}ms"
+    let ptExpr = FSharpToExpr.parsePTExpr code
+    let rtExpr = ptExpr.toRuntimeType ()
+    do! runOCamlBenchmark ptExpr warmUpCount
+    do! runFSharpBenchmark rtExpr warmUpCount
     return ()
   }
 
