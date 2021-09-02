f459812c3f1e59efa21964c7955259d394fc65e1
Paul Biggar <paul.biggar@gmail.com>
2021-09-02 17:32:24 +0000

    Refactor and ensure healthchecks only run on the healthcheck port

--- fsharp-backend/src/LibService/Kubernetes.fs
+++ fsharp-backend/src/LibService/Kubernetes.fs
@@ -1,7 +1,11 @@
 module LibService.Kubernetes
 
-// ASP.net healthchecks, mapping to k8s routes
+// Kubernetes configuration, managing ASP.NET healthchecks, and other endpoints from
+// the k8s control plane.
 // See https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks
+
+open FSharp.Control.Tasks
+open System.Threading.Tasks
 
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Builder
@@ -25,20 +29,29 @@ let configureServices (services : IServiceCollection) : IServiceCollection =
 
   services
 
+
+// https://blog.markvincze.com/graceful-termination-in-kubernetes-with-asp-net-core/
+// https://andrewlock.net/deploying-asp-net-core-applications-to-kubernetes-part-11-avoiding-downtime-in-rolling-deployments-by-blocking-sigterm/
+let terminateService (ctx : HttpContext) = task { return () } :> Task
+
 let configureApp (port : int) (app : IApplicationBuilder) : IApplicationBuilder =
-  let taggedWith (tag : string) =
-    HealthCheckOptions(Predicate = fun hcr -> hcr.Tags.Contains(tag))
-  app
-    .UseHealthChecks(PathString(""), port) // Just filter on port, allow any path
-    .UseEndpoints(
-      (fun endpoints ->
-        endpoints.MapHealthChecks("/k8s/livenessProbe", taggedWith livenessTag)
+
+  app.UseEndpoints(
+    (fun endpoints ->
+      let taggedWith (tag : string) =
+        HealthCheckOptions(Predicate = fun hcr -> hcr.Tags.Contains(tag))
+      let addHealthCheck (path : string) (tag : string) =
+        endpoints.MapHealthChecks(path, taggedWith tag).RequireHost($"*:{port}")
         |> ignore<IEndpointConventionBuilder>
-        endpoints.MapHealthChecks("/k8s/startupProbe", taggedWith startupTag)
-        |> ignore<IEndpointConventionBuilder>
-        endpoints.MapHealthChecks("/k8s/readinessProbe", taggedWith readinessTag)
-        |> ignore<IEndpointConventionBuilder>)
-    )
+      addHealthCheck "/k8s/livenessProbe" livenessTag
+      addHealthCheck "/k8s/startupProbe" startupTag
+      addHealthCheck "/k8s/readinessProbe" readinessTag
+
+      endpoints
+        .MapPost("/k8s/preStop", RequestDelegate terminateService)
+        .RequireHost($"*:{port}")
+      |> ignore<IEndpointConventionBuilder>)
+  )
 
 //FSTODO: things to check
 
