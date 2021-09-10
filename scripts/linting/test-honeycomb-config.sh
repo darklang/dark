59dc175a162acd6066fe3b34a61c96820301a5ba
Paul Biggar <paul.biggar@gmail.com>
2021-09-10 23:56:37 +0000

    Fix shell warning

--- scripts/linting/test-honeycomb-config.sh
+++ scripts/linting/test-honeycomb-config.sh
@@ -6,4 +6,4 @@ YAML=services/honeycomb-agent/honeycomb-agent-config.yaml
 
 set -x
 
-docker run -v $(pwd)/$YAML:/etc/honeycomb/config.yaml honeycombio/honeycomb-kubernetes-agent:head --validate
+docker run -v "$(pwd)/$YAML:/etc/honeycomb/config.yaml" honeycombio/honeycomb-kubernetes-agent:head --validate
