835fd76385057e6013664256bb80412a6e06002e
Paul Biggar <paul.biggar@gmail.com>
2021-09-11 00:58:53 +0000

    Fix honeycomb test file

--- scripts/linting/test-honeycomb-config.sh
+++ scripts/linting/test-honeycomb-config.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+# Validate the honeycomb agent config
+
+# Creates a docker container with the agent and the config in it and runs it to
+# validate the config. The honeycomb docs suggest using docker volumes, but that
+# doesn't work with remote docker hosts, like we use in CI.
+
 set -euo pipefail
 
 YAML=services/honeycomb-agent/honeycomb-agent-config.yaml
 
-set -x
+IMAGE=honeycombio/honeycomb-kubernetes-agent:head
 
-docker run -v "$(pwd)/$YAML:/etc/honeycomb/config.yaml" honeycombio/honeycomb-kubernetes-agent:head --validate
+TMP_DOCKERFILE=tmp_honeycomb_config.Dockerfile
+
+# This is a little silly, but easier than figuring out how to put
+# tmp_honeycomb_config.yaml in a volume for circle
+echo "FROM $IMAGE" > $TMP_DOCKERFILE
+echo "COPY $YAML /etc/honeycomb/config.yaml" >> $TMP_DOCKERFILE
+docker build -t test-honeycomb -f $TMP_DOCKERFILE .
+
+docker run -it test-honeycomb --validate
+
+rm $TMP_DOCKERFILE
