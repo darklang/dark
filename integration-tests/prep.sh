f1f9b894e6a9c8bc071005d7c94d896a9381ef98
Paul Biggar <paul.biggar@gmail.com>
2021-10-30 18:17:47 +0000

    Dont fail if files missing

--- integration-tests/prep.sh
+++ integration-tests/prep.sh
@@ -12,6 +12,6 @@ TEST_DIR="${DARK_CONFIG_RUNDIR}/integration-tests"
 echo "Clearing old test files"
 rm -Rf "${TEST_DIR}"
 mkdir -p "${TEST_DIR}"
-rm ${DARK_CONFIG_RUNDIR}/test_results/integration_tests.*
+rm -f ${DARK_CONFIG_RUNDIR}/test_results/integration_tests.*
 
 ./integration-tests/clear-db.sh
