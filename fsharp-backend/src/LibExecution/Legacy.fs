3153e0fa99a2e95be74d50f309432181acc34eb4
Paul Biggar <paul.biggar@gmail.com>
2022-01-05 20:41:53 +0000

    Convert legacy to use STJ

--- fsharp-backend/src/LibExecution/Legacy.fs
+++ fsharp-backend/src/LibExecution/Legacy.fs
@@ -4,64 +4,89 @@ open Prelude
 open VendoredTablecloth
 open RuntimeTypes
 
-open Newtonsoft.Json
-open Newtonsoft.Json.Linq
+open System.Text.Json
 
-let writePrettyJson (f : JsonWriter -> unit) : string =
-  let stream = new System.IO.StringWriter()
-  let w = new JsonTextWriter(stream)
-  // Match yojson
-  w.FloatFormatHandling <- FloatFormatHandling.Symbol
-  w.Formatting <- Formatting.Indented
+let writePrettyJson (f : Utf8JsonWriter -> unit) : string =
+  let stream = new System.IO.MemoryStream()
+  let mutable options = new JsonWriterOptions()
+  options.Indented <- true
+  options.SkipValidation <- true
+  let encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+  options.Encoder <- encoder
+  let w = new Utf8JsonWriter(stream, options)
   f w
-  stream.ToString()
+  w.Flush()
+  UTF8.ofBytesUnsafe (stream.ToArray())
 
-type JsonWriter with
+
+type Utf8JsonWriter with
 
   member this.writeObject(f : unit -> unit) =
     this.WriteStartObject()
     f ()
-    this.WriteEnd()
+    this.WriteEndObject()
 
   member this.writeArray(f : unit -> unit) =
     this.WriteStartArray()
     f ()
-    this.WriteEnd()
+    this.WriteEndObject()
+  member this.writeOCamlFloatValue(f : float) =
+    if System.Double.IsPositiveInfinity f then
+      this.WriteRawValue("Infinity", true)
+    else if System.Double.IsNegativeInfinity f then
+      this.WriteRawValue("-Infinity", true)
+    else if System.Double.IsNaN f then
+      this.WriteRawValue("NaN", true)
+    else if f = 0.0 && System.Double.IsNegative f then
+      // check for -0.0
+      this.WriteRawValue("-0.0", true)
+    else if System.Math.Abs(f % 1.0) <= System.Double.Epsilon then
+      // Check for int-valued floats. By default, STJ prints them with no decimal
+      let asString = string f
+      if asString.Contains('.') || asString.Contains('E') then // Don't add .0 at the end of 4.2E18
+        this.WriteRawValue(String.toLowercase asString, true)
+      else
+        this.WriteRawValue($"{asString}.0", true)
+    else
+      let v = f |> string |> String.toLowercase
+      this.WriteRawValue(v)
+  member this.writeFullInt64Value(i : int64) = this.WriteRawValue(string i)
+
+
 
 module PrettyResponseJsonV0 =
 
-  (* At time of writing, this is the same as Dval.unsafe_dval_to_yojson. It's being copied to be certain this format doesn't change. *)
-  let rec unsafeDvalToJsonValue (w : JsonWriter) (redact : bool) (dv : Dval) : unit =
+  // At time of writing, this is the same as Dval.unsafe_dval_to_yojson. It's being copied to be certain this format doesn't change.
+  let rec unsafeDvalToJsonValue
+    (w : Utf8JsonWriter)
+    (redact : bool)
+    (dv : Dval)
+    : unit =
     let writeDval = unsafeDvalToJsonValue w redact
 
     let wrapStringValue (typ : string) (str : string) =
       w.writeObject (fun () ->
-        w.WritePropertyName "type"
-        w.WriteValue(typ)
-        w.WritePropertyName "value"
-        w.WriteValue(str))
+        w.WriteString("type", typ)
+        w.WriteString("value", str))
 
     let wrapNullValue (typ : string) =
       w.writeObject (fun () ->
-        w.WritePropertyName "type"
-        w.WriteValue(typ)
-        w.WritePropertyName "value"
-        w.WriteNull())
+        w.WriteString("type", typ)
+        w.WriteNull("value"))
 
     let wrapNestedDvalValue (typ : string) (dv : Dval) =
       w.writeObject (fun () ->
-        w.WritePropertyName "type"
-        w.WriteValue(typ)
+        w.WriteString("type", typ)
         w.WritePropertyName "value"
         writeDval dv)
 
     match dv with
-    (* basic types *)
-    | DInt i -> w.WriteValue(int i)
-    | DFloat f -> w.WriteValue f
-    | DBool b -> w.WriteValue b
-    | DNull -> w.WriteNull()
-    | DStr s -> w.WriteValue s
+    // basic types
+    | DInt i -> w.writeFullInt64Value i
+    | DFloat f -> w.writeOCamlFloatValue f
+    | DBool b -> w.WriteBooleanValue b
+    | DNull -> w.WriteNullValue()
+    | DStr s -> w.WriteStringValue s
     | DList l -> w.writeArray (fun () -> List.iter writeDval l)
     | DObj o ->
       w.writeObject (fun () ->
@@ -75,32 +100,33 @@ module PrettyResponseJsonV0 =
       wrapNullValue "block"
     | DIncomplete _ -> wrapNullValue "incomplete"
     | DChar c -> wrapStringValue "character" c
-    | DError _ -> wrapStringValue "error" "error"
+    | DError (_, msg) ->
+      // Only used internally, so this is safe to save here
+      wrapStringValue "error" msg
     | DHttpResponse (h) ->
       w.writeObject (fun () ->
-        w.WritePropertyName "type"
-        w.WriteValue "response"
-        w.WritePropertyName "value"
+        w.WriteString("type", "response")
 
+        w.WritePropertyName "value"
         w.writeArray (fun () ->
           match h with
           | Redirect str ->
             w.writeArray (fun () ->
-              w.WriteValue "Redirect"
-              w.WriteValue str)
+              w.WriteStringValue "Redirect"
+              w.WriteStringValue str)
 
             writeDval DNull
           | Response (code, headers, hdv) ->
             w.writeArray (fun () ->
-              w.WriteValue "Response"
-              w.WriteValue code
+              w.WriteStringValue "Response"
+              w.writeFullInt64Value (code)
 
               w.writeArray (fun () ->
                 List.iter
                   (fun (k : string, v : string) ->
                     w.writeArray (fun () ->
-                      w.WriteValue k
-                      w.WriteValue v))
+                      w.WriteStringValue k
+                      w.WriteStringValue v))
                   headers))
 
             writeDval hdv))
@@ -121,22 +147,21 @@ module PrettyResponseJsonV0 =
       (match res with
        | Ok rdv ->
          w.writeObject (fun () ->
-           w.WritePropertyName "type"
-           w.WriteValue("result")
-           w.WritePropertyName "constructor"
-           w.WriteValue("Ok")
+           w.WriteString("type", "result")
+           w.WriteString("constructor", "Ok")
            w.WritePropertyName "values"
            w.writeArray (fun () -> writeDval rdv))
        | Error rdv ->
          w.writeObject (fun () ->
-           w.WritePropertyName "type"
-           w.WriteValue("result")
-           w.WritePropertyName "constructor"
-           w.WriteValue("Error")
+           w.WriteString("type", "result")
+           w.WriteString("constructor", "Error")
            w.WritePropertyName "values"
            w.writeArray (fun () -> writeDval rdv)))
     | DBytes bytes ->
-      bytes |> System.Text.Encoding.ASCII.GetString |> wrapStringValue "bytes"
+      // Note that the OCaml version uses the non-url-safe b64 encoding here
+      bytes |> System.Convert.ToBase64String |> wrapStringValue "bytes"
+
+
 
   let toPrettyResponseJsonV0 (dval : Dval) : string =
     writePrettyJson (fun w -> unsafeDvalToJsonValue w false dval)
