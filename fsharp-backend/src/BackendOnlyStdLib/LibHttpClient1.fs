477f2502ee09b4d52f362e93d4ae662980bc231a
Stachu Korick <hello@stachu.net>
2022-01-28 15:18:25 -0500

    Implement v1 of HttpClient lib fns

--- fsharp-backend/src/BackendOnlyStdLib/LibHttpClient1.fs
+++ fsharp-backend/src/BackendOnlyStdLib/LibHttpClient1.fs
@@ -1,112 +1,100 @@
 module BackendOnlyStdLib.LibHttpClient1
 
-open System.Threading.Tasks
-open System.Numerics
-open FSharp.Control.Tasks
+open System.Net.Http
 
 open LibExecution.RuntimeTypes
 open Prelude
+open LibExecution
 
 module Errors = LibExecution.Errors
 
 let fn = FQFnName.stdlibFnName
 
-let err (str : string) = Ply(Dval.errStr str)
-
-let incorrectArgs = LibExecution.Errors.incorrectArgs
-
 let varA = TVariable "a"
-let varB = TVariable "b"
 
-// let parameters =
-//   [Param.make "uri" TStr ""; Param.make "body" varA ""; Param.make "query" TObj ""; Param.make "headers" TObj ""]
-//
-//
-// let parametersNoBody = [Param.make "uri" TStr ""; Param.make "query" TObj ""; Param.make "headers" TObj ""]
-//
-// type headers = (string * string) list
+// CLEANUP: remove the obj type and use varA instead
+// this^ comment was copied from LibDB.fs - reevaluate
+let obj = TDict varA
+let returnType = obj
+
+let parameters =
+  [ Param.make "uri" TStr ""
+    Param.make "body" varA ""
+    Param.make "query" (TDict TStr) ""
+    Param.make "headers" (TDict TStr) "" ]
+
+let parametersNoBody =
+  [ Param.make "uri" TStr ""
+    Param.make "query" (TDict TStr) ""
+    Param.make "headers" (TDict TStr) "" ]
+
+let jsonFn = DvalReprExternal.toPrettyMachineJsonStringV1
+
+type headers = (string * string) list
 
 let fns : List<BuiltInFn> =
   [
-  // ; { name = fn "HttpClient" "post" 1
-//   ; parameters = parameters
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP POST call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.call
-//         Httpclient.POST
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "put" 1
-//   ; parameters = parameters
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP PUT call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.call
-//         Httpclient.PUT
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "get" 1
-//   ; parameters = parametersNoBody
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP GET call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.callNoBody
-//         Httpclient.GET
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "delete" 1
-//   ; infix_names =
-//       []
-//       (* https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE
-//        * the spec says it may have a body *)
-//   ; parameters = parametersNoBody
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP DELETE call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.callNoBody
-//         Httpclient.DELETE
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "options" 1
-//   ; parameters = parametersNoBody
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP OPTIONS call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.callNoBody
-//         Httpclient.OPTIONS
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "head" 1
-//   ; parameters = parametersNoBody
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP HEAD call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.callNoBody
-//         Httpclient.HEAD
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
-// ; { name = fn "HttpClient" "patch" 1
-//   ; parameters = parameters
-//   ; returnType = TObj
-//   ; description = "Make blocking HTTP PATCH call to `uri`"
-//   ; fn =
-//       Legacy.LibhttpclientV0.call
-//         Httpclient.PATCH
-//         Dval.to_pretty_machine_json_v1
-//   ; sqlSpec = NotYetImplementedTODO
-//   ; previewable = Impure
-//   ; deprecated = ReplacedBy(fn "" "" 0) }
+    { name = fn "HttpClient" "post" 1
+      parameters = parameters
+      returnType = returnType
+      description = "Make blocking HTTP POST call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.call HttpMethod.Post jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "post" 2) }
+    { name = fn "HttpClient" "put" 1
+      parameters = parameters
+      returnType = returnType
+      description = "Make blocking HTTP PUT call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.call HttpMethod.Put jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "put" 2) }
+    { name = fn "HttpClient" "get" 1
+      parameters = parametersNoBody
+      returnType = returnType
+      description = "Make blocking HTTP GET call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.callNoBody HttpMethod.Get jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "get" 2) }
+    { name = fn "HttpClient" "delete" 1
+      parameters = parametersNoBody
+      returnType = returnType
+      description = "Make blocking HTTP DELETE call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.callNoBody HttpMethod.Delete jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "delete" 2) }
+    { name = fn "HttpClient" "options" 1
+      parameters = parametersNoBody
+      returnType = returnType
+      description = "Make blocking HTTP OPTIONS call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.callNoBody HttpMethod.Options jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "options" 2) }
+    { name = fn "HttpClient" "head" 1
+      parameters = parametersNoBody
+      returnType = returnType
+      description = "Make blocking HTTP HEAD call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.callNoBody HttpMethod.Head jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "head" 2) }
+    { name = fn "HttpClient" "patch" 1
+      parameters = parameters
+      returnType = returnType
+      description = "Make blocking HTTP PATCH call to `uri`"
+      fn =
+        LegacyLibHttp.LibhttpclientV0.call HttpMethod.Patch jsonFn
+      sqlSpec = NotQueryable
+      previewable = Impure
+      deprecated = ReplacedBy(fn "HttpClient" "patch" 2) }
   ]
