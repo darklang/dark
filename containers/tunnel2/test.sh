0cd8ece7f8bf70ccfc2843372ec2ef0837db55e5
Paul Biggar <paul.biggar@gmail.com>
2022-01-29 20:20:30 +0000

    Fix bad merge

--- containers/tunnel2/test.sh
+++ containers/tunnel2/test.sh
@@ -14,10 +14,10 @@ function expect_success() {
     return 1
   fi
 }
-
-function expect_failure() {
-  if curl --insecure --connect-timeout 3 $1 > /dev/null 2>&1; then
-    echo -e "${green}success: unable to connect to ${1}${reset}"
+function expect_not_there () {
+  if curl --insecure --connect-timeout 3 $1 > /dev/null 2>&1 ; then
+    echo -e "${red}FAIL: ${1} shouldn't even exist${reset}"
+    return 1
   else
     echo -e "${green}success: non-existant destination ${1} wasn't there${reset}"
   fi
