e2aa3e5ae208d8d2dd088ce4ee7563abfbbf73f8
Paul Biggar <paul.biggar@gmail.com>
2022-02-02 06:59:23 +0000

    Add a way to ensure that the apiserver is being proxied before we accept the httpproxy as being successful

--- fsharp-backend/src/ApiServer/ApiServer.fs
+++ fsharp-backend/src/ApiServer/ApiServer.fs
@@ -36,6 +36,11 @@ let addRoutes (app : IApplicationBuilder) : IApplicationBuilder =
 
   let builder = RouteBuilder(ab)
 
+  // This route is used so that we know that the http proxy is actually proxying the server
+  let checkApiserver : HttpHandler =
+    (fun (ctx : HttpContext) ->
+      task { return ctx.Response.WriteAsync("success: this is apiserver") })
+
   let addRoute
     (verb : string)
     (pattern : string)
@@ -71,6 +76,7 @@ let addRoutes (app : IApplicationBuilder) : IApplicationBuilder =
   addRoute "POST" "/login" html None Login.loginHandler
   addRoute "GET" "/logout" html None Login.logout
   addRoute "POST" "/logout" html None Login.logout
+  addRoute "GET" "/check-apiserver" html None checkApiserver
 
   addRoute "GET" "/a/{canvasName}" html R (htmlHandler Ui.uiHandler)
 
