6b09b4cd544b34134a445519b267f7ace74984ea
Paul Biggar <paul.biggar@gmail.com>
2022-02-16 04:34:47 +0000

    Don't overwrite the hashreplacements with the hashreplacements

--- fsharp-backend/src/ApiServer/Ui.fs
+++ fsharp-backend/src/ApiServer/Ui.fs
@@ -76,7 +76,6 @@ let uiHtml
     if localhostAssets = None then Config.hashStaticFilenames else false
 
   let hashReplacements =
-
     if shouldHash then prodHashReplacementsString.Force() else "{}"
 
   let accountCreatedMsTs =
@@ -93,6 +92,13 @@ let uiHtml
 
 
   let t = StringBuilder(adminUiTemplate.Force())
+
+  // Replace any filenames in the response html with the hashed version
+  if shouldHash then
+    prodHashReplacements
+    |> Lazy.force
+    |> Map.iter (fun filename hashed ->
+      t.Replace(filename, hashed) |> ignore<StringBuilder>)
 
   // CLEANUP move functions into an API call, or even to the CDN
   // CLEANUP move the user info into an API call
@@ -111,13 +117,6 @@ let uiHtml
     .Replace("{{HASH_REPLACEMENTS}}", hashReplacements)
     .Replace("{{CSRF_TOKEN}}", csrfToken)
   |> ignore<StringBuilder>
-
-  // Replace any filenames in the file with the hashed version
-  if shouldHash then
-    prodHashReplacements
-    |> Lazy.force
-    |> Map.iter (fun filename hashed ->
-      t.Replace(filename, hashed) |> ignore<StringBuilder>)
 
   string t
 
