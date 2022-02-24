e1da7d7ad0583808d49cbee27985f24454403cf6
Stachu Korick <hello@stachu.net>
2022-02-24 11:47:00 -0500

    Change 'todo' into a CLEANUP

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # This deliberately can be run outside the container - sometimes you want to test on
 # the host
 
-# todo: this script fails if you cd to this directory.
+# CLEANUP this script fails if you cd to this directory.
 # we should adjust to either allow such, or warn so dev doesn't get confused
 
 PATTERN=".*"
