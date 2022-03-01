1c45b7c6562e5c0bbc8fbba9846332dd44ecb745
Stachu Korick <hello@stachu.net>
2022-02-28 22:51:37 -0500

    Remove try/catch in loading test canvas files

--- fsharp-backend/src/LibBackend/Canvas.fs
+++ fsharp-backend/src/LibBackend/Canvas.fs
@@ -679,20 +679,15 @@ let loadJsonFromDisk (root : Config.Root) (c : Meta) : List<tlid * PT.Oplist> =
 let loadAndResaveFromTestFile (meta : Meta) : Task<unit> =
   task {
     let oplists =
-      try
-        meta
-        |> loadJsonFromDisk Config.Testdata
-        |> List.map (fun (tlid, oplist) ->
-          let tl =
-            let oplist = fromOplist meta [] oplist
-            let tls = toplevels oplist
-            let dtls = deletedToplevels oplist
-            (Map.mergeFavoringLeft tls dtls) |> Map.get tlid |> Option.unwrapUnsafe
-          (tlid, oplist, tl, NotDeleted))
-      with
-      | ex ->
-        debuG "Couldn't find test file - assuming empty oplists" meta
-        []
+      meta
+      |> loadJsonFromDisk Config.Testdata
+      |> List.map (fun (tlid, oplist) ->
+        let tl =
+          let oplist = fromOplist meta [] oplist
+          let tls = toplevels oplist
+          let dtls = deletedToplevels oplist
+          (Map.mergeFavoringLeft tls dtls) |> Map.get tlid |> Option.unwrapUnsafe
+        (tlid, oplist, tl, NotDeleted))
 
     do! saveTLIDs meta oplists
     return ()
