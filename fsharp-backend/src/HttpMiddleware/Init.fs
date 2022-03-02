6cdbb39dc95b6975b279aff95fac622bf38177fc
Stachu Korick <hello@stachu.net>
2022-03-02 10:41:57 -0500

    Address PR feedback

--- fsharp-backend/src/HttpMiddleware/Init.fs
+++ fsharp-backend/src/HttpMiddleware/Init.fs
@@ -1,8 +1,8 @@
+/// Initialize HttpMiddleware
 module HttpMiddleware.Init
 
 open Prelude
 
-/// Initialize HttpMiddleware
 let init (serviceName : string) : unit =
   print $"Initing HttpMiddleware in {serviceName}"
   print $" Inited HttpMiddleware in {serviceName}"
