2514fdfa80f2ba0030037c7b1b445f245d709f10
Paul Biggar <paul.biggar@gmail.com>
2022-03-03 20:47:12 +0000

    Add cleanup

--- fsharp-backend/src/LibExecution/ProgramTypes.fs
+++ fsharp-backend/src/LibExecution/ProgramTypes.fs
@@ -751,10 +751,13 @@ module Handler =
     | HTTP of route : string * method : string * ids : ids
     | Worker of name : string * ids : ids
     // Deprecated but still supported form
+    // CLEANUP: convert these into regular workers (change module name to WORKER,
+    // check if they're unique first though)
     | OldWorker of modulename : string * name : string * ids : ids
     | Cron of name : string * interval : Option<CronInterval> * ids : ids
     | REPL of name : string * ids : ids
     // If there's no module
+    // CLEANUP: convert these into repl and get rid of this case
     | UnknownHandler of name : string * modifier : string * ids : ids
 
     member this.toRuntimeType() : RT.Handler.Spec =
