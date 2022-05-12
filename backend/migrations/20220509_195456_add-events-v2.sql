2ce65bdf9e2472aafc1f8c550b9752ac4c00b090
Paul Biggar <paul.biggar@gmail.com>
2022-05-12 18:44:32 +0000

    Remove unused fields

--- backend/migrations/20220509_195456_add-events-v2.sql
+++ backend/migrations/20220509_195456_add-events-v2.sql
@@ -5,8 +5,6 @@ events_v2
 , module text NOT NULL
 , name text NOT NULL
 , modifier text NOT NULL
-, retries integer NOT NULL DEFAULT 0
 , locked_at TIMESTAMP WITH TIME ZONE -- nullable
 , enqueued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-, delay_until TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 , value text NOT NULL)
