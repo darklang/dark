bdd12c468cf3f463b3932e48bccef805f819c4c7
Paul Biggar <paul.biggar@gmail.com>
2022-05-26 17:57:05 +0000

    Add even more info

--- fsharp-backend/src/BackendOnlyStdLib/HttpClient.fs
+++ fsharp-backend/src/BackendOnlyStdLib/HttpClient.fs
@@ -212,6 +212,8 @@ let httpCall'
         // From http://www.west-wind.com/WebLog/posts/102969.aspx
         let encoding = response.Content.Headers.ContentEncoding.ToString()
         Telemetry.addTag "response.encoding" encoding
+        Telemetry.addTag "response.status_code" response.StatusCode
+        Telemetry.addTag "response.version" response.Version
         use! responseStream = response.Content.ReadAsStreamAsync()
         use contentStream : Stream =
           let decompress = CompressionMode.Decompress
@@ -303,7 +305,21 @@ let httpCall'
     | :? HttpRequestException as e ->
       let code = if e.StatusCode.HasValue then int e.StatusCode.Value else 0
       let message = e |> Exception.getMessages |> String.concat " "
-      Telemetry.addTags [ "error", true; "error_msg", e.Message; "code", code ]
+      Telemetry.addTags [ "error", true
+                          "error_msg", e.Message
+                          "code", code
+                          "data", e.Data
+                          "hresult", e.HResult
+                          "stack_trace", e.StackTrace
+                          "help_link", e.HelpLink ]
+      let inner = e.InnerException
+      if not (isNull inner) then
+        Telemetry.addTags [ "inner.class_name", inner.GetType().ToString()
+                            "inner.error_msg", inner.Message
+                            "inner.data", inner.Data
+                            "inner.hresult", inner.HResult
+                            "inner.stack_trace", inner.StackTrace
+                            "inner.help_link", inner.HelpLink ]
       return Error { url = url; code = code; error = message }
   }
 
