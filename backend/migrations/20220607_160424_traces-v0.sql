3192d89748a977f85cfcb51476498b14b4539a51
Paul Biggar <paul.biggar@gmail.com>
2022-06-09 13:20:18 +0000

    Add table

---
+++ backend/migrations/20220607_160424_traces-v0.sql
@@ -0,0 +1,20 @@
+-- We don't use IF NOT EXISTS here because if they exist something is wrong and we
+-- don't want to create the indexes as the table will be non-empty - we'd rather have
+-- the error.
+
+-- Keep track of what traces are available for which handler/function/etc
+CREATE TABLE
+traces_v0
+( canvas_id UUID NOT NULL
+, tlid int64 NOT NULL
+, trace_id UUID NOT NULL
+, timestamp TIMESTAMPTZ NOT NULL
+-- allows looking up handler by trace
+, PRIMARY KEY (canvas_id, tlid, trace_id)
+);
+
+-- Look up by trace (allows for fast deletion)
+CREATE INDEX
+idx_traces_trace
+ON traces_v0
+(canvas_id, trace_id)
