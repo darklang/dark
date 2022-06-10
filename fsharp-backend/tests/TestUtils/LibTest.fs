15e3589ce88d1e996b8570cb7ca7d9f13309d8b0
Stachu Korick <hello@stachu.net>
2022-06-10 16:39:23 +0000

    Simplify tracking of side effects in F# tests
    The OCaml code needed a variable to track against,
    but the F# tests don't.

--- fsharp-backend/tests/TestUtils/LibTest.fs
+++ fsharp-backend/tests/TestUtils/LibTest.fs
@@ -108,16 +108,13 @@ let fns : List<BuiltInFn> =
       deprecated = NotDeprecated }
     { name = fn "Test" "incrementSideEffectCounter" 0
       parameters =
-        // CLEANUP This parameter is only needed for OCaml, which doesn't have test
-        // state and uses a global instead
-        [ Param.make "counterName" TStr "Name of the counter (unused)"
-          Param.make "passThru" (TVariable "a") "Ply which will be returned" ]
+        [ Param.make "passThru" (TVariable "a") "Ply which will be returned" ]
       returnType = TVariable "a"
       description =
         "Increases the side effect counter by one, to test real-world side-effects. Returns its argument."
       fn =
         (function
-        | state, [ _; arg ] ->
+        | state, [ arg ] ->
           state.test.sideEffectCount <- state.test.sideEffectCount + 1
           Ply(arg)
         | _ -> incorrectArgs ())
@@ -125,12 +122,12 @@ let fns : List<BuiltInFn> =
       previewable = Pure
       deprecated = NotDeprecated }
     { name = fn "Test" "sideEffectCount" 0
-      parameters = [ Param.make "counterName" (TStr) "Name of the counter (unused)" ]
+      parameters = []
       returnType = TInt
       description = "Return the value of the side-effect counter"
       fn =
         (function
-        | state, [ _ ] -> Ply(Dval.int state.test.sideEffectCount)
+        | state, [] -> Ply(Dval.int state.test.sideEffectCount)
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
