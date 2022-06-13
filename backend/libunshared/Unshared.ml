74bc29ed26c73cfd07b6105164238023f6f7894c
Stachu Korick <hello@stachu.net>
2022-06-13 17:42:33 -0400

    Provide additional commentary in legacy OCaml backend

--- backend/libunshared/Unshared.ml
+++ backend/libunshared/Unshared.ml
@@ -1,5 +1,7 @@
 (* can't use 52 bit interval as js_of_ocaml complained, so just use largest 31
  * bit for now. *)
+(* Edit: We no longer use OCaml-built code for Analysis, but the code should
+ * stay as-is as it's consistent with F#'s impl. *)
 let max = Base.Int63.of_int 2147483647
 
 let gid () : Base.Int63.t = Base.Int63.random max
