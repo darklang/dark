7be3c1d4058e5f4dc1b840908ec0a57ef4778478
Stachu Korick <hello@stachu.net>
2022-07-19 10:47:09 -0400

    Implement+test Tuple3 ::first,  ::second, and ::third stdlib fns

--- fsharp-backend/src/LibExecutionStdLib/LibTuple.fs
+++ fsharp-backend/src/LibExecutionStdLib/LibTuple.fs
@@ -13,16 +13,15 @@ let err (str : string) = Ply(Dval.errStr str)
 let incorrectArgs = Errors.incorrectArgs
 
 let fns : List<BuiltInFn> =
-  [ { name = fn "Tuple2" "first" 0
-      parameters = [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
+  [ // Tuple2
+    { name = fn "Tuple2" "first" 0
+      parameters =
+        [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
       returnType = TVariable "a"
-      description =
-        // TODO: improve this text
-        "Returns the first part of a 2-tuple."
+      description = "Returns the first part of a 2-tuple."
       fn =
         (function
-        | state, [ DTuple (first, _second, []) ] ->
-          Ply(first)
+        | state, [ DTuple (first, _second, []) ] -> Ply(first)
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
@@ -30,15 +29,65 @@ let fns : List<BuiltInFn> =
 
 
     { name = fn "Tuple2" "second" 0
-      parameters = [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
+      parameters =
+        [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
       returnType = TVariable "b"
-      description =
-        // TODO: improve this text
-        "Returns the second part of a 2-tuple."
+      description = "Returns the second part of a 2-tuple."
       fn =
         (function
-        | state, [ DTuple (_first, second, []) ] ->
-          Ply(second)
+        | state, [ DTuple (_first, second, []) ] -> Ply(second)
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+
+
+    // Tuple3
+    { name = fn "Tuple3" "first" 0
+      parameters =
+        [ Param.make
+            "tuple"
+            (TTuple(TVariable "a", TVariable "b", [ TVariable "c" ]))
+            "" ]
+      returnType = TVariable "a"
+      description = "Returns the first part of a 3-tuple."
+      fn =
+        (function
+        | state, [ DTuple (first, _second, [ _third ]) ] -> Ply(first)
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+
+
+    { name = fn "Tuple3" "second" 0
+      parameters =
+        [ Param.make
+            "tuple"
+            (TTuple(TVariable "a", TVariable "b", [ TVariable "c" ]))
+            "" ]
+      returnType = TVariable "b"
+      description = "Returns the second part of a 3-tuple."
+      fn =
+        (function
+        | state, [ DTuple (_first, second, [ _third ]) ] -> Ply(second)
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+
+
+    { name = fn "Tuple3" "third" 0
+      parameters =
+        [ Param.make
+            "tuple"
+            (TTuple(TVariable "a", TVariable "b", [ TVariable "c" ]))
+            "" ]
+      returnType = TVariable "c"
+      description = "Returns the third part of a 3-tuple."
+      fn =
+        (function
+        | state, [ DTuple (_first, _second, [ third ]) ] -> Ply(third)
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplementedTODO
       previewable = Pure
