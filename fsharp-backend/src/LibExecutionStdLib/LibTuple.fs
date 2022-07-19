33a9fedf80bbc565a510dea840e492c464eac8c0
Stachu Korick <hello@stachu.net>
2022-07-19 10:25:18 -0400

    Implement+test Tuple2 ::first and ::second stdlib fns

--- fsharp-backend/src/LibExecutionStdLib/LibTuple.fs
+++ fsharp-backend/src/LibExecutionStdLib/LibTuple.fs
@@ -12,4 +12,34 @@ let err (str : string) = Ply(Dval.errStr str)
 
 let incorrectArgs = Errors.incorrectArgs
 
-let fns : List<BuiltInFn> = []
+let fns : List<BuiltInFn> =
+  [ { name = fn "Tuple2" "first" 0
+      parameters = [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
+      returnType = TVariable "a"
+      description =
+        // TODO: improve this text
+        "Returns the first part of a 2-tuple."
+      fn =
+        (function
+        | state, [ DTuple (first, _second, []) ] ->
+          Ply(first)
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated }
+
+
+    { name = fn "Tuple2" "second" 0
+      parameters = [ Param.make "tuple" (TTuple(TVariable "a", TVariable "b", [])) "" ]
+      returnType = TVariable "b"
+      description =
+        // TODO: improve this text
+        "Returns the second part of a 2-tuple."
+      fn =
+        (function
+        | state, [ DTuple (_first, second, []) ] ->
+          Ply(second)
+        | _ -> incorrectArgs ())
+      sqlSpec = NotYetImplementedTODO
+      previewable = Pure
+      deprecated = NotDeprecated } ]
