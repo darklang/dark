f11a6669dfc53bb2e4cf027b894a986309cb5a79
Paul Biggar <paul.biggar@gmail.com>
2022-08-13 21:03:27 +0000

    Fix migration

--- backend/migrations/20220813_023655_add-tunnel-host.sql
+++ backend/migrations/20220813_023655_add-tunnel-host.sql
@@ -4,10 +4,10 @@ registered_tunnelhosts
 , tunnel_host VARCHAR(255) NOT NULL
 , created_at TIMESTAMP NOT NULL DEFAULT NOW()
 , updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-, PRIMARY_KEY(user_id, tunnel_host))
+, PRIMARY KEY (user_id, tunnel_host)
+);
 
 CREATE TRIGGER set_registered_tunnelhosts_timestamp
 BEFORE UPDATE ON registered_tunnelhosts
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp()
-
