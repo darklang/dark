2059496cc03e3f5143e77ca0d1d7d2aa56e75727
Paul Biggar <paul.biggar@gmail.com>
2022-09-27 01:34:57 +0000

    Delete unused code

--- client/src/appsupport.js
+++ client/src/appsupport.js
@@ -314,56 +314,6 @@ window.Dark = {
   },
 
   // ---------------------------
-  // Calculate AST positions
-  // ---------------------------
-  ast: {
-    positions: function (tlid) {
-      var extractId = function (elem) {
-        var className = elem.className;
-        var matches = /.*id-(\S+).*/g.exec(className);
-        var id = matches[1];
-
-        if (typeof id === "undefined")
-          throw (
-            "Dark.ast.atomPositions: Cannot match Blank(id) regex for " +
-            className
-          );
-
-        return id;
-      };
-
-      var find = function (tl, nested) {
-        var atoms = [];
-        tl.querySelectorAll(
-          nested ? ".blankOr.nested" : ".blankOr:not(.nested)",
-        ).forEach((v, i, l) => {
-          var rect = v.getBoundingClientRect();
-          atoms.push({
-            id: extractId(v),
-            left: "" + (rect.left | 0),
-            right: "" + (rect.right | 0),
-            top: "" + (rect.top | 0),
-            bottom: "" + (rect.bottom | 0),
-          });
-        });
-        return atoms;
-      };
-
-      var toplevels = document.getElementsByClassName("toplevel tl-" + tlid);
-
-      if (toplevels.length == 0)
-        throw "Dark.ast.atomPositions: Cannot find toplevel: " + tlid;
-
-      var tl = toplevels[0];
-
-      return {
-        atoms: find(tl, false),
-        nested: find(tl, true),
-      };
-    },
-  },
-
-  // ---------------------------
   // Fullstory
   // ---------------------------
   fullstory: {
