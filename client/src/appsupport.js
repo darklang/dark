dd7d6a56f2ab6fd2577fd3d69e08d3a13d17cdde
Paul Biggar <paul.biggar@gmail.com>
2022-10-06 14:56:23 +0000

    Inline unsupportedBrowser.js

--- client/src/appsupport.js
+++ client/src/appsupport.js
@@ -20,6 +20,17 @@ function unsupportedBrowser() {
   return !isSupported;
 }
 
+// This requires being loaded by defer, not as async
+document.addEventListener("DOMContentLoaded", event => {
+  if (unsupportedBrowser()) {
+    let htmlString =
+      "<div class='modal-overlay' id='unsupportedBrowser'><div class='modal'><div class='warning'><p class='title'>Dark is only fully supported on Desktop Chrome right now. We have a desktop client you can use if you prefer.</p><p class='title'>Unfortunately, as a small team, we don’t have the capacity to support other browsers at the moment. Once we’ve made the experience excellent on Chrome, we'll address cross-browser compatibility issues to support Firefox, Safari, and mobile devices.</p><p class='title'>Thanks for understanding ❤️</p></div></div></div>";
+    var div = document.createElement("div");
+    div.innerHTML = htmlString;
+    document.body.append(div);
+  }
+});
+
 window.unsupportedBrowser = unsupportedBrowser;
 
 if (unsupportedBrowser) {
