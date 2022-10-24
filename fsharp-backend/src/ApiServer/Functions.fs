4a4d9cf10e88037042e61720646cdf56471be6f8
Stachu Korick <hello@stachu.net>
2022-10-24 15:45:58 +0000

    Tidy logic used to gather fns to 'inject' in ui.html

--- fsharp-backend/src/ApiServer/Functions.fs
+++ fsharp-backend/src/ApiServer/Functions.fs
@@ -1,33 +1,19 @@
-/// Types used to send functions to the client
-///
-/// Many of these types exactly match runtime types, but often there are small
-/// changes
+/// Functions loaded with the Client's UI
 module ApiServer.Functions
 
 open Prelude
 open Tablecloth
 
-module RT = LibExecution.RuntimeTypes
-
-let functionsToString (fns : RT.BuiltInFn list) : string =
-  fns
-  |> List.map ClientTypes2BackendTypes.UI.Functions.BuiltInFn.toCT
-  |> List.sortBy (fun fn -> fn.name)
-  |> Json.Vanilla.prettySerialize
-
-let adminFunctions : string =
-  LibRealExecution.RealExecution.stdlibFns |> Map.values |> functionsToString
-
-let nonAdminFunctions : string =
-  LibRealExecution.RealExecution.stdlibFns
-  |> Map.values
-  |> List.filter (function
-    | { name = { module_ = "DarkInternal" } } -> false
-    | _ -> true)
-  |> functionsToString
-
 /// Returns a list of all standard library Functions
 ///
 /// Depending on `includeAdminFns` flag, may exclude Dark admin-only fns
 let functions (includeAdminFns : bool) : string =
-  if includeAdminFns then adminFunctions else nonAdminFunctions
+  let fnFilter (fn : LibExecution.RuntimeTypes.BuiltInFn) =
+    if includeAdminFns then true else fn.name.module_ <> "DarkInternal"
+
+  LibRealExecution.RealExecution.stdlibFns
+  |> Map.values
+  |> List.filter fnFilter
+  |> List.map ClientTypes2BackendTypes.UI.Functions.BuiltInFn.toCT
+  |> List.sortBy (fun fn -> fn.name)
+  |> Json.Vanilla.prettySerialize
