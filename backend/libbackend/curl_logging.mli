2db233a820962127b35184d7cf608fce7cfd1a18
Paul Biggar <paul.biggar@gmail.com>
2022-11-09 00:43:22 -0500

    Remove obvious ocaml stuff

--- backend/libbackend/curl_logging.mli
+++
@@ -1,7 +0,0 @@
-type bufs = string ref * string ref * string ref * string ref * string ref
-
-val new_debug_bufs : unit -> bufs
-
-val debugfn : bufs -> Curl.t -> Curl.curlDebugType -> string -> unit
-
-val log_debug_info : bufs -> string option -> unit
