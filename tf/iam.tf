2abb1b5467b1805607276d95514e549df8f32ac5
Paul Biggar <paul.biggar@gmail.com>
2023-01-03 16:55:47 +0000

    Add traces_storage service account

--- tf/iam.tf
+++ tf/iam.tf
@@ -4,3 +4,10 @@ resource "google_service_account" "cloud_run_runner" {
   display_name = "cloud-run-runner"
   project      = "balmy-ground-195100"
 }
+
+resource "google_service_account" "traces_storage" {
+  account_id   = "traces-storage"
+  description  = "Cloud Storage permissions for traces"
+  display_name = "Traces Storage"
+  project      = "balmy-ground-195100"
+}
