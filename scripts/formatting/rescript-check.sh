e9fc6bc255886b75f8a7a8f0e0169b1457aab966
Stachu Korick <hello@stachu.net>
2023-02-07 10:22:20 -0500

    Stop formatting rescript files

--- scripts/formatting/rescript-check.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Checks if a given ReScript file is formatted correctly
-
-TEMPFILENAME=$(mktemp)
-./node_modules/.bin/bsc -format $1 > "$TEMPFILENAME" && diff "$TEMPFILENAME" $1 && rm "$TEMPFILENAME"
