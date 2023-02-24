085d618d68f6259c53445a9cc148b163381b5e5c
Paul Biggar <paul.biggar@gmail.com>
2023-02-24 05:45:09 +0000

    Remove and inline unused code

--- backend/src/LibExecutionStdLib/LibHttpClientAuth.fs
+++ backend/src/LibExecutionStdLib/LibHttpClientAuth.fs
@@ -12,31 +12,6 @@ let fn = FQFnName.stdlibFnName
 
 let incorrectArgs = Errors.incorrectArgs
 
-/// Base64-encodes username/password combination for basic authentication
-///
-/// Deprecated in favor of [encodeBasicAuth u p] due to using non-unicode append
-let encodeBasicAuthBroken (u : string) (p : string) : string =
-  let input : byte [] =
-    if u.Contains("-") then
-      Exception.raiseCode "Username cannot contain a hyphen"
-    else
-      ([ (System.Text.Encoding.UTF8.GetBytes u)
-         [| byte ':' |]
-         (System.Text.Encoding.UTF8.GetBytes p) ]
-       |> Array.concat)
-
-  $"Basic {Base64.defaultEncodeToString input}"
-
-/// Base64-encodes username/password combination for basic authentication
-let encodeBasicAuth (u : string) (p : string) : string =
-  let input : byte [] =
-    if u.Contains("-") then
-      Exception.raiseCode "Username cannot contain a hyphen"
-    else
-      toBytes $"{u}:{p}"
-
-  $"Basic {Base64.defaultEncodeToString input}"
-
 let fns : List<BuiltInFn> =
   [ { name = fn "HttpClient" "basicAuth" 1
       parameters = [ Param.make "username" TStr ""; Param.make "password" TStr "" ]
@@ -46,6 +21,16 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, [ DStr u; DStr p ] ->
+          /// Base64-encodes username/password combination for basic authentication
+          let encodeBasicAuth (u : string) (p : string) : string =
+            let input : byte [] =
+              if u.Contains("-") then
+                Exception.raiseCode "Username cannot contain a hyphen"
+              else
+                toBytes $"{u}:{p}"
+
+            $"Basic {Base64.defaultEncodeToString input}"
+
           Ply(DObj(Map [ "Authorization", (DStr(encodeBasicAuth u p)) ]))
         | _ -> incorrectArgs ())
       previewable = Pure
