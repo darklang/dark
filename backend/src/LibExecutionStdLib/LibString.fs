b534c26d43d54978fc66ea4889fb623d6bbe316b
ditansu <ditansu@gmail.com>
2023-03-12 01:52:44 +0300

    String.padEnd returns Result

--- backend/src/LibExecutionStdLib/LibString.fs
+++ backend/src/LibExecutionStdLib/LibString.fs
@@ -796,19 +796,21 @@ let fns : List<BuiltInFn> =
         [ Param.make "string" TStr ""
           Param.make "padWith" TStr ""
           Param.make "goalLength" TInt "" ]
-      returnType = TStr
+      returnType = TResult(TStr, TStr)
       description =
         "If <param string> is shorter than <param goalLength> characters, returns a
          copy of <param string> ending with enough copies of <param padWith> for the
-         result have <param goalLength>.
+         result have <param goalLength>. A returning value is wrapped in a {{Result}}.
 
          If the <param string> is longer than <param goalLength>, returns an unchanged copy of
          <param string>."
       fn =
         (function
         | _, [ DStr s; DStr padWith as dv; DInt l ] ->
+          let errPipe e = e |> DStr |> Error |> DResult |> Ply
+          let okPipe r = r |> DStr |> Ok |> DResult |> Ply
           if String.lengthInEgcs padWith <> 1 then
-            err (Errors.argumentWasnt "1 character long" "padWith" dv)
+            Errors.argumentWasnt "1 character long" "padWith" dv |> errPipe
           else
             let targetLength = int l
             let currentLength = String.lengthInEgcs s
@@ -820,7 +822,7 @@ let fns : List<BuiltInFn> =
             for _ = 1 to requiredPads do
               stringBuilder.Append(padWith) |> ignore<StringBuilder>
 
-            stringBuilder |> string |> String.normalize |> DStr |> Ply
+            stringBuilder |> string |> String.normalize |> okPipe
 
         | _ -> incorrectArgs ())
       sqlSpec = NotYetImplemented
