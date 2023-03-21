cc3e29fb367bdfd95f1177c653bd82ffcd54f76d
OceanOak <oakocean9@gmail.com>
2023-03-21 15:47:43 +0000

    Delete workerV1Stats -table events doesn't exist anymore

--- backend/src/LibBackend/Stats.fs
+++ backend/src/LibBackend/Stats.fs
@@ -39,22 +39,6 @@ let dbStats (c : Canvas.T) (tlids : tlid list) : Task<DBStats> =
   |> Task.flatten
   |> Task.map Map.ofList
 
-// CLEANUP maybe keep one of them workerV2Stats or workerStats ?
-// CLEANUP table events doesn't exist anymore
-let workerV1Stats (canvasID : CanvasID) (tlid : tlid) : Task<int> =
-  Sql.query
-    "SELECT COUNT(1) AS num
-     FROM events E
-     INNER JOIN toplevel_oplists_v0 TL
-        ON TL.canvas_id = E.canvas_id
-       AND TL.module = E.space
-       AND TL.name = E.name
-     WHERE TL.tlid = @tlid
-       AND TL.canvas_id = @canvasID
-       AND E.status IN ('new', 'scheduled')"
-  |> Sql.parameters [ "tlid", Sql.tlid tlid; "canvasID", Sql.uuid canvasID ]
-  |> Sql.executeRowAsync (fun read -> read.int "num")
-
 let workerV2Stats (canvasID : CanvasID) (tlid : tlid) : Task<int> =
   Sql.query
     "SELECT COUNT(1) AS num
@@ -70,7 +54,6 @@ let workerV2Stats (canvasID : CanvasID) (tlid : tlid) : Task<int> =
 
 let workerStats (canvasID : CanvasID) (tlid : tlid) : Task<int> =
   task {
-    let! v1Stats = workerV1Stats canvasID tlid
     let! v2Stats = workerV2Stats canvasID tlid
-    return v1Stats + v2Stats
+    return v2Stats
   }
