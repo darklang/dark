c0e454427f3b1f8ccb698bbc59d8a500967b36eb
Paul Biggar <paul.biggar@gmail.com>
2023-03-23 23:38:29 +0000

    Remove dead code

--- backend/src/LibBackend/Account.fs
+++ backend/src/LibBackend/Account.fs
@@ -18,22 +18,11 @@ module RT = LibExecution.RuntimeTypes
 
 type Account = { username : UserName.T }
 
-
 type UserInfo = { username : UserName.T; id : UserID }
 
 // **********************
 // Adding
 // **********************
-
-let validateEmail (email : string) : Result<unit, string> =
-  // just checking it's roughly the shape of an email
-  let reString = "^.+@.+\\..+$"
-
-  if FsRegEx.isMatch reString email then
-    Ok()
-  else
-    Error($"Invalid email '{email}'")
-
 
 let validateAccount (account : Account) : Result<unit, string> =
   UserName.newUserAllowed (string account.username)
@@ -60,22 +49,12 @@ let upsertAccount (admin : bool) (account : Account) : Task<Result<unit, string>
 
 let upsertNonAdmin = upsertAccount false
 
-// Any external calls to this should also call Analytics.identifyUser;
-// we can't do it here because that sets up a module dependency cycle
-// CLEANUP remove the separate user creation functions
 let insertUser (username : UserName.T) : Task<Result<unit, string>> =
   task {
-    let account =
-      // As of the move to auth0, we  no longer store passwords in postgres. We do
-      // still use postgres locally, which is why we're not removing the field
-      // entirely. Local account creation is done in
-      // upsertAccount/upsertAdmin, so using Password.invalid here does
-      // not affect that
-      { username = username }
+    let account = { username = username }
 
     match validateAccount account with
     | Ok () ->
-
       try
         // insert
         do!
@@ -88,20 +67,7 @@ let insertUser (username : UserName.T) : Task<Result<unit, string>> =
                               "username", Sql.string (string username) ]
           |> Sql.executeStatementAsync
 
-        // verify insert worked
-        let! accountExists =
-          // CLEANUP: if this was added with a different email/name/etc this won't pick it up
-          Sql.query
-            "SELECT TRUE FROM accounts_v0
-              WHERE username = @username"
-          |> Sql.parameters [ "username", Sql.string (string username) ]
-          |> Sql.executeExistsAsync
-
-        if accountExists then
-          return Ok()
-        else
-          return
-            Error "Insert failed, probably because the username is already taken."
+        return Ok()
       with
       | e ->
         // CLEANUP this is a bad error message
@@ -109,9 +75,6 @@ let insertUser (username : UserName.T) : Task<Result<unit, string>> =
     // return Error "Insert failed, probably because the username is already taken."
     | Error e -> return Error e
   }
-
-
-
 
 
 
