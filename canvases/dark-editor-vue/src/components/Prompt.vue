aa06d4bf64b186d5be2efd01d1644ff8559c854b
OceanOak <oakocean9@gmail.com>
2023-04-18 17:40:09 +0000

    Add some components

--- canvases/dark-editor-vue/src/components/Prompt.vue
+++ canvases/dark-editor-vue/src/components/Prompt.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <input v-model="prompt" placeholder="Enter your prompt" />
-    <button @click="submitPrompt">Submit</button>
-  </div>
-</template>
+     <form class="flex flex-row stretch my-4 mx-auto max-w-2xl">
+      <div class="relative flex h-full flex-1">
+        <div class="w-full relative flex flex-col flex-grow py-3 pl-4 border border-white/10 rounded-md shadow-black/10">
+          <textarea aria-multiline="true" rows="1" placeholder="What are you building today?" class="w-full h-6 max-h-48 outline-none m-0 resize-none overflow-y-auto border-0 bg-transparent text-white py-0 pl-2 pr-7 "></textarea>
+          <button class="absolute bottom-2 right-2 py-1 px-2  mx-2 rounded-md text-white hover:bg-white/5">send</button>
+        </div>
+      </div>
+    </form>
 
-<script>
+</template>
+<script lang="ts">
 export default {
   data() {
     return {
@@ -20,3 +24,5 @@ export default {
   },
 };
 </script>
+
+
---
+++ canvases/dark-editor-vue/src/views/ChatView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <Header />
+    <Accordion class="mx-auto">
+      <template v-slot:accordion-header>
+        <span class="font-semibold text-xl">System prompt</span>
+      </template>
+      <template v-slot:accordion-body>
+        <textarea class="bg-transparent outline-0 resize-none w-full">
+          TODO: Fetch dark prompt here
+        </textarea>
+      </template>
+    </Accordion>
+    <div id="chat_container">
+    </div>
+    <div class="absolute bottom-0 left-0 w-full pt-2">
+    <Prompt />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+  import Header from "../components/Header.vue";
+  import Accordion from "../components/Accordion.vue";
+  import Prompt from "../components/Prompt.vue";
+
+</script>
+
