058e766618757234552c0c0bacfa3d158194b4f2
Paul Biggar <paul.biggar@gmail.com>
2023-04-19 17:30:19 +0000

    Extract out some reusable code

--- backend/src/LibExecution/RuntimeTypes.fs
+++ backend/src/LibExecution/RuntimeTypes.fs
@@ -1063,34 +1063,3 @@ let packageFnToFn (fn : Package.Fn) : Fn =
     previewable = Impure
     sqlSpec = NotQueryable
     fn = PackageFunction fn.body }
-
-// -------------------------
-// renamed fns
-// -------------------------
-
-// To cut down on the amount of code, when we rename a function and make no other
-// changes, we don't duplicate it. Instead, we rename it and add the rename to this
-// list. At startup, the renamed functions are created and added to the list.
-//
-// Renames is old name first, new name second. The new one should still be in the
-// codebase, the old one should not. If a function is renamed multiple times, add the
-// latest rename first.
-let renameFunctions
-  (renames : List<FQFnName.StdlibFnName * FQFnName.StdlibFnName>)
-  (existing : List<BuiltInFn>)
-  : List<BuiltInFn> =
-  let existingMap = existing |> List.map (fun fn -> fn.name, fn) |> Map
-  let newFns =
-    renames
-    |> List.fold Map.empty (fun renamedFns (oldName, newName) ->
-      let newFn =
-        Map.tryFind newName (Map.mergeFavoringLeft renamedFns existingMap)
-        |> Exception.unwrapOptionInternal
-             $"all fns should exist {oldName} -> {newName}"
-             [ "oldName", oldName; "newName", newName ]
-      Map.add
-        oldName
-        { newFn with name = oldName; deprecated = RenamedTo newName }
-        renamedFns)
-    |> Map.values
-  existing @ newFns
---
+++ backend/src/LibExecution/StdLib.fs
@@ -0,0 +1,48 @@
+/// Helper functions for stdlibs
+module LibExecution.StdLib
+
+open Prelude
+open VendoredTablecloth
+open RuntimeTypes
+
+
+// To cut down on the amount of code, when we rename a function and make no other
+// changes, we don't duplicate it. Instead, we rename it and add the rename to this
+// list. At startup, the renamed functions are created and added to the list.
+//
+// Renames is old name first, new name second. The new one should still be in the
+// codebase, the old one should not. If a function is renamed multiple times, add the
+// latest rename first.
+let renameFunctions
+  (renames : List<FQFnName.StdlibFnName * FQFnName.StdlibFnName>)
+  (existing : List<BuiltInFn>)
+  : List<BuiltInFn> =
+  let existingMap = existing |> List.map (fun fn -> fn.name, fn) |> Map
+  let newFns =
+    renames
+    |> List.fold Map.empty (fun renamedFns (oldName, newName) ->
+      let newFn =
+        Map.tryFind newName (Map.mergeFavoringLeft renamedFns existingMap)
+        |> Exception.unwrapOptionInternal
+             $"all fns should exist {oldName} -> {newName}"
+             [ "oldName", oldName; "newName", newName ]
+      Map.add
+        oldName
+        { newFn with name = oldName; deprecated = RenamedTo newName }
+        renamedFns)
+    |> Map.values
+  existing @ newFns
+
+module Shortcuts =
+  let fn = FQFnName.stdlibFnName
+  let typ = FQTypeName.stdlibTypeName
+
+  let incorrectArgs = Errors.incorrectArgs
+
+
+  let stdlibTypeRef
+    (module' : string)
+    (name : string)
+    (version : int)
+    : TypeReference =
+    TCustomType(FQTypeName.Stdlib(typ module' name version), [])
