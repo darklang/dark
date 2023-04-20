2e2e419bfbf407e0691435dcc7b9f4533498cf18
OceanOak <oakocean9@gmail.com>
2023-04-20 23:34:55 +0000

    Update UserChat component

--- canvases/dark-editor-vue/src/components/UserChat.vue
+++ canvases/dark-editor-vue/src/components/UserChat.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <div v-for="(message, index) in messages" :key="index">
-      <p>Your messages: {{ message }}</p>
+  <div class="my-4 mx-auto max-w-2xl">
+    <div class="flex items-start">
+      <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <span class="font-medium text-gray-600 dark:text-gray-300">User</span>
+  </div>
+      <div class="ml-3">
+        <p class="text-white">{{ promptValue }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   props: {
-    messages: {
-      type: Array,
-      default: () => [],
-    },
+    promptValue: {
+    type: String,
+    required: true,
+  },
   },
 };
 </script>
+
+
