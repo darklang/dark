46a9286c56e21f15179fabab2c1dc962219ede96
OceanOak <oakocean9@gmail.com>
2023-04-20 23:34:14 +0000

    Update ResponseChat component

--- canvases/dark-editor-vue/src/components/ResponseChat.vue
+++ canvases/dark-editor-vue/src/components/ResponseChat.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <div v-if="response">
-      <p class="font-bold ">Response: {{ response }}</p>
+  <div class="my-4 mx-auto max-w-2xl">
+    <div class="flex items-start">
+      <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <span class="font-medium text-gray-600 dark:text-gray-300">AI</span>
+</div>
+      <div class="ml-3">
+        <p class="text-white">{{ response }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   props: {
     response: {
-      type: String,
-      default: "",
-    },
+    type: String,
+    required: true,
+  },
   },
 };
 </script>
+
+
