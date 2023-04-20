bc89052edcc020d7b57e0558074bbf68b62aa736
OceanOak <oakocean9@gmail.com>
2023-04-20 15:33:02 +0000

    Get dark-editor canvas to return the vue app

--- canvases/dark-editor-vue/vite.config.ts
+++ canvases/dark-editor-vue/vite.config.ts
@@ -10,5 +10,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  //TODO: remove this to generate files with hashes
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
+
 })
