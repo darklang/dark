ebf9530126928ebc1ce7df9c1e77aa53b8cbba84
OceanOak <oakocean9@gmail.com>
2023-04-22 14:34:27 +0000

    style inputs

--- canvases/dark-editor-vue/src/components/ResponseChat.vue
+++ canvases/dark-editor-vue/src/components/ResponseChat.vue
@@ -2,19 +2,18 @@
   <div class="my-4 mx-auto max-w-2xl">
     <div class="flex items-start">
       <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-        <span class="font-medium text-gray-600 dark:text-gray-300"></span>
+        <span class="font-medium text-gray-600 dark:text-gray-300">AI</span>
       </div>
-      <div class="ml-3">
+      <div class="ml-3 w-fit">
         <p class="text-white">{{ response }}</p>
       </div>
     </div>
-
-    <form @submit.prevent="submitForm">
+    <form @submit.prevent="submitForm" class="flex mt-2 justify-center">
       <div v-if="variables" v-for="(variable, index) in variables" :key="index">
-        <label :for="'input-' + index">{{ variable }}</label>
-        <input :id="'input-' + index" type="text" v-model="variableValues[index]" />
+        <label :for="'input-' + index" class="text-white ml-2">{{ variable }}</label>
+        <input :id="'input-' + index" type="text" v-model="variableValues[index]" class="text-white bg-transparent my-2 ml-4 border-b border-white outline-none" />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" class="bg-[#C56AE4] text-white m-2 px-2 py-1 rounded">Submit</button>
     </form>
   </div>
 </template>
--- canvases/dark-editor-vue/src/components/UserChat.vue
+++ canvases/dark-editor-vue/src/components/UserChat.vue
@@ -4,7 +4,7 @@
       <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
     <span class="font-medium text-gray-600 dark:text-gray-300">User</span>
   </div>
-      <div class="ml-3">
+      <div class="ml-3 w-fit">
         <p class="text-white">{{ promptValue }}</p>
       </div>
     </div>
