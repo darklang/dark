941c4f7c981d10c837ac652b1abc624621342084
OceanOak <oakocean9@gmail.com>
2023-04-23 17:48:44 +0000

    Update the CSS to prevent the textarea from hiding the response div

--- canvases/dark-editor-vue/src/components/Prompt.vue
+++ canvases/dark-editor-vue/src/components/Prompt.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="h-[542px] overflow-y-scroll" v-if="prompts.length">
+  <div class="h-[542px] overflow-y-scroll pb-16" v-if="prompts.length">
     <div v-for="(prompt, index) in prompts" :key="index">
       <UserChat :promptValue="prompt" />
       <ResponseChat :response="responses[index]" v-if="responses[index]" />
     </div>
   </div>
 
-  <div class="absolute bottom-0 left-0 w-full pt-2">
+  <div class="absolute bottom-0 left-0 w-full pt-2 bg-[#1B1B1B]">
   <form @submit.prevent="submitPrompt" class="flex flex-row stretch my-4 mx-auto max-w-2xl">
     <div class="relative flex h-full flex-1">
       <div class="w-full relative flex flex-col flex-grow py-3 pl-4 border border-white/10 rounded-md shadow-black/10">
-        <textarea ref="content" autosize v-model="prompt" aria-multiline="true" rows="1" placeholder="What are you building today?" class="w-full h-6 max-h-48 outline-none m-0 resize-none overflow-y-auto border-0 bg-transparent text-white py-0 pl-2 pr-7 "></textarea>
+        <textarea ref="content" autosize v-model="prompt" aria-multiline="true" rows="1" placeholder="What are you building today?" class="w-full h-6 max-h-44 outline-none m-0 resize-none overflow-y-auto border-0 bg-transparent text-white py-0 pl-2 pr-11 "></textarea>
         <button type="submit" class="absolute bottom-2 right-2 py-1 px-2  mx-2 rounded-md text-white hover:bg-white/5">send</button>
       </div>
     </div>
