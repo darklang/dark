8f570ca1a705e5c3bd20425ab05547e351e96c04
OceanOak <oakocean9@gmail.com>
2023-04-23 14:00:09 +0000

    Add autosize textarea component

---
+++ canvases/dark-editor-vue/src/components/AutoSizeTextarea.vue
@@ -0,0 +1,14 @@
+<template>
+  <textarea
+    ref="content"
+    class="text-white bg-transparent w-full outline-none"
+  >{{ $props.response }}</textarea>
+</template>
+
+<script setup>
+import { ref, onMounted} from "vue";
+import autosize from "autosize";
+
+const content = ref();
+onMounted(() => autosize(content.value));
+</script>i
