393149a1f2a59b9237f2628b81b711a2fae50271
OceanOak <oakocean9@gmail.com>
2023-04-23 21:39:37 +0000

    Minor changes

--- canvases/dark-editor-vue/src/components/Prompt.vue
+++ canvases/dark-editor-vue/src/components/Prompt.vue
@@ -6,12 +6,12 @@
     </div>
   </div>
 
-  <div class="absolute bottom-0 left-0 w-full pt-2 bg-[#1B1B1B]">
+  <div class="absolute bottom-0 left-0 w-full pt-2 bg-[#151515]">
   <form @submit.prevent="submitPrompt" class="flex flex-row stretch my-4 mx-auto max-w-2xl">
     <div class="relative flex h-full flex-1">
       <div class="w-full relative flex flex-col flex-grow py-3 pl-4 border border-white/10 rounded-md shadow-black/10">
         <textarea ref="content" autosize v-model="prompt" aria-multiline="true" rows="1" placeholder="What are you building today?" class="w-full h-6 max-h-44 outline-none m-0 resize-none overflow-y-auto border-0 bg-transparent text-white py-0 pl-2 pr-11 "></textarea>
-        <button type="submit" class="absolute bottom-2 right-2 py-1 px-2  mx-2 rounded-md text-white hover:bg-white/5">send</button>
+        <button type="submit" class="absolute bottom-2 right-2 py-1 px-2  mx-2 rounded-md text-white bg-[#C56AE4] hover:bg-[#9f56b8]" >send</button>
       </div>
     </div>
   </form>
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeMount, onBeforeUnmount } from 'vue';
+import { ref, onMounted} from 'vue';
 import UserChat from './UserChat.vue';
 import ResponseChat from './ResponseChat.vue';
 import autosize from "autosize";
