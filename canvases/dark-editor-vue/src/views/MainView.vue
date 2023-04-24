13fb62eb1a40d2854d873432dc49127fb601d31f
OceanOak <oakocean9@gmail.com>
2023-04-24 13:20:09 +0000

    Move Blazor script to mainView

--- canvases/dark-editor-vue/src/views/MainView.vue
+++ canvases/dark-editor-vue/src/views/MainView.vue
@@ -28,6 +28,23 @@
   import { onMounted } from '@vue/runtime-core';
   const systemPromptValue= ref("");
   onMounted(() => {
+const blazorScript: HTMLScriptElement = document.createElement("script");
+blazorScript.setAttribute(
+  "src",
+  "http://dark-serve-blazor-assets.dlio.localhost:11003/blazor.webassembly.js",
+);
+blazorScript.setAttribute("autostart", "false");
+blazorScript.addEventListener('load', async () => {
+  await Blazor.start({
+    loadBootResource: function (type: string, name: string, defaultUri: string, integrity?: string) {
+      return `http://dark-serve-blazor-assets.dlio.localhost:11003/${name}`;
+    }
+  }).then(() => {
+    DotNet.invokeMethod("Wasm", "InitializeDarkRuntime");
+  });
+});
+document.head.appendChild(blazorScript);
+
   fetch('/get-prompt')
   .then(response => response.text())
   .then(data => systemPromptValue.value = data);
