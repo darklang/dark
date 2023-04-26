ee8436debbfac2465c28139a93d4bca337721372
OakOcean <oakocean9@gmail.com>
2023-04-26 13:00:56 +0000

    Format vue code

--- canvases/dark-editor-vue/src/components/Accordion.vue
+++ canvases/dark-editor-vue/src/components/Accordion.vue
@@ -1,7 +1,11 @@
-
 <template>
-  <div class="text-white rounded-md flex flex-col stretch my-4 mx-auto max-w-2xl border border-[#C56AE4] shadow-lg shadow-black/10">
-    <div @click="toggleAccordion()" class=" isOpen p-4 cursor-pointer flex items-center ">
+  <div
+    class="text-white rounded-md flex flex-col stretch my-4 mx-auto max-w-2xl border border-[#C56AE4] shadow-lg shadow-black/10"
+  >
+    <div
+      @click="toggleAccordion()"
+      class="isOpen p-4 cursor-pointer flex items-center"
+    >
       <slot name="accordion-header"></slot>
     </div>
     <div v-show="isOpen">
@@ -15,12 +19,12 @@ export default {
   data() {
     return {
       isOpen: false,
-    };
+    }
   },
   methods: {
     toggleAccordion() {
-      this.isOpen = !this.isOpen;
+      this.isOpen = !this.isOpen
     },
   },
-};
+}
 </script>
--- canvases/dark-editor-vue/src/components/UserChat.vue
+++ canvases/dark-editor-vue/src/components/UserChat.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="my-4 mx-auto max-w-2xl">
     <div class="flex items-start">
-      <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-[#845ec2] rounded-full">
-    <span class="font-medium text-white">User</span>
-  </div>
+      <div
+        class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-[#845ec2] rounded-full"
+      >
+        <span class="font-medium text-white">User</span>
+      </div>
       <div class="ml-3 w-fit">
         <p class="text-white">{{ promptValue }}</p>
       </div>
@@ -15,11 +17,9 @@
 export default {
   props: {
     promptValue: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
+    },
   },
-  },
-};
+}
 </script>
-
-
