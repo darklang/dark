e9a1b664032db2c8c817e898583955693afe0086
Paul Biggar <paul.biggar@gmail.com>
2023-04-27 00:49:58 +0000

    Print typenames better

--- backend/src/LibExecution/DvalReprDeveloper.fs
+++ backend/src/LibExecution/DvalReprDeveloper.fs
@@ -40,7 +40,7 @@ let rec typeName (t : TypeReference) : string =
     FQTypeName.toString t + typeArgsPortion
   | TBytes -> "Bytes"
 
-let dvalTypeName (dv : Dval) : string =
+let rec dvalTypeName (dv : Dval) : string =
   match dv with
   | DIncomplete _ -> "Incomplete"
   | DError _ -> "Error"
@@ -50,7 +50,8 @@ let dvalTypeName (dv : Dval) : string =
   | DUnit -> "Unit"
   | DChar _ -> "Char"
   | DString _ -> "String"
-  | DList _ -> "List"
+  | DList [] -> "List<'a>"
+  | DList (v :: _) -> "List<" + dvalTypeName v + ">"
   | DDict _ -> "Dict"
   | DFnVal (Lambda _) -> "Lambda"
   | DHttpResponse _ -> "HttpResponse"
@@ -58,9 +59,12 @@ let dvalTypeName (dv : Dval) : string =
   | DDateTime _ -> "DateTime"
   | DPassword _ -> "Password"
   | DUuid _ -> "Uuid"
-  | DOption _ -> "Option"
-  | DResult _ -> "Result"
-  | DTuple _ -> "Tuple"
+  | DOption (Some v) -> "Option<" + dvalTypeName v + ">"
+  | DOption None -> "Option<'a>"
+  | DResult (Ok v) -> "Result<" + dvalTypeName v + ", 'err>"
+  | DResult (Error v) -> "Result<'ok, " + dvalTypeName v + ">"
+  | DTuple (t1, t2, trest) ->
+    "(" + (t1 :: t2 :: trest |> List.map dvalTypeName |> String.concat ", ") + ")"
   | DBytes _ -> "Bytes"
   // TYPESCLEANUP - use the name
   | DRecord _ -> "Record"
