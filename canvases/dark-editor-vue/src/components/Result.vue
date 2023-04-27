fb1bc213f579385b6840ecef7dad51628f9a036e
OakOcean <oakocean9@gmail.com>
2023-04-27 13:08:40 +0000

    Create a Result component

---
+++ canvases/dark-editor-vue/src/components/Result.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-if="message" class="text-white">{{ message }}</div>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+})
+</script>
