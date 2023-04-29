006f84dc224f1994cc2d12b4113547f64faf8c56
OakOcean <oakocean9@gmail.com>
2023-04-29 22:44:18 +0000

    Temporary styling for result component

--- canvases/dark-editor-vue/src/components/Result.vue
+++ canvases/dark-editor-vue/src/components/Result.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <div v-if="message" class="text-white">{{ message }}</div>
+  <div
+    v-if="message"
+    class="flex flex-col bg-[#2e2e2e] rounded mx-auto max-w-2xl text-white"
+  >
+    <div class="border-b p-3 border-[#404040]">
+      <p class="text-[#C56AE4]">Result</p>
+    </div>
+    <div class="p-3">{{ message }}</div>
   </div>
 </template>
 <script setup lang="ts">
