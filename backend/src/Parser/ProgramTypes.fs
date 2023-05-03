aad0a2dd3f87aa83ff8bbf6e0d83f6379e5d804d
ditansu <ditansu@gmail.com>
2023-05-03 00:00:11 +0000

    Fix formating

--- backend/src/Parser/ProgramTypes.fs
+++ backend/src/Parser/ProgramTypes.fs
@@ -390,14 +390,12 @@ module Expr =
       PT.EEnum(id, typeName, name.idText, convertEnumArg arg)
 
     // Enum values (EEnums)
-    | SynExpr.Ident name when
-      List.contains name.idText [ "Nothing"; "Just" ] ->
+    | SynExpr.Ident name when List.contains name.idText [ "Nothing"; "Just" ] ->
       let typeName =
         PT.FQTypeName.Stdlib({ modules = []; typ = "Option"; version = 0 })
       PT.EEnum(id, typeName, name.idText, [])
 
-    | SynExpr.Ident name when
-      List.contains name.idText [ "Ok"; "Error" ] ->
+    | SynExpr.Ident name when List.contains name.idText [ "Ok"; "Error" ] ->
       let typeName =
         PT.FQTypeName.Stdlib({ modules = []; typ = "Result"; version = 0 })
       PT.EEnum(id, typeName, name.idText, [])
