f3bf1a6a3ab0e752f8bef89b8d2cde733193e01c
OceanOak <oakocean9@gmail.com>
2023-05-07 17:02:36 +0000

    Add an arrow icon to indicate that system prompt is clickable

--- canvases/dark-editor-vue/src/views/MainView.vue
+++ canvases/dark-editor-vue/src/views/MainView.vue
@@ -4,10 +4,16 @@
     <Accordion class="mx-auto">
       <template v-slot:accordion-header>
         <span class="font-semibold text-xl">System prompt</span>
+        <svg class="w-6 h-6 ml-2 transform transition-transform" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M12 16.5l6-6-1.5-1.5-4.5 4.5-4.5-4.5-1.5 1.5z"
+          />
+        </svg>
       </template>
       <template v-slot:accordion-body>
         <textarea
-          class="bg-transparent outline-0 p-4 w-full"
+          class="bg-transparent outline-0 p-4 w-full resize-none h-96 max-h-96 overflow-y-auto border-0 text-white"
           ref="prompt"
           v-model="systemPromptValue"
         ></textarea>
@@ -32,15 +38,15 @@ const systemPromptValue = ref('')
 
 const darklangJSScript: HTMLScriptElement = document.createElement('script')
 darklangJSScript.setAttribute(
-  "src",
-  "http://dark-serve-static.dlio.localhost:11003/darklang-wasm.js",
-);
-darklangJSScript.setAttribute("defer", "");
-darklangJSScript.addEventListener("load", async () => {
-  const darklang = await window.Darklang.init();
-  window.darklang = darklang;
-});
-document.head.appendChild(darklangJSScript);
+  'src',
+  'http://dark-serve-static.dlio.localhost:11003/darklang-wasm.js'
+)
+darklangJSScript.setAttribute('defer', '')
+darklangJSScript.addEventListener('load', async () => {
+  const darklang = await window.Darklang.init()
+  window.darklang = darklang
+})
+document.head.appendChild(darklangJSScript)
 
 onMounted(() => {
   fetch('/get-prompt')
