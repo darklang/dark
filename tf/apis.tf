a28d2b21956e4ed707b17c7a40f2aca00547f679
Paul Biggar <paul.biggar@gmail.com>
2023-06-01 14:39:32 +0000

    Be more explicit

--- tf/apis.tf
+++ tf/apis.tf
@@ -1,6 +1,6 @@
 # Cloud Run
 resource "google_project_service" "cloud_run_api" {
-  service = "run.googleapis.com"
-
+  provider           = google
+  service            = "run.googleapis.com"
   disable_on_destroy = false
 }
