2d9555059ebec34d53852b8cc1e01a312c127ea0
Paul Biggar <paul.biggar@gmail.com>
2023-06-04 00:07:49 +0000

    Add IAM for queues

--- tf/iam.tf
+++ tf/iam.tf
@@ -31,33 +31,27 @@ resource "google_service_account" "queue_pubsub_access_ai" {
   project      = local.project_name
 }
 
-resource "google_project_iam_member" "queue_pubsub_access_ai_member_pubsub_publisher" {
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.queue_pubsub_access_ai.email}"
-  project = local.project_id
-
-  condition {
-    title       = "Limit queue"
-    description = "Only allow access to ai deployment queue"
-    expression  = "resource.name == \"${google_pubsub_topic.topic_queue_ai.id}\""
-  }
-}
-
-resource "google_project_iam_member" "queue_pubsub_access_ai_member_pubsub_subscriber" {
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${google_service_account.queue_pubsub_access_ai.email}"
-  project = local.project_id
-
-  condition {
-    title       = "Limit queue"
-    description = "Only allow access to ai deployment queue"
-    expression  = "resource.name == \"${google_pubsub_subscription.topic_queue_ai_sub.id}\""
-  }
+##########
+# PubSub
+# PubSub have service-specific roles, instead of project-wide roles
+##########
+resource "google_pubsub_topic_iam_member" "queue_pubsub_access_ai_member_pubsub_publisher" {
+  topic  = google_pubsub_topic.topic_queue_ai.id
+  member = "serviceAccount:${google_service_account.queue_pubsub_access_ai.email}"
+  role   = "roles/pubsub.publisher"
 }
 
 
+resource "google_pubsub_subscription_iam_member" "queue_pubsub_access_ai_member_pubsub_subscriber" {
+  subscription = google_pubsub_subscription.topic_queue_ai_sub.id
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${google_service_account.queue_pubsub_access_ai.email}"
+}
 
 
+##########
+# Cloud Storage
+##########
 resource "google_service_account" "traces_storage_ai" {
   account_id   = "traces-storage-ai"
   description  = "Access to traces storage buckets for the AI deployment"
@@ -72,7 +66,8 @@ resource "google_project_iam_member" "traces_storage_ai_member_object_creator" {
   condition {
     title       = "Limit to bucket"
     description = "Only allow access to ai traces bucket"
-    expression  = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.dark_traces_ai.id}\")"
+    # This expression was hard to find, you can't use '=='
+    expression = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.dark_traces_ai.id}\")"
   }
 }
 
@@ -84,11 +79,10 @@ resource "google_project_iam_member" "traces_storage_ai_member_object_viewer" {
   condition {
     title       = "Limit to bucket"
     description = "Only allow access to ai traces bucket"
-    expression  = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.dark_traces_ai.id}\")"
+    # This expression was hard to find, you can't use '=='
+    expression = "resource.name.startsWith(\"projects/_/buckets/${google_storage_bucket.dark_traces_ai.id}\")"
   }
 }
-
-
 
 
 
