832eb80b5b2cfb0475aeda742d780e3cf95aa734
OceanOak <oakocean9@gmail.com>
2023-06-06 22:38:45 +0000

    Move tasksAndActions and codeAndContext from views to components

--- canvases/dark-editor-vue/src/views/ConversationView.vue
+++ canvases/dark-editor-vue/src/views/ConversationView.vue
@@ -2,12 +2,28 @@
 import type { Model } from '../types'
 
 import Conversation from '../components/Conversation.vue'
+import TasksAndActions from '../components/TasksAndActions.vue'
+import CodeAndContext from '../components/CodeAndContext.vue'
 
 let props = defineProps<{ state: Model }>()
 </script>
 
 <template>
   <main>
-    <Conversation :state="props.state" />
+    <div class="flex h-screen overflow-hidden">
+      <div class="w-1/5 overflow-auto pb-24">
+        <TasksAndActions
+          v-bind:state="state"
+          :tasks="state.tasks"
+          :actions="state.actions"
+        />
+      </div>
+      <div class="w-2/5 overflow-auto pb-24">
+        <Conversation :state="props.state" />
+      </div>
+      <div class="w-2/5 overflow-auto pb-32">
+        <CodeAndContext v-bind:state="state" :codeSnippets="state.codeSnippets" />
+      </div>
+    </div>
   </main>
 </template>
