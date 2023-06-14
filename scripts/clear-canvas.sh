62f7128b11d08f0bc6e9c9c59c6c89096454dacb
Stachu Korick <hello@stachu.net>
2023-06-14 11:46:37 -0400

    Remove references to non-existant tables

--- scripts/clear-canvas.sh
+++ scripts/clear-canvas.sh
@@ -16,7 +16,6 @@ SCRIPT=""
 for cid in $CANVASES; do
   SCRIPT+="DELETE FROM scheduling_rules_v0 WHERE canvas_id = '$cid';";
   SCRIPT+="DELETE FROM traces_v0 WHERE canvas_id = '$cid';";
-  SCRIPT+="DELETE FROM trace_old_events_v0 WHERE canvas_id = '$cid';";
   SCRIPT+="DELETE FROM user_data_v0 WHERE canvas_id = '$cid';";
   SCRIPT+="DELETE FROM cron_records_v0 WHERE canvas_id = '$cid';";
   SCRIPT+="DELETE FROM toplevel_oplists_v0 WHERE canvas_id = '$cid';";
