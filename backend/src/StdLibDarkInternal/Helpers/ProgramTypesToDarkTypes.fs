5ccbe14f87965d074089e5f8e03241606cb7d442
OceanOak <oakocean9@gmail.com>
2023-06-20 21:26:32 +0000

    Minor fix

--- backend/src/StdLibDarkInternal/Helpers/ProgramTypesToDarkTypes.fs
+++ backend/src/StdLibDarkInternal/Helpers/ProgramTypesToDarkTypes.fs
@@ -517,7 +517,7 @@ module UserFunction =
 
   let toDT (userFn : PT.UserFunction.T) : Dval =
     DRecord(
-      ptTyp [] "UserFunction" 0,
+      ptTyp [ "UserFunction" ] "T" 0,
       Map
         [ "tlid", DInt(int64 userFn.tlid)
           "name", FnName.UserProgram.toDT userFn.name
