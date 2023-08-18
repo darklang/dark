d80f04f6c175bf3b2c59a2275cf4e8de74e6678e
Ocean <oakocean9@gmail.com>
2023-08-18 11:07:39 +0000

    Use package Bool.and and Bool.or

--- backend/src/StdLibExecution/Libs/Bool.fs
+++ backend/src/StdLibExecution/Libs/Bool.fs
@@ -27,35 +27,6 @@ let fns : List<BuiltInFn> =
         | _ -> incorrectArgs ())
       sqlSpec = SqlFunction "not"
       previewable = Pure
-      deprecated = NotDeprecated }
-
-
-    { name = fn "and" 0
-      typeParams = []
-      parameters = [ Param.make "a" TBool ""; Param.make "b" TBool "" ]
-      returnType = TBool
-      description = "Returns {{true}} if both <param a> and <param b> are {{true}}"
-      fn =
-        (function
-        | _, _, [ DBool a; DBool b ] -> Ply(DBool(a && b))
-        | _ -> incorrectArgs ())
-      sqlSpec = SqlBinOp "AND"
-      previewable = Pure
-      deprecated = NotDeprecated }
-
-
-    { name = fn "or" 0
-      typeParams = []
-      parameters = [ Param.make "a" TBool ""; Param.make "b" TBool "" ]
-      returnType = TBool
-      description =
-        "Returns {{true}} if either <param a> is true or <param b> is {{true}}"
-      fn =
-        (function
-        | _, _, [ DBool a; DBool b ] -> Ply(DBool(a || b))
-        | _ -> incorrectArgs ())
-      sqlSpec = SqlBinOp "OR"
-      previewable = Pure
       deprecated = NotDeprecated } ]
 
 let contents = (fns, types, constants)
