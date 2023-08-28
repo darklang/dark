e290c3626eec8567a7248b89f1f1c4a8740d0547
Paul Biggar <paul.biggar@gmail.com>
2023-08-28 21:40:32 +0000

    Move Map functions to Map.fs

--- backend/src/LocalExec/Libs/Cli.fs
+++ backend/src/LocalExec/Libs/Cli.fs
@@ -22,9 +22,9 @@ let builtIns : RT.BuiltIns =
         StdLibCliHost.StdLib.contents ]
       []
       []
-  { types = types |> Tablecloth.Map.fromListBy (fun typ -> typ.name)
-    fns = fns |> Tablecloth.Map.fromListBy (fun fn -> fn.name)
-    constants = constants |> Tablecloth.Map.fromListBy (fun c -> c.name) }
+  { types = types |> Map.fromListBy (fun typ -> typ.name)
+    fns = fns |> Map.fromListBy (fun fn -> fn.name)
+    constants = constants |> Map.fromListBy (fun c -> c.name) }
 
 let packageManager : RT.PackageManager = RT.PackageManager.Empty
 
@@ -42,15 +42,15 @@ let execute
         fns =
           mod'.fns
           |> List.map (fun fn -> PT2RT.UserFunction.toRT fn)
-          |> Tablecloth.Map.fromListBy (fun fn -> fn.name)
+          |> Map.fromListBy (fun fn -> fn.name)
         types =
           mod'.types
           |> List.map (fun typ -> PT2RT.UserType.toRT typ)
-          |> Tablecloth.Map.fromListBy (fun typ -> typ.name)
+          |> Map.fromListBy (fun typ -> typ.name)
         constants =
           mod'.constants
           |> List.map (fun c -> PT2RT.UserConstant.toRT c)
-          |> Tablecloth.Map.fromListBy (fun c -> c.name)
+          |> Map.fromListBy (fun c -> c.name)
 
         dbs = Map.empty
         secrets = [] }
--- backend/src/Prelude/Map.fs
+++ backend/src/Prelude/Map.fs
@@ -8,3 +8,6 @@ let mergeFavoringLeft (m1 : Map<'k, 'v>) (m2 : Map<'k, 'v>) : Map<'k, 'v> =
 
 let ofNEList (l : NEList.NEList<'k * 'v>) : Map<'k, 'v> =
   NEList.fold (fun m (k, v) -> Map.add k v m) Map.empty l
+
+let fromListBy (f : 'v -> 'k) (l : List<'v>) : Map<'k, 'v> =
+  List.fold (fun (m : Map<'k, 'v>) v -> m.Add(f v, v)) Map.empty l
