966bc3f1f355319a8076e538fbb303201c077476
Stachu Korick <hello@stachu.net>
2023-09-10 10:52:16 -0400

    no-op reorg

--- backend/src/LibExecution/DvalUtils.fs
+++ backend/src/LibExecution/DvalUtils.fs
@@ -2,10 +2,37 @@ module LibExecution.DvalUtils
 
 open LibExecution.RuntimeTypes
 
-// Creating Dvals
+// -- Creating Dvals --
+
+
+// type DvalCreator = draft of an idea...
+//   { list: List<Dval> -> Dval }
 
 
 
+// CLEANUP - this fn was unused so I commented it out
+// remove? or will it be handy?
+// let dict (fields : List<string * Dval>) : Dval =
+//   // Give a warning for duplicate keys
+//   List.fold
+//     (DDict(Map.empty))
+//     (fun m (k, v) ->
+//       match m, k, v with
+//       // TYPESCLEANUP: remove hacks
+//       // If we're propagating a fakeval keep doing it. We handle it without this line but let's be certain
+//       | m, _k, _v when isFake m -> m
+//       // Errors should propagate (but only if we're not already propagating an error)
+//       | DDict _, _, v when isFake v -> v
+//       // Skip empty rows
+//       | _, "", _ -> DError(SourceNone, $"Empty key: {k}")
+//       // Error if the key appears twice
+//       | DDict m, k, _v when Map.containsKey k m ->
+//         DError(SourceNone, $"Duplicate key: {k}")
+//       // Otherwise add it
+//       | DDict m, k, v -> DDict(Map.add k v m)
+//       // If we haven't got a DDict we're propagating an error so let it go
+//       | m, _, _ -> m)
+//     fields
 
 let record
   (typeName : TypeName.TypeName)
@@ -37,13 +64,13 @@ let record
 
 let enum
   (typeName : TypeName.TypeName)
+  // TODO: (typeArgs: List<ValueType>)
   (caseName : string)
   (fields : List<Dval>)
   : Dval =
   match List.find Dval.isFake fields with
   | Some v -> v
   | None -> DEnum(typeName, typeName, caseName, fields)
-
 
 
 let optionType = TypeName.fqPackage "Darklang" [ "Stdlib"; "Option" ] "Option" 0
@@ -59,30 +86,6 @@ let option (dv : Option<Dval>) : Dval =
   | Some dv -> optionSome dv // checks isFake
   | None -> optionNone
 
-// CLEANUP - this fn was unused so I commented it out
-// remove? or will it be handy?
-// let dict (fields : List<string * Dval>) : Dval =
-//   // Give a warning for duplicate keys
-//   List.fold
-//     (DDict(Map.empty))
-//     (fun m (k, v) ->
-//       match m, k, v with
-//       // TYPESCLEANUP: remove hacks
-//       // If we're propagating a fakeval keep doing it. We handle it without this line but let's be certain
-//       | m, _k, _v when isFake m -> m
-//       // Errors should propagate (but only if we're not already propagating an error)
-//       | DDict _, _, v when isFake v -> v
-//       // Skip empty rows
-//       | _, "", _ -> DError(SourceNone, $"Empty key: {k}")
-//       // Error if the key appears twice
-//       | DDict m, k, _v when Map.containsKey k m ->
-//         DError(SourceNone, $"Duplicate key: {k}")
-//       // Otherwise add it
-//       | DDict m, k, v -> DDict(Map.add k v m)
-//       // If we haven't got a DDict we're propagating an error so let it go
-//       | m, _, _ -> m)
-//     fields
-
 
 let resultType = TypeName.fqPackage "Darklang" [ "Stdlib"; "Result" ] "Result" 0
 
@@ -96,6 +99,8 @@ let result (dv : Result<Dval, Dval>) : Dval =
   match dv with
   | Ok dv -> resultOk dv
   | Error dv -> resultError dv
+
+
 
 // Extracting data from Dvals
 
