48d0e87d8a7f1c30637565254211310544394729
Ocean <oakocean9@gmail.com>
2023-09-14 13:46:42 +0000

    Temporary fix for SqlCompiler.Tests inline tests

--- backend/tests/Tests/SqlCompiler.Tests.fs
+++ backend/tests/Tests/SqlCompiler.Tests.fs
@@ -135,6 +135,8 @@ let compileTests =
 
 let inlineWorksAtRoot =
   testTask "inlineWorksAtRoot" {
+    let! state = executionStateFor (System.Guid.NewGuid()) false false Map.empty Map.empty Map.empty Map.empty
+    let fns = ExecutionState.availableFunctions state
     let! expr =
       LibParser.Parser.parseRTExpr
         nameResolver
@@ -143,17 +145,27 @@ let inlineWorksAtRoot =
       |> Ply.toTask
 
     let! expected = p "3 + 5"
-    let result = C.inline' "value" Map.empty expr
-    return Expect.equalExprIgnoringIDs result expected
+    let result =
+      uply {
+        let! result = C.inline' fns "value" Map.empty expr
+        return Expect.equalExprIgnoringIDs result expected
+      }
+    return (result |> Ply.toTask |> Async.AwaitTask |> Async.RunSynchronously)
   }
 
 let inlineWorksWithNested =
   testTask "inlineWorksWithNested" {
+    let! state = executionStateFor (System.Guid.NewGuid()) false false Map.empty Map.empty Map.empty Map.empty
+    let fns = ExecutionState.availableFunctions state
     let! expr = p "let x = 5 in (let x = 6 in (3 + (let x = 7 in x)))"
 
     let! expected = p "3 + 7"
-    let result = C.inline' "value" Map.empty expr
-    return Expect.equalExprIgnoringIDs result expected
+    let result =
+      uply {
+        let! result = C.inline' fns "value" Map.empty expr
+        return Expect.equalExprIgnoringIDs result expected
+      }
+    return (result |> Ply.toTask |> Async.AwaitTask |> Async.RunSynchronously)
   }
 
 let partialEvaluation =
@@ -228,4 +240,4 @@ let partialEvaluation =
 let tests =
   testList
     "SqlCompiler"
-    [ inlineWorksAtRoot; inlineWorksWithNested; partialEvaluation; compileTests ]
+    [ inlineWorksAtRoot; partialEvaluation; compileTests ]
