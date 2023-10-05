fac6429b5295ed0fd05402c358545e0a52710846
Paul Biggar <paul.biggar@gmail.com>
2023-10-05 22:16:10 +0000

    Get containers building again

---
+++ scripts/deployment/newdeploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+. ./scripts/devcontainer/_assert-in-container "$0" "$@"
+
+set -euo pipefail
+
+set -x
+
+# build all dockerfiles in containers/
+for i in $(find containers -name Dockerfile); do
+  echo "Building $i"
+  dir=$(dirname $i)
+  file=$(basename $i)
+  docker build . -f $i
+done
