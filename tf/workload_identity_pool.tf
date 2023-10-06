2f2e73cf46de396f152fd7516968ca596b55b8df
Paul Biggar <paul.biggar@gmail.com>
2023-10-06 23:05:00 +0000

    Enable OIDC connection

--- tf/workload_identity_pool.tf
+++ tf/workload_identity_pool.tf
@@ -10,39 +10,39 @@
 ###################
 
 
-# resource "google_iam_workload_identity_pool" "circleci" {
-#   workload_identity_pool_id = "circleci"
-#   display_name              = "CircleCI"
-#   description               = "Access for CircleCI deployments"
-#   disabled                  = false
-# }
+resource "google_iam_workload_identity_pool" "circleci" {
+  workload_identity_pool_id = "circleci"
+  display_name              = "CircleCI"
+  description               = "Access for CircleCI deployments"
+  disabled                  = false
+}
 
-# resource "google_iam_workload_identity_pool_provider" "circleci" {
-#   workload_identity_pool_id          = "circleci"
-#   workload_identity_pool_provider_id = "circleci"
-#   display_name                       = "CircleCI"
+resource "google_iam_workload_identity_pool_provider" "circleci" {
+  workload_identity_pool_id          = "circleci"
+  workload_identity_pool_provider_id = "circleci"
+  display_name                       = "CircleCI"
 
-#   disabled = false
+  disabled = false
 
-#   attribute_condition = <<-EOT
-#     attribute.project=="1f60315c-0228-42dd-9205-ed25beb24371"
-#     && attribute.vcs_origin=="github.com/darklang/dark"
-#     && attribute.vcs_ref=="refs/heads/main"
-#     EOT
+  attribute_condition = <<-EOT
+    attribute.project=="1f60315c-0228-42dd-9205-ed25beb24371"
+    && attribute.vcs_origin=="github.com/darklang/dark"
+    && attribute.vcs_ref=="refs/heads/main"
+    EOT
 
-#   timeouts {}
+  timeouts {}
 
-#   attribute_mapping = {
-#     "attribute.project"    = "assertion['oidc.circleci.com/project-id']"
-#     "attribute.vcs_origin" = "assertion['oidc.circleci.com/vcs-origin']"
-#     "attribute.vcs_ref"    = "assertion['oidc.circleci.com/vcs-ref']"
-#     "attribute.org_id"     = "assertion.aud"
-#     "google.subject"       = "assertion.sub"
-#   }
+  attribute_mapping = {
+    "attribute.project"    = "assertion['oidc.circleci.com/project-id']"
+    "attribute.vcs_origin" = "assertion['oidc.circleci.com/vcs-origin']"
+    "attribute.vcs_ref"    = "assertion['oidc.circleci.com/vcs-ref']"
+    "attribute.org_id"     = "assertion.aud"
+    "google.subject"       = "assertion.sub"
+  }
 
-#   oidc {
-#     // https://app.circleci.com/settings/organization/github/darklang/overview
-#     issuer_uri        = "https://oidc.circleci.com/org/a5d9bde7-7922-4277-90ff-dbec5d9c200e"
-#     allowed_audiences = ["a5d9bde7-7922-4277-90ff-dbec5d9c200e"]
-#   }
-# }
+  oidc {
+    // https://app.circleci.com/settings/organization/github/darklang/overview
+    issuer_uri        = "https://oidc.circleci.com/org/a5d9bde7-7922-4277-90ff-dbec5d9c200e"
+    allowed_audiences = ["a5d9bde7-7922-4277-90ff-dbec5d9c200e"]
+  }
+}
