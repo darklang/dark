44a317325f34c62348c46766e0be98a359b6abdc
Paul Biggar <paul.biggar@gmail.com>
2023-10-06 22:58:51 +0000

    Add pubsub

--- tf/pubsub.tf
+++ tf/pubsub.tf
@@ -1,34 +1,34 @@
 # PubSub Topics
 
-# resource "google_pubsub_topic" "topic_queue" {
-#   name    = "topic-queue"
-#   project = local.project_name
-# }
+resource "google_pubsub_topic" "topic_queue" {
+  name    = "topic-queue"
+  project = local.project_name
+}
 
-# resource "google_pubsub_topic" "topic_queue_deadletter" {
-#   name    = "topic-queue-deadletter"
-#   project = local.project_name
-# }
+resource "google_pubsub_topic" "topic_queue_deadletter" {
+  name    = "topic-queue-deadletter"
+  project = local.project_name
+}
 
 # PubSub Subscriptions
 
-# resource "google_pubsub_subscription" "topic_queue_sub" {
-#   ack_deadline_seconds = 60
-#   dead_letter_policy {
-#     dead_letter_topic     = google_pubsub_topic.topic_queue_deadletter.id
-#     max_delivery_attempts = 5
-#   }
-#   enable_exactly_once_delivery = true
-#   message_retention_duration   = "604800s"
-#   name                         = "topic-queue-sub"
-#   project                      = local.project_name
-#   topic                        = google_pubsub_topic.topic_queue.name
-# }
+resource "google_pubsub_subscription" "topic_queue_sub" {
+  ack_deadline_seconds = 60
+  dead_letter_policy {
+    dead_letter_topic     = google_pubsub_topic.topic_queue_deadletter.id
+    max_delivery_attempts = 5
+  }
+  enable_exactly_once_delivery = true
+  message_retention_duration   = "604800s"
+  name                         = "topic-queue-sub"
+  project                      = local.project_name
+  topic                        = google_pubsub_topic.topic_queue.name
+}
 
-# resource "google_pubsub_subscription" "topic_queue_deadletter_sub" {
-#   ack_deadline_seconds       = 10
-#   message_retention_duration = "604800s"
-#   name                       = "topic-queue-deadletter-sub"
-#   project                    = local.project_name
-#   topic                      = google_pubsub_topic.topic_queue_deadletter.name
-# }
+resource "google_pubsub_subscription" "topic_queue_deadletter_sub" {
+  ack_deadline_seconds       = 10
+  message_retention_duration = "604800s"
+  name                       = "topic-queue-deadletter-sub"
+  project                    = local.project_name
+  topic                      = google_pubsub_topic.topic_queue_deadletter.name
+}
