4cbeb9f2a429ac2cd248eacf981bbae1d0dcfb3e
Paul Biggar <paul.biggar@gmail.com>
2023-10-06 16:43:23 +0000

    Pass shellcheck

--- scripts/deployment/newdeploy.sh
+++ scripts/deployment/newdeploy.sh
@@ -8,7 +8,7 @@ set -x
 # build all dockerfiles in containers/
 for i in $(find containers -name Dockerfile); do
   echo "Building $i"
-  dir=$(dirname $i)
-  file=$(basename $i)
+  # dir=$(dirname $i)
+  # file=$(basename $i)
   docker build . -f $i
 done
