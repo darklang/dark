db17e872886fede60a0ef37268354bd0b62b3eda
Stachu Korick <hello@stachu.net>
2023-10-10 23:58:45 +0000

    Fix Process.run builtin (tested on linux with "ls .")

--- backend/src/BuiltinCli/Libs/Process.fs
+++ backend/src/BuiltinCli/Libs/Process.fs
@@ -10,6 +10,7 @@ open LibExecution.RuntimeTypes
 module Dval = LibExecution.Dval
 module Builtin = LibExecution.Builtin
 open Builtin.Shortcuts
+open System.Runtime.InteropServices
 
 let types : List<BuiltInType> = []
 
@@ -17,9 +18,7 @@ let fns : List<BuiltInFn> =
   [ { name = fn [ "Process" ] "run" 0
       description = "Runs a process, return exitCode, stdout and stderr"
       typeParams = []
-      parameters =
-        [ Param.make "command" TString "The command to run"
-          Param.make "input" TString "The input to the command" ]
+      parameters = [ Param.make "command" TString "The command to run" ]
       returnType =
         TCustomType(
           Ok(TypeName.fqPackage "Darklang" [ "Stdlib"; "Process" ] "Result" 0),
@@ -28,9 +27,23 @@ let fns : List<BuiltInFn> =
       fn =
         (function
         | _, _, [ DString command ] ->
+          let cmdName, cmdArgs =
+            if RuntimeInformation.IsOSPlatform OSPlatform.Windows then
+              "cmd.exe", $"/c {command}"
+            else if
+              RuntimeInformation.IsOSPlatform OSPlatform.Linux
+              || RuntimeInformation.IsOSPlatform OSPlatform.OSX
+            then
+              "/bin/bash", $"-c \"{command}\""
+            else
+              raiseString
+                "Process.run not supported for your operating system (Linux, Windows, or Mac not detected)"
+
           let psi =
             System.Diagnostics.ProcessStartInfo(command)
             |> fun psi ->
+              psi.FileName <- cmdName
+              psi.Arguments <- cmdArgs
               psi.UseShellExecute <- false
               psi.RedirectStandardOutput <- true
               psi.RedirectStandardError <- true
@@ -39,6 +52,8 @@ let fns : List<BuiltInFn> =
 
           let p = System.Diagnostics.Process.Start(psi)
 
+          // TODO: read+return bytes, not strings, and update the corresponding `Process.Result` type
+          // (need an alternative to `p.StandardOutput.ReadToEnd()` here)
           let stdout = p.StandardOutput.ReadToEnd()
           let stderr = p.StandardError.ReadToEnd()
 
