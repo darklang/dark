23905e6028bd75453a789bc7c9a71eb388be0bd8
Paul Biggar <paul.biggar@gmail.com>
2023-10-23 19:14:09 +0000

    Add PSL entry

--- tf/darklangio.tf
+++ tf/darklangio.tf
@@ -7,6 +7,7 @@ resource "google_compute_global_address" "darklangio_ip_address" {
 }
 
 # dns
+# darklang.com is in darklang-classic project
 resource "google_dns_managed_zone" "darklangio" {
   name        = "darklang-io"
   dns_name    = "darklang.io."
@@ -32,7 +33,7 @@ resource "google_dns_managed_zone" "darklangio" {
   }
 }
 
-resource "google_dns_record_set" "darklangio" {
+resource "google_dns_record_set" "star_darklangio_a" {
   name         = "*.darklang.io."
   type         = "A"
   ttl          = 300
@@ -40,11 +41,26 @@ resource "google_dns_record_set" "darklangio" {
   rrdatas      = [google_compute_global_address.darklangio_ip_address.address]
 }
 
-import {
-  id = "darklang-io/*.darklang.io./A"
-  to = google_dns_record_set.darklangio
+resource "google_dns_record_set" "psl_darklangio" {
+  name         = "_psl.darklang.io."
+  type         = "TXT"
+  ttl          = 5
+  managed_zone = google_dns_managed_zone.darklangio.name
+  rrdatas      = ["https://github.com/publicsuffix/list/pull/1880"]
 }
 
-# dnssec
-# darklang.com is in darklang-classic project
-# cert manager
+
+# resource "google_certificate_manager_dns_authorization" "star_darklangio" {
+#   name        = "star-darklang-io-dnsauth-ksu48h2"
+#   description = "The default dns auth"
+#   domain      = "*.darklang.io"
+# }
+# resource "google_dns_record_set" "star_darklangio_dns_authorization_cname" {
+#   name         = google_certificate_manager_dns_authorization.star_darklangio.dns_resource_record[0].name
+#   managed_zone = google_dns_managed_zone.darklangio.name
+#   type         = google_certificate_manager_dns_authorization.star_darklangio.dns_resource_record[0].type
+#   ttl          = 5
+#   rrdatas      = [google_certificate_manager_dns_authorization.star_darklangio.dns_resource_record[0].data]
+# }
+
+# TODO dnssec
