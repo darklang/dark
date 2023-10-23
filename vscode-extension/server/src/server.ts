7142541bbc4bde8ebca7735943f31d856e2fe313
Stachu Korick <hello@stachu.net>
2023-10-23 15:03:46 -0400

    iterate on vscode extension syntax highlighting, providing hints for future work

--- vscode-extension/server/src/server.ts
+++ vscode-extension/server/src/server.ts
@@ -18,6 +18,8 @@ import {
   SemanticTokens,
 } from "vscode-languageserver/node";
 import { TextDocument } from "vscode-languageserver-textdocument";
+
+// docs: https://github.com/tree-sitter/tree-sitter/blob/master/lib/binding_web/tree-sitter-web.d.ts
 const Parser = require("web-tree-sitter");
 
 import { ComputeDiagnosticsOutput } from "./darkTypes";
@@ -235,50 +237,63 @@ documents.listen(connection);
 
 connection.listen();
 
-// handle requests for semantic tokens, by way of our treesitter parser
-//
-// note: a lot of the implementation here was ChatGPT-generated,
-// and sort of bad - needs overhaul, but this gets _something_ out.
+// following are attempts at semantic-tree-ifying our parsed code, to be highlighted by VS Code
+
+// v1 - this is ChatGPT-generated, largely, and is kinda broken but proved the point
 // connection.onRequest(
 //   SemanticTokensRequest.type,
 //   ({ textDocument }): SemanticTokens => {
 //     const tokens: number[] = [];
+//     let lastLine = 0;
+//     let lastStartChar = 0;
 
-//     console.log("pocessing semantic tokens request");
+//     const encodeToken = (
+//       line: number,
+//       startChar: number,
+//       length: number,
+//       tokenType: number,
+//     ) => {
+//       const deltaLine = line - lastLine;
+//       const deltaStart =
+//         deltaLine === 0 ? startChar - lastStartChar : startChar;
+
+//       tokens.push(deltaLine, deltaStart, length, tokenType, 0); // Assuming no tokenModifiers for simplicity
+
+//       lastLine = line;
+//       lastStartChar = startChar + length;
+//     };
+
+//     console.log("Processing semantic tokens request");
 
 //     const content = documents.get(textDocument.uri)?.getText() || "";
 //     const tree = parser.parse(content);
 
-//     // A recursive function to explore the tree
 //     const processNode = (node: any) => {
+//       //console.log("processing node", node);
 //       const { type, startPosition, endPosition } = node;
 //       const line = startPosition.row;
 //       const startChar = startPosition.column;
-
-//       //console.log(type, startPosition, endPosition);
-
 //       const length = endPosition.column - startPosition.column;
 
 //       switch (type) {
 //         case "let":
-//           tokens.push(line, startChar, length, 0);
+//           encodeToken(line, startChar, length, 0);
 //           break;
 
 //         case "identifier":
-//           // Distinguishing between function name and other identifiers:
 //           if (
 //             node.parent &&
 //             node.parent.type === "fn_def" &&
 //             node.parent.name === node
 //           ) {
-//             tokens.push(line, startChar, length, 1);
+//             encodeToken(line, startChar, length, 1);
 //           } else {
-//             tokens.push(line, startChar, length, 6);
+//             encodeToken(line, startChar, length, 6);
 //           }
 //           break;
 
 //         case "fn_param_def":
-//           tokens.push(line, startChar, length, 2);
+//           encodeToken(line, startChar, length, 2);
 //           break;
 
 //         case "Int":
@@ -286,23 +301,23 @@ connection.listen();
 //         case "Float":
 //         case "String":
 //         case "Char":
-//           tokens.push(line, startChar, length, 3);
+//           encodeToken(line, startChar, length, 3);
 //           break;
 
 //         case "string_literal":
-//           tokens.push(line, startChar, length, 4);
+//           encodeToken(line, startChar, length, 4);
 //           break;
 
 //         case "+":
 //         case "-":
-//           tokens.push(line, startChar, length, 5);
+//           encodeToken(line, startChar, length, 5);
 //           break;
 
 //         default:
 //           break;
 //       }
 
-//       // recurse against child nodes
+//       // Recurse through child nodes
 //       for (const child of node.children) {
 //         processNode(child);
 //       }
@@ -310,100 +325,113 @@ connection.listen();
 
 //     processNode(tree.rootNode);
 
-//     console.log("tokens", tokens);
+//     console.log("Encoded tokens:", tokens);
 //     return {
 //       data: tokens,
 //     };
 //   },
 // );
 
+// v2: _manually_ returning the tokens that we want, for the exact code of:
+//```
+//let add (a: Int) (b: Int): Int =
+//  let sum = a + b
+//  sum
+//```
 connection.onRequest(
   SemanticTokensRequest.type,
   ({ textDocument }): SemanticTokens => {
-    const tokens: number[] = [];
-    let lastLine = 0;
-    let lastStartChar = 0;
+    // our token types
+    // | 0 | keyword   | words 'let' and 'in'            |
+    // | 1 | function  | function names/identifiers      |
+    // | 2 | parameter | function parameter identifiers  |
+    // | 3 | type      | type names like Int, Bool, etc. |
+    // | 4 | string    | string literals                 |
+    // | 5 | operator  | operators like +, -             |
+    // | 6 | variable  | general identifiers             |
 
-    const encodeToken = (
-      line: number,
-      startChar: number,
-      length: number,
-      tokenType: number,
-    ) => {
-      const deltaLine = line - lastLine;
-      const deltaStart =
-        deltaLine === 0 ? startChar - lastStartChar : startChar;
+    // code sample:
+    // ```fsharp
+    // let add (a: Int) (b: Int): Int =
+    //   let sum = a + b
+    //   sum
+    // ```
 
-      tokens.push(deltaLine, deltaStart, length, tokenType, 0); // Assuming no tokenModifiers for simplicity
+    // | thing | ΔLine | ΔStart | length | type | modifier |
+    // |-------|-------|--------|--------|------|----------|
+    // | let   | 0     |      0 |      3 |    0 |        0 |
+    // | add   | 0     |      1 |      3 |    6 |        0 |
+    // | a     | ...   |        |        |      |          |
+    // | Int   |       |        |        |      |          |
+    // | b     |       |        |        |      |          |
+    // | Int   |       |        |        |      |          |
+    // | Int   |       |        |        |      |          |
+    // | let   |       |        |        |      |          |
+    // | sum   |       |        |        |      |          |
+    // | a     |       |        |        |      |          |
+    // | b     |       |        |        |      |          |
+    // | sum   |       |        |        |      |          |
+    // |       |       |        |        |      |          |
 
-      lastLine = line;
-      lastStartChar = startChar + length;
-    };
+    // deltaLine: The difference in lines from the previous token.
+    // deltaStart: The difference in characters (columns) from the previous token on the same line (or from the start of the line if it's the first token on that line).
+    // length: The length of the token.
+    // tokenType: The index in the token types array declared in the legend of the semantic token provider.
+    // tokenModifiers: A bitset representing the token modifiers. Each bit of the value represents an index in the token modifiers array declared in the legend of the semantic token provider.
 
-    console.log("Processing semantic tokens request");
+    // prettier-ignore
+    const tokens =
+      [ 0, 0, 3, 0, 0, // let
+        0, 1, 3, 6, 0. // add
+        // TODO: continue
+      ]
 
-    const content = documents.get(textDocument.uri)?.getText() || "";
-    const tree = parser.parse(content);
-
-    const processNode = (node: any) => {
-      const { type, startPosition, endPosition } = node;
-      const line = startPosition.row;
-      const startChar = startPosition.column;
-      const length = endPosition.column - startPosition.column;
-
-      switch (type) {
-        case "let":
-          encodeToken(line, startChar, length, 0);
-          break;
-
-        case "identifier":
-          if (
-            node.parent &&
-            node.parent.type === "fn_def" &&
-            node.parent.name === node
-          ) {
-            encodeToken(line, startChar, length, 1);
-          } else {
-            encodeToken(line, startChar, length, 6);
-          }
-          break;
-
-        case "fn_param_def":
-          encodeToken(line, startChar, length, 2);
-          break;
-
-        case "Int":
-        case "Bool":
-        case "Float":
-        case "String":
-        case "Char":
-          encodeToken(line, startChar, length, 3);
-          break;
-
-        case "string_literal":
-          encodeToken(line, startChar, length, 4);
-          break;
-
-        case "+":
-        case "-":
-          encodeToken(line, startChar, length, 5);
-          break;
-
-        default:
-          break;
-      }
-
-      // Recurse through child nodes
-      for (const child of node.children) {
-        processNode(child);
-      }
-    };
-
-    processNode(tree.rootNode);
-
-    console.log("Encoded tokens:", tokens);
     return {
       data: tokens,
     };
   },
 );
+
+// vFuture TODO: in a future pass, use this tree to replace all the below syntax-highlighting logic
+// our parser includes a lot of things that _shouldn't_ be tokenized
+// (like `fn_def`s which are really just wrappers around other tokens)
+// , and the other tokens should use context such as "in a `fn_def`" to determine their type
+
+// TODO: typescriptify this
+// function simplifyTree(cursor: any): any {
+//   let children = [];
+
+//   if (cursor.gotoFirstChild()) {
+//     do {
+//       children.push(simplifyTree(cursor));
+//     } while (cursor.gotoNextSibling());
+
+//     cursor.gotoParent();
+//   }
+
+//   return {
+//     typ: cursor.nodeType,
+//     text: cursor.nodeText,
+//     fieldName: cursor.currentFieldName(),
+//     children: children,
+//     startPosition: cursor.startPosition,
+//     endPosition: cursor.endPosition,
+//   };
+// }
+//
+// connection.onRequest(
+//   SemanticTokensRequest.type,
+//   ({ textDocument }): SemanticTokens => {
+//     console.log("Processing semantic tokens request");
+//
+//     const content = documents.get(textDocument.uri)?.getText() || "";
+//     const tree = parser.parse(content);
+//
+//     let simpleTree = simplifyTree(tree.rootNode.walk());
+//     console.log("simpletree", simpleTree);
+//
+//     // TODO: continue
+//
+//     return { data: [] };
+//   },
+// );
