b38f57f465be247f3631b8bb663734d85ede81a4
Paul Biggar <paul.biggar@gmail.com>
2023-11-21 18:24:00 -0500

    Start deploying again

--- scripts/deployment/new-push-containers.sh
+++ scripts/deployment/new-push-containers.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 
 set -x
 
-# docker build . -t dark-base-service:latest -f containers/base-service-Dockerfile
-# docker build . -t dark-fsharp-service:latest -f containers/fsharp-service-Dockerfile
-docker build . -t bwdserver:latest -f containers/bwdserver/Dockerfile
 gcloud auth configure-docker us-central1-docker.pkg.dev/production-containers
 docker tag bwdserver:latest us-central1-docker.pkg.dev/darklang-next/production-containers/bwdserver:latest
 docker image push us-central1-docker.pkg.dev/darklang-next/production-containers/bwdserver:latest
