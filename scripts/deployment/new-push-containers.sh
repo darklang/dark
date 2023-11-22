864f2e1c4af228329fe8fb47036cc160adfc2c89
Paul Biggar <paul.biggar@gmail.com>
2023-11-22 00:59:46 +0000

    Correct command

--- scripts/deployment/new-push-containers.sh
+++ scripts/deployment/new-push-containers.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 set -x
 
-gcloud auth configure-docker us-central1-docker.pkg.dev/production-containers
+gcloud auth configure-docker us-central1-docker.pkg.dev
 docker tag bwdserver:latest us-central1-docker.pkg.dev/darklang-next/production-containers/bwdserver:latest
 docker image push us-central1-docker.pkg.dev/darklang-next/production-containers/bwdserver:latest
