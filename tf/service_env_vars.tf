ecb5e831a7340755e720dee5a8ae43f5e0434a58
Paul Biggar <paul.biggar@gmail.com>
2023-11-29 18:16:47 +0000

    Add env vars

--- tf/service_env_vars.tf
+++ tf/service_env_vars.tf
@@ -30,6 +30,10 @@ variable "service_env_vars" {
     "DARK_CONFIG_TRIGGER_CRONS"         = { value : "y" }
     "DARK_CONFIG_PAUSE_BETWEEN_CRONS"   = { value : "0" }
 
+    ## ProdExec
+    "DARK_CONFIG_PRODEXEC_PORT"            = { value : "8080" }
+    "DARK_CONFIG_PRODEXEC_CHISEL_USERNAME" = { secret : "prodexec-chisel-username" }
+    "DARK_CONFIG_PRODEXEC_CHISEL_PASSWORD" = { secret : "prodexec-chisel-password" }
 
     # Http
     "DARK_CONFIG_HTTPCLIENT_TIMEOUT_IN_MS" = { value : "10000" }
