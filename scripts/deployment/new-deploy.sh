2caff1472934bcf4ff90dacf0ef9b6aeb1d3715f
Paul Biggar <paul.biggar@gmail.com>
2023-12-07 21:57:26 +0000

    Don't deploy prodexec automatically

--- scripts/deployment/new-deploy.sh
+++ scripts/deployment/new-deploy.sh
@@ -10,6 +10,11 @@ jq -c '.[]' image-digests.json | while IFS= read -r line; do
     container=$(echo "$line" | jq -r '.name')
     image=$(echo "$line" | jq -r '.digest')
 
+    # ProdExec is only deployed on demand
+    if [[ "$container" == "prodexec" ]]; then
+        continue
+    fi
+
     gcloud run services update "$container" \
       --project darklang-next \
       --region us-central1 \
