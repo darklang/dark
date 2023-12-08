1d294cd8716ffd492fa6d34106eace7a5bce2649
Paul Biggar <paul.biggar@gmail.com>
2023-12-08 01:37:02 +0000

    Clear moving the DB into canvas hack

--- backend/experiments/CanvasHack/Main.fs
+++ backend/experiments/CanvasHack/Main.fs
@@ -3,7 +3,11 @@ module CanvasHack.Main
 open System.Threading.Tasks
 open FSharp.Control.Tasks
 
+open Npgsql
+open Npgsql.FSharp
+
 open Prelude
+open LibCloud.Db
 
 module PT = LibExecution.ProgramTypes
 module C = LibCloud.Canvas
@@ -33,6 +37,25 @@ let parseYamlExn<'a> (filename : string) : 'a =
 
 let packageManager = LibCloud.PackageManager.packageManager
 
+
+let destroyCanvas (id : CanvasID) : Task<unit> =
+  task {
+    let runStmt stmt : Task<unit> =
+      Sql.query stmt
+      |> Sql.parameters [ "id", Sql.uuid id ]
+      |> Sql.executeStatementAsync
+
+    do! runStmt "DELETE FROM scheduling_rules_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM traces_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM user_data_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM cron_records_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM toplevels_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM canvases_v0 WHERE id = @id"
+    do! runStmt "DELETE FROM domains_v0 WHERE canvas_id = @id"
+    do! runStmt "DELETE FROM secrets_v0 WHERE canvas_id = @id"
+  }
+
+
 let seedCanvas (canvasName : string) =
   task {
     let canvasDir = $"{baseDir}/{canvasName}"
@@ -40,14 +63,22 @@ let seedCanvas (canvasName : string) =
 
     // Create the canvas - expect this to be empty
     let domain = $"{canvasName}.dlio.localhost"
-    let host = $"http://{domain}:{LibService.Config.bwdServerPort}"
-    let experimentalHost = $"http://{domain}:{LibService.Config.bwdDangerServerPort}"
     let canvasID =
       match config.CanvasId with
       | None -> System.Guid.NewGuid()
       | Some id -> System.Guid.Parse id
 
-    let! ownerID = LibCloud.Account.createUser ()
+    let! ownerID =
+      task {
+        match! LibCloud.Canvas.getOwner canvasID with
+        | Some id -> return id
+        | None ->
+          let! ownerID = LibCloud.Account.createUser ()
+          do! LibCloud.Canvas.createWithExactID canvasID ownerID domain
+          return ownerID
+      }
+
+    do! destroyCanvas canvasID
     do! LibCloud.Canvas.createWithExactID canvasID ownerID domain
 
     let resolver =
@@ -60,7 +91,8 @@ let seedCanvas (canvasName : string) =
             BuiltinDarkInternal.Builtin.contents ]
           []
       LibParser.NameResolver.fromBuiltins builtIns
-      |> fun nr -> { nr with packageManager = Some packageManager }
+      |> fun nr ->
+        { nr with packageManager = Some packageManager; allowError = false }
 
     let! tls =
       uply {
@@ -106,6 +138,8 @@ let seedCanvas (canvasName : string) =
       // we don't have secrets to load - we're done
       ()
 
+    let host = $"http://{domain}:{LibService.Config.bwdServerPort}"
+    let experimentalHost = $"http://{domain}:{LibService.Config.bwdDangerServerPort}"
     print
       $"Success saved canvas - endpoints available
        at {host} (bwdserver)
