673e4019092452218de4f01fbd56b022f2c5f043
Paul Biggar <paul.biggar@gmail.com>
2023-12-18 15:53:42 +0000

    Fix script

--- scripts/build/dotnet-publish-all-clis.sh
+++ scripts/build/dotnet-publish-all-clis.sh
@@ -4,19 +4,19 @@
 
 set -euo pipefail
 
-sha=git rev-parse HEAD
+sha=`git rev-parse HEAD`
 
 release="alpha-$sha"
 
-mkdir clis
+mkdir -p clis
 
 # Parallelism set to 1 here to avoid running out of memory.
 # TODO: do better with gnu parallel or with this solution that I couldn't make work:
 # https://stackoverflow.com/a/43951971/104021
 # runtimes="linux-x64 linux-musl-x64 linux-arm64 osx-x64 osx-arm64 win-x64 win-arm64"
 runtimes="linux-x64 linux-arm64"
-for rt in "$runtimes"; do
-  echo "Building for runtime: $i"
+for rt in $runtimes; do
+  echo "Building for runtime: $rt"
   ./scripts/build/_dotnet-wrapper publish \
     -c Release \
     src/Cli/Cli.fsproj \
@@ -27,7 +27,7 @@ for rt in "$runtimes"; do
     /p:PublishReadyToRun=false \
     --self-contained true \
     --runtime "$rt"
-  target="clis/darklang-$sha-$rt"
+  target="clis/darklang-$release-$rt"
   mv backend/Build/out/Cli/Release/net8.0/$rt/publish/Cli "$target"
   gzip "$target"
 done
