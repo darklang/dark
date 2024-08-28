8786f2e8ab8cf215cee6e3bf4693ceb32568e544
Ocean <oakocean9@gmail.com>
2024-08-28 15:29:34 +0000

    Small fixes to fn decl

--- tree-sitter-darklang/grammar.js
+++ tree-sitter-darklang/grammar.js
@@ -140,16 +140,13 @@ module.exports = grammar({
         field("keyword_let", alias("let", $.keyword)),
         field("name", $.fn_identifier),
         optional(field("type_params", $.type_params)),
-        choice(
-          field("params", $.fn_decl_params),
-          seq($.indent, field("params", $.fn_decl_params)),
-        ),
+        field("params", $.fn_decl_params),
         field("symbol_colon", alias(":", $.symbol)),
         field("return_type", $.type_reference),
         field("symbol_equals", alias("=", $.symbol)),
         choice(
           seq($.indent, field("body", $.expression), $.dedent),
-          field("body", $.expression),
+          seq(field("body", $.expression), optional($.newline)),
         ),
       ),
     fn_decl_params: $ => repeat1(choice($.unit, $.fn_decl_param)),
