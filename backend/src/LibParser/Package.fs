56940248881507139f161ae35abcf4a9fcb649cd
Stachu Korick <hello@stachu.net>
2025-08-25 20:02:32 -0400

    Allow for multi-level module declaration in .dark package files

--- backend/src/LibParser/Package.fs
+++ backend/src/LibParser/Package.fs
@@ -89,7 +89,6 @@ let rec parseDecls
                                    _,
                                    _,
                                    _) ->
-
         let moduleNames = moduleNames @ (nestedModuleNames |> List.map _.idText)
         let nestedDecls = parseDecls fileName moduleNames nested
 
@@ -120,11 +119,22 @@ let parse
                           _,
                           _,
                           _,
-                          [ SynModuleOrNamespace(_, _, _, decls, _, _, _, _, _) ],
+                          [ SynModuleOrNamespace(longId,
+                                                 _,
+                                                 kind,
+                                                 decls,
+                                                 _,
+                                                 _,
+                                                 _,
+                                                 _,
+                                                 _) ],
                           _,
                           _,
                           _) ->
-      let baseModule = []
+      let baseModule =
+        match kind with
+        | SynModuleOrNamespaceKind.NamedModule -> longIdentToList longId
+        | _ -> []
 
       let modul : WTPackageModule = parseDecls filename baseModule decls
 
